ed548e355ba8e6042c00f496aaecea32860b3c66
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-02-08 08:33:19 +0100

    Install jest to run tests
    Also added a first dummy test to check it works

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: "node",
+    moduleDirectories: ["node_modules", "<rootDir>/app"],
+    moduleNameMapper: {
+        "~/(.*)": "<rootDir>/app/$1",
+        "test/(.*)": "<rootDir>/test/$1",
+    },
+    preset: "ts-jest",
+};
---
+++ tests/unit/first.test.ts
@@ -0,0 +1,3 @@
+import App from "app/root"
+
+it("Ensures that 1 is 1", () => { expect(1).toEqual(1)});
