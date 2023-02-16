705261c07934cd08afc568e57896e95c186a953d
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-02-16 19:23:13 +0100

    feat: make the form p r e t t y

--- app/routes/types_showcase.tsx
+++ app/routes/types_showcase.tsx
@@ -19,20 +19,37 @@ export const action = async ({ request }: DataFunctionArgs) => {
 type FormInputProps = {
   name: string;
   label: string;
+  className?: string;
   type?: string;
 };
 
-export const FormInput = ({ name, label, type = "text" }: FormInputProps) => {
+export const FormInput = ({
+  name,
+  label,
+  type = "text",
+  className = "",
+}: FormInputProps) => {
   const { error, getInputProps } = useField(name);
+  // Floating label inspired by https://flowbite.com/docs/forms/floating-label/
   return (
-    <>
-      <label htmlFor={name}>{label}</label>
-      <input
-        {...getInputProps({ type, id: name })}
-        style={{ backgroundColor: error ? "yellow" : "white" }}
-      />
-      {error && <span>{error}</span>}
-    </>
+    <div className={`m-1 ${className}`}>
+      <div className="relative">
+        <input
+          {...getInputProps({ type, id: name })}
+          className={`${
+            error ? "border-red-600" : "border-gray-300"
+          } block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
+          placeholder=" "
+        />
+        <label
+          htmlFor={name}
+          className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+        >
+          {label}
+        </label>
+        {error && <span className="mt-2 text-xs text-red-600">{error}</span>}
+      </div>
+    </div>
   );
 };
 
@@ -40,24 +57,40 @@ type FormSelectProps = {
   name: string;
   label: string;
   options: string[];
+  className?: string;
 };
 
-export const FormSelect = ({ name, label, options }: FormSelectProps) => {
+export const FormSelect = ({
+  name,
+  label,
+  options,
+  className = "",
+}: FormSelectProps) => {
   const { error, getInputProps } = useField(name);
   return (
-    <>
-      <label htmlFor={name}>{label}</label>
-      <select {...getInputProps({ id: name })}>
-        {options.map((option) => {
-          return (
-            <option value={option} key={option}>
-              {option}
-            </option>
-          );
-        })}
-      </select>
-      {error && <span>{error}</span>}
-    </>
+    <div className={`m-1 ${className}`}>
+      <div className={`relative`}>
+        <label
+          htmlFor={name}
+          className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+        >
+          {label}
+        </label>
+        <select
+          {...getInputProps({ id: name })}
+          className="border-gray-300 block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        >
+          {options.map((option) => {
+            return (
+              <option value={option} key={option}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+        {error && <span className="mt-2 text-xs text-red-600 ">{error}</span>}
+      </div>
+    </div>
   );
 };
 
@@ -65,16 +98,17 @@ export default function Index() {
   const data = useActionData();
 
   return (
-    <div>
+    <div className="block p-6 rounded-lg shadow-lg max-w-xl">
       <h1>Zod Schema in Remix Formularen</h1>
-      <p>Data validation in Front & Backend</p>
+      <p>Data validation in Front & Backend 🤯</p>
 
       <ValidatedForm
         validator={validator}
         method="post"
+        className="my-6"
         defaultValues={{
-          name: { first: "jane", family: "doe" },
-          job: "-",
+          name: { first: "Jane", family: "Doe" },
+          job: "",
           phoneNumber: "030 123 123 1",
           birthday: "23.01.1993",
           address: {
@@ -85,29 +119,63 @@ export default function Index() {
           },
         }}
       >
-        <FormInput name="name.first" label="Vorname: " />
-        <FormInput name="name.family" label="Nachname: " />
-        <hr />
-        <FormInput name="job" label="Beruf: " />
+        <div className="flex flex-row">
+          <FormInput name="name.first" label="Vorname" className="basis-1/2" />
+          <FormInput
+            name="name.family"
+            label="Nachname"
+            className="basis-1/2"
+          />
+        </div>
 
-        <FormSelect
-          name="familienStand"
-          label="Familienstand: "
-          options={Object.keys(FamilienStandSchema.Values)}
-        />
-        <FormInput name="phoneNumber" label="Telefon: " />
-        <FormInput name="birthday" label="Geburtstag: " />
-        <hr />
-        <FormInput name="address.streetName" label="Adresse: " />
-        <FormInput name="address.streetNumber" label="Hausnummer: " />
-        <FormInput name="address.postcode" label="PLZ: " type="number" />
-        <FormInput name="address.city" label="Stadt: " />
-        <hr />
-        <hr />
-        <input type="submit" />
+        <div className="flex flex-row">
+          <FormInput name="birthday" label="Geburtstag" className="basis-1/2" />
+          <FormSelect
+            name="familienStand"
+            label="Familienstand "
+            options={Object.keys(FamilienStandSchema.Values)}
+            className="basis-1/2"
+          />
+        </div>
+
+        <div className="flex flex-row">
+          <FormInput name="job" label="Beruf" className="basis-1/2" />
+          <FormInput name="phoneNumber" label="Telefon" className="basis-1/2" />
+        </div>
+
+        <div className="flex flex-row">
+          <FormInput
+            name="address.streetName"
+            label="Adresse"
+            className="basis-4/5"
+          />
+          <FormInput
+            name="address.streetNumber"
+            label="Hausnummer"
+            className="basis-1/5"
+          />
+        </div>
+        <div className="flex flex-row">
+          <FormInput
+            name="address.postcode"
+            label="PLZ"
+            type="number"
+            className="basis-1/2"
+          />
+          <FormInput name="address.city" label="Stadt" className="basis-1/2" />
+        </div>
+        <div className="flex flex-row place-content-end">
+          <button type="submit" className="border-2 rounded-lg px-5 py-2">
+            Abschicken
+          </button>
+        </div>
       </ValidatedForm>
-      <h2>Erhaltene Daten:</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+
+      {data && (
+        <pre className="p-5 bg-gray-200 rounded-md">
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      )}
     </div>
   );
 }
