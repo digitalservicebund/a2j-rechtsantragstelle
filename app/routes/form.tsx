fa6120991fa9e243a1af9f2e8213ab0975c13a36
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-02-27 17:53:46 +0100

    feat: index route redirects to first step

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -1,4 +1,16 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import { initial } from "./form/formDefinition";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // Reroute to initial step on empty formStepID
+  const url = new URL(request.url);
+  if (url.pathname === "/form/" || url.pathname === "/form") {
+    return redirect(`/form/${initial}`);
+  }
+  return null;
+};
 
 export default function FormRoot() {
   return (
