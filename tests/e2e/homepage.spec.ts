6ca7c8aa6f3f11e8305569710a4877356505a009
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-03-07 11:00:12 +0100

    Cleanup code

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,30 +1,29 @@
 import { test, expect } from "@playwright/test";
 import { Homepage } from "./pom/Homepage";
 
-test("has title", async ({ page }) => {
-  const home = new Homepage(page);
+let home: Homepage;
+
+test.beforeEach(async ({ page }) => {
+  home = new Homepage(page);
   await home.goto();
+});
+
+test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/New Remix App/);
 });
 
 test.describe("links", () => {
   test("kitchensink link", async ({ page }) => {
-    const home = new Homepage(page);
-    await home.goto();
     await home.gotoKitchensink();
     await expect(page).toHaveURL(/.*kitchensink/);
   });
 
   test("multi-page form link", async ({ page }) => {
-    const home = new Homepage(page);
-    await home.goto();
     await home.gotoMultiPageForm();
     await expect(page).toHaveURL(/.*form\/welcome/);
   });
 
   test("types showcase link", async ({ page }) => {
-    const home = new Homepage(page);
-    await home.goto();
     await home.gotoTypesShowcase();
     await expect(page).toHaveURL(/.*types_showcase/);
   });
