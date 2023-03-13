77a8197dbac72f4c94050f4de7130f2334696d3a
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-03-13 17:36:16 +0100

    Add iconLeft button property

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
+import type { ReactElement } from "react";
 
 interface VisualProps {
   size?: "large" | "medium" | "small";
+  iconLeft?: ReactElement;
 }
 interface ButtonProps
   extends React.ComponentPropsWithoutRef<"button">,
@@ -11,20 +13,25 @@ interface ButtonLinkProps
     VisualProps {}
 
 const Button = (props: ButtonProps | ButtonLinkProps) => {
+  const { iconLeft, ...rest } = props;
   const isLink = "href" in props;
   const buttonClasses = classNames("ds-button", props.className, {
     "ds-button-large": props.size == "large",
     "ds-button-small": props.size == "small",
+    "ds-button-with-icon": iconLeft,
   });
+
   if (isLink) {
     return (
-      <a {...(props as ButtonLinkProps)} className={buttonClasses}>
+      <a {...(rest as ButtonLinkProps)} className={buttonClasses}>
+        {props.iconLeft}
         {props.children}
       </a>
     );
   } else {
     return (
-      <button {...(props as ButtonProps)} className={buttonClasses}>
+      <button {...(rest as ButtonProps)} className={buttonClasses}>
+        {props.iconLeft}
         {props.children}
       </button>
     );
--- app/routes/kitchensink.buttons.tsx
+++ app/routes/kitchensink.buttons.tsx
@@ -1,21 +1,64 @@
 import { Button } from "~/components";
 
 export default function KitchensinkButtons() {
+  const Cross = () => {
+    return (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="white"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M28.5 9.615L26.385 7.5L18 15.885L9.615 7.5L7.5 9.615L15.885 18L7.5 26.385L9.615 28.5L18 20.115L26.385 28.5L28.5 26.385L20.115 18L28.5 9.615Z" />
+      </svg>
+    );
+  };
+
   return (
     <div>
       <h1>Buttons</h1>
       <h2>Primary</h2>
       <h3>Large</h3>
-      <Button size="large">Registrieren</Button>
+      <div className="mb-4">
+        <Button size="large">Registrieren</Button>
+        <br />
+      </div>
+      <div>
+        <Button size="large" iconLeft={<Cross />}>
+          Registrieren
+        </Button>
+      </div>
 
       <h3>Medium</h3>
-      <Button>Registrieren</Button>
+      <div className="mb-4">
+        <Button>Registrieren</Button>
+      </div>
+      <div>
+        <Button iconLeft={<Cross />}>Registrieren</Button>
+      </div>
 
       <h3>Small</h3>
-      <Button size="small">Registrieren</Button>
+      <div className="mb-4">
+        <Button size="small">Registrieren</Button>
+        <br />
+      </div>
+      <div>
+        <Button size="small" iconLeft={<Cross />}>
+          Registrieren
+        </Button>
+      </div>
 
       <h3>Disabled</h3>
-      <Button size="small">Registrieren</Button>
+      <div className="mb-4">
+        <Button disabled>Registrieren</Button>
+        <br />
+      </div>
+      <div>
+        <Button disabled iconLeft={<Cross />}>
+          Registrieren
+        </Button>
+      </div>
     </div>
   );
 }
