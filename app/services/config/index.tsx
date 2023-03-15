798a54dab34af48c3e97c5da307da792e0090a98
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2023-03-15 07:07:46 +0100

    Add trim

--- app/services/config/index.tsx
+++ app/services/config/index.tsx
@@ -10,8 +10,9 @@ let instance: config | undefined = undefined;
 export default function get(): config {
   if (instance === undefined) {
     instance = {
-      STRAPI_API: process.env.STRAPI_API || "",
-      STRAPI_ACCESS_KEY: process.env.STRAPI_ACCESS_KEY || "",
+      // Removing trim because infrastructure adding a newline on secret
+      STRAPI_API: process.env.STRAPI_API?.trim() || "",
+      STRAPI_ACCESS_KEY: process.env.STRAPI_ACCESS_KEY?.trim() || "",
       CMS: process.env.CMS || "",
       ENV: process.env.NODE_ENV || "development",
     };
