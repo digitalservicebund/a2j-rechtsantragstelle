d2f781d25b028d8a2eb5f545b74ebb1fc9ee11a1
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-03-17 19:16:17 +0100

    fix: typo

--- app/components/form/steps/isErwaerbstaetig.tsx
+++ app/components/form/steps/isErwaerbstaetig.tsx
@@ -9,7 +9,7 @@ export const erwaerbstaetigStep = {
   component: () => {
     return (
       <div style={{ border: "solid black 1px", padding: "1rem" }}>
-        <h3>Sind sie momentan erwärbstätig?</h3>
+        <h3>Sind sie momentan erwerbstätig?</h3>
         <p>Als Erwerbstätigkeit zählen Arbeiten, ....</p>
         <RadioGroup
           name={schema.keyof().Values.isErwaerbstaetig}
