a94c7adf9565057f7e90d42599a387776f430069
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-03-20 18:26:06 +0100

    Add possible strapi content types

--- app/services/cms/getPageConfig.ts
+++ app/services/cms/getPageConfig.ts
@@ -1,9 +1,40 @@
 import cms from "~/services/cms";
 
+type HeadingContent = {
+  id: number;
+  __component: "basic.header";
+  text: string;
+  size: number;
+};
+
+type ParagraphContent = {
+  id: number;
+  __component: "basic.paragraph";
+  text: string;
+};
+
+type SelectOptionContent = {
+  id: number;
+  text: string;
+  value: string;
+};
+
+type SelectContent = {
+  id: number;
+  __component: "basic.select";
+  name: string;
+  label?: string;
+  options: SelectOptionContent[];
+};
+
+export type PageContent = Array<
+  HeadingContent | ParagraphContent | SelectContent
+>;
+
 export default async function (
   request: Request,
   options?: { dontThrow: boolean }
-) {
+): Promise<PageContent> {
   const { pathname } = new URL(request.url);
   const slug = pathname.slice(1);
   console.log({ slug });
