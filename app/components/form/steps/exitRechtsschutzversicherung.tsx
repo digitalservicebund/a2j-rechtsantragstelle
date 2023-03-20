acffe5699fe1fe142484672afce87b5da4464628
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-03-20 17:19:48 +0100

    Use strapi content for rechtsschutzversicherung steps - easy part

--- app/components/form/steps/exitRechtsschutzversicherung.tsx
+++ app/components/form/steps/exitRechtsschutzversicherung.tsx
@@ -2,14 +2,6 @@ import type { StepInterface } from "../steps";
 
 export const exitRechtsschutzversicherungStep: StepInterface = {
   component: () => {
-    return (
-      <div style={{ border: "solid red 1px", padding: "1rem" }}>
-        <h2>Klären Sie mit Ihrer Versicherung, ob sie den Fall bezahlt.</h2>
-        <p>
-          Wenn Sie eine Rechtsschutzversicherung haben, die diesen Fall abdeckt,
-          werden Sie keine Beratungshilfe bekommen.
-        </p>
-      </div>
-    );
+    return <></>;
   },
 };
--- app/components/form/steps/hasRechtsschutzversicherung.tsx
+++ app/components/form/steps/hasRechtsschutzversicherung.tsx
@@ -8,25 +8,13 @@ export const rechtsschutzversicherungStep = {
   schema,
   component: () => {
     return (
-      <div style={{ border: "solid black 1px", padding: "1rem" }}>
-        <h3>Haben Sie eine Rechtsschutzversicherung für diesen Fall?</h3>
-        <p>
-          Eine Rechtsschutzversicherung bezahlt Anwaltskosten. Je nach
-          Versicherungs-Bedingungen können unterschiedliche Tätigkeiten bezahlt
-          werden.
-        </p>
-        <p>
-          Eine Rechtsschutzversicherung ist keine Pflicht-Versicherung. Sie
-          müssen sie extra abgeschlossen haben.
-        </p>
-        <RadioGroup
-          name={schema.keyof().Values.hasRechtsschutzversicherung}
-          options={[
-            { label: "Nein", value: YesNoAnswer.enum.no },
-            { label: "Ja", value: YesNoAnswer.enum.yes },
-          ]}
-        />
-      </div>
+      <RadioGroup
+        name={schema.keyof().Values.hasRechtsschutzversicherung}
+        options={[
+          { label: "Nein", value: YesNoAnswer.enum.no },
+          { label: "Ja", value: YesNoAnswer.enum.yes },
+        ]}
+      />
     );
   },
 };
