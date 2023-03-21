214a98a94dbfca9e3ebd950563e446e9be60e2bd
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-03-21 18:01:15 +0100

    Add custom error messages for kidCount

--- app/components/form/steps/kidCount.tsx
+++ app/components/form/steps/kidCount.tsx
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import Input from "~/components/Input";
+import type { StepComponentProps } from "~/components/form/steps";
 
 const schema = z.object({
-  kids6Below: z.coerce.number().min(0, "Ungültige Anzahl").default(0),
-  kids7To14: z.coerce.number().min(0, "Ungültige Anzahl").default(0),
-  kids15To18: z.coerce.number().min(0, "Ungültige Anzahl").default(0),
-  kids18Above: z.coerce.number().min(0, "Ungültige Anzahl").default(0),
+  kids6Below: z.coerce.number().min(0, "min0").default(0),
+  kids7To14: z.coerce.number().min(0, "min0").default(0),
+  kids15To18: z.coerce.number().min(0, "min0").default(0),
+  kids18Above: z.coerce.number().min(0, "min0").default(0),
 });
 const varNames = schema.keyof().Values;
 
@@ -13,7 +14,7 @@ const varNames = schema.keyof().Values;
 
 export const kidsCountStep = {
   schema,
-  component: () => {
+  component: ({ content }: StepComponentProps) => {
     return (
       <div style={{ border: "solid black 1px", padding: "1rem" }}>
         <h3>
@@ -25,24 +26,28 @@ export const kidsCountStep = {
           label="0 - 6 Jahre"
           type="number"
           step="any" // decimals for partial custody
+          pageContent={content}
         />
         <Input
           name={varNames.kids7To14}
           label="7 - 14 Jahre"
           type="number"
           step="any"
+          pageContent={content}
         />
         <Input
           name={varNames.kids15To18}
           label="15 - 18 Jahre"
           type="number"
           step="any"
+          pageContent={content}
         />
         <Input
           name={varNames.kids18Above}
           label="Über 18 Jahre"
           type="number"
           step="any"
+          pageContent={content}
         />
       </div>
     );
