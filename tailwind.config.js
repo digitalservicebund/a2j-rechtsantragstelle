2bb133eed9f414d0aabe4b2d316b1609f141ca53
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-04-04 14:26:40 +0200

    Install Angie Design System Tailwind preset
    Makes available the colors, e.g. text-blue-800 and spacings, e.g. mb-xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("@digitalservice4germany/style-dictionary/tailwind")],
   theme: {
     extend: {},
   },
