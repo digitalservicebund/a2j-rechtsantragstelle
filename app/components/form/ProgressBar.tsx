d0a923d165e1729f584b3700d56f5bed63f69e2c
chris <earthaccount+gh@mailbox.org>
2023-04-04 17:14:05 +0200

    fix: ProgressBar formatting

--- app/components/form/ProgressBar.tsx
+++ app/components/form/ProgressBar.tsx
@@ -1,23 +1,22 @@
 interface ProgressBarProps {
-    progress: number;
-    max?: number;
-    fallback?: string;
-  }
-  
-  export function ProgressBar({ progress, max, fallback }: ProgressBarProps) {
-    return (
-      <progress
-        value={progress}
-        max={max}
-        aria-label="vorab-check progress"
-        className="w-full align-top"
-        style={{
-          height: "0.75rem",
-          accentColor: "var(--color-ui-primary)",
-        }}
-      >
-        {fallback}
-      </progress>
-    );
-  }
-  
+  progress: number;
+  max?: number;
+  fallback?: string;
+}
+
+export function ProgressBar({ progress, max, fallback }: ProgressBarProps) {
+  return (
+    <progress
+      value={progress}
+      max={max}
+      aria-label="vorab-check progress"
+      className="w-full align-top"
+      style={{
+        height: "0.75rem",
+        accentColor: "var(--color-ui-primary)",
+      }}
+    >
+      {fallback}
+    </progress>
+  );
+}
