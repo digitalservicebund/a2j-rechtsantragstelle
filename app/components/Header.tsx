9bdec191f3b37917956e86780403ceb6ac7cee43
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2023-04-11 13:28:33 +0200

    Change to attribute

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -23,10 +23,7 @@ export default function Header({
   };
 
   return (
-    <Container
-      hasBackground={hasBackground}
-      {...{ className: "header", ...props }}
-    >
+    <Container hasBackground={hasBackground} className="header" {...props}>
       <Heading {...headingProps} />
       {description && (
         <Paragraph {...description} {...{ className: "ds-heading-03-reg" }} />
