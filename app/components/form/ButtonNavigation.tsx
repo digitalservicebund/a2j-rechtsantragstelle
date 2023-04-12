3316a81553d0f36ee7a8444eff737d55bf683fc9
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-04-12 10:15:07 +0200

    Simplify back button

--- app/components/form/ButtonNavigation.tsx
+++ app/components/form/ButtonNavigation.tsx
@@ -1,7 +1,5 @@
-import { Link } from "@remix-run/react";
 import type { AllowedIDs } from "~/lib/vorabcheck/pages";
 import { Button } from "~/components";
-import classNames from "classnames";
 
 interface ButtonNavigationProps {
   backDestination?: AllowedIDs;
@@ -14,16 +12,11 @@ export function ButtonNavigation({
 }: ButtonNavigationProps) {
   return (
     <div>
-      <Link
-        to={backDestination ? `../${backDestination}` : "/"}
-        style={{ pointerEvents: !backDestination ? "none" : "auto" }}
-        className={classNames("no-underline", {
-          "text-blue-600": backDestination,
-          "text-gray-300": !backDestination,
-        })}
-      >
-        {"Zurück"}
-      </Link>
+      {backDestination && (
+        <Button href={backDestination} look="tertiary">
+          Zurück
+        </Button>
+      )}
 
       <Button type="submit" name="_action" value="next">
         {isLast ? "Von Vorne beginnen" : "Übernehmen & Weiter"}
