c0987f25f54d3c19b91d415a6dee0bc28cd5bd6f
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-04-17 13:36:30 +0200

    Tailwindify a few styles from styles.css

--- app/components/InfoBoxItem.tsx
+++ app/components/InfoBoxItem.tsx
@@ -21,11 +21,14 @@ const InfoBoxItem = ({
   button,
 }: InfoBoxItemProps) => {
   return (
-    <li className="flex flex-row items-center justify-center max-w-none max-[499px]:flex-col">
+    <li className="flex flex-row items-center justify-center max-w-none max-[499px]:flex-col pt-32 border-solid border-0 border-t-2 border-gray-400 first:border-none first:pt-0">
       {image?.data && (
         <Image
           {...image}
-          {...{ className: "max-[499px]:mb-24 max-[499px]:self-start" }}
+          {...{
+            className:
+              "max-[499px]:mb-24 max-[499px]:w-[80px] max-[499px]:self-start w-[176px]",
+          }}
         />
       )}
       <div
