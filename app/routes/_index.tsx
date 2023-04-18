23987e4002cdb7855ba6d6a91fed0aba739ea517
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-04-18 15:54:04 +0200

    feat: remove unused links from index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,8 @@
 import type { LoaderFunction, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { getPageConfig } from "~/services/cms/getPageConfig";
 import PageContent from "~/components/PageContent";
-import Container from "~/components/Container";
 
 const title = "A2J - Digitale RAST";
 
@@ -15,34 +14,6 @@ export const meta: V2_MetaFunction = () => [
   },
 ];
 
-export const loader: LoaderFunction = async ({ params }) => {
-  return json(await getPageConfig("landingpage"));
-};
-
-export const indexLinks = [
-  { url: "/vorabcheck", displayName: "Vorabcheck" },
-  { url: "/flowchart", displayName: "Flowchart" },
-  { url: "/kitchensink", displayName: "Kitchensink" },
-  { url: "/form-validation-example", displayName: "Form Validation Example" },
-  { url: "/strapi-example", displayName: "Strapi Integration Example" },
-] as const;
-
 export default function Index() {
-  const content = useLoaderData().content;
-
-  return (
-    <>
-      <PageContent content={content} />
-      <Container>
-        <h1>{title}</h1>
-        <ul>
-          {indexLinks.map((link) => (
-            <li key={link.url}>
-              <Link to={link.url}>{link.displayName}</Link>
-            </li>
-          ))}
-        </ul>
-      </Container>
-    </>
-  );
+  return <PageContent content={useLoaderData().content} />;
 }
--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "@playwright/test";
-import { indexLinks } from "~/routes/_index";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000");
@@ -7,19 +6,4 @@ test.beforeEach(async ({ page }) => {
 
 test("has title", async ({ page }) => {
   await expect(page).toHaveTitle("A2J - Digitale RAST");
-});
-
-test.describe("links", () => {
-  indexLinks.forEach((link) => {
-    test(`${link.displayName} link`, async ({ page }) => {
-      const responsePromise = page.waitForResponse(
-        (resp) => resp.url().includes(link.url) && resp.status() === 200,
-        { timeout: 500 }
-      );
-      await page
-        .getByRole("link", { name: link.displayName, exact: true })
-        .click();
-      await responsePromise;
-    });
-  });
 });
