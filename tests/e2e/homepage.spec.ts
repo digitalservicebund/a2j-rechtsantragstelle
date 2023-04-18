0b987ad754b1168a4bcafa0ad7837ed4edfee275
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-04-18 16:21:05 +0200

    test: add homepage link e2e tests

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,9 +1,26 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000");
 });
 
-test("has title", async ({ page }) => {
-  await expect(page).toHaveTitle("A2J - Digitale RAST");
+test.describe("links", () => {
+  const expectedLinks = {
+    Beratungshilfe: "/beratungshilfe",
+    Impressum: "/impressum",
+    Datenschutzerklärung: "/datenschutz",
+  };
+
+  let key: keyof typeof expectedLinks;
+  for (key in expectedLinks) {
+    const linkText = key;
+    const url = expectedLinks[linkText];
+    test(`${linkText} link`, async ({ page }) => {
+      const responsePromise = page.waitForResponse(
+        (resp) => resp.url().includes(url) && resp.status() === 200
+      );
+      await page.getByRole("link", { name: linkText }).click();
+      await responsePromise;
+    });
+  }
 });
