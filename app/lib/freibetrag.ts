db73b39aa15c1543363682d11d6bc2931f63aaf9
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2023-04-21 17:22:04 +0200

    Adjust calculation for child & parrtner income

--- app/lib/freibetrag.ts
+++ app/lib/freibetrag.ts
@@ -8,16 +8,36 @@ export function freibetrag(
   childrenAbove18?: number,
   childrenIncome?: number
 ): number {
-  let betrag = 552 + 20;
+  let betrag = 572;
 
-  if (working) betrag += 251;
-  if (partnership) betrag += 552;
-  if (childrenBelow6) betrag += childrenBelow6 * 350;
-  if (children7To14) betrag += children7To14 * 383;
-  if (children15To18) betrag += children15To18 * 462;
-  if (childrenAbove18) betrag += childrenAbove18 * 442;
+  if (working) {
+    betrag += 251;
+  }
 
-  return betrag - ((childrenIncome ?? 0) + (partnerIncome ?? 0));
+  if (partnership) {
+    betrag += 552;
+
+    if (partnerIncome) {
+      const partnerIcomeFreibetrag = Math.max(552 - partnerIncome, 0);
+      betrag += partnerIcomeFreibetrag;
+    }
+  }
+
+  let childrenBetrag =
+    (childrenBelow6 ?? 0) * 350 +
+    (children7To14 ?? 0) * 383 +
+    (children15To18 ?? 0) * 462 +
+    (childrenAbove18 ?? 0) * 442;
+
+  if (childrenIncome) {
+    const childrenIncomeFreibetrag = Math.max(
+      childrenBetrag - childrenIncome,
+      0
+    );
+    betrag += childrenIncomeFreibetrag;
+  }
+
+  return betrag - (childrenIncome ?? 0) - (partnerIncome ?? 0);
 }
 
 export function freibetragShort(
