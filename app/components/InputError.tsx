d8879055f3836c4b16321f3faec2e197088cec1f
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-04-27 11:31:06 +0200

    Add icon to error message below input field

--- app/components/InputError.tsx
+++ app/components/InputError.tsx
@@ -1,3 +1,4 @@
+import { ErrorOutline } from "@mui/icons-material";
 import type { ReactNode } from "react";
 
 type InputErrorProps = {
@@ -7,7 +8,11 @@ type InputErrorProps = {
 
 const InputError = ({ inputName, children }: InputErrorProps) => {
   return (
-    <div id={`${inputName}-error`} className="mt-2 text-xs text-red-600">
+    <div
+      id={`${inputName}-error`}
+      className="mt-4 text-xs text-red-800 relative pl-20"
+    >
+      <ErrorOutline className="!w-16 !h-16 absolute top-0 left-0" />
       <span className="sr-only">Fehler:</span> {children}
     </div>
   );
