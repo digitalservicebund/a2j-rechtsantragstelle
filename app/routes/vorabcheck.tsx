bc1ae75b29b72ffcd7229fb319d7eda476a98bbf
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-05-08 14:57:00 +0200

    feat: cleaner subdir forward

--- app/routes/vorabcheck.tsx
+++ app/routes/vorabcheck.tsx
@@ -3,11 +3,13 @@ import { redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import { initialStepID } from "~/lib/vorabcheck/flow.server";
 
+const subdir = "vorabcheck";
+
 export const loader: LoaderFunction = async ({ request }) => {
   // Reroute to initial step on empty formStepID
-  const url = new URL(request.url);
-  if (url.pathname === "/vorabcheck/" || url.pathname === "/vorabcheck") {
-    return redirect(`/vorabcheck/${initialStepID}`);
+  const { pathname } = new URL(request.url);
+  if (pathname === `/${subdir}/` || pathname === `/${subdir}`) {
+    return redirect(`/${subdir}/${initialStepID}`);
   }
   return null;
 };
