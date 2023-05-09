95668cab613cc1d52d2aaaa13e5eba785793eed4
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-05-09 17:08:40 +0200

    Allow loading content file

---
+++ app/services/cms/file/client.ts
@@ -0,0 +1,35 @@
+import type { Page } from "~/services/cms/models/Page";
+import type { ElementWithId } from "~/services/cms/models/ElementWithId";
+import type { ErrorCategory } from "~/services/cms/models/formComponents";
+import type { SingleComponentCMS } from "~/services/cms/models/singleComponents";
+import type { VorabCheckCommons } from "~/services/cms/models/commons/VorabCheckCommons";
+import type { ResultPage } from "~/services/cms/models/ResultPage";
+import type { VorabcheckPage } from "~/services/cms/models/VorabcheckPage";
+import fs from "fs";
+import path from "path";
+
+export const getContentFilePath = (
+  filePath: string,
+  workingDirectory?: string
+) => {
+  const directory = workingDirectory ? [workingDirectory] : [__dirname, ".."]; // leave build directory
+  return path.join(...directory, filePath);
+};
+
+export const loadContentFile = (filePath: string) => {
+  const content = fs.readFileSync(filePath);
+  const jsonContent = JSON.parse(content.toString());
+  // TODO refine type to avoid unknown casting
+  return jsonContent as unknown as Record<
+    string,
+    | {
+        attributes:
+          | Page
+          | VorabcheckPage
+          | ResultPage
+          | ElementWithId
+          | ErrorCategory;
+      }[]
+    | { attributes: SingleComponentCMS | VorabCheckCommons }
+  >;
+};
