7c643f7189caec90cf29a5e71d4cb3431ce920fe
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-05-26 18:34:58 +0200

    Fix missing fields in Footer zod schema

--- app/services/cms/models/Footer.ts
+++ app/services/cms/models/Footer.ts
@@ -2,11 +2,17 @@ import { z } from "zod";
 import { ImageSchema } from "./Image";
 import { LinkSchema } from "./Link";
 import { ParagraphSchema } from "./Paragraph";
+import { HasLocaleSchema } from "./HasLocale";
+import { HasTimestampsSchema } from "./HasTimestamps";
 
-export const FooterSchema = z.object({
-  image: ImageSchema,
-  paragraphs: z.array(ParagraphSchema),
-  links: z.array(LinkSchema),
-});
+export const FooterSchema = z
+  .object({
+    image: ImageSchema,
+    paragraphs: z.array(ParagraphSchema),
+    links: z.array(LinkSchema),
+  })
+  .merge(HasLocaleSchema)
+  .merge(HasTimestampsSchema)
+  .strict();
 
 export type Footer = z.infer<typeof FooterSchema>;
