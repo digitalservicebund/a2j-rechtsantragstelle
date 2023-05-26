01f21a12ee7ccdee60250c714392b59955642775
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-05-26 09:33:14 +0200

    Fix merge order

--- app/services/cms/models/Background.ts
+++ app/services/cms/models/Background.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 import { WrapperSchema } from "./Wrapper";
 
-export const BackgroundSchema = WrapperSchema.merge(
-  z.object({
+export const BackgroundSchema = z
+  .object({
     id: z.number(),
     __component: z.literal("meta.background").optional(),
   })
-);
+  .merge(WrapperSchema);
 
 export type Background = z.infer<typeof BackgroundSchema>;
--- app/services/cms/models/Container.ts
+++ app/services/cms/models/Container.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 import { WrapperSchema } from "./Wrapper";
 
-export const ContainerSchema = WrapperSchema.merge(
-  z.object({
+export const ContainerSchema = z
+  .object({
     id: z.number(),
     __component: z.literal("meta.container").optional(),
   })
-);
+  .merge(WrapperSchema);
 
 export type Container = z.infer<typeof ContainerSchema>;
