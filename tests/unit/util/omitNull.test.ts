4e96896696ce3fa9d79b803eaffdb57f0afd42bc
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-05-28 23:39:02 +0200

    Fix given examples are untested, add more test cases

--- tests/unit/util/omitNull.test.ts
+++ tests/unit/util/omitNull.test.ts
@@ -1,46 +1,78 @@
 import { omitNull } from "~/util/omitNull";
 
 describe("util/omitNull", () => {
-  it("removes entries where the value is null", () => {
-    const data = {
-      a: {
-        b: 1,
-        c: null,
-        d: [
-          {
-            e: 2,
-            f: null,
-          },
-          {
-            g: 3,
-            h: null,
-          },
-          null,
-        ],
-        i: undefined,
-        j: [],
-        k: {},
-        l: 0,
-      },
-    };
-    expect(omitNull(data)).toMatchInlineSnapshot(`
-      {
-        "a": {
-          "b": 1,
-          "d": [
+  const cases = [
+    {
+      input: { a: { b: null, c: 1 } },
+      output: { a: { c: 1 } },
+    },
+    {
+      input: { d: undefined, e: [null, { f: null }] },
+      output: { d: undefined, e: [{}] },
+    },
+    {
+      input: [],
+      output: [],
+    },
+    {
+      input: [null, null],
+      output: [],
+    },
+    {
+      input: [null, undefined, { x: false, y: "", z: 0 }],
+      output: [undefined, { x: false, y: "", z: 0 }],
+    },
+    {
+      input: null,
+      output: undefined,
+    },
+    {
+      input: undefined,
+      output: undefined,
+    },
+    {
+      input: {
+        a: {
+          b: 1,
+          c: null,
+          d: [
             {
-              "e": 2,
+              e: 2,
+              f: null,
             },
             {
-              "g": 3,
+              g: 3,
+              h: null,
+            },
+            null,
+          ],
+          i: undefined,
+          j: [],
+          k: {},
+          l: 0,
+        },
+      },
+      output: {
+        a: {
+          b: 1,
+          d: [
+            {
+              e: 2,
+            },
+            {
+              g: 3,
             },
           ],
-          "i": undefined,
-          "j": [],
-          "k": {},
-          "l": 0,
+          i: undefined,
+          j: [],
+          k: {},
+          l: 0,
         },
-      }
-    `);
+      },
+    },
+  ];
+
+  test.each(cases)("given $input, returns $output", ({ input, output }) => {
+    expect(omitNull(input)).toEqual(output);
   });
 });
