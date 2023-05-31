1dafa019b64758adff19631e41b75a57b5b832cd
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-05-31 11:27:01 +0200

    test: add all conversion tests

--- tests/unit/services/gerichtsfinder/convertData.test.ts
+++ tests/unit/services/gerichtsfinder/convertData.test.ts
@@ -2,10 +2,14 @@ import {
   conversions,
   gerbehIndex,
 } from "~/services/gerichtsfinder/convertJsonDataTable";
-import type { Jmtd14VTErwerberGerbeh } from "~/services/gerichtsfinder/types";
+import type {
+  Jmtd14VTErwerberGerbeh,
+  Jmtd14VTErwerberPlzortk,
+  Jmtd14VTErwerberPlzstrn,
+} from "~/services/gerichtsfinder/types";
 
-describe("data conversions", () => {
-  const gerbehData: Jmtd14VTErwerberGerbeh = {
+describe("gerbeh data conversions", () => {
+  const gerbehAmtsgericht: Jmtd14VTErwerberGerbeh = {
     AG: "string",
     BEZEICHNUNG: "string",
     LG: "string",
@@ -19,19 +23,97 @@ describe("data conversions", () => {
     XML_SUPPORT: "JA",
   };
 
-  const gerbehKey = gerbehIndex({
-    LKZ: gerbehData.LKZ,
-    OLG: gerbehData.OLG,
-    LG: gerbehData.LG,
-    AG: gerbehData.AG,
-    typInfo: gerbehData.TYP_INFO,
+  const gerbehAmtsgerichtKey = gerbehIndex({
+    LKZ: gerbehAmtsgericht.LKZ,
+    OLG: gerbehAmtsgericht.OLG,
+    LG: gerbehAmtsgericht.LG,
+    AG: gerbehAmtsgericht.AG,
+    typInfo: gerbehAmtsgericht.TYP_INFO,
   });
 
-  it("handles GERBEH file", () => {
+  it("handles valid Gerbeh data", () => {
     expect(
       conversions["JMTD14_VT_ERWERBER_GERBEH_DATA_TABLE.json"]({
-        JMTD14_VT_ERWERBER_GERBEH: [gerbehData],
+        JMTD14_VT_ERWERBER_GERBEH: [gerbehAmtsgericht],
       })
-    ).toEqual({ [gerbehKey]: gerbehData });
+    ).toEqual({ [gerbehAmtsgerichtKey]: gerbehAmtsgericht });
+  });
+
+  it("filters non-amtsgericht Gerbeh content", () => {
+    let nonAmtsgericht = { ...gerbehAmtsgericht };
+    nonAmtsgericht.TYP_INFO = "Arbeitsgericht";
+
+    expect(
+      conversions["JMTD14_VT_ERWERBER_GERBEH_DATA_TABLE.json"]({
+        JMTD14_VT_ERWERBER_GERBEH: [gerbehAmtsgericht, nonAmtsgericht],
+      })
+    ).toEqual({ [gerbehAmtsgerichtKey]: gerbehAmtsgericht });
+  });
+});
+
+describe("PlzOrtk data conversions", () => {
+  const plzOrtkEntry: Jmtd14VTErwerberPlzortk = {
+    ANGELEGENHEIT_INFO: "Prozesskostenhilfe eingehend",
+    GERBEH_AG: "string",
+    GERBEH_LG: "string",
+    GERBEH_LKZ: "string",
+    GERBEH_OLG: "string",
+    GERBEH_STAMM: "string",
+    GERBEH_TYP_INFO: "Zivilgericht - Amtsgericht",
+    ORT: "string",
+    ORTK: "string",
+    PLZ: "string",
+    PLZM_INFO: "Zustellbezirk",
+  };
+
+  it("handles valid PlzOrtk data", () => {
+    const input = { JMTD14_VT_ERWERBER_PLZORTK: [plzOrtkEntry] };
+    expect(
+      conversions["JMTD14_VT_ERWERBER_PLZORTK_DATA_TABLE.json"](input)
+    ).toEqual({ [plzOrtkEntry.PLZ]: [plzOrtkEntry] });
+  });
+
+  it("ignores entries without prozesskosten", () => {
+    let plzOrtkEntry2 = { ...plzOrtkEntry };
+    plzOrtkEntry2.ANGELEGENHEIT_INFO = "arbitrary value";
+    const input = { JMTD14_VT_ERWERBER_PLZORTK: [plzOrtkEntry, plzOrtkEntry2] };
+
+    expect(
+      conversions["JMTD14_VT_ERWERBER_PLZORTK_DATA_TABLE.json"](input)
+    ).toEqual({ [plzOrtkEntry.PLZ]: [plzOrtkEntry] });
+  });
+});
+
+describe("PlzStrn data conversions", () => {
+  const plzStrnEntry: Jmtd14VTErwerberPlzstrn = {
+    AG: "string",
+    ANGELEGENHEIT_INFO: "Prozesskostenhilfe eingehend",
+    HNR_BIS: "string",
+    HNR_MERKMAL_INFO: "fortlaufende Hausnummern",
+    HNR_VON: "string",
+    LG: "string",
+    LKZ: "string",
+    OLG: "string",
+    ORTK: "string",
+    PLZ: "string",
+    STRN: "string",
+    TYP_INFO: "Zivilgericht - Amtsgericht",
+  };
+
+  it("handles valid PlzStrn data", () => {
+    const input = { JMTD14_VT_ERWERBER_PLZSTRN: [plzStrnEntry] };
+    expect(
+      conversions["JMTD14_VT_ERWERBER_PLZSTRN_DATA_TABLE.json"](input)
+    ).toEqual({ [plzStrnEntry.PLZ]: [plzStrnEntry] });
+  });
+
+  it("filters invalid PlzStrn data", () => {
+    let plzStrnEntry2 = { ...plzStrnEntry };
+    plzStrnEntry2.TYP_INFO = "Arbeitsgericht";
+
+    const input = { JMTD14_VT_ERWERBER_PLZSTRN: [plzStrnEntry] };
+    expect(
+      conversions["JMTD14_VT_ERWERBER_PLZSTRN_DATA_TABLE.json"](input)
+    ).toEqual({ [plzStrnEntry.PLZ]: [plzStrnEntry] });
   });
 });
