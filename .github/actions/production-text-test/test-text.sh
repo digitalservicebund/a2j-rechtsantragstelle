153622e259be20e8c4b87ac975255cbda4a91590
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-06-01 19:05:15 +0200

    Fix test-production-text job

--- .github/actions/production-text-test/test-text.sh
+++ .github/actions/production-text-test/test-text.sh
@@ -1,7 +1,7 @@
 PRODUCTION_URL="https://a2j-rast.prod.ds4g.net/"
 TEXT="Onlineservices der Justiz"
 
-if curl -s $PRODUCTION_URL --header "Authorization: Basic $1" | grep -o $TEXT; then
+if curl -s $PRODUCTION_URL --header "Authorization: Basic $1" | grep -o "$TEXT"; then
   echo "Text found"
   exit 0
 else
