f15681d6884daf53a5bf038029792026ead6cb92
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-06-02 20:18:51 +0200

    Add test factories for Strapi models (incomplete)

---
+++ tests/factories/cmsModels/strapiLink.ts
@@ -0,0 +1,10 @@
+import { Factory } from "fishery";
+import { faker } from "@faker-js/faker";
+import type { StrapiLink } from "~/services/cms/models/StrapiLink";
+
+export const strapiLinkFactory = Factory.define<StrapiLink>(() => {
+  return {
+    url: faker.internet.url(),
+    text: faker.lorem.words(),
+  };
+});
