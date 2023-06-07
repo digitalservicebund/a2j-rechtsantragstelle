0e4c8105a38c80e7a5a5076f51158b52eb317b20
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-07 02:21:27 +0200

    refactor: remove unused S3 code

--- app/lib/io.ts
+++ app/lib/io.ts
@@ -1,5 +1,3 @@
-import fs from "node:fs";
-
 export function printFileReadError(error: unknown) {
   if (
     typeof error === "object" &&
@@ -14,20 +12,3 @@ export function printFileReadError(error: unknown) {
     }
   }
 }
-
-// Cache loading JSON files even during dev live reload, see https://remix.run/docs/en/main/tutorials/jokes#connect-to-the-database
-declare global {
-  var jsonData: Record<string, any> | undefined;
-}
-
-export const loadJsonFromFile = (filePath: string) => {
-  if (!global.jsonData) global.jsonData = {};
-  if (!global.jsonData[filePath]) {
-    try {
-      global.jsonData[filePath] = JSON.parse(fs.readFileSync(filePath, "utf8"));
-    } catch (error) {
-      printFileReadError(error);
-    }
-  }
-  return global.jsonData[filePath] || {};
-};
--- tests/unit/lib/io.test.ts
+++ tests/unit/lib/io.test.ts
@@ -1,4 +1,4 @@
-import { printFileReadError, loadJsonFromFile } from "~/lib/io";
+import { printFileReadError } from "~/lib/io";
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -7,27 +7,6 @@ afterEach(() => {
 const consoleErrorSpy = jest
   .spyOn(global.console, "error")
   .mockImplementation(() => jest.fn());
-
-describe("loadJsonFromFile", () => {
-  it("loads content from a json file", () => {
-    expect(loadJsonFromFile(`${__dirname}/test.json`)).toStrictEqual({
-      testContent: [{ a: 1 }],
-    });
-  });
-
-  it("returns an empty object for invalid paths", () => {
-    expect(loadJsonFromFile(`not_existing_file.json`)).toStrictEqual({});
-  });
-
-  it("returns an empty object for malformed files", () => {
-    expect(loadJsonFromFile(`${__dirname}/malformed.json`)).toStrictEqual({});
-  });
-
-  it("communicates an error for invalid path", () => {
-    loadJsonFromFile(`not_existing_file.json`);
-    expect(consoleErrorSpy).toHaveBeenCalled();
-  });
-});
 
 describe("handleIOError", () => {
   it("prints file not found error", () => {
