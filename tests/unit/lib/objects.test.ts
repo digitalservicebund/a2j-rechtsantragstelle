34c995b1d7ef4b0cc4845ffbd333f13632617318
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-08 10:45:43 +0200

    tests: add objects.test.ts

---
+++ tests/unit/lib/objects.test.ts
@@ -0,0 +1,23 @@
+import { isKeyOfObject, objectMap } from "~/lib/objects";
+
+describe("isKeyOfObject", () => {
+  it("detects existing keys", () => {
+    expect(isKeyOfObject("key", { key: 1 })).toBe(true);
+  });
+
+  it("fails on existing keys", () => {
+    expect(isKeyOfObject("key1", { key: 1 })).toBe(false);
+  });
+});
+
+describe("objectMap", () => {
+  it("maps objects", () => {
+    expect(objectMap({ key: 1 }, (v) => v * 2)).toEqual({ key: 2 });
+  });
+
+  it("receives key and index", () => {
+    expect(objectMap({ key: 1 }, (_, k, i) => `${k}${i}`)).toEqual({
+      key: "key0",
+    });
+  });
+});
