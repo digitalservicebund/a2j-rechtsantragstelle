70baa6d1c6dfe549b281dc4f33267119bb0bdbb7
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-06-12 20:40:57 +0200

    Add basic e2e tests for court search

---
+++ tests/e2e/amtsgericht.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+import { Amtsgericht } from "./pom/Amtsgericht";
+import { expectPageToBeAccessible } from "./util/expectPageToBeAccessible";
+
+let amtsgericht: Amtsgericht;
+
+test.beforeEach(async ({ page }) => {
+  amtsgericht = new Amtsgericht(page);
+  await amtsgericht.goto();
+});
+
+test("simple search with one court", async ({ page }) => {
+  // TODO: title missing
+  // await expectPageToBeAccessible({ page });
+  await amtsgericht.fillSearchField("13127");
+  await amtsgericht.submitSearchForm();
+  await expect(page.locator("h1")).toContainText(amtsgericht.resultHeading);
+  await expectPageToBeAccessible({ page });
+});
+
+test("simple search with edge cases", async ({ page }) => {
+  await amtsgericht.fillSearchField("20457");
+  await amtsgericht.submitSearchForm();
+  await expect(page.locator("h1")).toContainText(
+    "Im Bereich Ihrer Postleitzahl 20457 sind verschiedene Amtsgerichte zuständig. Wohnen Sie in einer dieser Straßen?"
+  );
+  // TODO: title missing
+  // await expectPageToBeAccessible({ page });
+  await page.getByRole("link", { name: "Berliner Ufer" }).click();
+  await expect(page.locator("h1")).toContainText(amtsgericht.resultHeading);
+});
+
+test("invalid search", async ({ page }) => {
+  await amtsgericht.fillSearchField("1234");
+  await amtsgericht.submitSearchForm();
+  await expect(page.locator("form")).toContainText("Ungültige Postleitzahl");
+  // TODO: title missing
+  // await expectPageToBeAccessible({ page });
+});
---
+++ tests/e2e/pom/Amtsgericht.ts
@@ -0,0 +1,25 @@
+import type { Page } from "@playwright/test";
+
+export class Amtsgericht {
+  readonly page: Page;
+  readonly resultHeading: string;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.resultHeading = "Ihr zuständiges Amtsgericht";
+  }
+
+  async goto() {
+    await this.page.goto("/amtsgericht/suche");
+  }
+
+  async fillSearchField(value: string) {
+    await this.page.locator("input[name=postcode]").fill(value);
+  }
+
+  async submitSearchForm() {
+    await this.page
+      .getByRole("button", { name: "Übernehmen & Weiter" })
+      .click();
+  }
+}
