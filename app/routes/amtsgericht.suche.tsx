ac96164812acbf05e1bcca227740419600d4bc06
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-12 13:57:47 +0200

    feat: add court search styling

--- app/routes/amtsgericht.suche.tsx
+++ app/routes/amtsgericht.suche.tsx
@@ -1,33 +1,75 @@
-import type { LoaderArgs } from "@remix-run/node";
+import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
-import { Container } from "~/components";
+import { useActionData } from "@remix-run/react";
+import { Background, Button, Container, Input } from "~/components";
+import { ButtonContainer } from "~/components/ButtonContainer";
 import { courtForPlz } from "~/services/gerichtsfinder/amtsgerichtData.server";
+import { ValidatedForm } from "remix-validated-form";
+import { withZod } from "@remix-validated-form/with-zod";
+import { z } from "zod";
 
-export async function loader({ request }: LoaderArgs) {
-  const url = new URL(request.url);
-  const postcode = url.searchParams.get("plz");
+function isValidPostcode(postcode: string) {
+  const postcodeNum = parseInt(postcode, 10);
+  return postcodeNum >= 1067 && postcodeNum <= 99998;
+}
 
-  if (!postcode) {
-    return json({ error: "" });
+const postcodeValidator = withZod(
+  z.object({
+    postcode: z.string().refine((postcode) => isValidPostcode(postcode), {
+      message: "Ungültige Postleitzahl",
+    }),
+  })
+);
+
+export async function action({ request }: ActionArgs) {
+  const validatedFormInput = await postcodeValidator.validate(
+    await request.formData()
+  );
+
+  let errorMessage =
+    validatedFormInput.error?.fieldErrors.postcode ??
+    !courtForPlz(validatedFormInput.data?.postcode)
+      ? "Postleitzahl nicht gefunden"
+      : "";
+
+  if (errorMessage == "") {
+    return redirect(`../amtsgericht/${validatedFormInput.data?.postcode}`);
   }
-  if (courtForPlz(postcode) === undefined) {
-    return json({ error: "Postleitzahl nicht gefunden" });
-  }
-
-  return redirect(`../amtsgericht/${postcode}`);
+  return json({ errorMessage });
 }
 
 export default function Index() {
-  const { error } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
   return (
-    <Container>
-      <Form method="get">
-        <input type="text" name="plz" placeholder="Postleitzahl" />
-        <button type="submit">Suche</button>
-      </Form>
-      {error}
-    </Container>
+    <Background backgroundColor="blue">
+      <Container>
+        <div className="ds-stack-24">
+          <div className="ds-label-03-reg">Amtsgericht finden</div>
+          <h1 className="ds-heading-02-reg">Wie ist Ihre Postleitzahl? </h1>
+        </div>
+      </Container>
+      <ValidatedForm method="post" validator={postcodeValidator} noValidate>
+        <Container>
+          <Input
+            name="postcode"
+            type="text"
+            label="Postleitzahl"
+            placeholder="12345"
+          />
+          {actionData?.errorMessage}
+        </Container>
+        <Container>
+          <ButtonContainer>
+            <Button href="#" look="tertiary" size="large">
+              Zurück
+            </Button>
+            <Button type="submit" size="large">
+              Übernehmen & Weiter
+            </Button>
+          </ButtonContainer>
+        </Container>
+      </ValidatedForm>
+    </Background>
   );
 }
