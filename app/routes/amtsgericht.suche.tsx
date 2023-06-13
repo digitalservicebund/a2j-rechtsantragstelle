361101c3980414f2c08ab2925c1d314853bf7280
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-13 10:48:49 +0200

    feat: use server side validation for PLZ lookup

--- app/routes/amtsgericht.suche.tsx
+++ app/routes/amtsgericht.suche.tsx
@@ -1,10 +1,10 @@
 import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { Background, Button, Container, Input } from "~/components";
 import ButtonContainer from "~/components/ButtonContainer";
 import { courtForPlz } from "~/services/gerichtsfinder/amtsgerichtData.server";
-import { ValidatedForm } from "remix-validated-form";
+import { ValidatedForm, validationError } from "remix-validated-form";
 import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 import { getStrapiAmtsgerichtCommon } from "~/services/cms";
@@ -15,18 +15,20 @@ function isValidPostcode(postcode: string) {
   return postcodeNum >= 1067 && postcodeNum <= 99998;
 }
 
-const postcodeValidator = withZod(
-  z.object({
-    postcode: z
-      .string()
-      .length(5, {
-        message: "Ungültige Postleitzahl",
-      })
-      .refine((postcode) => isValidPostcode(postcode), {
-        message: "Ungültige Postleitzahl",
-      }),
-  })
+const clientSchema = z.object({
+  postcode: z
+    .string()
+    .length(5)
+    .refine((postcode) => isValidPostcode(postcode)),
+});
+
+const serverSchema = clientSchema.refine(
+  (postcodeObj) => courtForPlz(postcodeObj.postcode) !== undefined,
+  { path: ["postcode"] }
 );
+
+const validatorClient = withZod(clientSchema);
+const validatorServer = withZod(serverSchema);
 
 export async function loader() {
   const common = await getStrapiAmtsgerichtCommon();
@@ -34,34 +36,20 @@ export async function loader() {
 }
 
 export async function action({ request }: ActionArgs) {
-  const validatedFormInput = await postcodeValidator.validate(
-    await request.formData()
-  );
-
-  let errorMessage =
-    validatedFormInput.error?.fieldErrors.postcode ??
-    !courtForPlz(validatedFormInput.data?.postcode)
-      ? "Postleitzahl nicht gefunden"
-      : "";
-
-  if (errorMessage == "") {
-    return redirect(
-      `../amtsgericht/ergebnis/${validatedFormInput.data?.postcode}`
-    );
-  }
-  return json({ errorMessage });
+  const result = await validatorServer.validate(await request.formData());
+  if (result.error) return validationError(result.error);
+  return redirect(`../amtsgericht/ergebnis/${result.data?.postcode}`);
 }
 
 export default function Index() {
   const { common } = useLoaderData<typeof loader>();
-  const actionData = useActionData<typeof action>();
   return (
     <Background backgroundColor="blue">
       <CourtFinderHeader label={common.featureName}>
         {common.searchHeading}
       </CourtFinderHeader>
 
-      <ValidatedForm method="post" validator={postcodeValidator} noValidate>
+      <ValidatedForm method="post" validator={validatorClient} noValidate>
         <Container>
           <Input
             name="postcode"
@@ -69,7 +57,6 @@ export default function Index() {
             label="Postleitzahl"
             placeholder="12345"
           />
-          {actionData?.errorMessage}
         </Container>
         <Container>
           <ButtonContainer>
