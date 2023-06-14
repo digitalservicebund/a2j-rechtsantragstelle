6c563eb34856cdfac52a06de896e92b2e52c0337
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-06-14 17:12:25 +0200

    Read new secret volume into env for strapi secrets

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 
 export GERICHTSFINDER_ENCRYPTION_KEY=$(cat /etc/courtdata-secrets/password)
 export SENTRY_DSN=$(cat /etc/sentry-secrets/password)
+export STRAPI_ACCESS_KEY=$(cat /etc/strapi-access-key-secret/password)
+export STRAPI_API=$(cat /etc/strapi-api-secret/password)
+
 node ./server.js
