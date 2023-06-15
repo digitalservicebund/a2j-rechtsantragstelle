742adeae505e93236ff2eda96e4c94e2506cc2fc
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-15 17:23:29 +0200

    tests: Merge & fix courtfinder e2e tests
    Co-authored-by: Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>

---
+++ tests/e2e/courtfinder.spec.ts
@@ -0,0 +1,116 @@
+import { test, expect } from "@playwright/test";
+import { CourtFinder } from "./pom/CourtFinder";
+import { expectPageToBeAccessible } from "./util/expectPageToBeAccessible";
+
+let courtfinder: CourtFinder;
+
+test.beforeEach(async ({ page }) => {
+  courtfinder = new CourtFinder(page);
+  await courtfinder.goto();
+});
+
+async function expectSingleCourtContent(courtfinder: CourtFinder) {
+  const { page } = courtfinder;
+  await expect(
+    page.getByRole("heading").filter({ hasText: "Adresse" })
+  ).toHaveCount(1);
+  await expect(
+    page.getByRole("heading").filter({ hasText: "Telefonnummer" })
+  ).toHaveCount(1);
+  await expect(
+    page.getByRole("link").filter({ hasText: "Webseite" })
+  ).toHaveCount(1);
+}
+
+test.describe("accessibility", () => {
+  test("search page", async () => {
+    await expectPageToBeAccessible(courtfinder);
+  });
+
+  test("single result page", async () => {
+    await courtfinder.searchPLZSingleResult();
+    await expectPageToBeAccessible(courtfinder);
+  });
+
+  test("edge cases page", async () => {
+    await courtfinder.searchPLZEdgeCases();
+    await expectPageToBeAccessible(courtfinder);
+  });
+
+  test("invalid PLZ search", async () => {
+    await courtfinder.searchPLZInvalid();
+    await expectPageToBeAccessible(courtfinder);
+  });
+
+  test("non-existant PLZ search", async () => {
+    await courtfinder.searchPLZNonExistant();
+    await expectPageToBeAccessible(courtfinder);
+  });
+});
+
+test.describe("form validation", () => {
+  test("Invalid PLZ", async ({ page }) => {
+    const expectedError = "Postleitzahl muss genau 5 Zeichen lang sein";
+    await courtfinder.searchPLZInvalid();
+    await expect(page.locator("form")).toContainText(expectedError);
+  });
+
+  test("Non-existant PLZ", async ({ page }) => {
+    const expectedError = "Postleitzahl existiert nicht";
+    await courtfinder.searchPLZNonExistant();
+    await expect(page.locator("form")).toContainText(expectedError);
+  });
+});
+
+test("single result contains relevant content", async ({ page }) => {
+  await courtfinder.searchPLZSingleResult();
+  const h1 = page.locator("h1");
+  await expect(h1).toContainText("zuständiges Amtsgericht");
+  await expect(page.getByText(courtfinder.singleResultPLZ)).toBeVisible();
+  await expectSingleCourtContent(courtfinder);
+});
+
+test.describe("edge cases results", () => {
+  test("heading contains PLZ", async ({ page }) => {
+    await courtfinder.searchPLZEdgeCases();
+    await expect(page.locator("h1")).toContainText(
+      courtfinder.multipleResultPLZ
+    );
+  });
+
+  test("leads to single court", async ({ page }) => {
+    await courtfinder.searchPLZEdgeCases();
+    await page.getByRole("link", { name: "Berliner Ufer" }).click();
+    await expectSingleCourtContent(courtfinder);
+  });
+
+  test("has working default button", async ({ page }) => {
+    await courtfinder.searchPLZEdgeCases();
+    await page.locator("#defaultButton").click();
+    await expectSingleCourtContent(courtfinder);
+  });
+});
+
+test.describe("back button", () => {
+  test("works with searching", async ({ page, baseURL }) => {
+    await courtfinder.gotoWithReferrer(baseURL);
+    await courtfinder.clickBackButton();
+    await page.waitForURL(courtfinder.referrer);
+  });
+
+  test("works after single result", async ({ page, baseURL }) => {
+    await courtfinder.gotoWithReferrer(baseURL);
+    await courtfinder.searchPLZSingleResult();
+    await page.getByRole("link", { name: "Suche wiederholen" }).click();
+    await courtfinder.clickBackButton();
+    await page.waitForURL(courtfinder.referrer);
+  });
+
+  test("works after edge cases", async ({ page, baseURL }) => {
+    await courtfinder.gotoWithReferrer(baseURL);
+    await courtfinder.searchPLZEdgeCases();
+    await courtfinder.clickBackButton();
+    await courtfinder.clickBackButton();
+    await page.waitForURL(courtfinder.referrer);
+  });
+});
---
+++ tests/e2e/pom/CourtFinder.ts
@@ -0,0 +1,63 @@
+import type { Page } from "@playwright/test";
+
+export class CourtFinder {
+  readonly page: Page;
+  readonly baseURL: string = "/beratungshilfe/zustaendiges-gericht";
+  readonly searchURL: string = `${this.baseURL}/suche`;
+  readonly resultURL: string = `${this.baseURL}/ergebnis`;
+  readonly singleResultPLZ = "13086";
+  readonly multipleResultPLZ = "20457";
+  readonly referrer = "/beratungshilfe";
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto(this.searchURL);
+  }
+
+  async gotoWithReferrer(baseURL: string | undefined) {
+    await this.page.goto(`${this.searchURL}?returnToHere`, {
+      referer: `${baseURL}${this.referrer}`,
+    });
+  }
+
+  async searchPLZSingleResult() {
+    await this.fillSearchField(this.singleResultPLZ);
+    await this.submitSearchForm();
+    await this.page.waitForURL(`${this.resultURL}/${this.singleResultPLZ}`);
+  }
+
+  async searchPLZEdgeCases() {
+    await this.fillSearchField(this.multipleResultPLZ);
+    await this.submitSearchForm();
+    await this.page.waitForURL(
+      `${this.baseURL}/auswahl/${this.multipleResultPLZ}`
+    );
+  }
+
+  async searchPLZInvalid() {
+    await this.fillSearchField("1234");
+    await this.submitSearchForm();
+  }
+
+  async searchPLZNonExistant() {
+    await this.fillSearchField("12345");
+    await this.submitSearchForm();
+  }
+
+  async clickBackButton() {
+    await this.page.getByRole("link", { name: "Zurück" }).click();
+  }
+
+  async fillSearchField(value: string) {
+    await this.page.locator("input[name=postcode]").fill(value);
+  }
+
+  async submitSearchForm() {
+    await this.page
+      .getByRole("button", { name: "Übernehmen & Weiter" })
+      .click();
+  }
+}
