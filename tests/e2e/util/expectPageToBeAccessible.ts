aeca2adcf27b0a1f0a86698040246f567a30b7b4
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-16 08:18:04 +0200

    test: rmv workaround (fixed by playwright update)

--- tests/e2e/util/expectPageToBeAccessible.ts
+++ tests/e2e/util/expectPageToBeAccessible.ts
@@ -1,10 +1,8 @@
 import type { Page } from "@playwright/test";
-import type { Page as CorePage } from "playwright-core";
 import { expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 
 export const expectPageToBeAccessible = async ({ page }: { page: Page }) => {
-  const axeConfig = { page } as { page: CorePage }; // TODO: check why type Page differs between playwright-core and @playwright/test
-  const accessibilityScanResults = await new AxeBuilder(axeConfig).analyze();
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 };
