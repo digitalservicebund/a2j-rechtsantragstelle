58edcb9d7c964cc06244001af653e2742c8e254d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-06-21 09:53:12 +0200

    Undefine-proof getVerfuegbaresEinkommenFreibetrag

--- app/models/beratungshilfe.ts
+++ app/models/beratungshilfe.ts
@@ -4,9 +4,9 @@ import type { Context } from "~/models/flows/beratungshilfe/guards";
 
 export const getVerfuegbaresEinkommenFreibetrag = (context: Context) => {
   const isWorking =
-    context["erwerbstaetigkeit"]["isErwerbstaetig"] == YesNoAnswer.Enum.yes;
+    context["erwerbstaetigkeit"]?.["isErwerbstaetig"] == YesNoAnswer.Enum.yes;
   const isInPartnership =
-    context["partnerschaft"]["partnerschaft"] == YesNoAnswer.Enum.yes;
-  const kidsCountTotal = parseFloat(context["kinderAnzahlKurz"]["kidsTotal"]);
+    context["partnerschaft"]?.["partnerschaft"] == YesNoAnswer.Enum.yes;
+  const kidsCountTotal = parseFloat(context["kinderAnzahlKurz"]?.["kidsTotal"]);
   return freibetragShort(isWorking, isInPartnership, kidsCountTotal);
 };
