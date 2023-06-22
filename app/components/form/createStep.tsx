9de167543d07d3fed36586c309856cbdd0f7ef48
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-22 11:10:16 +0200

    chore: cleanup whitespace

--- app/components/form/createStep.tsx
+++ app/components/form/createStep.tsx
@@ -1,19 +1,13 @@
 import { buildMoneyValidationSchema } from "~/services/validation/money/buildMoneyValidationSchema";
 import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
-import type { StepInterface } from "~/components/form/steps";
 import { z } from "zod";
 
-export function yesNoStep(inputName: string): StepInterface {
-  const schema = z.object({ [inputName]: YesNoAnswer });
-  return {
-    schema,
-  };
+export function yesNoStep(inputName: string) {
+  return { schema: z.object({ [inputName]: YesNoAnswer }) };
 }
 
 export function moneyInputStep(inputName: string) {
   return {
-    schema: z.object({
-      [inputName]: buildMoneyValidationSchema({ min: 0 }),
-    }),
+    schema: z.object({ [inputName]: buildMoneyValidationSchema({ min: 0 }) }),
   };
 }
