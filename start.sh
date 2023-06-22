21b95f8bb48fa6b4bee3d961162441a1a2add098
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-06-22 13:08:02 +0200

    fix(start.sh): continue on file not found

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 set -euf
 
 # Declare and assign separately to avoid masking return values, see https://www.shellcheck.net/wiki/SC2155
-GERICHTSFINDER_ENCRYPTION_KEY=$(cat /etc/courtdata-secrets/password)
+GERICHTSFINDER_ENCRYPTION_KEY=$(cat /etc/courtdata-secrets/password) || true
 export GERICHTSFINDER_ENCRYPTION_KEY
-SENTRY_DSN=$(cat /etc/sentry-secrets/password)
+SENTRY_DSN=$(cat /etc/sentry-secrets/password) || true
 export SENTRY_DSN
-STRAPI_ACCESS_KEY=$(cat /etc/strapi-access-key-secret/password)
+STRAPI_ACCESS_KEY=$(cat /etc/strapi-access-key-secret/password) || true
 export STRAPI_ACCESS_KEY
-STRAPI_API=$(cat /etc/strapi-api-secret/password)
+STRAPI_API=$(cat /etc/strapi-api-secret/password) || true
 export STRAPI_API
-POSTHOG_API_KEY=$(cat /etc/posthog-secrets/password)
+POSTHOG_API_KEY=$(cat /etc/posthog-secrets/password) || true
 export POSTHOG_API_KEY
 
 node ./server.js
