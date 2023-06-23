dedc34df21e35e0a82cd507ad27835f54ae14ecc
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-06-23 12:20:40 +0200

    Quick clone of visualisierung for beratungshilfe

---
+++ app/routes/beratungshilfe.visualisierung.tsx
@@ -0,0 +1,48 @@
+import { createMachine } from "xstate";
+import type { Context } from "~/models/flows/beratungshilfe/guards";
+import { guards } from "~/models/flows/beratungshilfe/guards";
+import beratungshilfeFlow from "~/models/flows/beratungshilfe/config.json";
+import { toDirectedGraph } from "@xstate/graph";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+function mermaidFlowchart(digraph: ReturnType<typeof toDirectedGraph>) {
+  // Converts a graph into mermaid.js flowchart syntax
+  // See https://mermaid.js.org/syntax/flowchart.html
+  let flowchartDiagram = `---
+title: Vorabcheck Flowchart
+---
+flowchart TD\n`;
+
+  digraph.children.forEach((state) => {
+    state.edges.forEach((edge) => {
+      const source = edge.source.path[0];
+      const target = edge.target.path[0];
+      let arrow = edge.label.text === "SUBMIT" ? "-->" : ".->";
+
+      if (edge.transition.cond?.name !== undefined)
+        arrow = `${arrow}|${edge.transition.cond?.name}|`;
+
+      flowchartDiagram = flowchartDiagram.concat(
+        `    ${source} ${arrow} ${target}\n`
+      );
+    });
+  });
+
+  return flowchartDiagram;
+}
+
+export const loader = async () => {
+  //@ts-ignore
+  const machine = createMachine<Context>(beratungshilfeFlow, { guards });
+  const digraph = toDirectedGraph(machine);
+
+  // Mermaid generates a picture when given a base64 encoded chart description
+  const base64Graph = Buffer.from(mermaidFlowchart(digraph)).toString("base64");
+  return json({ url: `https://mermaid.ink/img/${base64Graph}` });
+};
+
+export default function Index() {
+  const { url } = useLoaderData<typeof loader>();
+  return <img alt="current flow chart" src={url}></img>;
+}
