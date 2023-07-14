54cdc85eb228b1b4618e800ea6b10b77ed889d32
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-07-14 12:01:10 +0200

    fix: new versionComment field

--- app/services/cms/models/HasStrapiVersions.ts
+++ app/services/cms/models/HasStrapiVersions.ts
@@ -5,6 +5,7 @@ export const HasStrapiVersionsSchema = z.object({
   versionNumber: z.number(),
   isVisibleInListView: z.boolean(),
   versions: z.object({ data: z.array(z.any()) }),
+  versionComment: z.string().nullable(),
 });
 
 export type HasStrapiVersions = z.infer<typeof HasStrapiVersionsSchema>;
