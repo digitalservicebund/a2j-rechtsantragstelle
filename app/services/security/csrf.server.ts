ed9b2453205c4523ae18d9ab0c92986daeade921
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-07-17 12:24:56 +0200

    Simplify CSRF token parsing

--- app/services/security/csrf.server.ts
+++ app/services/security/csrf.server.ts
@@ -4,26 +4,17 @@ import { randomBytes } from "crypto";
 import { getSession } from "~/sessions";
 import { CSRFKey } from "./csrf";
 
-export function createCSRFToken() {
+function createCSRFToken() {
   return randomBytes(100).toString("base64");
 }
 
-export async function validateCSRFToken(request: Request, session: Session) {
-  // first we parse the body, be sure to clone the request so you can parse the body again in the future
-  const body = Object.fromEntries(
-    new URLSearchParams(await request.clone().text()).entries(),
-  ) as { csrf?: string };
-  // then we throw an error if one of our validations didn't pass
-  if (!session.has(CSRFKey))
-    throw new Error("Session: CSRF Token not included.");
-  if (!body.csrf) throw new Error("Body: CSRF Token not included.");
-  if (body.csrf !== session.get(CSRFKey))
-    throw new Error(
-      `CSRF tokens do not match. body: ${body.csrf}, session: ${session.get(
-        CSRFKey,
-      )}`,
-    );
-  // we don't need to return anything, if the validation fail it will throw an error
+async function validateCSRFToken(request: Request, session: Session) {
+  const bodyToken = (await request.clone().formData()).get(CSRFKey);
+  const sessionToken = session.get(CSRFKey) as string | undefined;
+  if (!sessionToken) throw new Error("Session: CSRF Token not included.");
+  if (!bodyToken) throw new Error("Body: CSRF Token not included.");
+  if (bodyToken !== session.get(CSRFKey))
+    throw new Error(`CSRF tokens between body and session do not match.`);
 }
 
 export async function createCSRFSession(request: Request) {
@@ -39,7 +30,6 @@ export async function validatedSession(request: Request) {
     await validateCSRFToken(request, session);
   } catch (error) {
     if (error instanceof Error) {
-      session.flash("error", error.message);
       console.error(error.message);
     }
     return;
