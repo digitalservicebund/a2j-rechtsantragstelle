18a87be862a64f33686873babb83b14a48d4d375
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-07-18 10:11:31 +0200

    use chained logic when possible

--- app/services/props/getImageProps.ts
+++ app/services/props/getImageProps.ts
@@ -3,6 +3,6 @@ import { omitNull } from "~/util/omitNull";
 import type { StrapiImage } from "../cms/models/StrapiImage";
 
 export const getImageProps = (cmsData: StrapiImage | null | undefined) => {
-  if (!cmsData || !cmsData.data) return undefined;
+  if (!cmsData?.data) return undefined;
   return ImagePropsSchema.parse(omitNull(cmsData.data.attributes));
 };
