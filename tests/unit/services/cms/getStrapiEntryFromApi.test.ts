9ea0d1d02a35983b5f8d2984d991289048c948c8
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-07-24 14:19:34 +0200

    test: fix staging locale request order
    co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
    co-authored-by: Frederike Ramin <frederike.ramin@digitalservice.bund.de>
    co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

--- tests/unit/services/cms/getStrapiEntryFromApi.test.ts
+++ tests/unit/services/cms/getStrapiEntryFromApi.test.ts
@@ -8,42 +8,60 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("services/cms", () => {
   describe("getStrapiEntryFromApi", () => {
+    afterEach(() => {
+      axiosGetSpy.mockClear();
+    });
+
     const data = "data";
     const defaultOptions: GetStrapiEntryOpts = {
       apiId: "api-id",
       locale: "de",
     };
     const defaultResponseData = { data: { data } };
+    const emptyResponseData = { data: [] };
     const expectedRequestUrl =
       "http://localhost/api/api-id?populate=deep&locale=de";
+    const expectedStagingRequestUrl =
+      "http://localhost/api/api-id?populate=deep&locale=sg";
+    const axiosGetSpy = jest.spyOn(mockedAxios, "get");
 
     test("request url", async () => {
-      mockedAxios.get.mockResolvedValue(defaultResponseData);
-      const axiosGetSpy = jest.spyOn(mockedAxios, "get");
+      mockedAxios.get
+        .mockResolvedValue(defaultResponseData)
+        .mockResolvedValueOnce(emptyResponseData);
       await getStrapiEntryFromApi(defaultOptions);
-      expect(axiosGetSpy).toHaveBeenCalledWith(
+      expect(axiosGetSpy).toHaveBeenNthCalledWith(
+        1,
+        expectedStagingRequestUrl,
+        expect.anything(),
+      );
+      expect(axiosGetSpy).toHaveBeenNthCalledWith(
+        2,
         expectedRequestUrl,
         expect.anything(),
       );
     });
 
-    describe("with optional slug given", () => {
-      test("request url", async () => {
-        mockedAxios.get.mockResolvedValue(defaultResponseData);
-        const axiosGetSpy = jest.spyOn(mockedAxios, "get");
-        await getStrapiEntryFromApi({ ...defaultOptions, slug: "foobar" });
-        expect(axiosGetSpy).toHaveBeenCalledWith(
-          `${expectedRequestUrl}&filters[slug][$eq]=foobar`,
-          expect.anything(),
-        );
-      });
+    test("request url with optional slug given", async () => {
+      mockedAxios.get
+        .mockResolvedValue(defaultResponseData)
+        .mockResolvedValueOnce(emptyResponseData);
+      await getStrapiEntryFromApi({ ...defaultOptions, slug: "foobar" });
+      expect(axiosGetSpy).toHaveBeenNthCalledWith(
+        1,
+        `${expectedStagingRequestUrl}&filters[slug][$eq]=foobar`,
+        expect.anything(),
+      );
+      expect(axiosGetSpy).toHaveBeenNthCalledWith(
+        2,
+        `${expectedRequestUrl}&filters[slug][$eq]=foobar`,
+        expect.anything(),
+      );
     });
 
-    describe("with api returning array", () => {
-      test("response handling", async () => {
-        mockedAxios.get.mockResolvedValue({ data: { data: [data] } });
-        expect(await getStrapiEntryFromApi(defaultOptions)).toEqual(data);
-      });
+    test("response handling with api returning array", async () => {
+      mockedAxios.get.mockResolvedValue({ data: { data: [data] } });
+      expect(await getStrapiEntryFromApi(defaultOptions)).toEqual(data);
     });
   });
 });
