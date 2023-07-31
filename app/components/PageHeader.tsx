cea795423800d6d2bdffbbba31dbf80dee694ab2
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-07-31 17:54:59 +0200

    A11y(Header): More descriptive Root link

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -1,16 +1,18 @@
+// TODO: Move to CMS
 const headerProps = {
   headerTitle: "Justiz-Services",
-  headerURL: "/",
+  headerLinkLabel: "Justiz Service - Zurück zur Startseite",
 };
 
 export default function Header() {
-  const { headerTitle, headerURL } = headerProps;
+  const { headerTitle, headerLinkLabel } = headerProps;
 
   return (
     <header className="py-20 px-16">
       <a
-        href={headerURL}
+        href="/"
         className="ds-label-01-bold no-underline hover:underline mr-8 text-black focus:outline active:underline active:decoration-4"
+        aria-label={headerLinkLabel}
       >
         {headerTitle}
       </a>
