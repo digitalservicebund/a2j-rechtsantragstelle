d68f31ef9f3e11ef99d0d35479c5f084b32577b8
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-09-04 17:07:40 +0200

    fix: check against empty or undefined E2E_BASE_URL
    Co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,8 +40,9 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.E2E_BASE_URL ?? "http://127.0.0.1:3000",
-
+    baseURL: ["", undefined].includes(process.env.E2E_BASE_URL)
+      ? "http://127.0.0.1:3000"
+      : process.env.E2E_BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
