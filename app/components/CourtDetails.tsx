10b041b2878d2ecd3a2a64f871ef2482c80dc4d5
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-09-05 11:14:45 +0200

    fix: address element should include the court name

--- app/components/CourtDetails.tsx
+++ app/components/CourtDetails.tsx
@@ -25,42 +25,40 @@ const CourtDetails = ({
   phoneLabel,
 }: CourtDetailsProps) => {
   return (
-    <article className="ds-stack-16">
+    <address className="not-italic ds-stack-16">
       <h2 className="ds-heading-03-reg">{name}</h2>
-      <address className="not-italic ds-stack-16">
+      <div>
+        <h3 className="ds-label-02-bold">{addressLabel}</h3>
+        <p className="ds-label-01-reg">
+          {street}
+          <br />
+          {city}
+        </p>
+      </div>
+      {website && (
         <div>
-          <h3 className="ds-label-02-bold">{addressLabel}</h3>
-          <p className="ds-label-01-reg">
-            {street}
-            <br />
-            {city}
+          <h3 className="sr-only">{websiteLabel}</h3>
+          <p>
+            <a
+              href={normalizeURL(website)}
+              rel="noopener"
+              target="_blank"
+              className="text-link"
+            >
+              {websiteLabel} {name}
+            </a>
           </p>
         </div>
-        {website && (
-          <div>
-            <h3 className="sr-only">{websiteLabel}</h3>
-            <p>
-              <a
-                href={normalizeURL(website)}
-                rel="noopener"
-                target="_blank"
-                className="text-link"
-              >
-                {websiteLabel} {name}
-              </a>
-            </p>
-          </div>
-        )}
-        {phone && (
-          <div>
-            <h3 className="ds-label-02-bold">{phoneLabel}</h3>
-            <a className="text-link" href={`tel:${phone}`}>
-              {phone}
-            </a>
-          </div>
-        )}
-      </address>
-    </article>
+      )}
+      {phone && (
+        <div>
+          <h3 className="ds-label-02-bold">{phoneLabel}</h3>
+          <a className="text-link" href={`tel:${phone}`}>
+            {phone}
+          </a>
+        </div>
+      )}
+    </address>
   );
 };
 
