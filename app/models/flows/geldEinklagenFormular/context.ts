7f451aa1063e23d20214810c256270fe6a4cc369
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-10-12 12:23:28 +0200

    feat(ZOV): add persoenliche Daten steps

--- app/models/flows/geldEinklagenFormular/context.ts
+++ app/models/flows/geldEinklagenFormular/context.ts
@@ -1,6 +1,19 @@
 import { z } from "zod";
+import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
+import { postcodeSchema } from "~/services/validation/plz";
 
-export const context = {} as const;
+export const context = {
+  titel: z.enum(["Keine Auswahl", "Dr."]),
+  nachname: z.string().min(1),
+  vorname: z.string().min(1),
+  geburtsdatum: z.string().min(1),
+  strasse: z.string().min(1),
+  plz: postcodeSchema,
+  ort: z.string().min(1),
+  telefonnummer: z.string().min(1),
+  gesetzlicheVertretung: YesNoAnswer,
+  bevollmaechtigtePerson: z.enum(["lawyer", "yes", "no"]),
+} as const;
 
 const contextObject = z.object(context).partial();
 export type GeldEinklagenFormularContext = z.infer<typeof contextObject>;
