db8c82793f94c379244db5c3f528da35f9bd94fa
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-10-16 15:50:48 +0200

    fix: Run prettier

--- app/models/flows/geldEinklagenFormular/guards.ts
+++ app/models/flows/geldEinklagenFormular/guards.ts
@@ -1,14 +1,14 @@
-import {GeldEinklagenFormularContext} from "~/models/flows/geldEinklagenFormular/context";
+import { GeldEinklagenFormularContext } from "~/models/flows/geldEinklagenFormular/context";
 
 const gegenseiteTypPrivatperson = (context: GeldEinklagenFormularContext) => {
-  return context.gegenseite?.typ === "privatperson"
-}
+  return context.gegenseite?.typ === "privatperson";
+};
 
 const gegenseiteTypUnternehmen = (context: GeldEinklagenFormularContext) => {
-  return context.gegenseite?.typ === "unternehmen"
-}
+  return context.gegenseite?.typ === "unternehmen";
+};
 
 export const guards = {
   gegenseiteTypPrivatperson,
-  gegenseiteTypUnternehmen
+  gegenseiteTypUnternehmen,
 };
