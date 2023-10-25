1fa4d36e9a450d87a1c95852abedd33a5f626e8a
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-10-25 13:56:28 +0200

    feat(fluggastrechte): Add first vorabcheck condition

--- app/models/flows/fluggastrechte/guards.ts
+++ app/models/flows/fluggastrechte/guards.ts
@@ -14,9 +14,20 @@ function yesNoGuards<Field extends keyof FluggastrechtVorabcheckContext>(
   };
 }
 
+const isPartnerAirport = (context: FluggastrechtVorabcheckContext) => {
+  const partnerAirports = ["BRE", "BER", "DUS", "FRA", "HAM", "MUC", "STR"];
+  return (
+    partnerAirports.includes(context.startAirport ?? "") ||
+    partnerAirports.includes(context.endAirport ?? "")
+  );
+};
+
 export const guards = {
   bereichVerspaetet: (context: FluggastrechtVorabcheckContext) =>
     context.bereich === "verspaetet",
+  isPartnerAirport,
+  fluggesellschaftFilled: (context: FluggastrechtVorabcheckContext) =>
+    Boolean(context.startAirport && context.endAirport),
   ...yesNoGuards("verspaetung"),
   ...yesNoGuards("checkin"),
   ...yesNoGuards("gruende"),
