e12f1736845d59a3dad86274beb27071c2536fe4
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-11-21 16:25:34 +0100

    refactor: move to enum states

--- app/components/FlowNavigation.tsx
+++ app/components/FlowNavigation.tsx
@@ -2,73 +2,74 @@ import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import RadioButtonCheckedOutlinedIcon from "@mui/icons-material/RadioButtonCheckedOutlined";
 
+enum NavState {
+  DoneDisabled,
+  Done,
+  Current,
+  Open,
+  OpenDisabled,
+}
+
 const navigationLinks = [
   {
     destination: ".",
     labelText: "Grundvoraussetzungen",
-    isDisabled: true,
-    isDone: true,
-    isCurrent: false,
+    state: NavState.DoneDisabled,
   },
   {
     destination: ".",
     labelText: "Das Rechtsproblem",
-    isDisabled: false,
-    isDone: true,
-    isCurrent: false,
+    state: NavState.Done,
   },
   {
     destination: ".",
     labelText: "Finanzielle Angaben",
-    isDisabled: false,
-    isDone: false,
-    isCurrent: true,
+    state: NavState.Current,
   },
   {
     destination: ".",
     labelText: "Persönliche Daten",
-    isDisabled: false,
-    isDone: false,
-    isCurrent: false,
+    state: NavState.Open,
   },
   {
     destination: ".",
     labelText: "Abgabe",
-    isDisabled: true,
-    isDone: false,
-    isCurrent: false,
+    state: NavState.OpenDisabled,
   },
 ];
 
 export default function FlowNavigation() {
   return (
     <ul>
-      {navigationLinks.map(
-        ({ destination, labelText, isDisabled, isCurrent, isDone }) => (
-          <li
-            key={destination}
-            className="p-16 list-none border-b-2 border-white"
-            style={{ width: "256px" }}
+      {navigationLinks.map(({ destination, labelText, state }) => (
+        <li
+          key={destination}
+          className="p-16 list-none border-b-2 border-white"
+          style={{ width: "256px" }}
+        >
+          <a
+            href={destination}
+            className={`flex gap-x-16 items-center ${
+              state === NavState.Current
+                ? "ds-label-02-bold"
+                : "ds-label-02-reg"
+            } ${
+              [NavState.DoneDisabled, NavState.OpenDisabled].includes(state)
+                ? "text-gray-600"
+                : ""
+            }`}
           >
-            <a
-              href={destination}
-              className={`flex gap-x-16 items-center
-              ${isCurrent ? "ds-label-02-bold" : "ds-label-02-reg"} ${
-                isDisabled ? "text-gray-600" : ""
-              } `}
-            >
-              {isDone ? (
-                <CheckCircleOutlineIcon />
-              ) : isCurrent ? (
-                <RadioButtonCheckedOutlinedIcon />
-              ) : (
-                <CircleOutlinedIcon />
-              )}
-              {labelText}
-            </a>
-          </li>
-        ),
-      )}
+            {state === NavState.Current ? (
+              <RadioButtonCheckedOutlinedIcon />
+            ) : [NavState.DoneDisabled, NavState.Done].includes(state) ? (
+              <CheckCircleOutlineIcon />
+            ) : (
+              <CircleOutlinedIcon />
+            )}
+            {labelText}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
