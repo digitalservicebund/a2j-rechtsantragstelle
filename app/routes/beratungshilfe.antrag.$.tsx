9899a59e0c0009bf5b67df65352b04d978d35211
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-11-21 12:41:00 +0100

    fix(beratungshilfeAntrag): replace step component
    Co-Authored-By: Pram Gurusinga <9930966+pgurusinga@users.noreply.github.com>

--- app/routes/beratungshilfe.antrag.$.tsx
+++ app/routes/beratungshilfe.antrag.$.tsx
@@ -1,7 +1,7 @@
 import { type LoaderFunctionArgs } from "@remix-run/node";
 import { loader as stepLoader } from "~/routes/shared/step";
 import { throw404OnProduction } from "~/services/errorPages/throw404";
-export { action, StepWithProgressBar as default } from "~/routes/shared/step";
+export { action, StepWithPreHeading as default } from "~/routes/shared/step";
 
 export function loader(args: LoaderFunctionArgs) {
   throw404OnProduction();
