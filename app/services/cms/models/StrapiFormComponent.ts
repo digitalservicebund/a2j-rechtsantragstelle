12a597ce55461b871bf048703b6893668988b6fc
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-11-22 16:47:20 +0100

    feat(date input): Add to expected components for forms

--- app/services/cms/models/StrapiFormComponent.ts
+++ app/services/cms/models/StrapiFormComponent.ts
@@ -5,9 +5,11 @@ import { StrapiDropdownSchema } from "./StrapiDropdown";
 import { StrapiTextareaSchema } from "./StrapiTextarea";
 import { StrapiCheckboxSchema } from "./StrapiCheckbox";
 import { StrapiTileGroupSchema } from "./StrapiTileGroup";
+import { StrapiDateInputSchema } from "~/services/cms/models/StrapiDateInput";
 
 export const StrapiFormComponentSchema = z.discriminatedUnion("__component", [
   StrapiInputSchema.required({ __component: true }),
+  StrapiDateInputSchema.required({ __component: true }),
   StrapiTextareaSchema.required({ __component: true }),
   StrapiSelectSchema.required({ __component: true }),
   StrapiDropdownSchema.required({ __component: true }),
