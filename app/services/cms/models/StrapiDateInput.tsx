2cb7de9baf2dce5d878a1a35715cdf1d9a85a943
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-11-24 06:04:35 +0100

    fix(date input): Do not override default placeholder

--- app/services/cms/models/StrapiDateInput.tsx
+++ app/services/cms/models/StrapiDateInput.tsx
@@ -1,33 +1,37 @@
 import { z } from "zod";
-import { StrapiErrorCategorySchema } from "./StrapiErrorCategory";
-import { HasOptionalStrapiIdSchema, HasStrapiIdSchema } from "./HasStrapiId";
-import { InputPropsSchema } from "~/components/inputs/Input";
-import { omitNull } from "~/util/omitNull";
-import Checkbox from "~/components/inputs/Checkbox";
-import { StrapiCheckboxSchema } from "~/services/cms/models/StrapiCheckbox";
+import { HasOptionalStrapiIdSchema } from "./HasStrapiId";
 import DateInput from "~/components/inputs/DateInput";
 import {
   flattenStrapiErrors,
   StrapiErrorRelationSchema,
 } from "~/services/cms/flattenStrapiErrors";
+import type { InputProps } from "~/components/inputs/Input";
 
 export const StrapiDateInputSchema = z
   .object({
     __component: z.literal("form-elements.date-input").optional(),
     name: z.string(),
     label: z.string().nullable(),
-    placeholder: z.string().nullable(),
+    placeholder: z.string().nullable().optional(),
     errors: StrapiErrorRelationSchema,
   })
   .merge(HasOptionalStrapiIdSchema);
 
 export type StrapiDateInput = z.infer<typeof StrapiDateInputSchema>;
 
-export const renderDateInputFromStrapi = (strapiDateInput: StrapiDateInput) => (
-  <DateInput
-    name={strapiDateInput.name}
-    label={strapiDateInput.label ?? undefined}
-    placeholder={strapiDateInput.placeholder ?? undefined}
-    errorMessages={flattenStrapiErrors(strapiDateInput.errors)}
-  />
-);
+export const renderDateInputFromStrapi = (strapiDateInput: StrapiDateInput) => {
+  const props: InputProps = {
+    name: strapiDateInput.name,
+    errorMessages: flattenStrapiErrors(strapiDateInput.errors),
+  };
+
+  return (
+    <DateInput
+      {...props}
+      {...(strapiDateInput.label && { label: strapiDateInput.label })}
+      {...(strapiDateInput.placeholder && {
+        placeholder: strapiDateInput.placeholder,
+      })}
+    />
+  );
+};
