a75b6ce6a4609dfd46b1dcfc09b0326f171e21b2
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-11-27 18:18:00 +0100

    test(unit): navItemsFromFlowSpecifics
    Co-Authored-By: Pram Gurusinga <9930966+pgurusinga@users.noreply.github.com>
    Co-Authored-By: SannyNguyenHung <91742583+SannyNguyenHung@users.noreply.github.com>

--- tests/unit/services/flowNavigation.test.ts
+++ tests/unit/services/flowNavigation.test.ts
@@ -1,4 +1,10 @@
-import { navState, NavState } from "~/services/flowNavigation";
+import {
+  navItemsFromFlowSpecifics,
+  navState,
+  NavState,
+} from "~/services/flowNavigation";
+import { buildFlowController } from "~/services/flow/buildFlowController";
+import _ from "lodash";
 
 describe("flowNavigation", () => {
   describe("navState", () => {
@@ -19,6 +25,84 @@ describe("flowNavigation", () => {
 
     test.each(validStates)("navState(%o) => %i", (testValues, expected) => {
       expect(navState({ ...defaultValues, ...testValues })).toBe(expected);
+    });
+  });
+
+  describe("flow to NavState", () => {
+    const flow = {
+      id: "/test/flow/",
+      initial: "step1",
+      predictableActionArguments: true,
+      states: {
+        step1: {
+          initial: "step1a",
+          states: { step1a: {} },
+        },
+        step2: {},
+      },
+    };
+
+    describe("checks open state", () => {
+      const flowController = buildFlowController({ flow });
+      expect(
+        navItemsFromFlowSpecifics("step1.step1a", flowController)[0].state,
+      ).toBe(NavState.Current);
+
+      expect(navItemsFromFlowSpecifics("step2", flowController)[0].state).toBe(
+        NavState.Open,
+      );
+    });
+
+    describe("checks done state", () => {
+      const flowController = buildFlowController({
+        flow: _.merge(_.cloneDeep(flow), {
+          states: { step1: { meta: { done: () => true } } },
+        }),
+      });
+
+      expect(
+        navItemsFromFlowSpecifics("step1.step1a", flowController)[0].state,
+      ).toBe(NavState.Current);
+
+      expect(navItemsFromFlowSpecifics("step2", flowController)[0].state).toBe(
+        NavState.Done,
+      );
+    });
+
+    describe("works with isUneditable", () => {
+      const flowController = buildFlowController({
+        flow: _.merge(_.cloneDeep(flow), {
+          states: { step1: { meta: { isUneditable: true, done: () => true } } },
+        }),
+      });
+
+      expect(
+        navItemsFromFlowSpecifics("step1.step1a", flowController)[0].state,
+      ).toBe(NavState.Current);
+
+      expect(navItemsFromFlowSpecifics("step2", flowController)[0].state).toBe(
+        NavState.DoneDisabled,
+      );
+    });
+
+    describe("checks open state", () => {
+      const flowController = buildFlowController({
+        flow: _.merge(_.cloneDeep(flow), {
+          states: {
+            step2: {
+              initial: "step2a",
+              states: { step2a: {} },
+            },
+          },
+        }),
+      });
+
+      expect(
+        navItemsFromFlowSpecifics("step1.step1a", flowController)[0].state,
+      ).toBe(NavState.Current);
+      expect(
+        navItemsFromFlowSpecifics("step1.step1a", flowController)[1].state,
+      ).toBe(NavState.OpenDisabled);
     });
   });
 });
