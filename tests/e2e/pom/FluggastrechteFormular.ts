cb01767b2902c3d7d4953ee99c81c19928bd85d4
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2023-12-12 14:50:26 +0100

    feat(fluggastrechteFormular): Wrap first steps in intro section

--- tests/e2e/pom/FluggastrechteFormular.ts
+++ tests/e2e/pom/FluggastrechteFormular.ts
@@ -2,5 +2,5 @@ import { Formular } from "tests/e2e/pom/Formular";
 
 export class FluggastrechteFormular extends Formular {
   readonly url = "/fluggastrechte/formular";
-  readonly initialStep = "start";
+  readonly initialStep = "intro/start";
 }
