b5bc4ec9cb85b37b5a6e39f25898f2f4c3ba4ad2
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2023-12-13 09:35:43 +0100

    feat: add non numeric list

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -17,19 +17,14 @@ const List = ({ identifier, items, heading, isNumeric }: ListProps) => {
       {heading && <Heading {...heading} />}
       <ol className="list-none ds-stack-32 ps-0" key={identifier}>
         {items.map((item, index) => (
-          <li
-            key={item.identifier}
-            className={`pt-6 first:pt-0 scroll-my-40 ${
-              !isNumeric
-                ? "border-0 border-solid border-0 border-t-2 border-gray-400 first:border-none max-w-none max-[499px]:flex-col"
-                : ""
-            }`}
-          >
-            <div className="flex flex-row gap-32 items-center">
-              {isNumeric && (
-                <div className="w-[40px] h-[40px] pt-[6px] text-center border border-solid border-gray-400 rounded-full">
+          <li key={item.identifier} className="pt-6 first:pt-0 scroll-my-40">
+            <div className="flex flex-row gap-16">
+              {isNumeric ? (
+                <div className="min-w-[40px] w-[44px] h-[40px] pt-[4px] text-center border-2 border-solid border-gray-400 rounded-full">
                   {index + 1}
                 </div>
+              ) : (
+                <div className="w-[16px] min-h-[1px] border border-solid border-black mt-[17px] mr-[5px] ml-[15px]" />
               )}
               <ListItem {...item} key={item.headline?.text ?? item.content} />
             </div>
