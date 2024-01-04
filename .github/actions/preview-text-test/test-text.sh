d3fcf7dbb8b9e8604622fb7f544c3c211978d374
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-01-04 14:19:50 +0100

    chore(preview): fix variable name

--- .github/actions/preview-text-test/test-text.sh
+++ .github/actions/preview-text-test/test-text.sh
@@ -2,10 +2,10 @@
 PREVIEW_URL="https://a2j-preview.dev.ds4g.net"
 TEXT="Justiz-Services"
 
-if curl -s $PRODUCTION_URL | grep -o "$TEXT"; then
+if curl -s $PREVIEW_URL | grep -o "$TEXT"; then
   echo "Text found"
   exit 0
 else
-  echo "Text '$TEXT' not found on '$PRODUCTION_URL'"
+  echo "Text '$TEXT' not found on '$PREVIEW_URL'"
   exit 1
 fi
