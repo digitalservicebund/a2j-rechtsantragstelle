b487314d23011241892b5a6aae0ecd0cb8171a75
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-01-19 18:49:06 +0100

    feat(dateInput): Dont shift on delete, disable helperText

--- app/components/inputs/DateInput.tsx
+++ app/components/inputs/DateInput.tsx
@@ -5,11 +5,11 @@ const DateInput = (props: InputProps) => {
   // TODO: Move helperText into CMS common-space.
   return (
     <MaskedInput
-      mask={"00.00.0000"}
+      mask={"0`0`.0`0`.0`0`0`0"}
       type="number"
       placeholder="TT.MM.JJJJ"
       width="10"
-      helperText="Bitte im Format TT.MM.JJJJ angeben"
+      // helperText="Bitte im Format TT.MM.JJJJ angeben"
       {...props}
     />
   );
