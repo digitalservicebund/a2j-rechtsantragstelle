82101d3248ccbd2bd5224f3ee7bbf64bf504a168
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-01-22 11:01:28 +0100

    chore(MaskedInput): remove helperText

--- app/components/inputs/DateInput.tsx
+++ app/components/inputs/DateInput.tsx
@@ -2,14 +2,12 @@ import { type InputProps } from "./Input";
 import MaskedInput from "./MaskedInput";
 
 const DateInput = (props: InputProps) => {
-  // TODO: Move helperText into CMS common-space.
   return (
     <MaskedInput
       mask={"0`0`.0`0`.0`0`0`0"}
       type="number"
       placeholder="TT.MM.JJJJ"
       width="10"
-      // helperText="Bitte im Format TT.MM.JJJJ angeben"
       {...props}
     />
   );
--- app/components/inputs/TimeInput.tsx
+++ app/components/inputs/TimeInput.tsx
@@ -2,16 +2,7 @@ import { type InputProps } from "./Input";
 import MaskedInput from "./MaskedInput";
 
 const TimeInput = (props: InputProps) => {
-  // TODO: Move helperText into CMS common-space.
-  return (
-    <MaskedInput
-      mask={"00:00"}
-      type="number"
-      width="10"
-      helperText="Bitte im Format HH:MM angeben"
-      {...props}
-    />
-  );
+  return <MaskedInput mask={"`0`0:`0`0"} type="number" width="10" {...props} />;
 };
 
 export default TimeInput;
