1bbcfd84d9e1f3ee800735556780318577cdaaa0
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-01-29 10:29:22 +0100

    chore: update jest config to ESM

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
-module.exports = {
+const config = {
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "<rootDir>/app"],
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/app/$1",
     "tests/(.*)": "<rootDir>/tests/$1",
-    "^.+\\.css$": "<rootDir>/tests/cssStub.cjs",
+    "^.+\\.css$": "<rootDir>/tests/cssStub.js",
   },
   preset: "ts-jest",
   setupFiles: ["<rootDir>/tests/setup.tests.ts"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setupAfterEnv.tests.cjs"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setupAfterEnv.tests.js"],
   resolver: "<rootDir>/tests/resolver.cjs",
 };
+
+export default config;
---
+++ tests/cssStub.js
@@ -0,0 +1,1 @@
+// Mock for .css files. See https://jestjs.io/docs/webpack#handling-static-assets
