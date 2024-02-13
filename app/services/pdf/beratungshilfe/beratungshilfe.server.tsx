0033989deabb60ae2f0785177bb30ead03ef4ab6
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2024-02-13 08:02:36 +0100

    feat: add financial F section to pdf export

--- app/services/pdf/beratungshilfe/beratungshilfe.server.tsx
+++ app/services/pdf/beratungshilfe/beratungshilfe.server.tsx
@@ -11,12 +11,14 @@ import { type PDFForm, PDFDocument, PDFTextField, PDFCheckBox } from "pdf-lib";
 import { normalizePropertyName } from "../pdf.server";
 import FormAttachment from "~/components/FormAttachment";
 
-import _ from "lodash";
 import { CheckboxValue } from "~/components/inputs/Checkbox";
 import type { BeratungshilfeAntragContext } from "~/models/flows/beratungshilfeFormular";
-import invariant from "tiny-invariant";
 
-const isANewAttachmentPageNeeded = (context: BeratungshilfeAntragContext) => {
+const newPageHint = "Bitte im Anhang prüfen";
+
+const isANewAttachmentPageNeeded = (
+  context: BeratungshilfeAntragContext,
+): Attachment => {
   const descriptions = [];
 
   if (context.bereich) {
@@ -67,6 +69,11 @@ const isANewAttachmentPageNeeded = (context: BeratungshilfeAntragContext) => {
       descriptions.map((x) => x.title + x.text).join(" ").length > 255,
     descriptions,
   };
+};
+
+type Attachment = {
+  shouldCreateNewPage: boolean;
+  descriptions: { title: string; text: string }[];
 };
 
 function getSelectedOptions(
@@ -169,13 +176,10 @@ export async function getBeratungshilfePdfFromContext(
     staatlicheLeistungMapping,
   );
 
-  fillFinancial(pdfFields, context);
-
   const attachment = isANewAttachmentPageNeeded(context);
-
   if (attachment.shouldCreateNewPage) {
     pdfFields.ichbeantrageBeratungshilfeinfolgenderAngelegenheitbitteSachverhaltkurzerlaeutern.value =
-      "Bitte im Anhang prüfen";
+      newPageHint;
   } else {
     pdfFields.ichbeantrageBeratungshilfeinfolgenderAngelegenheitbitteSachverhaltkurzerlaeutern.value =
       attachment.descriptions.map((x) => `${x.title} ${x.text} `).join("\n");
@@ -203,8 +207,10 @@ export async function getBeratungshilfePdfFromContext(
       title: "Beruf / Erwerbstätigkeit:",
       text: getOccupationDetails(context, false),
     });
-    pdfFields.berufErwerbstaetigkeit.value = "Bitte im Anhang prüfen";
+    pdfFields.berufErwerbstaetigkeit.value = newPageHint;
   }
+
+  fillFinancial(pdfFields, context, attachment);
 
   fillPartner(context, pdfFields);
 
@@ -298,9 +304,67 @@ function fillPersonalData(
 function fillFinancial(
   pdfFields: BeratungshilfePDF,
   context: BeratungshilfeAntragContext,
+  attachment: Attachment,
 ) {
-  fillFinancialBankkonto(pdfFields, context);
+  fillFinancialBankkonto(pdfFields, context, attachment);
+  fillFinancialGrundeigentum(pdfFields, context, attachment);
+  fillFinancialKraftfahrzeug(pdfFields, context, attachment);
+  fillFinancialWertsachen(pdfFields, context, attachment);
+}
 
+const eigentuemerMapping = {
+  myself: "Ich alleine",
+  partner: "Ehe-Partner:in",
+  myselfAndPartner: "Mein:e Ehe-Partner:in und ich gemeinsam",
+  myselfAndSomeoneElse: "Ich gemeinsam mit jemand anderem",
+};
+
+function fillFinancialBankkonto(
+  pdfFields: BeratungshilfePDF,
+  context: BeratungshilfeAntragContext,
+  attachment: Attachment,
+) {
+  const hasBankkonten = context.bankkonten
+    ? context.bankkonten?.length > 0
+    : false;
+  pdfFields.f1Konten1.value = !hasBankkonten;
+  pdfFields.f1Konten2.value = hasBankkonten;
+
+  if (context.bankkonten && context.bankkonten.length > 0) {
+    if (context.bankkonten.length == 1) {
+      const bankkonto = context.bankkonten.pop();
+
+      pdfFields.f1InhaberA.value = bankkonto?.kontoEigentuemer == "myself";
+      pdfFields.f2InhaberB.value = bankkonto?.kontoEigentuemer == "partner";
+      pdfFields.f2InhaberC.value =
+        bankkonto?.kontoEigentuemer == "myselfAndPartner";
+
+      const bezeichnung = getBankkontoBezeichnung(bankkonto);
+
+      pdfFields.f3Bank1.value = bezeichnung.join(", ");
+      pdfFields.f4Kontostand.value = `${bankkonto?.kontostand} €`;
+    } else {
+      pdfFields.f3Bank1.value = newPageHint;
+      attachment.shouldCreateNewPage = true;
+      const bezeichnung: string[] = [];
+
+      context.bankkonten.forEach((bankkonto) => {
+        bezeichnung.push(getBankkontoBezeichnung(bankkonto, true).join("\n"));
+      });
+
+      attachment.descriptions.unshift({
+        title: "Bankkonten",
+        text: bezeichnung.join("\n\n"),
+      });
+    }
+  }
+}
+
+function fillFinancialGrundeigentum(
+  pdfFields: BeratungshilfePDF,
+  context: BeratungshilfeAntragContext,
+  attachment: Attachment,
+) {
   const hasGrundeigentum = context.grundeigentum
     ? context.grundeigentum?.length > 0
     : false;
@@ -313,77 +377,462 @@ function fillFinancial(
   pdfFields.f5Grundeigentum2.value =
     hasGrundeigentum || hasGrundeigentumBewohnt;
 
+  if (
+    context.grundeigentum &&
+    context.grundeigentum?.length == 1 &&
+    context.grundeigentumBewohnt?.length == 0
+  ) {
+    const grundeigentumBewohnt = context.grundeigentumBewohnt.pop();
+
+    pdfFields.f1InhaberA.value = grundeigentumBewohnt?.eigentuemer == "myself";
+    pdfFields.f2InhaberB.value = grundeigentumBewohnt?.eigentuemer == "partner";
+    pdfFields.f2InhaberC.value =
+      grundeigentumBewohnt?.eigentuemer == "myselfAndPartner";
+
+    const bezeichnung =
+      getGrundeigentumBewohntBezeichnung(grundeigentumBewohnt);
+
+    pdfFields.f7Nutzungsart.value = bezeichnung.join(", ");
+  } else if (
+    (context.grundeigentumBewohnt?.length ?? 0) > 0 ||
+    (context.grundeigentum?.length ?? 0) > 0
+  ) {
+    const bezeichnung: string[] = [];
+    console.log(
+      context.grundeigentumBewohnt !== undefined ||
+        context.grundeigentum !== undefined,
+    );
+
+    context.grundeigentum?.forEach((grundeigentum) => {
+      bezeichnung.push(
+        getGrundeigentumBezeichnung(grundeigentum, true).join("\n"),
+      );
+    });
+
+    context.grundeigentumBewohnt?.forEach((grundeigentum) => {
+      bezeichnung.push(
+        getGrundeigentumBewohntBezeichnung(grundeigentum, true).join("\n"),
+      );
+    });
+
+    pdfFields.f7Nutzungsart.value = newPageHint;
+    attachment.shouldCreateNewPage = true;
+    attachment.descriptions.unshift({
+      title: "Grundeigentum",
+      text: bezeichnung.join("\n\n"),
+    });
+  }
+}
+
+function fillFinancialKraftfahrzeug(
+  pdfFields: BeratungshilfePDF,
+  context: BeratungshilfeAntragContext,
+  attachment: Attachment,
+) {
   const hasKraftfahrzeug = context.bankkonten
     ? context.bankkonten?.length > 0
     : false;
   pdfFields.f9Kraftfahrzeug1.value = !hasKraftfahrzeug;
   pdfFields.f9Kraftfahrzeuge2.value = hasKraftfahrzeug;
 
+  if (context.kraftfahrzeuge && context.kraftfahrzeuge.length > 0) {
+    if (context.kraftfahrzeuge.length == 1) {
+      const kraftfahrzeug = context.kraftfahrzeuge.pop();
+
+      pdfFields.f10KraftfahrzeugeA.value =
+        kraftfahrzeug?.eigentuemer == "myself";
+      pdfFields.f10KraftfahrzeugB.value =
+        kraftfahrzeug?.eigentuemer == "partner";
+      pdfFields.f10KraftfahrzeugC.value =
+        kraftfahrzeug?.eigentuemer == "myselfAndPartner";
+
+      pdfFields.f11Fahrzeugart.value =
+        getKraftfahrzeugShortBezeichnung(kraftfahrzeug);
+      pdfFields.f12Verkehrswert.value =
+        kraftfahrzeug?.verkaufswert ?? "Keine Angaben";
+    } else {
+      pdfFields.f11Fahrzeugart.value = newPageHint;
+      attachment.shouldCreateNewPage = true;
+      const bezeichnung: string[] = [];
+
+      context.kraftfahrzeuge.forEach((kraftfahrzeug) => {
+        bezeichnung.push(
+          getKraftfahrzeugBezeichnung(kraftfahrzeug, true).join("\n"),
+        );
+      });
+
+      attachment.descriptions.unshift({
+        title: "Kraftfahrzeuge",
+        text: bezeichnung.join("\n\n"),
+      });
+    }
+  }
+}
+
+function fillFinancialWertsachen(
+  pdfFields: BeratungshilfePDF,
+  context: BeratungshilfeAntragContext,
+  attachment: Attachment,
+) {
   const hasWertsachen = context.wertsachen
     ? context.wertsachen?.length > 0
     : false;
   pdfFields.f13Vermoegenswerte1.value = !hasWertsachen;
-  pdfFields.f13Vermoegenswerte1.value = hasWertsachen;
+  pdfFields.f13Vermoegenswerte2.value = hasWertsachen;
+
+  if (context.wertsachen) {
+    if (context.wertsachen?.length == 1) {
+      const wertsache = context.wertsachen.pop();
+      pdfFields.f14InhaberA.value = wertsache?.eigentuemer == "myself";
+      pdfFields.f14InhaberB.value = wertsache?.eigentuemer == "partner";
+      pdfFields.f14VermoegenswerteC.value =
+        wertsache?.eigentuemer == "myselfAndPartner";
+      pdfFields.f15Bezeichnung.value =
+        getWertsachenBezeichnung(wertsache).join(", ");
+      pdfFields.f16RueckkaufswertoderVerkehrswertinEUR.value =
+        wertsache?.wert ?? "Keine Angaben";
+    } else {
+      pdfFields.f15Bezeichnung.value = newPageHint;
+      attachment.shouldCreateNewPage = true;
+
+      const bezeichnung: string[] = [];
+
+      context.wertsachen.forEach((wertsache) => {
+        bezeichnung.push(getWertsachenBezeichnung(wertsache, true).join("\n"));
+      });
+
+      attachment.descriptions.unshift({
+        title: "Wertsachen",
+        text: bezeichnung.join("\n"),
+      });
+    }
+  }
 }
-const eigentuemerMapping = {
-  myself: "Ich alleine",
-  partner: "Ehe-Partner:in",
-  myselfAndPartner: "Mein:e Ehe-Partner:in und ich gemeinsam",
-  myselfAndSomeoneElse: "Ich gemeinsam mit jemand anderem",
+
+const wertsachenMapping = {
+  cash: "Bargeld",
+  valuableItem: "Wertgegenstand",
+  digitalMoney: "Paypal- oder Kryprowährung",
+  securities: "Wertpapiere, Aktien, Fonds",
+  claim: "Forderung",
+  equalizationOfGains: "Anspruch auf Zugewinnausgleich",
+  other: "Sonstiges",
 };
 
-function fillFinancialBankkonto(
-  pdfFields: BeratungshilfePDF,
-  context: BeratungshilfeAntragContext,
+function getWertsachenBezeichnung(
+  wertsachen:
+    | {
+        art:
+          | "cash"
+          | "valuableItem"
+          | "digitalMoney"
+          | "securities"
+          | "claim"
+          | "equalizationOfGains"
+          | "other";
+        eigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+        wert: string;
+      }
+    | undefined,
+  hasMultipleWertsachen = false,
 ) {
-  const hasBankkonten = context.bankkonten
-    ? context.bankkonten?.length > 0
-    : false;
-  pdfFields.f1Konten1.value = !hasBankkonten;
-  pdfFields.f1Konten2.value = hasBankkonten;
+  const bezeichnung = [];
 
-  if (context.bankkonten && context.bankkonten.length == 1) {
-    const bankkonto = context.bankkonten.pop();
-
-    pdfFields.f1InhaberA.value = bankkonto?.kontoEigentuemer == "myself";
-    pdfFields.f2InhaberB.value = bankkonto?.kontoEigentuemer == "partner";
-    pdfFields.f2InhaberC.value =
-      bankkonto?.kontoEigentuemer == "myselfAndPartner";
-
-    const bezeichnung = [];
-    if (bankkonto?.bankName) {
-      bezeichnung.push(`Bank: ${bankkonto.bankName}`);
-    }
-
-    if (
-      bankkonto?.kontoEigentuemer &&
-      eigentuemerMapping[bankkonto?.kontoEigentuemer]
-    ) {
-      bezeichnung.push(
-        `Eigentümer:in: ${eigentuemerMapping[bankkonto?.kontoEigentuemer]}`,
-      );
-    }
-
-    pdfFields.f3Bank1.value = bezeichnung.join(", ");
+  if (wertsachen?.art && wertsachenMapping[wertsachen?.art]) {
+    bezeichnung.push(`${wertsachenMapping[wertsachen?.art]}`);
   }
+
+  if (wertsachen?.eigentuemer && eigentuemerMapping[wertsachen?.eigentuemer]) {
+    bezeichnung.push(
+      `Eigentümer:in: ${eigentuemerMapping[wertsachen?.eigentuemer]}`,
+    );
+  }
+
+  if (hasMultipleWertsachen && wertsachen?.wert) {
+    bezeichnung.push(`Verkehrswert: ${wertsachen?.wert} €`);
+  }
+
+  return bezeichnung;
+}
+
+function getKraftfahrzeugShortBezeichnung(
+  kraftfahrzeug:
+    | {
+        art: string;
+        marke: string;
+        eigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+        verkaufswert: string;
+        kilometerstand: string;
+        anschaffungsjahr: string;
+        baujahr: string;
+        bemerkung: string;
+        arbeitsweg: string;
+      }
+    | undefined,
+) {
+  const bezeichnung = [];
+
+  if (kraftfahrzeug?.art) {
+    bezeichnung.push(`${kraftfahrzeug?.art}`);
+  }
+
+  if (kraftfahrzeug?.marke) {
+    bezeichnung.push(`${kraftfahrzeug?.marke}`);
+  }
+
+  if (kraftfahrzeug?.baujahr) {
+    bezeichnung.push(`Baujahr: ${kraftfahrzeug?.baujahr}`);
+  }
+
+  if (kraftfahrzeug?.kilometerstand) {
+    bezeichnung.push(`km-Stand: ${kraftfahrzeug?.kilometerstand}`);
+  }
+
+  if (kraftfahrzeug?.arbeitsweg) {
+    bezeichnung.push(`Wird für den Arbeitsweg gebraucht`);
+  }
+
+  return bezeichnung.join(", ");
+}
+
+function getKraftfahrzeugBezeichnung(
+  kraftfahrzeug:
+    | {
+        art: string;
+        marke: string;
+        eigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+        verkaufswert: string;
+        kilometerstand: string;
+        anschaffungsjahr: string;
+        baujahr: string;
+        bemerkung: string;
+        arbeitsweg: string;
+      }
+    | undefined,
+  hasMultipleKraftfahrzeug = false,
+) {
+  const bezeichnung = [];
+
+  if (kraftfahrzeug?.arbeitsweg) {
+    bezeichnung.push(`Fahrzeug wird für den Arbeitsweg genutzt`);
+  }
+
+  if (
+    kraftfahrzeug?.eigentuemer &&
+    eigentuemerMapping[kraftfahrzeug?.eigentuemer]
+  ) {
+    bezeichnung.push(
+      `Eigentümer:in: ${eigentuemerMapping[kraftfahrzeug?.eigentuemer]}`,
+    );
+  }
+
+  if (kraftfahrzeug?.art) {
+    bezeichnung.push(`Art des Fahrzeugs:: ${kraftfahrzeug?.art}`);
+  }
+
+  if (kraftfahrzeug?.marke) {
+    bezeichnung.push(`Marke: ${kraftfahrzeug?.marke}`);
+  }
+
+  if (kraftfahrzeug?.anschaffungsjahr) {
+    bezeichnung.push(`Anschaffungsjahr: ${kraftfahrzeug?.anschaffungsjahr}`);
+  }
+
+  if (kraftfahrzeug?.baujahr) {
+    bezeichnung.push(`Baujahr: ${kraftfahrzeug?.baujahr} km`);
+  }
+
+  if (kraftfahrzeug?.kilometerstand) {
+    bezeichnung.push(
+      `Kilometerstand (ca.): ${kraftfahrzeug?.kilometerstand} km`,
+    );
+  }
+
+  if (hasMultipleKraftfahrzeug && kraftfahrzeug?.verkaufswert) {
+    bezeichnung.push(`Verkehrswert: ${kraftfahrzeug?.verkaufswert} €`);
+  }
+
+  return bezeichnung;
+}
+
+function getGrundeigentumBezeichnung(
+  grundeigentum:
+    | {
+        eigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+        art:
+          | "apartment"
+          | "houseForFamily"
+          | "houseWithMultipleApartments"
+          | "property"
+          | "hereditaryBuildingLaw";
+        verkaufswert: string;
+        flaeche: string;
+        strassehausnummer: string;
+        plz: string;
+        ort: string;
+        land: string;
+      }
+    | undefined,
+  hasMultipleGrundeigentum = false,
+) {
+  const bezeichnung = [];
+  const artMapping = {
+    apartment: "Wohnung",
+    houseForFamily: "Hauf für Familie",
+    houseWithMultipleApartments: "Haus mit mehreren Wohnungen",
+    property: "Grundstück",
+    hereditaryBuildingLaw: "Erbbaurecht",
+  };
+
+  bezeichnung.push(`Grundeigentum wird nicht vom Antragsteller bewohnt`);
+
+  if (grundeigentum?.art) {
+    bezeichnung.push(`Art des Eigentums: ${artMapping[grundeigentum.art]}`);
+  }
+
+  if (
+    grundeigentum?.eigentuemer &&
+    eigentuemerMapping[grundeigentum?.eigentuemer]
+  ) {
+    bezeichnung.push(
+      `Eigentümer:in: ${eigentuemerMapping[grundeigentum?.eigentuemer]}`,
+    );
+  }
+
+  if (grundeigentum?.flaeche) {
+    bezeichnung.push(`Fläche: ${grundeigentum?.flaeche} m²`);
+  }
+
+  if (hasMultipleGrundeigentum && grundeigentum?.verkaufswert) {
+    bezeichnung.push(`Verkehrswert: ${grundeigentum?.verkaufswert} €`);
+  }
+
+  return bezeichnung;
+}
+
+function getGrundeigentumBewohntBezeichnung(
+  grundeigentum:
+    | {
+        eigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+        art:
+          | "apartment"
+          | "houseForFamily"
+          | "houseWithMultipleApartments"
+          | "property"
+          | "hereditaryBuildingLaw";
+        verkaufswert: string;
+      }
+    | undefined,
+  hasMultipleGrundeigentum = false,
+) {
+  const bezeichnung = [];
+  const artMapping = {
+    apartment: "Wohnung",
+    houseForFamily: "Hauf für Familie",
+    houseWithMultipleApartments: "Haus mit mehreren Wohnungen",
+    property: "Grundstück",
+    hereditaryBuildingLaw: "Erbbaurecht",
+  };
+
+  bezeichnung.push(`Grundeigentum ist der Hauptwohnsitz des Antragstellers`);
+
+  if (grundeigentum?.art) {
+    bezeichnung.push(`Art des Eigentums: ${artMapping[grundeigentum.art]}`);
+  }
+
+  if (
+    grundeigentum?.eigentuemer &&
+    eigentuemerMapping[grundeigentum?.eigentuemer]
+  ) {
+    bezeichnung.push(
+      `Eigentümer:in: ${eigentuemerMapping[grundeigentum?.eigentuemer]}`,
+    );
+  }
+
+  if (hasMultipleGrundeigentum && grundeigentum?.verkaufswert) {
+    bezeichnung.push(`Verkehrswert: ${grundeigentum?.verkaufswert} €`);
+  }
+
+  return bezeichnung;
+}
+
+function getBankkontoBezeichnung(
+  bankkonto:
+    | {
+        bankName: string;
+        kontostand: string;
+        iban: string;
+        kontoEigentuemer:
+          | "myself"
+          | "partner"
+          | "myselfAndPartner"
+          | "myselfAndSomeoneElse";
+      }
+    | undefined,
+  hasMultipleBankkonto = false,
+) {
+  const bezeichnung = [];
+
+  if (bankkonto?.bankName) {
+    bezeichnung.push(`Bank: ${bankkonto.bankName}`);
+  }
+
+  if (
+    bankkonto?.kontoEigentuemer &&
+    eigentuemerMapping[bankkonto?.kontoEigentuemer]
+  ) {
+    bezeichnung.push(
+      `Eigentümer:in: ${eigentuemerMapping[bankkonto?.kontoEigentuemer]}`,
+    );
+  }
+
+  if (hasMultipleBankkonto) {
+    bezeichnung.push(
+      `Kontostand: ${bankkonto?.kontostand ? bankkonto?.kontostand + " €" : "Keine Angabe"}`,
+    );
+  }
+
+  return bezeichnung;
 }
 
 function fillBeratungsperson(
   pdfFields: BeratungshilfePDF,
   context: BeratungshilfeAntragContext,
 ) {
-  invariant(
-    context.anwaltName &&
-      context.anwaltStrasseUndHausnummer &&
-      context.anwaltPlz &&
-      context.anwaltOrt,
-    "Anwaltliche Vertretung not filled out",
-  );
-  pdfFields.beratungsperson.value = `${context.anwaltName}, ${context.anwaltStrasseUndHausnummer}, ${context.anwaltPlz} ${context.anwaltOrt}`;
+  const address = [
+    context.anwaltName ?? "",
+    context.anwaltStrasseUndHausnummer ?? "",
+    context.anwaltPlz ?? "",
+    context.anwaltOrt ?? "",
+  ]
+    .filter((entry) => entry)
+    .join(", ");
+
+  pdfFields.beratungsperson.value = address;
   pdfFields.datumBeratung.value = context.beratungStattgefundenDatum;
 }
 
-export async function getBeratungshilfeParameters() {
+async function getBeratungshilfeParameters() {
   const json: { [key: string]: StringField | BooleanField } = {};
   await PDFDocument.load(getBeratungshilfePdfBuffer()).then((pdfDoc) => {
     const form = pdfDoc.getForm();
@@ -444,7 +893,7 @@ async function handleOutOfLimitDescription(
   return pdfDoc.save();
 }
 
-export async function fillOutBeratungshilfe(
+async function fillOutBeratungshilfe(
   values: BeratungshilfePDF,
   descriptions: { title: string; text: string }[],
   shouldCreateNewPage: boolean,
