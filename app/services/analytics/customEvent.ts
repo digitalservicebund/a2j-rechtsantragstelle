44cdc2758b4442a67464f5fe64b863606992822c
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-02-22 12:47:08 +0100

    chore: rmv comments

--- app/services/analytics/customEvent.ts
+++ app/services/analytics/customEvent.ts
@@ -3,7 +3,6 @@ import { parse } from "cookie";
 import { getPosthogClient } from "./posthogClient.server";
 
 function idFromCookie(request: Request) {
-  // console.log("request", request)
   // Note: can't use cookie.parse(): https://github.com/remix-run/remix/discussions/5198
   // Returns ENVIRONMENT if posthog's distinct_id can't be extracted
   const { POSTHOG_API_KEY, ENVIRONMENT } = config();
