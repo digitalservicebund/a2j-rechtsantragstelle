419e72361220d17b29f137d1400eef7600fa973d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-02-28 17:28:07 +0100

    fix(test): use slash instead of forward slash

--- tests/unit/models/flows/fluggastrechteFormular/testcases.ts
+++ tests/unit/models/flows/fluggastrechteFormular/testcases.ts
@@ -7,16 +7,16 @@ import type { FluggastrechtContext } from "~/models/flows/fluggastrechteFormular
 import type { FlowStateMachine } from "~/services/flow/server/buildFlowController";
 
 const machine: FlowStateMachine = createMachine(
-  { ...fluggastrechtFlow, context: {} },
+  { ...fluggastrechtFlow.config, context: {} },
   { guards: fluggastrechteGuards },
 );
 
 const happyPathSteps = [
-  "intro.start",
-  "intro.dokumente",
-  "intro.daten-uebernahme",
-  "flugdaten.zwischenstopps",
-  "flugdaten.ankunft",
+  "intro/start",
+  "intro/dokumente",
+  "intro/daten-uebernahme",
+  "flugdaten/zwischenstopps",
+  "flugdaten/ankunft",
 ];
 
 const cases = [
@@ -24,35 +24,35 @@ const cases = [
   [
     { zwischenstopps: "no" },
     [
-      "flugdaten.zwischenstopps",
-      "flugdaten.flug-details-single-flugnummer",
-      "flugdaten.flug-details-single-abflug",
-      "flugdaten.flug-details-single-ankunft",
-      "flugdaten.ankunft",
-      "flugdaten.ankunftszeit",
+      "flugdaten/zwischenstopps",
+      "flugdaten/flug-details-single-flugnummer",
+      "flugdaten/flug-details-single-abflug",
+      "flugdaten/flug-details-single-ankunft",
+      "flugdaten/ankunft",
+      "flugdaten/ankunftszeit",
     ],
   ],
   [
     { zwischenstopps: "yes" },
     [
-      "flugdaten.zwischenstopps",
-      "flugdaten.zwischenstopps-eingabe",
-      "flugdaten.flug-details-zwischenstopp-flugnummer",
-      "flugdaten.flug-details-zwischenstopp-abflug",
-      "flugdaten.flug-details-zwischenstopp-ankunft",
-      "flugdaten.flug-details-zwischenstopp-2-flugnummer",
-      "flugdaten.flug-details-zwischenstopp-2-abflug",
-      "flugdaten.flug-details-zwischenstopp-2-ankunft",
-      "flugdaten.ankunft",
-      "flugdaten.ankunftszeit",
+      "flugdaten/zwischenstopps",
+      "flugdaten/zwischenstopps-eingabe",
+      "flugdaten/flug-details-zwischenstopp-flugnummer",
+      "flugdaten/flug-details-zwischenstopp-abflug",
+      "flugdaten/flug-details-zwischenstopp-ankunft",
+      "flugdaten/flug-details-zwischenstopp-2-flugnummer",
+      "flugdaten/flug-details-zwischenstopp-2-abflug",
+      "flugdaten/flug-details-zwischenstopp-2-ankunft",
+      "flugdaten/ankunft",
+      "flugdaten/ankunftszeit",
     ],
   ],
   [
     { ankunftWithSameFlight: "no" },
     [
-      "flugdaten.ankunft",
-      "flugdaten.ankunft-flugnummer",
-      "flugdaten.ankunftszeit",
+      "flugdaten/ankunft",
+      "flugdaten/ankunft-flugnummer",
+      "flugdaten/ankunftszeit",
     ],
   ],
 ] as const satisfies TestCases<FluggastrechtContext>;
