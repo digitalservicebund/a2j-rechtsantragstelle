956e10fab918a9736c3497d692f1aa0f67f4adf1
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2024-03-11 08:56:24 +0100

    chore: remove newline

--- tests/unit/components/storybook.test.ts
+++ tests/unit/components/storybook.test.ts
@@ -1,5 +1,4 @@
 import initStoryshots from "@storybook/addon-storyshots";
-
 initStoryshots({
   // excluded stories by story title (= kind):
   storyKindRegex: /^((?!.*?Component\/(DateInput|TimeInput)).)*$/,
