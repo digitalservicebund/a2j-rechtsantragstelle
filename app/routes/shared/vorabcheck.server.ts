5fb2b1aa99762650ff886bba0c59868871d43ff4
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-03-11 10:25:42 +0100

    refactor(vorabcheck.server): use fieldsFromContext
    Co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
    Co-authored-by: Rafael Falk <rafael.falk@digitalservice.bund.de>

--- app/routes/shared/vorabcheck.server.ts
+++ app/routes/shared/vorabcheck.server.ts
@@ -22,12 +22,12 @@ import { logError } from "~/services/logging";
 import { sendCustomAnalyticsEvent } from "~/services/analytics/customEvent";
 import { parentFromParams } from "~/services/params";
 import { interpolateDeep } from "~/util/fillTemplate";
-import _ from "lodash";
 import { isStrapiHeadingComponent } from "~/services/cms/models/StrapiHeading";
 import { getButtonNavigationProps } from "~/util/buttonProps";
 import { stepMeta } from "~/services/meta/formStepMeta";
 import { getProgressProps } from "~/services/flow/server/progress";
 import { updateMainSession } from "~/services/session.server/updateSessionInHeader";
+import { fieldsFromContext } from "~/services/session.server/fieldsFromContext";
 
 export const loader = async ({
   params,
@@ -84,7 +84,7 @@ export const loader = async ({
 
   // filter user data for current step
   const fieldNames = formElements.map((entry) => entry.name);
-  const stepData = _.pick(userData, fieldNames);
+  const stepData = fieldsFromContext(userData, fieldNames);
 
   const { headers, csrf } = await updateMainSession({
     cookieHeader,
