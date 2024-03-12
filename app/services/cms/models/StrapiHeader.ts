71515aaab672dfa07d0d10a90f00052dacccab7c
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-03-12 14:38:32 +0100

    refactor(metaFromMatches): split into type-safe functions

--- app/services/cms/models/StrapiHeader.ts
+++ app/services/cms/models/StrapiHeader.ts
@@ -6,10 +6,11 @@ import { StrapiHeadingSchema } from "./StrapiHeading";
 import { StrapiParagraphSchema, getRichTextProps } from "./StrapiParagraph";
 import { HeaderPropsSchema } from "~/components/Header";
 import { omitNull } from "~/util/omitNull";
+import type { StrapiContent } from "./StrapiContent";
 
 export const StrapiHeaderSchema = z
   .object({
-    __component: z.literal("page.header").optional(),
+    __component: z.literal("page.header"),
     heading: StrapiHeadingSchema,
     content: StrapiParagraphSchema.nullable(),
     outerBackground: StrapiBackgroundSchema.nullable(),
@@ -23,3 +24,7 @@ export const getHeaderProps = (cmsData: StrapiHeader) => {
   const content = cmsData.content && getRichTextProps(cmsData.content);
   return HeaderPropsSchema.parse(omitNull({ ...cmsData, content }));
 };
+
+export const isStrapiHeader = (
+  content: StrapiContent,
+): content is StrapiHeader => content.__component === "page.header";
