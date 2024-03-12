a1479e06d8b2e0e463723a7357395b00b26f876e
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2024-03-12 08:51:55 +0100

    feat: add kinder to pdf export

--- app/services/pdf/beratungshilfe/sections/E_unterhalt.ts
+++ app/services/pdf/beratungshilfe/sections/E_unterhalt.ts
@@ -1,38 +1,135 @@
 import type { BeratungshilfeFormularContext } from "~/models/flows/beratungshilfeFormular";
 import type { BeratungshilfePDF } from "data/pdf/beratungshilfe/beratungshilfe.generated";
 import type { DescriptionField } from "../descriptionField";
+import { reactRouterV3Instrumentation } from "@sentry/remix";
+
+type UnterhaltPdfFields = {
+  name: string;
+  geburtsdatum: string;
+  familienverhaeltnis: string;
+  unterhaltSumme: string;
+  hatEinnahmen: boolean;
+  einnahmenSumme: string;
+};
 
 export function fillUnterhalt(
   descriptionField: DescriptionField,
   pdfFields: BeratungshilfePDF,
   context: BeratungshilfeFormularContext,
 ) {
-  if (
-    context.partnerschaft === "yes" &&
-    context.zusammenleben === "no" &&
-    context.unterhalt === "yes"
-  ) {
-    pdfFields.e1Person1.value = [
-      context.partnerVorname ?? "",
-      context.partnerNachname ?? "",
-    ].join(" ");
-    pdfFields.e3Familienverhaeltnis.value = "Partner:in";
-    pdfFields.e4Zahlung1.value = context.unterhaltsSumme;
+  const unterhaltPdfFields: UnterhaltPdfFields[] = [];
 
-    descriptionField.shouldCreateAttachment = true;
-
-    const description = getUnerhaltDescription(
-      context.partnerVorname,
-      context.partnerNachname,
-      context.unterhaltsSumme,
-      context.zusammenleben,
-    );
-
-    descriptionField.descriptions.unshift({
-      title: "Unterhalt",
-      text: description.join("\n\n"),
+  if (context.partnerschaft === "yes" && context.unterhalt === "yes") {
+    unterhaltPdfFields.push({
+      name: [context.partnerVorname ?? "", context.partnerNachname ?? ""].join(
+        " ",
+      ),
+      geburtsdatum: "", // FIXME: Geburtsdatum is not in the context
+      familienverhaeltnis: "Partner:in",
+      unterhaltSumme: context.unterhaltsSumme ?? "",
+      hatEinnahmen: context.partnerEinkommen === "yes",
+      einnahmenSumme: context.partnerEinkommen ?? "",
     });
   }
+
+  if (context.kinder && context.kinder.length > 0) {
+    context.kinder.forEach((kind) => {
+      unterhaltPdfFields.unshift({
+        name: [kind.vorname ?? "", kind.nachname ?? ""].join(" "),
+        geburtsdatum: kind.geburtsdatum ?? "",
+        familienverhaeltnis: "Kind",
+        unterhaltSumme: kind.unterhaltsSumme ?? "",
+        hatEinnahmen: kind.eigeneEinnahmen === "yes",
+        einnahmenSumme: kind.einnahmen ?? "",
+      });
+    });
+  }
+
+  if (unterhaltPdfFields.length == 0) return;
+
+  descriptionField.shouldCreateAttachment = true;
+
+  const description: string[] = [];
+
+  unterhaltPdfFields.forEach((unterhaltPdfField) => {
+    description.push(
+      getUnerhaltDescription(
+        unterhaltPdfField.name,
+        unterhaltPdfField.geburtsdatum,
+        unterhaltPdfField.unterhaltSumme,
+        context.zusammenleben,
+      ).join("\n"),
+    );
+  });
+
+  descriptionField.descriptions.unshift({
+    title: "Unterhalt",
+    text: description.join("\n\n"),
+  });
+
+  const person1 = unterhaltPdfFields.pop();
+  if (!person1) return;
+  fillPerson1(pdfFields, person1);
+
+  const person2 = unterhaltPdfFields.pop();
+  if (!person2) return;
+  fillPerson2(pdfFields, person2);
+
+  const person3 = unterhaltPdfFields.pop();
+  if (!person3) return;
+  fillPerson3(pdfFields, person3);
+
+  const person4 = unterhaltPdfFields.pop();
+  if (!person4) return;
+  fillPerson4(pdfFields, person4);
+}
+
+function fillPerson1(
+  pdfFields: BeratungshilfePDF,
+  unterhaltPdfFields: UnterhaltPdfFields,
+) {
+  pdfFields.e1Person1.value = unterhaltPdfFields.name;
+  pdfFields.e2Geburtsdatum.value = unterhaltPdfFields.geburtsdatum;
+  pdfFields.e3Familienverhaeltnis.value =
+    unterhaltPdfFields.familienverhaeltnis;
+  pdfFields.e4Zahlung1.value = unterhaltPdfFields.unterhaltSumme;
+  pdfFields.e5Einnahmen1.value = unterhaltPdfFields.hatEinnahmen;
+}
+
+function fillPerson2(
+  pdfFields: BeratungshilfePDF,
+  unterhaltPdfFields: UnterhaltPdfFields,
+) {
+  pdfFields.e1Person2.value = unterhaltPdfFields.name;
+  pdfFields.e2Geburtsdatum2.value = unterhaltPdfFields.geburtsdatum;
+  pdfFields.e3Familienverhaeltnis2.value =
+    unterhaltPdfFields.familienverhaeltnis;
+  pdfFields.e4Zahlung2.value = unterhaltPdfFields.unterhaltSumme;
+  pdfFields.e5Einnahmen2.value = unterhaltPdfFields.hatEinnahmen;
+}
+
+function fillPerson3(
+  pdfFields: BeratungshilfePDF,
+  unterhaltPdfFields: UnterhaltPdfFields,
+) {
+  pdfFields.e1Person3.value = unterhaltPdfFields.name;
+  pdfFields.e2Geburtsdatum3.value = unterhaltPdfFields.geburtsdatum;
+  pdfFields.e3Familienverhaeltnis3.value =
+    unterhaltPdfFields.familienverhaeltnis;
+  pdfFields.e4Zahlung3.value = unterhaltPdfFields.unterhaltSumme;
+  pdfFields.e5Einnahmen3.value = unterhaltPdfFields.hatEinnahmen;
+}
+
+function fillPerson4(
+  pdfFields: BeratungshilfePDF,
+  unterhaltPdfFields: UnterhaltPdfFields,
+) {
+  pdfFields.e1Person4.value = unterhaltPdfFields.name;
+  pdfFields.e2Geburtsdatum4.value = unterhaltPdfFields.geburtsdatum;
+  pdfFields.e3Familienverhaeltnis4.value =
+    unterhaltPdfFields.familienverhaeltnis;
+  pdfFields.e4Zahlung4.value = unterhaltPdfFields.unterhaltSumme;
+  pdfFields.e5Einnahmen4.value = unterhaltPdfFields.hatEinnahmen;
 }
 
 function getUnerhaltDescription(
--- tests/unit/services/pdf/beratungshilfe/sections/E_unterhalt.test.ts
+++ tests/unit/services/pdf/beratungshilfe/sections/E_unterhalt.test.ts
@@ -11,7 +11,7 @@ describe("E_unterhalt", () => {
   it("should fill unterhalt pdf fields when correct context is given", async () => {
     const context: BeratungshilfeFormularContext = {
       partnerschaft: "yes",
-      zusammenleben: "no",
+      zusammenleben: "yes",
       unterhalt: "yes",
       partnerVorname: "Donald",
       partnerNachname: "Duck",
@@ -30,11 +30,73 @@ describe("E_unterhalt", () => {
     expect(descriptionField.shouldCreateAttachment).toBe(true);
     expect(descriptionField.descriptions[0]).toEqual({
       title: "Unterhalt",
-      text: [
-        "Unterhalt für Partner:in Donald Duck",
-        "Gemeinsame Wohnung: Nein",
+      text:
+        "Unterhalt für Partner:in Donald Duck \n" +
+        "Gemeinsame Wohnung: Ja\n" +
         "Monatliche Summe: 1000 €",
-      ].join("\n\n"),
+    });
+  });
+
+  it("should fill kinder unterhalt pdf fields when correct context is given", async () => {
+    const context: BeratungshilfeFormularContext = {
+      partnerschaft: "yes",
+      zusammenleben: "yes",
+      unterhalt: "yes",
+      partnerVorname: "Donald",
+      partnerNachname: "Duck",
+      klageEingereicht: "yes",
+      unterhaltsSumme: "1000",
+      kinder: [
+        {
+          eigeneEinnahmen: "yes",
+          vorname: "Dagobert",
+          nachname: "Duck",
+          geburtsdatum: "01.01.2000",
+          unterhaltsSumme: "500",
+          einnahmen: "100",
+          unterhalt: "yes",
+          wohnortBeiAntragsteller: "yes",
+        },
+        {
+          eigeneEinnahmen: "no",
+          vorname: "Dagobert",
+          nachname: "Duck",
+          geburtsdatum: "01.01.2001",
+          unterhaltsSumme: "200",
+          einnahmen: "0",
+          unterhalt: "yes",
+          wohnortBeiAntragsteller: "no",
+        },
+      ],
+    };
+    const pdfFields = await getBeratungshilfeParameters();
+    const descriptionField = createDescriptionField(context);
+
+    fillUnterhalt(descriptionField, pdfFields, context);
+
+    expect(pdfFields.e1Person1.value).toBe("Donald Duck");
+    expect(pdfFields.e3Familienverhaeltnis.value).toBe("Partner:in");
+    expect(pdfFields.e4Zahlung1.value).toBe("1000");
+    expect(pdfFields.e1Person2.value).toBe("Dagobert Duck");
+    expect(pdfFields.e3Familienverhaeltnis2.value).toBe("Kind");
+    expect(pdfFields.e4Zahlung2.value).toBe("500");
+    expect(pdfFields.e5Einnahmen2.value).toBe(true);
+
+    expect(descriptionField.shouldCreateAttachment).toBe(true);
+    expect(descriptionField.descriptions[0]).toEqual({
+      title: "Unterhalt",
+      text:
+        "Unterhalt für Partner:in Dagobert Duck 01.01.2001\n" +
+        "Gemeinsame Wohnung: Ja\n" +
+        "Monatliche Summe: 200 €\n" +
+        "\n" +
+        "Unterhalt für Partner:in Dagobert Duck 01.01.2000\n" +
+        "Gemeinsame Wohnung: Ja\n" +
+        "Monatliche Summe: 500 €\n" +
+        "\n" +
+        "Unterhalt für Partner:in Donald Duck \n" +
+        "Gemeinsame Wohnung: Ja\n" +
+        "Monatliche Summe: 1000 €",
     });
   });
 });
