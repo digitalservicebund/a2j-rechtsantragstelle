cd61cc6a79c7be98cb81f4391942201459635347
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-03-19 11:00:21 +0100

    fix(e2e): adapt to new besitz flow

--- tests/e2e/beratungshilfeFormular/finanzielleAngabenBesitz.ts
+++ tests/e2e/beratungshilfeFormular/finanzielleAngabenBesitz.ts
@@ -6,23 +6,35 @@ export async function startFinanzielleAngabenBesitz(
   page: Page,
   beratungshilfeFormular: BeratungshilfeFormular,
 ) {
+  // beratungshilfe/antrag/finanzielleAngaben/besitz/eigentum-info
+  await expectPageToBeAccessible({ page });
+  await beratungshilfeFormular.clickNext();
+
+  // beratungshilfe/antrag/finanzielleAngaben/besitz/heirat-info
+  await expectPageToBeAccessible({ page });
+  await beratungshilfeFormular.clickNext();
+
   // beratungshilfe/antrag/finanzielleAngaben/besitz/bankkonten-frage
   await expectPageToBeAccessible({ page });
   await beratungshilfeFormular.fillRadioPage("hasBankkonto", "yes");
-
-  // beratungshilfe/antrag/finanzielleAngaben/besitz/kraftfahrzeuge-frage
-  await expectPageToBeAccessible({ page });
-  await beratungshilfeFormular.fillRadioPage("hasKraftfahrzeug", "yes");
 
   // beratungshilfe/antrag/finanzielleAngaben/besitz/geldanlagen-frage
   await expectPageToBeAccessible({ page });
   await beratungshilfeFormular.fillRadioPage("hasGeldanlage", "yes");
 
+  // beratungshilfe/antrag/finanzielleAngaben/besitz/wertsachen-frage
+  await expectPageToBeAccessible({ page });
+  await beratungshilfeFormular.fillRadioPage("hasWertsache", "yes");
+
   // beratungshilfe/antrag/finanzielleAngaben/besitz/grundeigentum-frage
   await expectPageToBeAccessible({ page });
   await beratungshilfeFormular.fillRadioPage("hasGrundeigentum", "yes");
 
-  // beratungshilfe/antrag/finanzielleAngaben/besitz/wertsachen-frage
+  // beratungshilfe/antrag/finanzielleAngaben/besitz/kraftfahrzeuge-frage
   await expectPageToBeAccessible({ page });
-  await beratungshilfeFormular.fillRadioPage("hasWertsache", "yes");
+  await beratungshilfeFormular.fillRadioPage("hasKraftfahrzeug", "yes");
+
+  // beratungshilfe/antrag/finanzielleAngaben/besitz/gesamtwert
+  await expectPageToBeAccessible({ page });
+  await beratungshilfeFormular.fillRadioPage("besitzTotalWorth", "more10000");
 }
