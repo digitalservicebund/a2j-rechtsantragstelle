2a03a0eb7486f6b4489fab1cd51c7c8fe9d9a115
aaschlote <alex.schlote@digitalservice.bund.de>
2024-03-21 17:12:02 +0100

    feat: add liviing situation to pdf

---
+++ tests/unit/services/pdf/beratungshilfe/sections/D_wohnen.test.ts
@@ -0,0 +1,90 @@
+/**
+ * @jest-environment node
+ */
+
+import type { BeratungshilfeFormularContext } from "~/models/flows/beratungshilfeFormular";
+import { getBeratungshilfeParameters } from "~/services/pdf/beratungshilfe/beratungshilfe.server";
+import { fillWohnen } from "~/services/pdf/beratungshilfe/sections/D_wohnen";
+
+describe("fillWohnen", () => {
+  it("should fill wohnen section for living situation alone correct", async () => {
+    const context: BeratungshilfeFormularContext = {
+      apartmentSizeSqm: 10,
+      apartmentCostFull: "100",
+      livingSituation: "alone",
+    };
+
+    const pdfFields = await getBeratungshilfeParameters();
+
+    fillWohnen(pdfFields, context);
+
+    expect(pdfFields.d1Wohnung.value).toEqual(
+      context.apartmentSizeSqm?.toString(),
+    );
+    expect(pdfFields.d2Wohnkosten.value).toEqual(
+      context.apartmentCostFull?.toString(),
+    );
+    expect(pdfFields.d3Teilwohnkosten.value).toEqual("");
+    expect(pdfFields.d4Wohnungalleine.value).toEqual(true);
+    expect(pdfFields.d5Wohnunggemeinsam.value).toEqual(false);
+    expect(pdfFields.d6WonungweiterePersonen.value).toEqual("");
+  });
+
+  it("should fill wohnen section for living situation withOthers correct", async () => {
+    const context: BeratungshilfeFormularContext = {
+      apartmentSizeSqm: 10,
+      apartmentCostFull: "100",
+      apartmentCostOwnShare: "20",
+      apartmentPersonCount: 5,
+      livingSituation: "withOthers",
+    };
+
+    const pdfFields = await getBeratungshilfeParameters();
+
+    fillWohnen(pdfFields, context);
+
+    expect(pdfFields.d1Wohnung.value).toEqual(
+      context.apartmentSizeSqm?.toString(),
+    );
+    expect(pdfFields.d2Wohnkosten.value).toEqual(
+      context.apartmentCostFull?.toString(),
+    );
+    expect(pdfFields.d3Teilwohnkosten.value).toEqual(
+      context.apartmentCostOwnShare?.toString(),
+    );
+    expect(pdfFields.d4Wohnungalleine.value).toEqual(false);
+    expect(pdfFields.d5Wohnunggemeinsam.value).toEqual(true);
+    expect(pdfFields.d6WonungweiterePersonen.value).toEqual(
+      context.apartmentPersonCount?.toString(),
+    );
+  });
+
+  it("should fill wohnen section for living situation withRelatives correct", async () => {
+    const context: BeratungshilfeFormularContext = {
+      apartmentSizeSqm: 10,
+      apartmentCostFull: "100",
+      apartmentCostOwnShare: "20",
+      apartmentPersonCount: 5,
+      livingSituation: "withRelatives",
+    };
+
+    const pdfFields = await getBeratungshilfeParameters();
+
+    fillWohnen(pdfFields, context);
+
+    expect(pdfFields.d1Wohnung.value).toEqual(
+      context.apartmentSizeSqm?.toString(),
+    );
+    expect(pdfFields.d2Wohnkosten.value).toEqual(
+      context.apartmentCostFull?.toString(),
+    );
+    expect(pdfFields.d3Teilwohnkosten.value).toEqual(
+      context.apartmentCostOwnShare?.toString(),
+    );
+    expect(pdfFields.d4Wohnungalleine.value).toEqual(false);
+    expect(pdfFields.d5Wohnunggemeinsam.value).toEqual(true);
+    expect(pdfFields.d6WonungweiterePersonen.value).toEqual(
+      context.apartmentPersonCount?.toString(),
+    );
+  });
+});
