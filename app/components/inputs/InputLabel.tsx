541b80e7fcafa7ca84e68f43dea83c58636153a2
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-03-25 18:10:33 +0100

    feat: allow textarea to have heading and description
    Co-authored-by: aaschlote <alex.schlote@digitalservice.bund.de>

--- app/components/inputs/InputLabel.tsx
+++ app/components/inputs/InputLabel.tsx
@@ -2,10 +2,15 @@ import type { PropsWithChildren } from "react";
 
 type InputLabelProps = PropsWithChildren<{
   readonly id: string;
+  readonly classname?: string;
 }>;
 
-const InputLabel = ({ id, children }: InputLabelProps) => {
-  return <label htmlFor={id}>{children}</label>;
+const InputLabel = ({ id, children, classname }: InputLabelProps) => {
+  return (
+    <label className={classname} htmlFor={id}>
+      {children}
+    </label>
+  );
 };
 
 export default InputLabel;
--- app/components/inputs/Textarea.tsx
+++ app/components/inputs/Textarea.tsx
@@ -5,9 +5,11 @@ import InputError from "./InputError";
 import InputLabel from "./InputLabel";
 import { z } from "zod";
 import { ErrorMessagePropsSchema } from ".";
+import RichText from "../RichText";
 
 export const TextareaPropsSchema = z.object({
   name: z.string(),
+  description: z.string().optional(),
   label: z.custom<ReactNode>().optional(),
   placeholder: z.string().optional(),
   errorMessages: z.array(ErrorMessagePropsSchema).optional(),
@@ -17,6 +19,7 @@ type TextareaProps = z.infer<typeof TextareaPropsSchema>;
 
 const Textarea = ({
   name,
+  description,
   label,
   placeholder,
   errorMessages,
@@ -25,8 +28,18 @@ const Textarea = ({
   const errorId = `${name}-error`;
 
   return (
-    <div>
-      {label && <InputLabel id={name}>{label}</InputLabel>}
+    <div className="ds-stack-8">
+      {label && (
+        <InputLabel
+          classname={description ? "ds-heading-03-reg" : ""}
+          id={name}
+        >
+          {label}
+        </InputLabel>
+      )}
+      {description && (
+        <RichText className={"ds-body-01-reg"} markdown={description} />
+      )}
       <textarea
         {...getInputProps({
           id: name,
