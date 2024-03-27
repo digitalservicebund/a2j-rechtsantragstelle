9d20031642426ea3601fa59cd8bac931d7f7dc15
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-03-27 15:25:32 +0100

    feat: Move flowNavigation from kitchensink to storybook

--- app/routes/kitchensink._index.tsx
+++ app/routes/kitchensink._index.tsx
@@ -1,14 +1,9 @@
 import { useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/node";
 import type { LoaderFunctionArgs } from "@remix-run/node";
-
-import Container from "~/components/Container";
+import { json } from "@remix-run/node";
 import PageContent from "~/components/PageContent";
 import { strapiPageFromRequest } from "~/services/cms/index.server";
 import { throw404OnProduction } from "~/services/errorPages/throw404";
-import List from "~/components/List";
-import FlowNavigation, { NavState } from "~/components/FlowNavigation";
-import Heading from "~/components/Heading";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   throw404OnProduction();
@@ -20,55 +15,6 @@ export default function Kitchensink() {
   const loaderData = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>{loaderData.meta?.title}</h1>
-
-      <Container>
-        <h2 className="ds-heading-02-reg">React components</h2>
-      </Container>
-      <Container>
-        <h3 className="mb-16">List</h3>
-        <List
-          isNumeric={true}
-          items={[
-            { content: "Test 1", identifier: "test1" },
-            { content: "Test 2", identifier: "test2" },
-          ]}
-        />
-      </Container>
-      <Container>
-        <Heading text="FlowNavigation" tagName="h2" look="ds-heading-01-reg" />
-        <FlowNavigation
-          navItems={[
-            {
-              destination: ".",
-              label: "Grundvoraussetzungen",
-              state: NavState.DoneDisabled,
-            },
-            {
-              destination: ".",
-              label: "Das Rechtsproblem",
-              state: NavState.Done,
-            },
-            {
-              destination: ".",
-              label: "Finanzielle Angaben",
-              state: NavState.Current,
-            },
-            {
-              destination: ".",
-              label: "Persönliche Daten",
-              state: NavState.Open,
-            },
-            {
-              destination: ".",
-              label: "Abgabe",
-              state: NavState.OpenDisabled,
-            },
-          ]}
-        />
-      </Container>
-
-      <h2>CMS components</h2>
       {loaderData.content ? (
         <PageContent content={loaderData.content} />
       ) : (
