eb72da72cb1fd24d0e05b0729906e3cb174cf770
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-03-27 15:10:57 +0100

    feat: Move infoBox from kitchensink to storybook

--- app/routes/kitchensink._index.tsx
+++ app/routes/kitchensink._index.tsx
@@ -1,29 +1,14 @@
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 
-import { z } from "zod";
-import { withZod } from "@remix-validated-form/with-zod";
-import { validationError } from "remix-validated-form";
-import Background from "~/components/Background";
 import Container from "~/components/Container";
-import Header from "~/components/Header";
-import InfoBox from "~/components/InfoBox";
 import PageContent from "~/components/PageContent";
 import { strapiPageFromRequest } from "~/services/cms/index.server";
 import { throw404OnProduction } from "~/services/errorPages/throw404";
 import List from "~/components/List";
 import FlowNavigation, { NavState } from "~/components/FlowNavigation";
 import Heading from "~/components/Heading";
-
-const DummySchema = z.object({
-  text: z.string().min(1),
-  textarea: z.string(),
-  options: z.enum(["1", "2", "3"]),
-  radioOptions: z.enum(["yes", "no", "maybe"]),
-});
-
-const validator = withZod(DummySchema);
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   throw404OnProduction();
@@ -40,23 +25,6 @@ export default function Kitchensink() {
       <Container>
         <h2 className="ds-heading-02-reg">React components</h2>
       </Container>
-
-      <Background backgroundColor="blue">
-        <Container>
-          <h3>InfoBox</h3>
-          <InfoBox
-            heading={{
-              text: "Heading",
-              tagName: "h3",
-              look: "ds-heading-02-reg",
-            }}
-            items={[
-              { label: { text: "Lorem ipsum1", tagName: "h4" } },
-              { label: { text: "Lorem ipsum2", tagName: "h4" } },
-            ]}
-          />
-        </Container>
-      </Background>
       <Container>
         <h3 className="mb-16">List</h3>
         <List
