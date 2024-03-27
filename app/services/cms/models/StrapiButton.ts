f7a51303d63f0fecf1b813d06fa57af46bb1a9b0
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-03-27 18:01:59 +0100

    feat(newtab): openInNewTab should be nullable

--- app/services/cms/models/StrapiButton.ts
+++ app/services/cms/models/StrapiButton.ts
@@ -10,6 +10,6 @@ export const StrapiButtonSchema = z
     href: z.string().nullable(),
     text: z.string().nullable(),
     downloadFile: z.string().nullable(),
-    openInNewTab: z.boolean(),
+    openInNewTab: z.boolean().nullable(),
   })
   .merge(HasOptionalStrapiIdSchema);
