65b30528e463c7f0eb3e870346dc5dfd874319cd
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-04-03 17:02:07 +0200

    feat(grundeigentum): Remove all validation from plz

--- tests/unit/services/validation/postcode.test.ts
+++ tests/unit/services/validation/postcode.test.ts
@@ -1,7 +1,4 @@
-import {
-  postcodeOptionalSchema,
-  postcodeSchema,
-} from "~/services/validation/postcode";
+import { postcodeSchema } from "~/services/validation/postcode";
 import type { SafeParseError } from "zod";
 
 describe("postcode validation", () => {
@@ -38,50 +35,6 @@ describe("postcode validation", () => {
       "given $input, returns $errorMessage",
       ({ input, errorMessage }) => {
         const actual = postcodeSchema.safeParse(input);
-        expect(actual.success).toBe(false);
-        expect(
-          (actual as SafeParseError<unknown>).error.issues[0].message,
-        ).toBe(errorMessage);
-      },
-    );
-  });
-});
-
-describe("postcode optional validation", () => {
-  describe("success cases", () => {
-    const cases = [
-      { input: "01067", expected: "01067" },
-      { input: "99998", expected: "99998" },
-      { input: " 12345  ", expected: "12345" },
-      { input: "", expected: "" },
-      { input: "   ", expected: "" },
-    ];
-
-    test.each(cases)(
-      "given $input, returns $expected",
-      ({ input, expected }) => {
-        const actual = postcodeOptionalSchema.safeParse(input);
-        expect(actual).toEqual({ data: expected, success: true });
-      },
-    );
-  });
-
-  describe("failing cases", () => {
-    const cases = [
-      { input: "1", errorMessage: "length" },
-      { input: "123456", errorMessage: "length" },
-      { input: "abcde", errorMessage: "invalid" },
-      { input: "99999", errorMessage: "invalid" },
-      { input: "01066", errorMessage: "invalid" },
-      { input: "4000x", errorMessage: "invalid" },
-      { input: "4000-", errorMessage: "invalid" },
-      { input: "40 00", errorMessage: "invalid" },
-    ];
-
-    test.each(cases)(
-      "given $input, returns $errorMessage",
-      ({ input, errorMessage }) => {
-        const actual = postcodeOptionalSchema.safeParse(input);
         expect(actual.success).toBe(false);
         expect(
           (actual as SafeParseError<unknown>).error.issues[0].message,
