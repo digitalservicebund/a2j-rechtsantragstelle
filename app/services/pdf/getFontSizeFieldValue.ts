99065f4a8905e149c11e9c2aa0f6f802cfe91b76
aaschlote <alex.schlote@digitalservice.bund.de>
2024-04-05 10:48:23 +0200

    refactor: fix section D pdf

--- app/services/pdf/getFontSizeFieldValue.ts
+++ app/services/pdf/getFontSizeFieldValue.ts
@@ -1,17 +1,15 @@
 const FONT_SIZE_10 = 10;
-const FONT_SIZE_5 = 5;
 const FONT_SIZE_6 = 6;
 const FONT_SIZE_8 = 8;
 
 export function getFontSizeFieldValue(fieldName: string): number {
   switch (fieldName) {
-    case "D3-Teilwohnkosten":
-      return FONT_SIZE_5;
     case "E2-Geburtsdatum":
     case "E2-Geburtsdatum1":
     case "E2-Geburtsdatum2":
     case "E2-Geburtsdatum3":
     case "E2-Geburtsdatum4":
+    case "D3-Teilwohnkosten":
       return FONT_SIZE_8;
     case "E3-Familienverhältnis":
     case "E3-Familienverhältnis2":
