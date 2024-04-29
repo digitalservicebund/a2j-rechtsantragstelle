50167f581196ce812aefbcb716532a6cfceaed68
aaschlote <alex.schlote@digitalservice.bund.de>
2024-04-29 11:30:29 +0200

    refactor: duplicate resultpages

---
+++ app/components/resultPage/FluggastrechteResultPage.tsx
@@ -0,0 +1,32 @@
+import { useLoaderData } from "@remix-run/react";
+import type { loader } from "../../routes/shared/result.server";
+import { ResultPage } from "./ResultPage";
+
+export function FluggastrechteResultPage() {
+  const {
+    flowId,
+    common,
+    cmsData,
+    reasons,
+    progress,
+    nextButton,
+    backButton,
+    bannerState,
+    amtsgerichtCommon,
+  } = useLoaderData<typeof loader>();
+
+  return (
+    <ResultPage
+      flowId={flowId}
+      common={common}
+      cmsData={cmsData}
+      reasons={reasons}
+      progress={progress}
+      nextButton={nextButton}
+      backButton={backButton}
+      bannerState={bannerState}
+      amtsgerichtCommon={amtsgerichtCommon}
+      courts={[]}
+    />
+  );
+}
