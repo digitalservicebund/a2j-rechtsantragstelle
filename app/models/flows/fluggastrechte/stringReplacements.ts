e9fe01c1e263183fffd2d0d05be2059d41784172
aaschlote <alex.schlote@digitalservice.bund.de>
2024-05-07 14:30:40 +0200

    refactor: get last day of the year from 4 years ago

--- app/models/flows/fluggastrechte/stringReplacements.ts
+++ app/models/flows/fluggastrechte/stringReplacements.ts
@@ -1,9 +1,13 @@
 import { calculateDistanceBetweenAirportsInKilometers } from "~/util/calculateDistanceBetweenAirports";
 import type { FluggastrechtVorabcheckContext } from "./context";
+import { toGermanDateFormat, today } from "~/services/validation/date";
 
 export const COMPENSATION_VALUE_UNTIL_1500_KM = "250";
 export const COMPENSATION_VALUE_UNTIL_3000_KM = "400";
 export const COMPENSATION_VALUE_ABOVE_3000_KM = "600";
+const FOUR_YEARS_AGO = 4;
+const LAST_DAY_YEAR = 31;
+const LAST_MONTH_YEAR = 11; // Date.setMonth starts from 0 to 11, where 11 is December
 
 export function getCompensantionPaymentString({
   startAirport = "",
@@ -29,4 +33,12 @@ export function getCompensantionPaymentString({
   }
 
   return {};
+}
+
+export function getLastDaytFromFourYearsAgoDate(): string {
+  const date = today();
+  date.setFullYear(date.getFullYear() - FOUR_YEARS_AGO);
+  date.setMonth(LAST_MONTH_YEAR);
+  date.setDate(LAST_DAY_YEAR);
+  return toGermanDateFormat(date);
 }
