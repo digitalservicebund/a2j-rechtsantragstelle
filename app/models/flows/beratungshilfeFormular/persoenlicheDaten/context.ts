79fc59180925eead319dff46e954213fe1219fbc
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-05-10 15:33:04 +0200

    feat(BH): validate birthday

--- app/models/flows/beratungshilfeFormular/persoenlicheDaten/context.ts
+++ app/models/flows/beratungshilfeFormular/persoenlicheDaten/context.ts
@@ -1,13 +1,16 @@
 import { z } from "zod";
 import { namePrivatPerson, adresse } from "../../persoenlicheDaten/context";
-import { createDateSchema } from "~/services/validation/date";
+import { addYears, createDateSchema, today } from "~/services/validation/date";
 import { phoneNumberSchema } from "~/services/validation/phoneNumber";
 import type { GenericGuard } from "../../guards.server";
 import { optionalOrSchema } from "~/services/validation/optionalOrSchema";
 
 export const beratungshilfePersoenlicheDaten = {
   ...namePrivatPerson,
-  geburtsdatum: createDateSchema(),
+  geburtsdatum: createDateSchema({
+    earliest: () => addYears(today(), -150),
+    latest: () => today(),
+  }),
   ...adresse,
   telefonnummer: optionalOrSchema(phoneNumberSchema),
 };
