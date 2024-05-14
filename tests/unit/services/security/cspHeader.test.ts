36efcb95a0ca9fbc1a979d683a2714be3edd0ab2
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-05-14 09:16:29 +0200

    test(cspHeaders): extend tests

--- tests/unit/services/security/cspHeader.test.ts
+++ tests/unit/services/security/cspHeader.test.ts
@@ -4,11 +4,16 @@ describe("cspHeader", () => {
   const header = cspHeader("");
   it("should contain all OWASP recommended directives", () => {
     expect(header).toContain("script-src");
-    // expect(header).toContain("object-src");
-    // expect(header).toContain("base-uri");
+    expect(header).toContain("strict-dynamic");
+    expect(header).toContain("object-src");
+    expect(header).toContain("base-uri");
   });
 
   it("should contain a nonce directives", () => {
     expect(header).toContain("nonce-");
+  });
+
+  it("disables all framing", () => {
+    expect(header).toContain("frame-ancestors 'none'");
   });
 });
