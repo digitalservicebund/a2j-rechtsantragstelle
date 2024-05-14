642c4dc8f577219ffbddc8e52d9e0c980c0407b8
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-05-14 09:40:29 +0200

    refactor(cspHeaders): explicit locahost over *

--- app/services/security/cspHeader.server.tsx
+++ app/services/security/cspHeader.server.tsx
@@ -1,5 +1,3 @@
-import { config } from "~/services/env/env.server";
-
 // References:
 // https://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.html
 // https://web.dev/articles/strict-csp
@@ -10,11 +8,7 @@ export const cspHeader = (cspNonce: string) =>
     `default-src 'self'`,
     `script-src 'self' 'nonce-${cspNonce}' https://*.posthog.com 'strict-dynamic' https: 'unsafe-inline'`,
     `style-src 'self' 'unsafe-inline'`,
-    `connect-src ${
-      config().ENVIRONMENT === "development"
-        ? "*"
-        : "'self' https://*.ingest.sentry.io https://*.posthog.com"
-    }`,
+    `connect-src 'self' ws://localhost:24678 https://*.ingest.sentry.io https://*.posthog.com`, // localhost is vite's HMR server
     `img-src 'self' localhost:* https://a2j-rechtsantragstelle-infra-public-assets-bucket.obs.eu-de.otc.t-systems.com https://mermaid.ink data:`,
     `object-src 'none'`,
     `base-uri 'none'`,
