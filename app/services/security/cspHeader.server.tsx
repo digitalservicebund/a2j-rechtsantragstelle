13d31bc2fc80f44487e7849733545f0cc5eb32fe
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-05-14 09:12:04 +0200

    sec(cspHeaders): stricter directives, add fallbacks + references

--- app/services/security/cspHeader.server.tsx
+++ app/services/security/cspHeader.server.tsx
@@ -1,9 +1,14 @@
 import { config } from "~/services/env/env.server";
 
+// References:
+// https://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.html
+// https://web.dev/articles/strict-csp
+// https://csp-evaluator.withgoogle.com/
+
 export const cspHeader = (cspNonce: string) =>
   [
     `default-src 'self'`,
-    `script-src 'self' 'nonce-${cspNonce}' https://*.posthog.com`,
+    `script-src 'self' 'nonce-${cspNonce}' https://*.posthog.com 'strict-dynamic' https: 'unsafe-inline'`,
     `style-src 'self' 'unsafe-inline'`,
     `connect-src ${
       config().ENVIRONMENT === "development"
@@ -11,4 +16,7 @@ export const cspHeader = (cspNonce: string) =>
         : "'self' https://*.ingest.sentry.io https://*.posthog.com"
     }`,
     `img-src 'self' localhost:* https://a2j-rechtsantragstelle-infra-public-assets-bucket.obs.eu-de.otc.t-systems.com https://mermaid.ink data:`,
+    `object-src 'none'`,
+    `base-uri 'none'`,
+    `frame-ancestors 'none'`,
   ].join("; ");
