25a1b7bc5c8a293e40779398fa237e9a065de331
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-05-14 08:41:32 +0200

    chore: update posthog api endpoint

--- app/services/env/web.ts
+++ app/services/env/web.ts
@@ -7,7 +7,8 @@ export function config() {
   const env = getNodeOrWebEnv();
 
   return {
-    POSTHOG_API_HOST: env.POSTHOG_API_HOST?.trim() ?? "https://eu.posthog.com",
+    POSTHOG_API_HOST:
+      env.POSTHOG_API_HOST?.trim() ?? "https://eu.i.posthog.com	",
     POSTHOG_API_KEY: env.POSTHOG_API_KEY?.trim(),
     SENTRY_DSN: env.SENTRY_DSN?.trim(),
     ENVIRONMENT: env.ENVIRONMENT ?? "local",
