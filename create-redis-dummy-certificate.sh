dc2fc4b4d4da852cd153bcd22ea87880a2ed108f
aaschlote <alex.schlote@digitalservice.bund.de>
2024-06-07 09:45:14 +0200

    ci: change file permission

--- create-redis-dummy-certificate.sh
+++ create-redis-dummy-certificate.sh
@@ -5,7 +5,7 @@ then
 else
     mkdir certs
     openssl genrsa 2048 > ./certs/redis-server.key
-    chmod 400 ./certs/redis-server.key
+    chmod 444 ./certs/redis-server.key
     openssl req -new -x509 -nodes -sha256 -days 365 -subj "/C=DE/ST=Test/L=Test/O=Test/CN=Test" -key ./certs/redis-server.key -out ./certs/redis-server.crt
     echo "Created new certificates"
 fi
