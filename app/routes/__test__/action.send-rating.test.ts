c1bdaf9e230f16a90b2c23f89c22455d6a289f38
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-06-11 15:54:02 +0200

    test: /action/send-rating route tests

---
+++ app/routes/__test__/action.send-rating.test.ts
@@ -0,0 +1,41 @@
+import { action } from "../action.send-rating";
+
+vi.stubEnv("POSTHOG_API_KEY", "-");
+
+describe("/action/send-rating route", () => {
+  const options = {
+    method: "POST",
+    body: new URLSearchParams(),
+  };
+
+  it("returns ok", async () => {
+    const request = new Request(
+      `http://localhost:3000/action/send-rating?url=/asd&js=true`,
+      options,
+    );
+    const response = await action({ request, params: {}, context: {} });
+    expect(response.status).toEqual(200);
+    expect(response.ok).toBeTruthy();
+  });
+
+  it("returns redirect without JS", async () => {
+    const ratingPath = "/relative/path";
+    const request = new Request(
+      `http://localhost:3000/action/send-rating?url=${ratingPath}&js=false`,
+      options,
+    );
+    const response = await action({ request, params: {}, context: {} });
+    expect(response.status).toEqual(302);
+    expect(response.headers.get("location")).toEqual(ratingPath);
+  });
+
+  it("fails for non-relative URLs", async () => {
+    const request = new Request(
+      `http://localhost:3000/action/send-rating?url=http://external.com&js=false`,
+      options,
+    );
+    const response = await action({ request, params: {}, context: {} });
+    expect(response.status).toEqual(400);
+    expect(response.ok).not.toBeTruthy();
+  });
+});
