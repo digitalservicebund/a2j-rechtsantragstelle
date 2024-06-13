4acfe8530e69b55ea2e32436a1b743eb28acc756
aaschlote <alex.schlote@digitalservice.bund.de>
2024-06-13 17:12:26 +0200

    test: add unit test

---
+++ app/components/inputs/autoSuggestInput/__test__/getDataListOptions.test.ts
@@ -0,0 +1,23 @@
+import { getDataListOptions } from "../getDataListOptions";
+
+describe("getDataListOptions", () => {
+  it("it should return data options for airports and find the BER airport", () => {
+    const actual = getDataListOptions("airports");
+
+    expect(actual.length).greaterThan(0);
+    expect(actual.find((airport) => airport.value === "BER")).toBeTruthy();
+  });
+
+  it("it should return data options for airlines and find the LH airline", () => {
+    const actual = getDataListOptions("airlines");
+
+    expect(actual.length).greaterThan(0);
+    expect(actual.find((airline) => airline.value === "LH")).toBeTruthy();
+  });
+
+  it("it should return no data options in case parameter is not defined", () => {
+    const actual = getDataListOptions();
+
+    expect(actual.length).toBe(0);
+  });
+});
