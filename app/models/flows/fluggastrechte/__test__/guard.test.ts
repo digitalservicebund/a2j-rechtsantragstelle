13760ccc2ba7aa3d0455d2e1860568a61d36b3da
aaschlote <alex.schlote@digitalservice.bund.de>
2024-06-19 11:23:21 +0200

    test: add unit test for guards

--- app/models/flows/fluggastrechte/__test__/guard.test.ts
+++ app/models/flows/fluggastrechte/__test__/guard.test.ts
@@ -119,4 +119,36 @@ describe("fluggastrechteGuard", () => {
       expect(actual).toBe(false);
     });
   });
+
+  describe("isFluggesellschaftNoEU", () => {
+    it("should return true in case is an airline outside of EU", () => {
+      const context: FluggastrechtVorabcheckContext = {
+        fluggesellschaft: "DL",
+      };
+
+      const actual = guards.isFluggesellschaftNoEU({ context });
+
+      expect(actual).toBe(true);
+    });
+
+    it("should return false in case is an airline inside of EU", () => {
+      const context: FluggastrechtVorabcheckContext = {
+        fluggesellschaft: "LH",
+      };
+
+      const actual = guards.isFluggesellschaftNoEU({ context });
+
+      expect(actual).toBe(false);
+    });
+
+    it("should return true in case is the airline is undefined", () => {
+      const context: FluggastrechtVorabcheckContext = {
+        fluggesellschaft: undefined,
+      };
+
+      const actual = guards.isFluggesellschaftNoEU({ context });
+
+      expect(actual).toBe(true);
+    });
+  });
 });
