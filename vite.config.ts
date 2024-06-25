6f73cf26a1832db53155cf6a94a3e8d653be91ff
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-06-25 17:42:28 +0200

    chore(remix): opt-in future flags

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,15 @@ export default defineConfig({
   },
   plugins: [
     envOnlyMacros(),
-    !isStorybook && !isVitest && remix(),
+    !isStorybook &&
+      !isVitest &&
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
     !isStorybook &&
       sentryActive &&
       sentryVitePlugin({
