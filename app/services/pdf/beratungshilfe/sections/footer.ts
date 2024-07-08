5c9de38950e8038cab42e2a385d84ae67a5b6bc8
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-07-08 16:06:56 +0200

    feat(BH pdf): print notice if lawyer has been contacted

--- app/services/pdf/beratungshilfe/sections/footer.ts
+++ app/services/pdf/beratungshilfe/sections/footer.ts
@@ -6,15 +6,23 @@ export function fillFooter(
   pdfFields: BeratungshilfePDF,
   context: BeratungshilfeFormularContext,
 ) {
-  pdfFields.beratungsperson.value = [
-    context.anwaltName ?? "",
-    context.anwaltStrasseUndHausnummer ?? "",
-    context.anwaltPlz ?? "",
-    context.anwaltOrt ?? "",
-  ]
-    .filter((entry) => entry)
-    .join(", ");
-  pdfFields.datumBeratung.value = context.beratungStattgefundenDatum ?? "";
+  if (context.anwaltskanzlei === "yes") {
+    if (context.beratungStattgefunden === "no") {
+      pdfFields.beratungsperson.value =
+        "Kontakt mit Kanzlei bereits aufgenommen, aber eine Beratung hat noch nicht stattgefunden";
+    } else if (context.beratungStattgefunden === "yes") {
+      pdfFields.beratungsperson.value = [
+        context.anwaltName,
+        context.anwaltStrasseUndHausnummer,
+        context.anwaltPlz,
+        context.anwaltOrt,
+      ]
+        .filter((entry) => entry)
+        .join(", ");
+      pdfFields.datumBeratung.value = context.beratungStattgefundenDatum ?? "";
+    }
+  }
+
   pdfFields.ortDatum2.value = `${uppercaseFirstLetter(context.ort)}, ${new Date().toLocaleDateString("de-DE", { day: "numeric", month: "long", year: "numeric" })}`;
 
   if (context.abgabeArt === "online") {
