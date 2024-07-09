45e7df6c6d1658b7e2621e624c348023b3f6ee4f
aaschlote <alex.schlote@digitalservice.bund.de>
2024-07-09 09:11:22 +0200

    test: fix e2e tests

--- tests/e2e/pom/BeratungshilfeFormular.ts
+++ tests/e2e/pom/BeratungshilfeFormular.ts
@@ -2,7 +2,7 @@ import { Formular } from "tests/e2e/pom/Formular";
 
 export class BeratungshilfeFormular extends Formular {
   readonly url = "/beratungshilfe/antrag";
-  readonly initialStep = "start";
+  readonly initialStep = "start/start";
 
   async hasAntragDownloadButton() {
     return await this.page.locator("#Download Button").isVisible();
