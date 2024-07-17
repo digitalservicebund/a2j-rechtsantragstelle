518a9be16b3c22ab4d0c926de25b71bda7f5b194
Alex August Schlote <alex.schlote@digitalservice.bund.de>
2024-07-17 09:16:37 +0200

    Solution sorting airports (#979)
    * refactor: sort airlines by the code
    
    * test: skip tests for now
    
    * fix: add subDescription as sorting key
    
    Co-authored-by: aaschlote <alex.schlote@digitalservice.bund.de>
    
    * refactor: call validation onBlur if an option was selected
    
    * refactor: extra code improvements
    
    ---------
    
    Co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>

--- app/components/inputs/autoSuggestInput/AutoSuggestInput.tsx
+++ app/components/inputs/autoSuggestInput/AutoSuggestInput.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
+import { matchSorter } from "match-sorter";
 import { RefObject, useEffect, useRef, useState } from "react";
-import Select from "react-select";
+import Select, { InputActionMeta } from "react-select";
 import { useField } from "remix-validated-form";
 import { DataListType } from "~/services/cms/components/StrapiAutoSuggestInput";
 import { DataListOptions } from "~/services/dataListOptions/getDataListOptions";
@@ -19,6 +20,8 @@ import InputError from "../InputError";
 import InputLabel from "../InputLabel";
 
 const MINIMUM_SEARCH_SUGGESTION_CHARACTERS = 3;
+const AIRPORT_CODE_LENGTH = 3;
+const MILLISECONDS_TIME_OUT_FOCUS_INPUT = 10;
 
 const widthClass = (width: string) => {
   return {
@@ -60,18 +63,16 @@ function getDescriptionByValue(
   );
 }
 
-const focusOnInput = (action: string, inputId: string) => {
-  if (action === "clear" || action === "select-option") {
-    setTimeout(function () {
-      const inputElement = document.querySelector<HTMLInputElement>(
-        `#${inputId}`,
-      );
+const focusOnInput = (inputId: string) => {
+  setTimeout(function () {
+    const inputElement = document.querySelector<HTMLInputElement>(
+      `#${inputId}`,
+    );
 
-      if (inputElement) {
-        inputElement.focus();
-      }
-    }, 100);
-  }
+    if (inputElement) {
+      inputElement.focus();
+    }
+  }, MILLISECONDS_TIME_OUT_FOCUS_INPUT);
 };
 
 // When leave the input, it should focus on the exclusion button when it uses the tab, due problems for change the className in the component CustomValueContainer
@@ -93,6 +94,20 @@ const keyDownOnInput = (
       }
     });
   }
+};
+
+const getSortingAirportsByCode = (
+  filteredOptions: DataListOptions[],
+  inputCode: string,
+): DataListOptions[] => {
+  if (inputCode.length === AIRPORT_CODE_LENGTH) {
+    return matchSorter(filteredOptions, inputCode, {
+      keys: ["value", "subDescription"],
+      threshold: matchSorter.rankings.NO_MATCH,
+    });
+  }
+
+  return filteredOptions;
 };
 
 const AutoSuggestInput = ({
@@ -118,6 +133,26 @@ const AutoSuggestInput = ({
   const [jsAvailable, setJsAvailable] = useState(false);
   const [optionWasSelected, setOptionWasSelected] = useState(false);
   useEffect(() => setJsAvailable(true), []);
+  const [options, setOptions] = useState<DataListOptions[]>([]);
+
+  const onInputChange = (value: string, { action }: InputActionMeta) => {
+    if (action === "input-change") {
+      focusOnInput(inputId);
+
+      if (value.length < MINIMUM_SEARCH_SUGGESTION_CHARACTERS) {
+        setOptions([]);
+        return;
+      }
+      let filteredOptions = items.filter((item) => filterOption(item, value));
+
+      // In case is the airports list, sorting by the code
+      if (dataList === "airports") {
+        filteredOptions = getSortingAirportsByCode(filteredOptions, value);
+      }
+
+      setOptions(filteredOptions);
+    }
+  };
 
   useEffect(() => {
     keyDownOnInput(inputId, buttonExclusionRef);
@@ -158,28 +193,34 @@ const AutoSuggestInput = ({
         )}
         isSearchable
         isClearable
-        options={items}
+        options={options}
         aria-invalid={error !== undefined}
         aria-describedby={error && errorId}
         aria-errormessage={error && errorId}
         id={name}
         name={name}
+        onInputChange={onInputChange}
         inputId={inputId}
-        filterOption={filterOption}
+        filterOption={() => true}
         value={currentItemValue}
         placeholder={placeholder ?? ""}
         instanceId={name}
         formatOptionLabel={FormatOptionLabel}
-        onChange={(newValue, actionMeta) => {
+        onChange={(newValue, { action }) => {
           validate();
           // remix remove the focus on the input when clicks with the keyboard to clear the value, so we need to force the focus again
-          setOptionWasSelected(actionMeta.action === "select-option");
-          focusOnInput(actionMeta.action, inputId);
+          setOptionWasSelected(action === "select-option");
+          if (action === "clear" || action === "select-option") {
+            focusOnInput(inputId);
+          }
           setCurrentItemValue(newValue);
         }}
         onBlur={() => {
-          validate();
-          setOptionWasSelected(false);
+          // call the validation only if an option was selected
+          if (optionWasSelected) {
+            validate();
+            setOptionWasSelected(false);
+          }
         }}
         noOptionsMessage={({ inputValue }) =>
           inputValue.length > 2 ? noSuggestionMessage : null
