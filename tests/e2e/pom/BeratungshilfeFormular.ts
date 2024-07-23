1a7033cc5785b52849dc05782a021e49f5010da8
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-07-23 13:47:17 +0200

    refactor(e2e): simplify DL button click

--- tests/e2e/pom/BeratungshilfeFormular.ts
+++ tests/e2e/pom/BeratungshilfeFormular.ts
@@ -1,22 +1,6 @@
-import { expect } from "@playwright/test";
 import { Formular } from "tests/e2e/pom/Formular";
 
 export class BeratungshilfeFormular extends Formular {
   readonly url = "/beratungshilfe/antrag";
   readonly initialStep = "start/start";
-  readonly downloadButtonLocator = '[href="/beratungshilfe/antrag/pdf"]';
-
-  async hasAntragDownloadButton() {
-    await expect(this.page.locator(this.downloadButtonLocator)).toBeVisible();
-  }
-
-  async pressAntragDownloadButton() {
-    const [resp] = await Promise.all([
-      this.page.waitForResponse((resp) =>
-        resp.url().includes("/beratungshilfe/antrag/pdf"),
-      ),
-      this.page.locator(this.downloadButtonLocator).click(),
-    ]);
-    expect(resp.status()).toEqual(200);
-  }
 }
