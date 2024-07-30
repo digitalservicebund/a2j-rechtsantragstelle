7ede1a4c57dcdce9e4377badadded65cd6d86549
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-07-30 20:37:35 +0200

    fix(fluggastrechte): fix e2e

--- tests/e2e/fluggastrechte/fluggastrechte.vorabcheck.spec.ts
+++ tests/e2e/fluggastrechte/fluggastrechte.vorabcheck.spec.ts
@@ -3,7 +3,7 @@ import { startFluggastrechteVorabcheckAnnullierung } from "./vorabcheckAnnullier
 import { startFluggastrechteVorabcheckFunnelCheck } from "./vorabcheckFunnelCheck";
 import { startFluggastrechteVorabcheckNichtBefoerderung } from "./vorabcheckNichtBefoerderung";
 import { startFluggastrechteVorabcheckVerspaetung } from "./vorabcheckVerspaetung";
-import { CookieSettings } from "../pom/CookieSettings";
+import { startFluggastrechteVorabcheckVerspaetungNoJS } from "./vorabcheckVerspaetungNoJS";
 import { FluggastrechteVorabcheck } from "../pom/FluggastrechteVorabcheck";
 
 let vorabcheck: FluggastrechteVorabcheck;
@@ -14,11 +14,6 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("js enabled", () => {
-  test.beforeEach(async ({ page }) => {
-    const cookieSettings = new CookieSettings(page);
-    await cookieSettings.acceptCookieBanner();
-  });
-
   test("forwarded to intial step", async ({ page }) => {
     await expect(page).toHaveURL(
       new RegExp(`.+${vorabcheck.url}/${vorabcheck.initialStep}$`),
@@ -28,84 +23,32 @@ test.describe("js enabled", () => {
   test("fluggastrechte vorabcheck: Verspaetung can be traversed", async ({
     page,
   }) => {
-    startFluggastrechteVorabcheckVerspaetung(page, vorabcheck);
+    await startFluggastrechteVorabcheckVerspaetung(page, vorabcheck);
   });
 
   test("fluggastrechte vorabcheck: Nicht-Befoerderung can be traversed", async ({
     page,
   }) => {
-    startFluggastrechteVorabcheckNichtBefoerderung(page, vorabcheck);
+    await startFluggastrechteVorabcheckNichtBefoerderung(page, vorabcheck);
   });
 
   test("fluggastrechte vorabcheck: Annullierung can be traversed", async ({
     page,
   }) => {
-    startFluggastrechteVorabcheckAnnullierung(page, vorabcheck);
+    await startFluggastrechteVorabcheckAnnullierung(page, vorabcheck);
   });
 
   test("funnel: funnel check", async ({ page }) => {
-    startFluggastrechteVorabcheckFunnelCheck(page, vorabcheck);
+    await startFluggastrechteVorabcheckFunnelCheck(page, vorabcheck);
   });
 });
 
 test.describe("js disabled", () => {
   test.use({ javaScriptEnabled: false });
-  test("fluggastrechte vorabcheck can be traversed", () => {
-    test.describe("js disabled", () => {
-      test.use({ javaScriptEnabled: false });
 
-      test("fluggastrechte vorabcheck can be traversed", async ({ page }) => {
-        await page.getByRole("button").filter({ hasText: "Ablehnen" }).click();
-        await vorabcheck.goto();
-        await vorabcheck.clickNextWithoutJavaScript();
-
-        // fluggastrechte/vorabcheck/bereich
-        await vorabcheck.fillRadioPageNonJavascript("bereich", "verspaetet");
-
-        // fluggastrechte/vorabcheck/verspaetung
-        await vorabcheck.fillRadioPageNonJavascript("verspaetung", "yes");
-
-        // fluggastrechte/vorabcheck/gruende
-        await vorabcheck.fillRadioPageNonJavascript("gruende", "no");
-
-        // fluggastrechte/vorabcheck/verjaehrung
-        await vorabcheck.fillRadioPageNonJavascript("verjaehrung", "yes");
-
-        // fluggastrechte/vorabcheck/flughaefen
-        await vorabcheck.fillMultipleInputPageNonJavascript([
-          { field: "startAirport", value: "BER" },
-          { field: "endAirport", value: "FRA" },
-        ]);
-
-        // fluggastrechte/vorabcheck/fluggesellschaft
-        await vorabcheck.fillInputPageNonJavascript("fluggesellschaft", "LH");
-
-        // fluggastrechte/vorabcheck/checkin
-        await vorabcheck.fillRadioPageNonJavascript("checkin", "yes");
-
-        // fluggastrechte/vorabcheck/kostenlos
-        await vorabcheck.fillRadioPageNonJavascript("kostenlos", "no");
-
-        // fluggastrechte/vorabcheck/rabatt
-        await vorabcheck.fillRadioPageNonJavascript("rabatt", "no");
-
-        // fluggastrechte/vorabcheck/buchung
-        await vorabcheck.fillRadioPageNonJavascript("buchung", "yes");
-
-        // fluggastrechte/vorabcheck/abtretung
-        await vorabcheck.fillRadioPageNonJavascript("abtretung", "no");
-
-        // fluggastrechte/vorabcheck/entschaedigung
-        await vorabcheck.fillRadioPageNonJavascript("entschaedigung", "yes");
-
-        // fluggastrechte/vorabcheck/gericht
-        await vorabcheck.fillRadioPageNonJavascript("gericht", "no");
-
-        // fluggastrechte/vorabcheck/ergebnis/erfolg
-        await expect(page).toHaveURL(
-          new RegExp(`.+${vorabcheck.url}/ergebnis/erfolg$`),
-        );
-      });
-    });
+  test("fluggastrechte vorabcheck: Verspätung can be traversed", async ({
+    page,
+  }) => {
+    await startFluggastrechteVorabcheckVerspaetungNoJS(page, vorabcheck);
   });
 });
