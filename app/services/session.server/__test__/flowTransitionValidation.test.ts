b29e2b72d444db49ca31e222f6d6cba1ba2f18bd
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-08-01 09:59:08 +0200

    Add eligibility check for flow transition (#1033)
    * feat(fluggastrechte): enable flow transition under certain rules
    
    * feat(fluggastrechte): restructure e2e test
    
    since formular is depending on the vorabcheck, we need to restructure in a way that it leads almost no duplication
    
    * fix(fluggastrechte): fix e2e
    
    * feat(fluggastrechte): improve error handling
    
    * feat(fluggastrechte): fix logic that other flow affected
    
    * fix(fluggastrechte): simplify transition validator API
    
    * chore(fluggastrechte): use isEmpty lodash instead

---
+++ app/services/session.server/__test__/flowTransitionValidation.test.ts
@@ -0,0 +1,125 @@
+import type { FlowId } from "~/flows/flowIds";
+import type { Flow } from "~/flows/flows.server";
+import {
+  buildFlowController,
+  FlowController,
+} from "~/services/flow/server/buildFlowController";
+import { getSessionData } from "..";
+import {
+  FlowTransitionConfig,
+  validateFlowTransition,
+} from "../flowTransitionValidation.server";
+
+vi.mock("app/services/session.server/index", () => ({
+  getSessionData: vi.fn(),
+}));
+
+vi.mock("app/services/flow/server/buildFlowController", () => ({
+  buildFlowController: vi.fn(),
+}));
+
+type CmsSlug =
+  | "result-pages"
+  | "vorab-check-pages"
+  | "form-flow-pages"
+  | "pages"
+  | "translations";
+
+const createMockFlow = (cmsSlug: CmsSlug): Flow => ({
+  config: {},
+  guards: {},
+  cmsSlug,
+});
+
+describe("validateFlowTransition", () => {
+  const mockFlowId: FlowId = "/fluggastrechte/vorabcheck";
+  const mockCookieHeader = "mock-cookie";
+  const mockFlows: Record<FlowId, Flow> = {
+    "/beratungshilfe/vorabcheck": createMockFlow("form-flow-pages"),
+    "/beratungshilfe/antrag": createMockFlow("form-flow-pages"),
+    "/geld-einklagen/vorabcheck": createMockFlow("vorab-check-pages"),
+    "/geld-einklagen/formular": createMockFlow("form-flow-pages"),
+    "/fluggastrechte/vorabcheck": createMockFlow("vorab-check-pages"),
+    "/fluggastrechte/formular": createMockFlow("form-flow-pages"),
+  };
+  const mockController: FlowController = {
+    getMeta: vi.fn().mockReturnValue(undefined),
+    getRootMeta: vi.fn().mockReturnValue(undefined),
+    stepStates: vi.fn().mockReturnValue([]),
+    isDone: vi.fn().mockReturnValue(false),
+    getProgress: vi.fn().mockReturnValue({}),
+    isReachable: vi
+      .fn()
+      .mockImplementation((page: string) => page === "ergebnis/erfolg"),
+    getConfig: vi.fn().mockReturnValue({}),
+    isFinal: vi.fn().mockReturnValue(false),
+    getPrevious: vi.fn().mockReturnValue(undefined),
+    getNext: vi.fn().mockReturnValue(undefined),
+    getInitial: vi.fn().mockReturnValue("mock"),
+  };
+
+  it("should return eligibility as true if at least one eligible source page is reachable", async () => {
+    const config: FlowTransitionConfig = {
+      targetFlowId: mockFlowId,
+      sourceFlowId: mockFlowId,
+      eligibleSourcePages: ["ergebnis/erfolg-totally", "ergebnis/erfolg"],
+    };
+
+    vi.mocked(getSessionData).mockResolvedValueOnce({
+      userData: {},
+      debugId: "debugId",
+    });
+
+    vi.mocked(buildFlowController).mockReturnValue(mockController);
+
+    const result = await validateFlowTransition(
+      mockFlows,
+      mockFlowId,
+      mockCookieHeader,
+      config,
+    );
+
+    expect(result).toStrictEqual({
+      isEligible: true,
+    });
+  });
+
+  it("should return eligibility as false if none of the eligible source pages are reachable", async () => {
+    const config: FlowTransitionConfig = {
+      targetFlowId: mockFlowId,
+      sourceFlowId: mockFlowId,
+      eligibleSourcePages: ["page1", "page2"],
+    };
+
+    vi.mocked(getSessionData).mockResolvedValueOnce({
+      userData: {},
+      debugId: "debugId",
+    });
+
+    vi.mocked(buildFlowController).mockReturnValue(mockController);
+
+    const result = await validateFlowTransition(
+      mockFlows,
+      mockFlowId,
+      mockCookieHeader,
+      config,
+    );
+
+    expect(result).toStrictEqual({
+      isEligible: false,
+      redirectTo: "/fluggastrechte/vorabcheck",
+    });
+  });
+
+  it("should throw an error if eligibleSourcePages is an empty array", async () => {
+    const config: FlowTransitionConfig = {
+      targetFlowId: mockFlowId,
+      sourceFlowId: mockFlowId,
+      eligibleSourcePages: [],
+    };
+
+    await expect(
+      validateFlowTransition(mockFlows, mockFlowId, mockCookieHeader, config),
+    ).rejects.toThrow("This property should not be empty");
+  });
+});
