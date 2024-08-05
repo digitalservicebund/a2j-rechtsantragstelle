a97f4caebff26d645f8d62f34d74521891bf6a4c
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-08-05 14:51:22 +0200

    refactor(slug): remove slug from formFlow, Result and Vorabcheck page
    Co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

--- app/services/cms/models/HasStrapiSlug.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod";
-
-export const HasStrapiSlugSchema = z.object({ slug: z.string() });
--- app/services/cms/models/StrapiPage.ts
+++ app/services/cms/models/StrapiPage.ts
@@ -2,18 +2,17 @@ import { z } from "zod";
 import { HasOptionalStrapiIdSchema } from "./HasStrapiId";
 import { HasStrapiLocaleSchema } from "./HasStrapiLocale";
 import { HasStrapiMetaSchema } from "./HasStrapiMeta";
-import { HasStrapiSlugSchema } from "./HasStrapiSlug";
 import { HasStrapiTimestampsSchema } from "./HasStrapiTimestamps";
 import { StrapiContentComponentSchema } from "./StrapiContentComponent";
 
 export const StrapiPageSchema = z
   .object({
     content: z.array(StrapiContentComponentSchema),
+    slug: z.string(),
   })
   .merge(HasOptionalStrapiIdSchema)
   .merge(HasStrapiLocaleSchema)
   .merge(HasStrapiMetaSchema)
-  .merge(HasStrapiSlugSchema)
   .merge(HasStrapiTimestampsSchema);
 
 export type StrapiPage = z.infer<typeof StrapiPageSchema>;
