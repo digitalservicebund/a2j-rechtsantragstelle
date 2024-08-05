b6453157a97e6f499b8472878a17b865f72bb828
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-08-05 15:29:17 +0200

    refactor(featureFlags): add distinct FFs for relevant flows
    Co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

--- app/services/errorPages/throw404.ts
+++ app/services/errorPages/throw404.ts
@@ -8,14 +8,28 @@ export const throw404OnProduction = () => {
 
 export const throw404IfFeatureFlagEnabled = async (request: Request) => {
   const { POSTHOG_API_KEY, POSTHOG_API_HOST } = config();
+  const { url } = request;
+  const phCallerId = "backend";
   if (POSTHOG_API_KEY && config().ENVIRONMENT === "production") {
     const client = new PostHog(POSTHOG_API_KEY, { host: POSTHOG_API_HOST });
     if (
-      (request.url.includes("geld-einklagen") ||
-        request.url.includes("fluggastrechte/formular")) &&
-      [true, undefined].includes(
-        await client.isFeatureEnabled("hideOVFlow", "backend"),
-      ) // isFeatureEnabled() returns undefined if posthog is down, in which case we also want to throw
+      url.includes("/fluggastrechte/formular") &&
+      !(await client.isFeatureEnabled("showFluggastrechteFormular", phCallerId))
+    ) {
+      throw new Response(null, { status: 404 });
+    }
+
+    if (
+      (url.includes("/geld-einklagen/vorabcheck") ||
+        url.includes("/geld-einklagen/formular")) &&
+      !(await client.isFeatureEnabled("showGeldEinklagenFlow", phCallerId))
+    ) {
+      throw new Response(null, { status: 404 });
+    }
+
+    if (
+      url.includes("/prozesskostenhilfe") &&
+      !(await client.isFeatureEnabled("showProzesskostenhilfeFlow", phCallerId))
     ) {
       throw new Response(null, { status: 404 });
     }
