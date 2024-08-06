1b4707217860490ac06e817bc36efed6cea6e04d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-08-06 13:37:03 +0200

    feat(beratungshilfe): Remove "feature flag" from antrag

--- app/routes/beratungshilfe.antrag._index.tsx
+++ app/routes/beratungshilfe.antrag._index.tsx
@@ -1,9 +1,2 @@
-import { type LoaderFunctionArgs } from "@remix-run/node";
-import { throw404OnProduction } from "~/services/errorPages/throw404";
-import { loader as lastStepLoader } from "~/services/flow/server/lastStep";
+export { loader } from "~/services/flow/server/lastStep";
 export { FormFlowPage as default } from "~/routes/shared/components/FormFlowPage";
-
-export function loader(args: LoaderFunctionArgs) {
-  throw404OnProduction();
-  return lastStepLoader(args);
-}
