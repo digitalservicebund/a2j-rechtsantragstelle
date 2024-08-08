a3a262b1491cc3812fc93f6400137d35c02ad20d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-08-08 15:34:39 +0200

    fix(verifyUrl): refactor and use match

--- app/services/cms/verifyExternalLinks.ts
+++ app/services/cms/verifyExternalLinks.ts
@@ -24,6 +24,12 @@ const allowedWebsites = [
   "https://www.verbraucherzentrale.de",
   "https://www.zugang-zum-recht-projekte.de",
   "https://www.evz.de",
+  "https://www.justiz.nrw.de/",
+  "https://rechtohnestreit.de/",
+  "https://e-justice.europa.eu/",
+  "https://www.ausweisapp.bund.de/",
+  "https://id.bund.de/",
+  "https://ebo.bund.de/",
 ];
 
 const allowedEmails = [
@@ -47,23 +53,23 @@ function verifyExternalLinks(allowedList: Array<string>, regexPattern: RegExp) {
   const filePath = process.env.CONTENT_FILE_PATH ?? "./content.json";
   const content = fs.readFileSync(filePath, "utf-8");
 
-  const linksFromContent = regexPattern.exec(content);
+  const foundLinks = extractLinks(content, regexPattern);
 
-  if (linksFromContent) {
-    for (const link of linksFromContent) {
-      // skip this for now
-      if (link == "https") continue;
-
-      const rejectedUrl = !allowedList.some((allowedLinks) =>
-        link.includes(allowedLinks),
+  foundLinks.forEach((link) => {
+    if (isLinkRejected(link, allowedList)) {
+      throw new Error(
+        `${link} is not allowed. Please verify the link and add it to the allowed list`,
       );
-
-      if (rejectedUrl)
-        throw Error(
-          `${link} is not allowed. Please verify the link and add to the allowed list`,
-        );
     }
-  }
+  });
+}
+
+function extractLinks(content: string, regexPattern: RegExp) {
+  return content.match(regexPattern) || [];
+}
+
+function isLinkRejected(link: string, allowedList: Array<string>) {
+  return !allowedList.some((allowedLink) => link.includes(allowedLink));
 }
 
 if (process.argv[2] === "verifyWebsites")
