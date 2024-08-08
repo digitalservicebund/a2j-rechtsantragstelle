2de41aef0dd601b2bdf36d33948407bf8bd53b9c
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-08-08 12:11:04 +0200

    Update direct flight flow for the formular (#1057)
    * feat(fgr): update direct flight for the formular
    
    * fix(fgr): rename wrong const
    
    * fix(fgr): remove zwischenstopp from the eingabe section
    
    * fix(fgr): move testcase to its own space
    
    * fix(fgr): add validation for date
    
    * fix(fgr): simplify flow and rename flugdaten vars
    
    * fix(fgr): use dynamic date for playwright
    
    Co-authored-by: aaschlote <alex.schlote@digitalservice.bund.de>

---
+++ app/flows/fluggastrechteFormular/flugdaten/context.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { createDateSchema } from "~/services/validation/date";
+import { flightNumberSchema } from "~/services/validation/flightNumber";
+import { stringRequiredSchema } from "~/services/validation/stringRequired";
+import { timeSchema } from "~/services/validation/time";
+import {
+  customRequiredErrorMessage,
+  YesNoAnswer,
+} from "~/services/validation/YesNoAnswer";
+import { addYears, today } from "~/util/date";
+
+const fourYearsAgoSchema = createDateSchema({
+  earliest: () => addYears(today(), -4),
+  latest: () => today(),
+});
+
+export const fluggastrechteFlugdaten = {
+  zwischenstopps: YesNoAnswer,
+  direktFlugnummer: flightNumberSchema,
+  direktAbflugsDatum: fourYearsAgoSchema,
+  direktAbflugsZeit: timeSchema,
+  direktAnkunftsDatum: fourYearsAgoSchema,
+  direktAnkunftsZeit: timeSchema,
+  tatsaechlicherFlug: YesNoAnswer,
+  tatsaechlicherAnkunftsDatum: fourYearsAgoSchema,
+  tatsaechlicherAnkunftsZeit: timeSchema,
+  ersatzverbindungArt: z.enum(
+    ["flug", "etwasAnderes", "keineAnkunft"],
+    customRequiredErrorMessage,
+  ),
+  andereErsatzverbindungBeschreibung: stringRequiredSchema,
+  andereErsatzverbindungStartDatum: fourYearsAgoSchema,
+  andereErsatzverbindungStartZeit: timeSchema,
+  andereErsatzverbindungAnkunftsDatum: fourYearsAgoSchema,
+  andereErsatzverbindungAnkunftsZeit: timeSchema,
+  ersatzFlugnummer: flightNumberSchema,
+  ersatzFlugAnkunftsDatum: fourYearsAgoSchema,
+  ersatzFlugAnkunftsZeit: timeSchema,
+};
+
+const _contextObject = z.object(fluggastrechteFlugdaten).partial();
+export type FluggastrechteFlugdatenContext = z.infer<typeof _contextObject>;
---
+++ app/services/validation/__test__/flightNumber.test.ts
@@ -0,0 +1,48 @@
+// @vitest-environment node
+import type { SafeParseError } from "zod";
+import { flightNumberSchema } from "../flightNumber";
+
+describe("airport validation", () => {
+  describe("success cases", () => {
+    const cases = [
+      { input: " LH 20 ", expected: "LH20" },
+      { input: "Lh20", expected: "LH20" },
+      { input: " Lh20 ", expected: "LH20" },
+      { input: " lh20 ", expected: "LH20" },
+      { input: " LH20 ", expected: "LH20" },
+      { input: "lH 20 ", expected: "LH20" },
+    ];
+
+    test.each(cases)(
+      "given $input, returns $expected",
+      ({ input, expected }) => {
+        const actual = flightNumberSchema.safeParse(input);
+        expect(actual).toEqual({ data: expected, success: true });
+      },
+    );
+  });
+
+  describe("failing cases", () => {
+    const cases = [
+      { input: "", errorMessage: "invalid_flight_number_format" },
+      { input: "LH", errorMessage: "invalid_flight_number_format" },
+      { input: "123LH", errorMessage: "invalid_flight_number_format" },
+      { input: " :) ", errorMessage: "invalid_flight_number_format" },
+      { input: "foobar ", errorMessage: "invalid_flight_number_format" },
+      { input: "XXX", errorMessage: "invalid_flight_number_format" },
+      { input: "XXX12345", errorMessage: "invalid_flight_number_format" },
+      { input: "randomtext", errorMessage: "invalid_flight_number_format" },
+    ];
+
+    test.each(cases)(
+      "given $input, returns $errorMessage",
+      ({ input, errorMessage }) => {
+        const actual = flightNumberSchema.safeParse(input);
+        expect(actual.success).toBe(false);
+        expect(
+          (actual as SafeParseError<unknown>).error.issues[0].message,
+        ).toBe(errorMessage);
+      },
+    );
+  });
+});
