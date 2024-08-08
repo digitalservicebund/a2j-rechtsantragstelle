499db89fe43194a1ad4e6b3f325d1fdcd763477a
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-08-08 11:55:48 +0200

    feat(pkh flow): add partner, kinder and unterhaltszahlungen flow

--- app/flows/prozesskostenhilfeFormular/index.ts
+++ app/flows/prozesskostenhilfeFormular/index.ts
@@ -1,31 +1,65 @@
 import _ from "lodash";
+import abgabeFlow from "./abgabe/flow.json";
 import { ProzesskostenhilfeEigentumContext } from "./finanzielleAngaben/context";
 import finanzielleAngabenFlow from "./finanzielleAngaben/flow.json";
 import prozesskostenhilfeFormularFlow from "./flow.json";
+import { beratungshilfeAbgabeGuards } from "../beratungshilfeFormular/abgabe/guards";
+import { finanzielleAngabenArrayConfig } from "../beratungshilfeFormular/finanzielleAngaben/arrayConfiguration";
 import { eigentumZusammenfassungDone } from "../beratungshilfeFormular/finanzielleAngaben/eigentumZusammenfassungDone";
 import {
   eigentumDone,
   finanzielleAngabeGuards,
 } from "../beratungshilfeFormular/finanzielleAngaben/guards";
+import {
+  andereUnterhaltszahlungenDone,
+  kinderDone,
+  partnerDone,
+} from "../beratungshilfeFormular/finanzielleAngaben/navStates";
+import {
+  eigentumZusammenfassungShowWarnings,
+  geldAnlagenStrings,
+  getArrayIndexStrings,
+  getKinderStrings,
+} from "../beratungshilfeFormular/stringReplacements";
 
 export const prozesskostenhilfeFormular = {
   cmsSlug: "form-flow-pages",
   config: _.merge(prozesskostenhilfeFormularFlow, {
+    meta: {
+      arrays: finanzielleAngabenArrayConfig(
+        "/prozesskostenhilfe/antrag/finanzielle-angaben",
+      ),
+    },
     states: {
       start: { meta: { done: () => true } },
       "finanzielle-angaben": _.merge(finanzielleAngabenFlow, {
         states: {
+          partner: { meta: { done: partnerDone } },
+          kinder: { meta: { done: kinderDone } },
+          "andere-unterhaltszahlungen": {
+            meta: { done: andereUnterhaltszahlungenDone },
+          },
           eigentum: { meta: { done: eigentumDone } },
           "eigentum-zusammenfassung": {
             meta: { done: eigentumZusammenfassungDone },
           },
         },
       }),
+      abgabe: _.merge(abgabeFlow, {
+        meta: { done: () => false },
+      }),
     },
   }),
   guards: {
     ...finanzielleAngabeGuards,
+    ...beratungshilfeAbgabeGuards,
   },
+  stringReplacements: (context: ProzesskostenhilfeFormularContext) => ({
+    ...getKinderStrings(context),
+    ...getArrayIndexStrings(context),
+    ...eigentumZusammenfassungShowWarnings(context),
+    ...geldAnlagenStrings(context),
+  }),
 } as const;
 
 export type ProzesskostenhilfeFormularContext =
