8a7d58787cf2594da5d4d66f2b2f4035acf0fba3
aaschlote <alex.schlote@digitalservice.bund.de>
2024-08-09 15:59:21 +0200

    refactor: avoid clear menu when press enter

--- app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
+++ app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
@@ -3,7 +3,15 @@ import { DataListOptions } from "~/services/dataListOptions/getDataListOptions";
 import { INPUT_CHAR_LIMIT } from "~/services/validation/inputlimits";
 
 const CustomInput = (props: InputProps<DataListOptions, false>) => (
-  <components.Input {...props} maxLength={INPUT_CHAR_LIMIT} />
+  <components.Input
+    onKeyDown={(e) => {
+      if (e.key === "Enter" && !props.selectProps.menuIsOpen) {
+        e.preventDefault();
+      }
+    }}
+    {...props}
+    maxLength={INPUT_CHAR_LIMIT}
+  />
 );
 
 export default CustomInput;
