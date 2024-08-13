c73e8a336240298d815b81ededa34630789cf658
Frederike Ramin <9320199+frederike-ramin@users.noreply.github.com>
2024-08-13 16:25:38 +0200

    refactor(featureFlag): Use feature flags in every route, not catch-all (#1071)
    * refactor(featureFlag): Use feature flags in every route, not catch-all
    
    * refactor(featureFlag): Only evaluate on production
    
    * fix(geld-einklagen): Use correct loader for meta page
    
    * refactor(feature-flag): Rename enabled -> disabled
    
    * chore: rename phCallerId -> posthogDistinctId
    
    * test(featureFlag): Add unit tests
    
    * test(throw404): Add unit tests

--- app/routes/geld-einklagen.vorabcheck.ergebnis.$.tsx
+++ app/routes/geld-einklagen.vorabcheck.ergebnis.$.tsx
@@ -1,2 +1,10 @@
-export { loader } from "~/routes/shared/result.server";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { loader as stepLoader } from "~/routes/shared/result.server";
+import { throw404IfFeatureFlagDisabled } from "~/services/errorPages/throw404";
+
 export { DefaultResultPage as default } from "../components/resultPage/DefaultResultPage";
+
+export const loader = async (opts: LoaderFunctionArgs) => {
+  await throw404IfFeatureFlagDisabled("showGeldEinklagenFlow");
+  return await stepLoader(opts);
+};
