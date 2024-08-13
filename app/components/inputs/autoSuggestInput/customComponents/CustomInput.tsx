cc7b4630e27f18a2df8f08eceb59564a78fa9527
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-08-13 10:52:42 +0200

    feat(autosuggest): add comment

--- app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
+++ app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
@@ -5,6 +5,7 @@ import { INPUT_CHAR_LIMIT } from "~/services/validation/inputlimits";
 const CustomInput = (props: InputProps<DataListOptions, false>) => (
   <components.Input
     // avoid to clear the previous auto suggestion input when press enter
+    // this cannot be tested, thus manually tested with device(s)
     onKeyDown={(e) => {
       if (e.key === "Enter" && !props.selectProps.menuIsOpen) {
         e.preventDefault();
