83718f52f863de2a2d5440594fa4a1419d29be8b
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-08-19 17:36:29 +0200

    feat(no-cache): enable no-store (no caching) preview and staging

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -102,6 +102,11 @@ function handleBrowserRequest(
       cspHeader({ nonce: cspNonce, environment: config().ENVIRONMENT }),
     );
 
+    // Obverse for 1-2 weeks if the no cache at all would affect our application
+    if (config().ENVIRONMENT !== "production") {
+      responseHeaders.set("Cache-Control", "no-store, must-revalidate");
+    }
+
     const { pipe, abort } = renderToPipeableStream(
       <NonceContext.Provider value={cspNonce}>
         <RemixServer context={remixContext} url={request.url} />
