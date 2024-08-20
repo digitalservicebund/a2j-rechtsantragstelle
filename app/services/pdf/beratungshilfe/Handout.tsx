495f2178cbbf2394c82da9217de5ad17e04835a9
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-08-20 13:23:40 +0200

    dynamically generate Hinweisblatt content

--- app/services/pdf/beratungshilfe/Handout.tsx
+++ app/services/pdf/beratungshilfe/Handout.tsx
@@ -3,6 +3,7 @@ import {
   beratungshilfeFormular,
   type BeratungshilfeFormularContext,
 } from "~/flows/beratungshilfeFormular";
+import { beratungshilfeAbgabe } from "~/flows/beratungshilfeFormular/abgabe/context";
 import { HandoutRow } from "../attachment/HandoutRow";
 import { PdfFooter } from "../attachment/PdfFooter";
 import { styles } from "../attachment/styles";
@@ -44,6 +45,47 @@ const documents = {
     "Unterlagen, die Ihre Ausgaben belegen, wenn diese nicht auf den Kontoauszügen zu sehen sind",
 } as const satisfies Partial<Record<ReplacementKey, string>>;
 
+type Step = {
+  title: string;
+  value?: string | ((validAmtsgericht: boolean) => string);
+};
+
+const dynamicSteps: { [key: string]: Step[] } = {
+  [beratungshilfeAbgabe.abgabeArt.Enum.ausdrucken]: [
+    { title: "Antrag ausdrucken" },
+    {
+      title: "Antrag unterschreiben",
+      value:
+        "Unterschreiben Sie den fertigen Antrag auf der letzten Seite im Feld “Unterschrift des Antragstellers/der Antragstellerin”",
+    },
+    {
+      title: "Benötigte Dokumente kopieren",
+      value: "Diese Dokumente müssen Sie zusammen mit Ihrem Antrag abgeben:",
+    },
+    {
+      title: "Antrag abgeben",
+      value: (validAmtsgericht) =>
+        `Sie können den Antrag direkt im Amtsgericht abgeben oder per Post schicken. ${validAmtsgericht ? "Die Adresse des zuständigen Amtsgericht finden Sie auf der ersten Seite des Antrags im Adressfeld." : "Ihr zuständiges Amtsgericht finden Sie über den Service “Amtsgericht finden” auf https://service.justiz.de/beratungshilfe."}`,
+    },
+  ],
+  [beratungshilfeAbgabe.abgabeArt.Enum.online]: [
+    {
+      title: "Antrag prüfen und speichern",
+      value:
+        "Sie müssen den Antrag nicht unterschreiben, da Sie sich später mit Ihrem BundID Konto anmelden.",
+    },
+    {
+      title: "Benötigte Dokumente scannen",
+      value: "Diese Dokumente müssen Sie zusammen mit Ihrem Antrag abgeben:",
+    },
+    {
+      title: "Antrag über das Portal Mein Justizpostfach versenden",
+      value:
+        "Melden Sie sich mit Ihrem BundID Konto bei Mein Justizpostfach an. Dort können Sie alle Unterlagen hochladen und versenden. Mein Justizpostfach finden Sie hier: https://ebo.bund.de",
+    },
+  ],
+};
+
 const Handout = (userdata: BeratungshilfeFormularContext, footer: string) => {
   const conditions = stringReplacements(userdata);
 
@@ -67,40 +109,46 @@ const Handout = (userdata: BeratungshilfeFormularContext, footer: string) => {
           }
           <Text style={styles.h1}>Ihre nächsten Schritte</Text>
           <Text style={styles.h2}>
-            So schicken Sie den Antrag ins Amtsgericht
+            {userdata.abgabeArt === "online"
+              ? "So stellen Sie den Antrag online"
+              : "So schicken Sie den Antrag ins Amtsgericht"}
           </Text>
-          <Text style={styles.h3}>1. Antrag ausdrucken</Text>
-          <Text style={styles.h3}>2. Antrag unterschreiben</Text>
-          <Text style={styles.sectionIndented}>
-            Unterschreiben Sie den fertigen Antrag auf der letzten Seite im Feld
-            “Unterschrift des Antragstellers/der Antragstellerin”
-          </Text>
-          <Text style={styles.h3}>3. Benötigte Dokumente kopieren</Text>
-          <Text style={styles.sectionIndented}>
-            Diese Dokumente müssen Sie zusammen mit Ihrem Antrag abgeben:
-          </Text>
-          <View
-            style={{
-              border: "1px solid black",
-              marginHorizontal: "13px",
-              marginVertical: "5px",
-              paddingHorizontal: "16px",
-              paddingVertical: "8px",
-              gap: "8px",
-            }}
-          >
-            {relevantDocuments.map((doc) => (
-              <HandoutRow key={doc} text={doc} />
-            ))}
-          </View>
-          <Text style={styles.h3}>4. Antrag abgeben</Text>
-          <Text style={styles.sectionIndented}>
-            Sie können den Antrag direkt im Amtsgericht abgeben oder per Post
-            schicken.{" "}
-            {conditions.courtName
-              ? "Die Adresse des zuständigen Amtsgericht finden Sie auf der ersten Seite des Antrags im Adressfeld."
-              : "Ihr zuständiges Amtsgericht finden Sie über den Service 'Amtsgericht finden' auf https://service.justiz.de/beratungshilfe."}
-          </Text>
+          {dynamicSteps[userdata.abgabeArt ?? "ausdrucken"].map(
+            (step, idx, stepArr) => {
+              return (
+                <View key={`step-${idx}`}>
+                  {/* Always print the required documents list second-to-last */}
+                  {idx === stepArr.length - 1 && (
+                    <View
+                      style={{
+                        border: "1px solid black",
+                        marginHorizontal: "13px",
+                        marginVertical: "5px",
+                        paddingHorizontal: "16px",
+                        paddingVertical: "8px",
+                        gap: "8px",
+                      }}
+                    >
+                      {relevantDocuments.map((doc) => (
+                        <HandoutRow key={doc} text={doc} />
+                      ))}
+                    </View>
+                  )}
+                  <Text style={styles.h3}>
+                    {idx + 1}. {step.title}
+                  </Text>
+                  {step.value && (
+                    <Text style={styles.sectionIndented}>
+                      {typeof step.value === "string"
+                        ? step.value
+                        : // Print the conditional Amtsgericht message
+                          step.value(Boolean(conditions.courtName))}
+                    </Text>
+                  )}
+                </View>
+              );
+            },
+          )}
         </View>
         <PdfFooter footer={footer} />
       </Page>
