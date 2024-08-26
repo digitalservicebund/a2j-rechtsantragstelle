8bc377c89e1c6d577646a08e50b5093ae6e82915
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-08-26 18:44:04 +0200

    fix: also prune array data

--- app/routes/shared/formular.server.ts
+++ app/routes/shared/formular.server.ts
@@ -107,7 +107,7 @@ export const loader = async ({
   const arraySummaryData = getSummaryData(
     arrayCategories,
     arrayConfigurations,
-    userData,
+    userDataWithPageData,
   );
 
   const stringTranslations = arrayConfigurations
