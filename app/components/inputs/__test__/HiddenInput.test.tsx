0e7e3d5416311680bd88e1bb488b617880c2f04b
aaschlote <alex.schlote@digitalservice.bund.de>
2024-08-28 10:50:36 +0200

    test: add tests for the new component

---
+++ app/components/inputs/__test__/HiddenInput.test.tsx
@@ -0,0 +1,35 @@
+import { createRemixStub } from "@remix-run/testing";
+import { render } from "@testing-library/react";
+import * as remixValidatedForm from "remix-validated-form";
+import HiddenInput from "../HiddenInput";
+
+vi.mock("remix-validated-form", () => ({
+  useField: vi.fn(),
+}));
+
+beforeEach(() => {
+  vi.spyOn(remixValidatedForm, "useField").mockReturnValue({
+    getInputProps: vi.fn().mockReturnValue({
+      id: "selectId",
+    }),
+    clearError: vi.fn(),
+    validate: vi.fn(),
+    touched: false,
+    setTouched: vi.fn(),
+  });
+});
+
+describe("HiddenInput", () => {
+  it("should render an input hidden", () => {
+    const RemixStub = createRemixStub([
+      {
+        path: "",
+        Component: () => <HiddenInput name="hiddenInput" formId="formId" />,
+      },
+    ]);
+
+    const { getByRole } = render(<RemixStub />);
+
+    expect(getByRole("textbox", { hidden: true })).toBeInTheDocument();
+  });
+});
