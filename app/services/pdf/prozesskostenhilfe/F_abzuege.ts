0851939a1e2378e86c53da4e2a57eb96d9c07829
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-09-02 18:51:41 +0200

    feat(pkh pdf): finish abzüge versicherungen

--- app/services/pdf/prozesskostenhilfe/F_abzuege.ts
+++ app/services/pdf/prozesskostenhilfe/F_abzuege.ts
@@ -1,14 +1,53 @@
+import type { ProzesskostenhilfeFormularContext } from "~/flows/prozesskostenhilfeFormular";
 import type { PkhPdfFillFunction } from "./fillOutFunction";
+import { SEE_IN_ATTACHMENT_DESCRIPTION } from "../beratungshilfe/sections/E_unterhalt/E_unterhalt";
 
-export const fillAbzuege: PkhPdfFillFunction = ({ userData, pdfValues }) => {
-  if (userData.hasAusgaben !== "yes") return { pdfValues }; // TODO: remove after pruning
+const versicherungMapping = {
+  haftpflichtversicherung: "Haftpflichtversicherung",
+  hausratsversicherung: "Hausratsversicherung",
+  kfzVersicherung: "KFZ-Versicherung",
+  pivateKrankenzusatzversicherung: "Pivate Krankenzusatzversicherung",
+  unfallversicherung: "Unfallversicherung",
+  sonstige: "Sonstige",
+} as const;
 
-  pdfValues.sonstigeVersicherungen.value = userData.versicherungen
-    ?.map(
-      (versicherung) =>
-        `${versicherung.art !== "sonstige" ? versicherung.art : versicherung.sonstigeArt}: ${versicherung.beitrag} €`,
-    )
-    .join("\n");
+function mapVersicherungsArt(
+  versicherung: NonNullable<
+    ProzesskostenhilfeFormularContext["versicherungen"]
+  >[number],
+) {
+  return versicherung.art === "sonstige" && versicherung.sonstigeArt
+    ? versicherung.sonstigeArt
+    : versicherungMapping[versicherung.art];
+}
+
+export const fillAbzuege: PkhPdfFillFunction = ({
+  userData: { versicherungen, hasAusgaben },
+  pdfValues,
+}) => {
+  if (hasAusgaben !== "yes" || !versicherungen || versicherungen.length === 0)
+    return { pdfValues };
+
+  if (versicherungen.length > 1) {
+    pdfValues.sonstigeVersicherungen.value = SEE_IN_ATTACHMENT_DESCRIPTION;
+
+    return {
+      pdfValues,
+      attachment: [
+        { title: "Abzüge", level: "h2" },
+        { title: "Versicherungen", level: "h3" },
+        ...versicherungen.map((versicherung) => ({
+          title: mapVersicherungsArt(versicherung),
+          text: `${versicherung.beitrag}€ / Monat`,
+        })),
+      ],
+    };
+  }
+
+  pdfValues.sonstigeVersicherungen.value = mapVersicherungsArt(
+    versicherungen[0],
+  );
+  pdfValues.monatlicheAbzuegeinEuro3.value = versicherungen[0].beitrag;
 
   return { pdfValues };
 };
