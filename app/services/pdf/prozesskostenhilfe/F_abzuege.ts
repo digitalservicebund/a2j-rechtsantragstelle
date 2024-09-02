50b7181e9ba72d03d5302a344ae983d5046602fd
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-09-02 17:03:16 +0200

    revert abzuege function to a modifying function, as immutability is now handled by the reducer

--- app/services/pdf/prozesskostenhilfe/F_abzuege.ts
+++ app/services/pdf/prozesskostenhilfe/F_abzuege.ts
@@ -3,18 +3,12 @@ import type { PkhPdfFillFunction } from "./fillOutFunction";
 export const fillAbzuege: PkhPdfFillFunction = ({ userData, pdfValues }) => {
   if (userData.hasAusgaben !== "yes") return { pdfValues }; // TODO: remove after pruning
 
-  return {
-    pdfValues: {
-      ...pdfValues,
-      sonstigeVersicherungen: {
-        ...pdfValues.sonstigeVersicherungen,
-        value: userData.versicherungen
-          ?.map(
-            (versicherung) =>
-              `${versicherung.art !== "sonstige" ? versicherung.art : versicherung.sonstigeArt}: ${versicherung.beitrag} €`,
-          )
-          .join("\n"),
-      },
-    },
-  };
+  pdfValues.sonstigeVersicherungen.value = userData.versicherungen
+    ?.map(
+      (versicherung) =>
+        `${versicherung.art !== "sonstige" ? versicherung.art : versicherung.sonstigeArt}: ${versicherung.beitrag} €`,
+    )
+    .join("\n");
+
+  return { pdfValues };
 };
