f0907c1bcb683d661d83ea5bc5d82127f0719801
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-09-06 15:29:44 +0200

    feat(pdf-service): add pruner to pdf service

--- app/routes/shared/pdfDownloadLoader.ts
+++ app/routes/shared/pdfDownloadLoader.ts
@@ -2,6 +2,7 @@ import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import _ from "lodash";
 import type { Context } from "~/flows/contexts";
 import { parsePathname, type FlowId } from "~/flows/flowIds";
+import { pruneIrrelevantData } from "~/services/flow/pruner";
 import { beratungshilfePdfFromUserdata } from "~/services/pdf/beratungshilfe";
 import { prozesskostenhilfePdfFromUserdata } from "~/services/pdf/prozesskostenhilfe";
 import { getSessionData } from "~/services/session.server";
@@ -26,16 +27,17 @@ export async function pdfDownloadLoader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get("Cookie");
   const { userData } = await getSessionData(flowId, cookieHeader);
   if (_.isEmpty(userData)) return redirect(flowId);
+  const prunedUserData = await pruneIrrelevantData(userData, flowId);
   if (!(flowId in pdfConfigs))
     return new Response(`No pdf config for flowId: ${flowId}`, { status: 501 });
 
   const { pdfFunction, filenameFunction } =
     pdfConfigs[flowId as keyof typeof pdfConfigs];
 
-  return new Response(await (await pdfFunction(userData)).save(), {
+  return new Response(await (await pdfFunction(prunedUserData)).save(), {
     headers: {
       "Content-Type": "application/pdf",
-      "Content-Disposition": `inline; filename=${filenameFunction(userData)}`,
+      "Content-Disposition": `inline; filename=${filenameFunction(prunedUserData)}`,
     },
   });
 }
