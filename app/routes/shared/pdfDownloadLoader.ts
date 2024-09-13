3f1c92a3d77e5417a58f8966bd92c381bc1ce242
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-09-13 14:21:18 +0200

    refactor(pdfDownloadLoader): simplify

--- app/routes/shared/pdfDownloadLoader.ts
+++ app/routes/shared/pdfDownloadLoader.ts
@@ -37,19 +37,18 @@ export function createHeaders(filename: string) {
 export async function pdfDownloadLoader({ request }: LoaderFunctionArgs) {
   const { pathname } = new URL(request.url);
   const { flowId } = parsePathname(pathname);
-  const cookieHeader = request.headers.get("Cookie");
-  const { userData } = await getSessionData(flowId, cookieHeader);
-  if (_.isEmpty(userData)) return redirect(flowId);
-  const prunedUserData = await pruneIrrelevantData(userData, flowId);
   if (!(flowId in pdfConfigs))
     return new Response(`No pdf config for flowId: ${flowId}`, { status: 501 });
-
   const { pdfFunction, filenameFunction } =
     pdfConfigs[flowId as keyof typeof pdfConfigs];
 
-  const filename = filenameFunction(userData);
+  const userData = await pruneIrrelevantData(
+    (await getSessionData(flowId, request.headers.get("Cookie"))).userData,
+    flowId,
+  );
+  if (_.isEmpty(userData)) return redirect(flowId);
 
   return new Response(await (await pdfFunction(userData)).save(), {
-    headers: createHeaders(filename),
+    headers: createHeaders(filenameFunction(userData)),
   });
 }
