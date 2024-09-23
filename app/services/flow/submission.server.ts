6f1573e189d0f82390b171fc8761f54cf539e450
Florian <florian.zechmeister@digitalservice.bund.de>
2024-09-23 13:54:56 +0200

    feat(ERV): add typing for `submitRequest`s

--- app/services/flow/submission.server.ts
+++ app/services/flow/submission.server.ts
@@ -2,16 +2,22 @@ import axios from "axios";
 import { configDotenv } from "dotenv";
 import { config } from "../env/env.server";
 
+type SubmitRequest = {
+  message: string;
+};
+
 export async function submit() {
   configDotenv();
   const { FIT_CONNECT_ADAPTER_ENDPOINT } = config();
 
+  const data: SubmitRequest = {
+    message: "hello World",
+  };
+
   return (
     await axios.post(
       `${FIT_CONNECT_ADAPTER_ENDPOINT}/api/sender/submit`,
-      {
-        message: "hello World",
-      },
+      data,
       { headers: { "Content-Type": "application/json" } },
     )
   ).data;
