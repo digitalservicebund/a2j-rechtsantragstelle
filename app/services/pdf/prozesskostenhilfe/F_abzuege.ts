2223fe82fcfd71c92ac8f48d567fe3667609b6ab
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-09-24 13:17:02 +0200

    split fillAbzuege into self/partner subfunctions, add some unit tests

--- app/services/pdf/prozesskostenhilfe/F_abzuege.ts
+++ app/services/pdf/prozesskostenhilfe/F_abzuege.ts
@@ -1,4 +1,5 @@
 import type { ProzesskostenhilfeFormularContext } from "~/flows/prozesskostenhilfeFormular";
+import { pdfFillReducer } from "~/services/pdf/fillOutFunction";
 import { getTotalMonthlyFinancialEntries } from "~/services/pdf/util";
 import type { PkhPdfFillFunction } from ".";
 import { zahlungsfrequenzMapping } from "./E_bruttoEinnahmen";
@@ -25,6 +26,21 @@ export function mapVersicherungsArt(versicherung: Versicherung) {
 }
 
 export const fillAbzuege: PkhPdfFillFunction = ({ userData, pdfValues }) => {
+  const { pdfValues: filledValues, attachment } = pdfFillReducer({
+    userData,
+    pdfParams: pdfValues,
+    fillFunctions: [fillSelfAbzuege, fillPartnerAbzuege],
+  });
+  return {
+    pdfValues: filledValues,
+    attachment,
+  };
+};
+
+export const fillSelfAbzuege: PkhPdfFillFunction = ({
+  userData,
+  pdfValues,
+}) => {
   if (userData.selbststaendigAbzuege) {
     pdfValues.monatlicheAbzuegeinEuro1.value = `${userData.selbststaendigAbzuege} €`;
     pdfValues.steuernSolidaritaetszuschlag1.value = "Abzüge zusammengerechnet";
@@ -84,6 +100,69 @@ export const fillAbzuege: PkhPdfFillFunction = ({ userData, pdfValues }) => {
       text: `Durchschnittliche Gesamtkosten pro Monat: ${averageMonthlyExpenses} €`,
     });
     userData.arbeitsausgaben!.forEach((arbeitsausgabe) => {
+      attachment.push({
+        title: arbeitsausgabe.beschreibung,
+        text: `${arbeitsausgabe.betrag} € (${zahlungsfrequenzMapping[arbeitsausgabe.zahlungsfrequenz]})`,
+      });
+    });
+  }
+
+  return { pdfValues, attachment };
+};
+
+export const fillPartnerAbzuege: PkhPdfFillFunction = ({
+  userData,
+  pdfValues,
+}) => {
+  if (
+    userData.partnerschaft !== "yes" ||
+    userData.partnerEinkommen === "no" ||
+    userData["partner-staatlicheLeistungen"] === "grundsicherung" ||
+    userData["partner-staatlicheLeistungen"] === "asylbewerberleistungen"
+  ) {
+    return { pdfValues };
+  }
+
+  if (userData["partner-selbststaendigAbzuege"]) {
+    pdfValues.monatlicheAbzuegeinEuro6.value = `${userData["partner-selbststaendigAbzuege"]} €`;
+    pdfValues.steuernSolidaritaetszuschlag2.value = "Abzüge zusammengerechnet";
+  }
+
+  if (userData["partner-arbeitsweg"] === "publicTransport") {
+    pdfValues.fahrtkostenEhegatte.value = "ÖPNV";
+    pdfValues.monatlicheAbzuegeinEuro9.value = `${userData["partner-monatlicheOPNVKosten"]} €`;
+  } else if (userData["partner-arbeitsweg"] === "privateVehicle") {
+    pdfValues.fahrtkostenEhegatte.value = "KFZ";
+    pdfValues.monatlicheAbzuegeinEuro9.value = `${userData["partner-arbeitsplatzEntfernung"]}km`;
+  }
+
+  const attachment: AttachmentEntries = [];
+  const arbeitsausgabenNeedsAttachment =
+    userData["partner-arbeitsausgaben"] &&
+    userData["partner-arbeitsausgaben"].length > 1;
+
+  if (arbeitsausgabenNeedsAttachment) {
+    attachment.push({ title: "F Abzüge", level: "h2" });
+  }
+
+  if (userData["partner-arbeitsausgaben"]?.length === 1) {
+    const { beschreibung, betrag } = userData["partner-arbeitsausgaben"][0];
+    pdfValues.sonstigewerbungskostenEhegatte.value = beschreibung;
+    pdfValues.monatlicheAbzuegeinEuro10.value = `${betrag} €`;
+  } else if (arbeitsausgabenNeedsAttachment) {
+    pdfValues.sonstigewerbungskostenEhegatte.value =
+      SEE_IN_ATTACHMENT_DESCRIPTION;
+
+    const averageMonthlyExpenses = getTotalMonthlyFinancialEntries(
+      userData["partner-arbeitsausgaben"]!,
+    );
+    pdfValues.monatlicheAbzuegeinEuro10.value = `${averageMonthlyExpenses} €`;
+    attachment.push({
+      title: "Sonstige Werbungskosten/Betriebsausgaben - Partner:in",
+      level: "h3",
+      text: `Durchschnittliche Gesamtkosten pro Monat: ${averageMonthlyExpenses} €`,
+    });
+    userData["partner-arbeitsausgaben"]!.forEach((arbeitsausgabe) => {
       attachment.push({
         title: arbeitsausgabe.beschreibung,
         text: `${arbeitsausgabe.betrag} € (${zahlungsfrequenzMapping[arbeitsausgabe.zahlungsfrequenz]})`,
