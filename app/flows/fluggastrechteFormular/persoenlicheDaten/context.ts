45abed9806283604d2548abe5c2d2bb263e8a7d3
judithmh <40442827+judithmh@users.noreply.github.com>
2024-10-08 15:53:08 +0200

    Feat/add payment details plaintiff (#1267)
    * feat/add-payment-details-plaintiff: add iban and kontoinhaber fields to fluggsatrechte persoenliche-daten
    
    * feat/add-payment-details-plaintiff: remove double key in package.json
    
    * feat/add-payment-details-plaintiff: removed comments and toUpperCase for iban validation
    
    * feat/add-payment-details-plaintiff: add code improvements and more tests

--- app/flows/fluggastrechteFormular/persoenlicheDaten/context.ts
+++ app/flows/fluggastrechteFormular/persoenlicheDaten/context.ts
@@ -1,11 +1,20 @@
 import { z } from "zod";
 import { persoenlicheDaten } from "~/flows/shared/persoenlicheDaten/context";
 import { pageDataSchema } from "~/services/flow/pageDataSchema";
+import { ibanSchema } from "~/services/validation/iban";
+import { optionalOrSchema } from "~/services/validation/optionalOrSchema";
+import { stringOptionalSchema } from "~/services/validation/stringOptional";
 import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
+
+export const paymentDetailsSchema = {
+  iban: optionalOrSchema(ibanSchema),
+  kontoinhaber: stringOptionalSchema,
+};
 
 export const fluggastrechtePersoenlichDaten = {
   ...persoenlicheDaten,
   isWeiterePersonen: YesNoAnswer,
+  ...paymentDetailsSchema,
   weiterePersonen: z.array(
     z
       .object({
