ac7fcadf42b5e6663418b5334b97b4257435241b
judithmh <40442827+judithmh@users.noreply.github.com>
2024-10-08 14:43:19 +0200

    feat/add-payment-details-plaintiff: add code improvements and more tests

--- app/services/validation/iban.ts
+++ app/services/validation/iban.ts
@@ -3,14 +3,7 @@ import { z } from "zod";
 
 export const ibanSchema = z
   .string()
-  .transform((ibanInput) => {
-    return ibanInput.split(" ").join("");
-  })
-  .refine(
-    (ibanInput) => {
-      return isValidIBAN(ibanInput);
-    },
-    {
-      message: "invalid_iban_format",
-    },
-  );
+  .transform((ibanInput) => ibanInput.replaceAll(" ", ""))
+  .refine(isValidIBAN, {
+    message: "invalid_iban_format",
+  });
