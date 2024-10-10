e7c7ea0c11b05218782a2f04c323e00787f29619
aaschlote <alex.schlote@digitalservice.bund.de>
2024-10-10 12:03:39 +0200

    refactor: return a result instead of throw an exception

--- app/services/security/csrf/validatedSession.server.ts
+++ app/services/security/csrf/validatedSession.server.ts
@@ -1,20 +1,36 @@
 import type { Session } from "@remix-run/node";
+import { Result } from "true-myth";
 import { mainSessionFromCookieHeader } from "~/services/session.server";
 import { CSRFKey } from "./csrfKey";
 import { getCSRFFromSession } from "./getCSRFFromSession.server";
 
-async function validateCSRFToken(request: Request, session: Session) {
+export const ERROR_MESSAGE_TOKEN_FORM = "Form: CSRF Token not included.";
+export const ERROR_MESSAGE_TOKEN_SESSION = "Session: CSRF Token not included.";
+export const ERROR_MESSAGE_TOKEN_SESSION_NOT_MATCH =
+  "CSRF tokens between form and session do not match.";
+
+async function validateCSRFToken(
+  request: Request,
+  session: Session,
+): Promise<Result<string, string>> {
   const formData = await request.clone().formData();
   const csrfTokenForm = formData.get(CSRFKey) as string | null;
+
+  if (!csrfTokenForm) return Result.err(ERROR_MESSAGE_TOKEN_FORM);
+
   const csrfTokensSession = getCSRFFromSession(session);
-  if (!csrfTokensSession) throw new Error("Session: CSRF Token not included.");
-  if (!csrfTokenForm) throw new Error("Form: CSRF Token not included.");
+  if (!csrfTokensSession) return Result.err(ERROR_MESSAGE_TOKEN_SESSION);
+
   if (!csrfTokensSession.includes(csrfTokenForm))
-    throw new Error(`CSRF tokens between form and session do not match.`);
+    return Result.err(ERROR_MESSAGE_TOKEN_SESSION_NOT_MATCH);
+
+  return Result.ok("OK");
 }
 
-export async function validatedSession(request: Request) {
+export async function validatedSession(
+  request: Request,
+): Promise<Result<string, string>> {
   const cookieHeader = request.headers.get("Cookie");
   const session = await mainSessionFromCookieHeader(cookieHeader);
-  await validateCSRFToken(request, session);
+  return await validateCSRFToken(request, session);
 }
