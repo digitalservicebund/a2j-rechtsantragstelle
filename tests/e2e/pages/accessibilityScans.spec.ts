5e6d63f24a5521cec9138472f9859d5fe14fd02f
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-10-11 16:35:03 +0200

    refactor(a11y checks): share & extend URLs

--- tests/e2e/pages/accessibilityScans.spec.ts
+++ tests/e2e/pages/accessibilityScans.spec.ts
@@ -1,17 +1,10 @@
 import { test } from "@playwright/test";
+import { footerLinks, headerLinks, pageUrls } from "./urlsToCheck";
 import { expectPageToBeAccessible } from "../util/expectPageToBeAccessible";
-const urlsToCheck = [
-  "/beratungshilfe",
-  "/datenschutz",
-  "/geld-einklagen",
-  "/impressum",
-  "/cookie-einstellungen",
-  "/barrierefreiheit",
-];
 
 test.describe("Accessibility checks", () => {
-  urlsToCheck.forEach((url) => {
-    test(`${url} a11y check`, async ({ page }) => {
+  [...pageUrls, ...headerLinks, ...footerLinks].forEach((url) => {
+    test(url, async ({ page }) => {
       await page.goto(url);
       await expectPageToBeAccessible({ page });
     });
