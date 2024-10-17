1bec847edbf3b1a7cc9c5c21b295c0226ba4ce39
aaschlote <alex.schlote@digitalservice.bund.de>
2024-10-17 15:00:57 +0200

    refactor: get apiId based the flowId

--- app/services/cms/fetchAllFormFields.ts
+++ app/services/cms/fetchAllFormFields.ts
@@ -3,10 +3,23 @@ import { getStrapiEntry } from "./getStrapiEntry";
 
 export type FormFieldsMap = Record<string, string[]>;
 
+const getApiIdByFlowId = (
+  flowId: FlowId,
+): "form-flow-pages" | "vorab-check-pages" => {
+  switch (flowId) {
+    case "/beratungshilfe/vorabcheck":
+    case "/fluggastrechte/vorabcheck":
+    case "/geld-einklagen/vorabcheck":
+      return "vorab-check-pages";
+    default:
+      return "form-flow-pages";
+  }
+};
+
 export async function fetchAllFormFields(
   flowId: FlowId,
 ): Promise<FormFieldsMap> {
-  const apiId = "form-flow-pages";
+  const apiId = getApiIdByFlowId(flowId);
   const filters = [{ field: "flow_ids", nestedField: "flowId", value: flowId }];
   const populate = "form";
   const strapiEntries = await getStrapiEntry({ apiId, filters, populate });
