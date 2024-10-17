4ba7e1acc76859e682ae15dcebb9fbc04d255549
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-10-17 11:23:00 +0200

    feat(fgr): add more expectation for plaintiff details

--- app/services/pdf/fluggastrechte/sections/firstPage/claimData/__test__/addPlaintiffDetails.test.ts
+++ app/services/pdf/fluggastrechte/sections/firstPage/claimData/__test__/addPlaintiffDetails.test.ts
@@ -3,19 +3,31 @@ import {
   mockPdfKitDocument,
   mockPdfKitDocumentStructure,
 } from "tests/factories/mockPdfKit";
-import { addPlaintiffDetails, PLAINTIFF_TEXT } from "../addPlaintiffDetails";
+import {
+  FONTS_BUNDESSANS_BOLD,
+  FONTS_BUNDESSANS_REGULAR,
+} from "~/services/pdf/fluggastrechte/createPdfKitDocument";
+import {
+  addPlaintiffDetails,
+  PLAINTIFF_TEXT,
+  SEPARATOR,
+} from "../addPlaintiffDetails";
 
 describe("addPlaintiffDetails", () => {
-  it("should create document with plaintiff details", () => {
+  it("should create document with full plaintiff details", () => {
     const mockStruct = mockPdfKitDocumentStructure();
     const mockDoc = mockPdfKitDocument(mockStruct);
-
     addPlaintiffDetails(mockDoc, userDataMock);
 
+    expect(mockDoc.fontSize).toHaveBeenCalledWith(10);
+    expect(mockDoc.font).toHaveBeenCalledWith(FONTS_BUNDESSANS_BOLD);
     expect(mockDoc.text).toHaveBeenCalledWith("Fr. Test-test Test", {
       continued: true,
     });
-
+    expect(mockDoc.font).toHaveBeenCalledWith(FONTS_BUNDESSANS_REGULAR);
+    expect(mockDoc.text).toHaveBeenCalledWith(SEPARATOR, { continued: true });
+    expect(mockDoc.text).toHaveBeenCalledWith(userDataMock.strasseHausnummer);
+    expect(mockDoc.text).toHaveBeenCalledWith(userDataMock.telefonnummer);
     expect(mockDoc.text).toHaveBeenCalledWith(PLAINTIFF_TEXT, {
       align: "left",
     });
