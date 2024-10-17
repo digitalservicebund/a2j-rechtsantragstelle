9f98e34b11f8c738408620774ddd1e5588ffc3b0
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2024-10-17 16:49:12 +0200

    feat(fgr): add test for planned flight

--- app/services/pdf/fluggastrechte/sections/firstPage/claimData/__test__/addPlannedFlightDetails.test.ts
+++ app/services/pdf/fluggastrechte/sections/firstPage/claimData/__test__/addPlannedFlightDetails.test.ts
@@ -3,6 +3,7 @@ import {
   mockPdfKitDocument,
   mockPdfKitDocumentStructure,
 } from "tests/factories/mockPdfKit";
+import { getCompensationPayment } from "~/services/airports/getCompensationPayment";
 import {
   addPlannedFlightDetails,
   AFFECTED_FLIGHT_TEXT,
@@ -11,11 +12,12 @@ import {
   PLANNED_DEPARTURE_DATE_TEXT,
 } from "../addPlannedFlightDetails";
 
+vi.mock("~/services/airports/getCompensationPayment");
+
 describe("addPlannedFlightDetails", () => {
   it("should create document with flight details", () => {
     const mockStruct = mockPdfKitDocumentStructure();
     const mockDoc = mockPdfKitDocument(mockStruct);
-
     addPlannedFlightDetails(mockDoc, userDataMock);
 
     expect(mockDoc.text).toHaveBeenCalledWith(DUE_REASON_TEXT);
@@ -23,8 +25,31 @@ describe("addPlannedFlightDetails", () => {
     expect(mockDoc.text).toHaveBeenCalledWith(FLIGHT_NUMBER_TEXT, {
       continued: true,
     });
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      ` ${userDataMock.direktFlugnummer}`,
+    );
     expect(mockDoc.text).toHaveBeenCalledWith(PLANNED_DEPARTURE_DATE_TEXT, {
       continued: true,
     });
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      ` ${userDataMock.direktAbflugsDatum}`,
+    );
+  });
+
+  it("should calculate compensation based on start and end airport", () => {
+    const mockStruct = mockPdfKitDocumentStructure();
+    const mockDoc = mockPdfKitDocument(mockStruct);
+    const mockCompensation = "400";
+    vi.mocked(getCompensationPayment).mockReturnValue(mockCompensation);
+
+    addPlannedFlightDetails(mockDoc, userDataMock);
+
+    expect(getCompensationPayment).toHaveBeenCalledWith({
+      startAirport: userDataMock.startAirport,
+      endAirport: userDataMock.endAirport,
+    });
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      `Streitwert: ${mockCompensation}€`,
+    );
   });
 });
