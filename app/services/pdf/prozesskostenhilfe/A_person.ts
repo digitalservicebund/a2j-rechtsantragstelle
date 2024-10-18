7ae72ca7af2637ad55512f9e60977d8f0d5bc680
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-10-18 14:16:18 +0200

    feat(pdf-service): fill gesetzlicher vetreter in pkh form

---
+++ app/services/pdf/prozesskostenhilfe/A_person.ts
@@ -0,0 +1,37 @@
+import type { PkhPdfFillFunction } from "~/services/pdf/prozesskostenhilfe";
+import type { AttachmentEntries } from "../attachment";
+import { newPageHint } from "../attachment";
+
+export const GESETZLICHERVERTRETER_FIELD_MAX_CHARS = 80;
+
+export const fillPerson: PkhPdfFillFunction = ({ userData, pdfValues }) => {
+  const attachment: AttachmentEntries = [];
+
+  const gesetzlicherVertreterString =
+    `${userData.gesetzlicheVertretungDaten?.vorname} ${userData.gesetzlicheVertretungDaten?.nachname}, ` +
+    `${userData.gesetzlicheVertretungDaten?.strasseHausnummer}, ` +
+    `${userData.gesetzlicheVertretungDaten?.plz} ${userData.gesetzlicheVertretungDaten?.ort}, ` +
+    `${userData.gesetzlicheVertretungDaten?.telefonnummer}`;
+
+  if (
+    gesetzlicherVertreterString.length > GESETZLICHERVERTRETER_FIELD_MAX_CHARS
+  ) {
+    pdfValues.sofernvorhandenGesetzlicherVertreterNameVornameAnschriftTelefon.value =
+      newPageHint;
+
+    attachment.push({
+      title: "Feld A: Angaben zu Ihrer Person",
+      level: "h2",
+    });
+
+    attachment.push({
+      title: "Gesetzlicher Vertreter",
+      text: gesetzlicherVertreterString,
+    });
+  } else {
+    pdfValues.sofernvorhandenGesetzlicherVertreterNameVornameAnschriftTelefon.value =
+      gesetzlicherVertreterString;
+  }
+
+  return { pdfValues, attachment };
+};
---
+++ app/services/pdf/prozesskostenhilfe/__test__/A_person.test.ts
@@ -0,0 +1,84 @@
+import {
+  getProzesskostenhilfeParameters,
+  type ProzesskostenhilfePDF,
+} from "data/pdf/prozesskostenhilfe/prozesskostenhilfe.generated";
+import type { ProzesskostenhilfeFormularContext } from "~/flows/prozesskostenhilfeFormular";
+import { newPageHint } from "../../attachment";
+import { fillPerson, GESETZLICHERVERTRETER_FIELD_MAX_CHARS } from "../A_person";
+
+let pdfParams: ProzesskostenhilfePDF;
+const userData: ProzesskostenhilfeFormularContext = {
+  hasGesetzlicheVertretung: "yes",
+  gesetzlicheVertretungDaten: {
+    vorname: "Max",
+    nachname: "M",
+    strasseHausnummer: "Musterstraße 123",
+    plz: "12345",
+    ort: "Musterstadt",
+    telefonnummer: "0123456789",
+  },
+};
+const getGesetzlicherVertreterString = (
+  userData: ProzesskostenhilfeFormularContext,
+): string => {
+  const { vorname, nachname, strasseHausnummer, plz, ort, telefonnummer } =
+    userData.gesetzlicheVertretungDaten!;
+  return `${vorname} ${nachname}, ${strasseHausnummer}, ${plz} ${ort}, ${telefonnummer}`;
+};
+
+describe("A_person", () => {
+  beforeEach(() => {
+    pdfParams = getProzesskostenhilfeParameters();
+  });
+  describe("gesetzlicher Vertreter", () => {
+    const gesetzlicherVertreterString =
+      getGesetzlicherVertreterString(userData);
+    it("should fill the gesetzlicher Vertreter section if the user has a legal guardian", () => {
+      const { pdfValues, attachment } = fillPerson({
+        userData,
+        pdfValues: pdfParams,
+      });
+      expect(
+        pdfValues
+          .sofernvorhandenGesetzlicherVertreterNameVornameAnschriftTelefon
+          .value,
+      ).toBe(gesetzlicherVertreterString);
+      expect(attachment).toHaveLength(0);
+    });
+
+    it("should fill the gesetzlicher Vertreter section with a new page hint if the string is too long", () => {
+      const userDataWithLongString = {
+        gesetzlicheVertretungDaten: {
+          ...userData.gesetzlicheVertretungDaten,
+          nachname: "a".repeat(
+            GESETZLICHERVERTRETER_FIELD_MAX_CHARS -
+              gesetzlicherVertreterString.length +
+              2, // one more char than GESETZLICHERVERTRETER_FIELD_MAX_CHARS
+          ),
+        },
+      } as ProzesskostenhilfeFormularContext;
+
+      const gesetzlicherVertreterStringLong = getGesetzlicherVertreterString(
+        userDataWithLongString,
+      );
+      const { pdfValues, attachment } = fillPerson({
+        userData: userDataWithLongString,
+        pdfValues: pdfParams,
+      });
+      expect(
+        pdfValues
+          .sofernvorhandenGesetzlicherVertreterNameVornameAnschriftTelefon
+          .value,
+      ).toBe(newPageHint);
+      expect(attachment).toHaveLength(2);
+      expect(
+        attachment?.some(
+          (element) => element.title === "Gesetzlicher Vertreter",
+        ),
+      ).toBe(true);
+      expect(attachment?.at(0)?.title).toBe("Feld A: Angaben zu Ihrer Person");
+      expect(attachment?.at(1)?.title).toBe("Gesetzlicher Vertreter");
+      expect(attachment?.at(1)?.text).toBe(gesetzlicherVertreterStringLong);
+    });
+  });
+});
