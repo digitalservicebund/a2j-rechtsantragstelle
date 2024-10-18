c726162fb59450768ea5064717bac73a2777b697
flo drews <florian.drews@digitalservice.bund.de>
2024-10-18 08:36:54 +0200

    refactor(test): update pathname shouldSetCacheControlHeader -  for more realistic testing

--- app/util/__test__/shouldSetCacheControlHeader.test.ts
+++ app/util/__test__/shouldSetCacheControlHeader.test.ts
@@ -1,12 +1,8 @@
-import { random } from "lodash";
-import { flowIds } from "~/flows/flowIds";
 import { shouldSetCacheControlHeader } from "../shouldSetCacheControlHeader";
 
 const mockHeaders = (headers: Record<string, string>): Headers => {
   return new Headers(headers);
 };
-
-const flowId = flowIds[random(flowIds.length - 1)];
 
 describe("shouldSetCacheControlHeader", () => {
   it("should return false when tracking consent is not set and no flowId in pathname", () => {
@@ -28,7 +24,7 @@ describe("shouldSetCacheControlHeader", () => {
   it("should return true when tracking consent is no set and flowId is in pathname", () => {
     const loaderHeaders = mockHeaders({
       trackingConsentSet: "false",
-      pathname: flowId,
+      pathname: "/fluggastrechte/vorabcheck/bereich",
     });
     expect(shouldSetCacheControlHeader(loaderHeaders)).toBe(true);
   });
@@ -36,7 +32,8 @@ describe("shouldSetCacheControlHeader", () => {
   it("should return true when tracking consent is set and flowId is in pathname", () => {
     const loaderHeaders = mockHeaders({
       trackingConsentSet: "true",
-      pathname: flowId,
+      pathname:
+        "/fluggastrechte/formular/grundvoraussetzungen/daten-uebernahme",
     });
     expect(shouldSetCacheControlHeader(loaderHeaders)).toBe(true);
   });
