b2d47c28473e00ce8b5a68ea41f12bfb62bda3be
Alex August Schlote <alex.schlote@digitalservice.bund.de>
2024-10-22 17:05:19 +0200

    Both streams: Remove accessibility tests of forms flow pages (#1333)
    * test: remove accessibility tests of forms flow pages
    
    * test: remove more accessibility tests

--- tests/e2e/flowPages/shared/finanzielleAngaben/finanzielleAngabenPartner.ts
+++ tests/e2e/flowPages/shared/finanzielleAngaben/finanzielleAngabenPartner.ts
@@ -1,29 +1,23 @@
 import type { Page } from "@playwright/test";
 import type { Formular } from "tests/e2e/pom/Formular";
-import { expectPageToBeAccessible } from "tests/e2e/util/expectPageToBeAccessible";
 
 export async function startFinanzielleAngabenPartner(
   page: Page,
   formular: Formular,
 ) {
   // /finanzielle-angaben/partner/partnerschaft
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("partnerschaft", "yes");
 
   // /finanzielle-angaben/partner/zusammenleben
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("zusammenleben", "no");
 
   // /finanzielle-angaben/partner/unterhalt
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("unterhalt", "yes");
 
   // /finanzielle-angaben/partner/unterhalts-summe
-  await expectPageToBeAccessible({ page });
   await formular.fillInputPage("unterhaltsSumme", "100");
 
   // /finanzielle-angaben/partner/partner-name
-  await expectPageToBeAccessible({ page });
   await formular.fillInput("partnerVorname", "Dagobert");
   await formular.fillInput("partnerNachname", "Duck");
   await formular.clickNext();
