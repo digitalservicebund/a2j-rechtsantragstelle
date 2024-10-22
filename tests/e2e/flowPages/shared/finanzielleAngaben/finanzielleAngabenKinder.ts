eeaa4b9b72cbd62df291d29ca6002eb06908ca3f
aaschlote <alex.schlote@digitalservice.bund.de>
2024-10-22 12:35:41 +0200

    test: remove more accessibility tests

--- tests/e2e/flowPages/shared/finanzielleAngaben/finanzielleAngabenKinder.ts
+++ tests/e2e/flowPages/shared/finanzielleAngaben/finanzielleAngabenKinder.ts
@@ -7,7 +7,6 @@ export async function startFinanzielleAngabenKinder(
   formular: Formular,
 ) {
   // /finanzielle-angaben/kinder/kinder-frage
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("hasKinder", "yes");
 
   // /finanzielle-angaben/kinder/uebersicht
@@ -15,25 +14,20 @@ export async function startFinanzielleAngabenKinder(
   await formular.clickAnchorByText("Kind hinzufügen");
 
   // /finanzielle-angaben/kinder/kinder/0/name
-  await expectPageToBeAccessible({ page });
   await formular.fillInput("kinder#vorname", "Clara");
   await formular.fillInput("kinder#nachname", "Musterian");
   await formular.fillInput("kinder#geburtsdatum", "12.12.2020");
   await formular.clickNext();
 
   // /finanzielle-angaben/kinder/kinder/0/wohnort
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("kinder#wohnortBeiAntragsteller", "yes");
 
   // /finanzielle-angaben/kinder/kinder/0/kind-eigene-einnahmen-frage
-  await expectPageToBeAccessible({ page });
   await formular.fillRadioPage("kinder#eigeneEinnahmen", "yes");
 
   // /finanzielle-angaben/kinder/kinder/0/kind-eigene-einnahmen
-  await expectPageToBeAccessible({ page });
   await formular.fillInputPage("kinder#einnahmen", "5");
 
   // /finanzielle-angaben/kinder/uebersicht
-  await expectPageToBeAccessible({ page });
   await formular.clickNext();
 }
