14790031de6746e4020cca154896ba64d6f3a94d
Eric Klemm <eric.klemm@digitalservice.bund.de>
2024-10-23 18:49:05 +0200

    add unterhalt attachement to generated pdf

--- app/services/pdf/prozesskostenhilfe/D_angehoerige.ts
+++ app/services/pdf/prozesskostenhilfe/D_angehoerige.ts
@@ -1,20 +1,110 @@
 import type { PkhPdfFillFunction } from "~/services/pdf/prozesskostenhilfe";
+import type { AttachmentEntries } from "../attachment";
+import { SEE_IN_ATTACHMENT_DESCRIPTION } from "../attachment";
+const ATTACHMENT_DESCRIPTION_SECTION_D =
+  "FELD D: Angehörige, denen Sie Bar- oder Naturalunterhalt gewähren";
+
+const familyRelationshipMap = {
+  mother: "Meine Mutter",
+  father: "Mein Vater",
+  grandmother: "Meine Großmutter",
+  grandfather: "Mein Großvater",
+  kid: "Mein Kind",
+  "ex-spouse-f": "Meine Ex-Ehepartnerin",
+  "ex-spouse-m": "Mein Ex-Ehepartner",
+  grandchild: "Mein Enkelkind",
+} as const;
 
 export const fillAngehoerige: PkhPdfFillFunction = ({
   userData,
   pdfValues,
 }) => {
-  if (
-    userData.partnerschaft === "yes" &&
-    userData.zusammenleben === "no" &&
-    userData.unterhalt === "yes" &&
-    userData.unterhaltsSumme &&
-    userData.partnerVorname &&
-    userData.partnerNachname
-  ) {
-    pdfValues.verhaeltnis1.value = "Partner:in";
-    pdfValues.angehoerigerNummereins.value = `${userData.partnerVorname} ${userData.partnerNachname}, lebt getrennt`;
-    pdfValues.monatsbetrag1.value = userData.unterhaltsSumme + " €";
+  const attachment: AttachmentEntries = [];
+  const zahltPartnerUnterhalt = userData.unterhaltsSumme !== undefined;
+  const kinder = userData.kinder ?? [];
+  const hasKinder = kinder.length > 0;
+
+  const unterhaltszahlungen = userData.unterhaltszahlungen ?? [];
+  const hasUnterhaltszahlungen = unterhaltszahlungen.length > 0;
+
+  if (!zahltPartnerUnterhalt && !hasUnterhaltszahlungen && !hasKinder)
+    return { pdfValues };
+
+  pdfValues.angehoerigerNummereins.value = SEE_IN_ATTACHMENT_DESCRIPTION;
+
+  attachment.push({ title: ATTACHMENT_DESCRIPTION_SECTION_D, level: "h2" });
+
+  if (zahltPartnerUnterhalt) {
+    attachment.push({ title: "Partner", level: "h3" });
+    attachment.push({
+      title: "Name",
+      text: `${userData.partnerVorname} ${userData.partnerNachname}`,
+    });
+    attachment.push({
+      title: "Monatliche Unterhaltszahlungen",
+      text: userData.unterhaltsSumme + " €",
+    });
+    attachment.push({
+      title: "Gemeinsame Wohnung",
+      text: userData.zusammenleben === "yes" ? "Ja" : "Nein",
+    });
+    if (userData.partnerEinkommenSumme)
+      attachment.push({
+        title: "Eigene monatlichen Einnahmen",
+        text: userData.partnerEinkommenSumme + " €",
+      });
   }
-  return { pdfValues };
+
+  if (hasKinder) {
+    attachment.push({ title: "Kinder", level: "h3" });
+
+    kinder.forEach((kind, index) => {
+      attachment.push({ title: `Kind ${index + 1}`, level: "h4" });
+      attachment.push({
+        title: "Name",
+        text: `${kind.vorname} ${kind.nachname}`,
+      });
+      attachment.push({ title: "Geburtsdatum", text: kind.geburtsdatum });
+      attachment.push({
+        title: "Monatliche Unterhaltszahlungen",
+        text: kind.unterhaltsSumme
+          ? kind.unterhaltsSumme + " €"
+          : "Keine Angabe",
+      });
+      attachment.push({
+        title: "Gemeinsame Wohnung",
+        text: kind.wohnortBeiAntragsteller === "yes" ? "Ja" : "Nein",
+      });
+      if (kind.einnahmen)
+        attachment.push({
+          title: "Eigene monatlichen Einnahmen",
+          text: kind.einnahmen + " €",
+        });
+    });
+  }
+
+  if (hasUnterhaltszahlungen) {
+    attachment.push({
+      title: "Andere Personen",
+      level: "h3",
+    });
+
+    unterhaltszahlungen.forEach((person, index) => {
+      attachment.push({ title: `Person ${index + 1}`, level: "h4" });
+      attachment.push({
+        title: "Name",
+        text: `${person.firstName} ${person.surname}`,
+      });
+      attachment.push({ title: "Geburtsdatum", text: person.birthday });
+      attachment.push({
+        title: "Familienverhältnis",
+        text: familyRelationshipMap[person.familyRelationship],
+      });
+      attachment.push({
+        title: "Monatliche Unterhaltszahlungen",
+        text: `${person.monthlyPayment} €`,
+      });
+    });
+  }
+  return { pdfValues, attachment };
 };
