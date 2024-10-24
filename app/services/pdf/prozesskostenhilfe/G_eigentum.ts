d8aa7650207850c384f6779def81b1bfc418c466
ekl176 <eric.klemm@digitalservice.bund.de>
2024-10-24 18:16:39 +0200

    fix: only add attachments in pkh wertsachen when needed (#1344)

--- app/services/pdf/prozesskostenhilfe/G_eigentum.ts
+++ app/services/pdf/prozesskostenhilfe/G_eigentum.ts
@@ -119,7 +119,11 @@ export const fillBargeldOderWertgegenstaende: PkhPdfFillFunction = ({
     : [];
   const hasBargeldOderWertgegenstaende =
     hasWertsache === "yes" || hasGeldanlage === "yes";
-  pdfValues.nein_43.value = !hasBargeldOderWertgegenstaende;
+  if (!hasBargeldOderWertgegenstaende) {
+    pdfValues.nein_43.value = true;
+    return { pdfValues };
+  }
+
   pdfValues.ja_39.value = hasBargeldOderWertgegenstaende;
   if (bargeld.length + (wertsachen?.length ?? 0) === 1) {
     const singleBargeld = bargeld[0];
