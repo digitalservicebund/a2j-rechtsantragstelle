cd155a709aad84e703afd533f3e757f2492dbe63
aaschlote <alex.schlote@digitalservice.bund.de>
2024-10-31 11:08:21 +0100

    refactor(getFullPlaintiffName): improve how to import capitalize from lodash

--- app/services/pdf/fluggastrechte/sections/getFullPlaintiffName.ts
+++ app/services/pdf/fluggastrechte/sections/getFullPlaintiffName.ts
@@ -1,11 +1,11 @@
-import _ from "lodash";
+import capitalize from "lodash/capitalize";
 import type { FluggastrechtContext } from "~/domains/fluggastrechte/formular/context";
 
 export const getFullPlaintiffName = (userData: FluggastrechtContext) => {
   const { anrede, title, vorname, nachname } = userData;
 
   const mappedTitle = title === "dr" ? "Dr." : title;
-  const capitalizedVorname = _.capitalize(vorname);
+  const capitalizedVorname = capitalize(vorname);
 
   return [anrede, mappedTitle, capitalizedVorname, nachname]
     .filter(Boolean)
