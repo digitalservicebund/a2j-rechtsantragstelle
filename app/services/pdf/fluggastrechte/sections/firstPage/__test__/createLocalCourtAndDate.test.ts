b6b01f485bfdf800a34531c587ceb00847c5c102
aaschlote <alex.schlote@digitalservice.bund.de>
2024-10-31 14:33:56 +0100

    refactor(createLocalCourtAndDate): get the court using the function getCourtByStartAndEndAirport

--- app/services/pdf/fluggastrechte/sections/firstPage/__test__/createLocalCourtAndDate.test.ts
+++ app/services/pdf/fluggastrechte/sections/firstPage/__test__/createLocalCourtAndDate.test.ts
@@ -3,22 +3,41 @@ import {
   mockPdfKitDocument,
   mockPdfKitDocumentStructure,
 } from "tests/factories/mockPdfKit";
+import { getCourtByStartAndEndAirport } from "~/domains/fluggastrechte/services/getCourtByStartAndEndAirport";
+import type { Jmtd14VTErwerberGerbeh } from "~/services/gerichtsfinder/types";
 import {
   createLocalCourtAndDate,
   CREATION_PDF_TEXT,
   TO_THE_COURT_TEXT,
 } from "../createLocalCourtAndDate";
 
+vi.mock("~/domains/fluggastrechte/services/getCourtByStartAndEndAirport");
+
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
 describe("createLocalCourtAndDate", () => {
-  beforeEach(() => {
-    vi.useFakeTimers();
-  });
-
-  afterEach(() => {
-    vi.useRealTimers();
-  });
-
   it("should create the document with the local court and date", () => {
+    const amtsgericht: Jmtd14VTErwerberGerbeh = {
+      AG: "",
+      BEZEICHNUNG: "Amtsgericht",
+      LG: "",
+      LKZ: "",
+      OLG: "",
+      ORT: "Wusterhausen",
+      ORTK: "",
+      PLZ_ZUSTELLBEZIRK: "15711",
+      STR_HNR: "Schlossplatz 4",
+      TYP_INFO: "Zivilgericht - Amtsgericht",
+      XML_SUPPORT: "JA",
+    } as const;
+
+    vi.mocked(getCourtByStartAndEndAirport).mockReturnValue(amtsgericht);
     const mockDate = new Date("2024-10-14");
     vi.setSystemTime(mockDate);
 
@@ -40,21 +59,20 @@ describe("createLocalCourtAndDate", () => {
       align: "left",
     });
 
+    expect(mockDoc.text).toHaveBeenCalledWith(amtsgericht.BEZEICHNUNG, {
+      align: "left",
+    });
+    expect(mockDoc.text).toHaveBeenCalledWith(amtsgericht.STR_HNR, {
+      align: "left",
+    });
     expect(mockDoc.text).toHaveBeenCalledWith(
-      userDataMock.zustaendigesAmtsgericht.bezeichnung,
-      { align: "left" },
-    );
-    expect(mockDoc.text).toHaveBeenCalledWith(
-      userDataMock.zustaendigesAmtsgericht.strasseMitHausnummer,
-      { align: "left" },
-    );
-    expect(mockDoc.text).toHaveBeenCalledWith(
-      userDataMock.zustaendigesAmtsgericht.plzUndStadt,
+      `${amtsgericht.PLZ_ZUSTELLBEZIRK} ${amtsgericht.ORT}`,
       { align: "left" },
     );
   });
 
   it("should handle missing local court gracefully", () => {
+    vi.mocked(getCourtByStartAndEndAirport).mockReturnValue(undefined);
     const mockStruct = mockPdfKitDocumentStructure();
     const mockDoc = mockPdfKitDocument(mockStruct);
 
