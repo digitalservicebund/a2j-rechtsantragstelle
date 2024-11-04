4dc81f962b108ea81ad6a6aa52e22f9c3d193cb9
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-11-04 10:58:22 +0100

    bump minor eslint version, move typescript eslint rules to new format

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,21 @@
 import vitest from "@vitest/eslint-plugin";
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
+import sonarjs from "eslint-plugin-sonarjs";
 
-export default [
-  js.configs.recommended,
+const { recommended, stylistic } = tseslint.configs;
+const { recommended: importRecommended, typescript: importTypescript } =
+  importPlugin.flatConfigs;
+
+export default tseslint.config(
+  eslint.configs.recommended,
   // Global ignores
   {
-    ignores: ["/*", "!/app", "!/tests", "*.generated.ts", "!/scripts"],
+    ignores: ["**/*", "!app/**", "!tests/**", "!scripts/**"],
   },
   // Global overrides
   {
@@ -22,6 +29,7 @@ export default [
         ...globals.browser,
         ...globals.commonjs,
         ...globals.es6,
+        ...globals.node,
       },
     },
   },
@@ -53,6 +61,63 @@ export default [
       "react/jsx-no-leaked-render": ["off", { validStrategies: ["ternary"] }], // TODO: enable later
     },
   },
+  // Typescript
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      ...recommended,
+      ...stylistic,
+      importRecommended,
+      importTypescript,
+      sonarjs.configs.recommended,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    settings: {
+      "import/internal-regex": "^~/",
+      "import/resolver": {
+        node: { extensions: [".ts", ".tsx"] },
+        typescript: { alwaysTryTypes: true },
+      },
+    },
+    rules: {
+      "import/no-cycle": "warn",
+      "no-console": "warn",
+      "sonarjs/no-duplicate-string": "off",
+      "sonarjs/prefer-enum-initializers": "off", // currently breaks at runtime with TypeError: Cannot read properties of undefined (reading 'members')
+      "sonarjs/todo-tag": "warn",
+      "sonarjs/fixme-tag": "warn",
+      "sonarjs/no-commented-code": "warn",
+      "sonarjs/jsx-no-constructed-context-values": "warn",
+      "sonarjs/new-cap": "off",
+      "sonarjs/no-array-index-key": "warn",
+      "sonarjs/no-empty-test-file": "off",
+      "sonarjs/no-redeclare": "off",
+      "sonarjs/no-nested-functions": "warn",
+      "sonarjs/sonar-no-unused-vars": "off",
+      "sonarjs/no-unstable-nested-components": "warn", //enable later
+      "@typescript-eslint/ban-ts-comment": "off", // enable later
+      "@typescript-eslint/consistent-type-definitions": "off", // enable later
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/array-type": "off", // enable later
+      "@typescript-eslint/consistent-indexed-object-style": "off", // enable later
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+
+      "import/order": [
+        "warn",
+        {
+          alphabetize: { caseInsensitive: true, order: "asc" },
+          groups: ["builtin", "external", "internal"],
+          "newlines-between": "never",
+        },
+      ],
+    },
+  },
   // Vitest
   {
     files: ["app/**/*.test.{js,jsx,ts,tsx}"],
@@ -65,4 +130,4 @@ export default [
       "require-await": "error",
     },
   },
-];
+);
