5deeec74c7fa652fc8379bd45d0d33bf850058da
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-11-07 17:11:37 +0000

    refactor(flows): replace cmsSlug with flowType (#1355)

---
+++ app/services/cms/apiFromFlowType.ts
@@ -0,0 +1,10 @@
+import type { FlowType } from "~/domains/flows.server";
+import type { FlowPageId } from "./schemas";
+
+const apiMapping = {
+  formFlow: "form-flow-pages",
+  vorabCheck: "vorab-check-pages",
+} satisfies Record<FlowType, FlowPageId>;
+
+export const flowPageApiIdFromFlowType = (flowType: FlowType) =>
+  apiMapping[flowType];
