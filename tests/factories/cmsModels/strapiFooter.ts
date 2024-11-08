902ba6757066fb804f6deb379a020491beff8b11
ekl176 <eric.klemm@digitalservice.bund.de>
2024-11-08 17:14:34 +0100

    Refactor remove fishery factories (#1390)
    Co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

--- tests/factories/cmsModels/strapiFooter.ts
+++ tests/factories/cmsModels/strapiFooter.ts
@@ -1,19 +1,17 @@
 import { faker } from "@faker-js/faker";
-import { Factory } from "fishery";
-import type { StrapiFooter } from "~/services/cms/models/StrapiFooter";
 import { defaultLocale } from "~/services/cms/models/StrapiLocale";
-import { strapiImageFactory } from "./strapiImage";
-import { strapiLinkFactory } from "./strapiLink";
-import { strapiParagraphFactory } from "./strapiParagraph";
+import { getStrapiImage } from "./strapiImage";
+import { getStrapiLink } from "./strapiLink";
+import { getStrapiParagraph } from "./strapiParagraph";
 
-export const strapiFooterFactory = Factory.define<StrapiFooter>(() => {
+export function getStrapiFooter() {
   return {
     locale: defaultLocale,
-    image: strapiImageFactory.build(),
-    paragraphs: strapiParagraphFactory.buildList(2),
-    links: strapiLinkFactory.buildList(7),
+    image: getStrapiImage(),
+    paragraphs: faker.helpers.multiple(getStrapiParagraph, { count: 2 }),
+    links: faker.helpers.multiple(getStrapiLink, { count: 7 }),
     createdAt: faker.date.past().toISOString(),
     updatedAt: faker.date.recent().toISOString(),
     publishedAt: faker.date.recent().toISOString(),
   };
-});
+}
--- tests/factories/cmsModels/strapiLink.ts
+++ tests/factories/cmsModels/strapiLink.ts
@@ -1,10 +1,9 @@
 import { faker } from "@faker-js/faker";
-import { Factory } from "fishery";
 import type { StrapiLink } from "~/services/cms/models/StrapiLink";
 
-export const strapiLinkFactory = Factory.define<StrapiLink>(() => {
+export function getStrapiLink(): StrapiLink {
   return {
     url: faker.internet.url(),
     text: faker.lorem.words(),
   };
-});
+}
