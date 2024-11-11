a5fc4f3cd7ea41a8a2a76e19277e63bcca6d2680
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-11-11 13:43:48 +0000

    feat(CSPHeaders): strengthen headers (#1354)
    - adds form-action & upgrade-insecure-requests
    - adds optional CSP_REPORT_URI env & sets up CSP reporting
    - removes unsafe-inline -> pre-CSP3 browsers will block JS (which we support)
    - auto-generated trusted domains from env variables

---
+++ app/util/__test__/originFromUrlString.test.ts
@@ -0,0 +1,16 @@
+// @vitest-environment node
+
+import { originFromUrlString } from "../originFromUrlString";
+
+describe("originFromUrlString", () => {
+  it("returns the origin of valid URLS", () => {
+    expect(originFromUrlString("https://example.com/asd")).toEqual(
+      "https://example.com",
+    );
+  });
+
+  it("returns undefined for invalid or missing URLs", () => {
+    expect(originFromUrlString()).toBeUndefined();
+    expect(originFromUrlString("nonValidUrl")).toBeUndefined();
+  });
+});
