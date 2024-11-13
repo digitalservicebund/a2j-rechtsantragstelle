c9aba4cf32ff9bdedf7b250e9ffbc34d6fb16044
Eric Klemm <eric.klemm@digitalservice.bund.de>
2024-11-13 17:20:50 +0100

    shorten pkh e2e path and include them to testsuite again

--- tests/e2e/flowPages/prozesskostenhilfe/formular/prozesskostenhilfe.formular.spec.ts
+++ tests/e2e/flowPages/prozesskostenhilfe/formular/prozesskostenhilfe.formular.spec.ts
@@ -1,23 +1,13 @@
-import { faker } from "@faker-js/faker";
 import { expect, test } from "@playwright/test";
 import { startAbgabe } from "tests/e2e/flowPages/prozesskostenhilfe/formular/abgabe";
 import { startAntragstellendePerson } from "tests/e2e/flowPages/prozesskostenhilfe/formular/antragstellendePerson";
 import { startFinanzielleAngabenEinkuenfte } from "tests/e2e/flowPages/prozesskostenhilfe/formular/finanzielleAngabenEinkuenfte";
-import { startFinanzielleAngabenEinkuenftePartner } from "tests/e2e/flowPages/prozesskostenhilfe/formular/finanzielleAngabenEinkuenftePartner";
 import { startGrundvoraussetzungen } from "tests/e2e/flowPages/prozesskostenhilfe/formular/grundvoraussetzungen";
 import { CookieSettings } from "tests/e2e/pom/CookieSettings";
 import { ProzesskostenhilfeFormular } from "tests/e2e/pom/ProzesskostenhilfeFormular";
 import { expectPageToBeAccessible } from "tests/e2e/util/expectPageToBeAccessible";
-import { startFinanzielleAngabenAusgaben } from "./finanzielleAngabenAusgaben";
-import { startFinanzielleAngabenEigentum } from "./finanzielleAngabenEigentum";
 import { startGesetzlicheVertretung } from "./gesetzlicheVertretung";
 import { startPersoenlicheDaten } from "./persoenlicheDaten";
-import { startRechtsschutzversicherung } from "./rechtsschutzversicherung";
-import { startFinanzielleAngabenAndereUnterhaltszahlungen } from "../../shared/finanzielleAngaben/finanzielleAngabenAndereUnterhaltszahlungen";
-import { startFinanzielleAngabenEigentumZusammenfassung } from "../../shared/finanzielleAngaben/finanzielleAngabenEigentumZusammenfassung";
-import { startFinanzielleAngabenKinder } from "../../shared/finanzielleAngaben/finanzielleAngabenKinder";
-import { startFinanzielleAngabenPartner } from "../../shared/finanzielleAngaben/finanzielleAngabenPartner";
-import { startFinanzielleAngabenWohnung } from "../finanzielleAngabenWohnungs";
 
 let prozesskostenhilfeFormular: ProzesskostenhilfeFormular;
 
@@ -46,7 +36,7 @@ test("forwarded to initial step", async ({ page }) => {
   );
 });
 
-test.skip("prozesskostenhilfe formular can be traversed", async ({ page }) => {
+test("prozesskostenhilfe formular can be traversed", async ({ page }) => {
   // /prozesskostenhilfe/formular/start/start
   await expectPageToBeAccessible({ page });
   await prozesskostenhilfeFormular.clickNext();
@@ -57,54 +47,15 @@ test.skip("prozesskostenhilfe formular can be traversed", async ({ page }) => {
   // /prozesskostenhilfe/formular/antragstellende-person/empfaenger
   await startAntragstellendePerson(page, prozesskostenhilfeFormular);
 
-  // /prozesskostenhilfe/formular/rechtsschutzversicherung/rsv-frage
-  await startRechtsschutzversicherung(page, prozesskostenhilfeFormular);
-
-  // /prozesskostenhilfe/formular/finanzielle-angaben/einkommen/start
-  await expectPageToBeAccessible({ page });
-  await prozesskostenhilfeFormular.clickNext();
-
   // /prozesskostenhilfe/formular/finanzielle-angaben/einkuenfte/staatliche-leistungen
   await startFinanzielleAngabenEinkuenfte(page, prozesskostenhilfeFormular);
 
-  // /prozesskostenhilfe/formular/finanzielle-angaben/partner/partnerschaft
-  await startFinanzielleAngabenPartner(prozesskostenhilfeFormular);
-  await startFinanzielleAngabenEinkuenftePartner(
-    page,
-    prozesskostenhilfeFormular,
-  );
-  // /prozesskostenhilfe/formular/finanzielle-angaben/partner-einkuenfte/partner-besonders-ausgaben
-  await prozesskostenhilfeFormular.fillRadioPage(
-    "partnerHasBesondersAusgaben",
-    "yes",
-  );
-
-  // /prozesskostenhilfe/formular/finanzielle-angaben/partner-einkuenfte/add-partner-besonders-ausgaben
-  await prozesskostenhilfeFormular.fillInput(
-    "partnerBesondersAusgabe.beschreibung",
-    faker.word.sample(),
-  );
-  await prozesskostenhilfeFormular.fillInput(
-    "partnerBesondersAusgabe.betrag",
-    faker.finance.amount(),
-  );
-  await prozesskostenhilfeFormular.clickNext();
-
-  await startFinanzielleAngabenKinder(page, prozesskostenhilfeFormular);
-  await startFinanzielleAngabenAndereUnterhaltszahlungen(
-    page,
-    prozesskostenhilfeFormular,
-  );
-  await startFinanzielleAngabenWohnung(page, prozesskostenhilfeFormular);
-  await startFinanzielleAngabenEigentum(page, prozesskostenhilfeFormular);
-  await startFinanzielleAngabenEigentumZusammenfassung(
-    page,
-    prozesskostenhilfeFormular,
-  );
-  await prozesskostenhilfeFormular.clickNext();
-  await startFinanzielleAngabenAusgaben(page, prozesskostenhilfeFormular);
+  // //prozesskostenhilfe/formular/gesetzliche-vertretung/frage
   await startGesetzlicheVertretung(page, prozesskostenhilfeFormular);
+
+  // /prozesskostenhilfe/formular/persoenliche-daten/start
   await startPersoenlicheDaten(page, prozesskostenhilfeFormular);
 
+  // /prozesskostenhilfe/formular/abgabe/ende
   await startAbgabe(page);
 });
