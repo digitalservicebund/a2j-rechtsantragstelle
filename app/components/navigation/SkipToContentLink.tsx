431d50f209d9ecef7c7874e9f8eda600c94ae6da
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-11-13 16:11:14 +0100

    fix(replace hover: pseudoselector with focus: for skip link)

--- app/components/navigation/SkipToContentLink.tsx
+++ app/components/navigation/SkipToContentLink.tsx
@@ -8,7 +8,7 @@ export function SkipToContentLink({
   return (
     <a
       href={target}
-      className="bg-yellow-300 px-8 py-6 text-blue-800 ds-label-01-reg absolute transform -translate-y-full hover:translate-y-0 hover:relative"
+      className="bg-yellow-300 px-8 py-6 text-blue-800 ds-label-01-reg absolute transform -translate-y-full focus:translate-y-0 focus:relative"
       id="skip-to-content-link"
     >
       {label}
