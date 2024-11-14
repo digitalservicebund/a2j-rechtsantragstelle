ff34f6f47e0e34a5ce2af406f22bbb33576a989e
chris <christoph.hohnerlein@digitalservice.bund.de>
2024-11-14 14:17:43 +0100

    refactor(rateLimit): move to own file

--- app/expressApp.ts
+++ app/expressApp.ts
@@ -5,10 +5,9 @@ import type { ServerBuild } from "@remix-run/node";
 import * as Sentry from "@sentry/remix";
 import compression from "compression";
 import express from "express";
-import { rateLimit } from "express-rate-limit";
-import { RedisStore, type RedisReply } from "rate-limit-redis";
 import type { ViteDevServer } from "vite";
 import { getPosthogClient } from "./services/analytics/posthogClient.server";
+import { createRateLimitRequestHandler } from "./services/rateLimit";
 import { createRedisClient, quitRedis } from "./services/redis/redisClient";
 
 const isStagingOrPreviewEnvironment = process.env.ENVIRONMENT !== "production";
@@ -56,21 +55,7 @@ export const expressApp = (
   app.set("trust proxy", 2);
 
   // Limit calls to routes ending in /pdf or /pdf/, as they are expensive
-  app.use(
-    "/*/pdf",
-    rateLimit({
-      windowMs: 2 * 1000,
-      max: 2, // Limit each IP to 2 request per 2s
-      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-      message:
-        '<!DOCTYPE html><html lang="de"><head><meta charset="utf-8"><title>Justiz Services - Fehler aufgetreten</title><style>html{font-family:BundesSansWeb,Calibri,Verdana,Arial,Helvetica,sans-serif;font-size:1.125rem;line-height:1.75rem}body{max-width:59rem;margin:6rem auto;padding:0 2rem}h1{font-size:1.875rem;padding-bottom:1.5rem}</style></head><body><h1>Justiz Service ist vorübergehend nicht erreichbar</h1><p>Es sind zu viele Anfragen zum Herunterladen des PDFs innerhalb kurzer Zeit gestellt worden. Bitte versuchen Sie es später noch einmal.</p></body></html>',
-      store: new RedisStore({
-        sendCommand: (command: string, ...args: string[]) =>
-          redisClient.call(command, args) as Promise<RedisReply>,
-      }),
-    }),
-  );
+  app.use("/*/pdf", createRateLimitRequestHandler(redisClient));
 
   // handle SSR requests
   const remixHandler = createRequestHandler({ build });
