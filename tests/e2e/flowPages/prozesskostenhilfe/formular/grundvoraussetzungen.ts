b5b2cc18374b85c580dc2567659573aa6eda9d1f
ekl176 <eric.klemm@digitalservice.bund.de>
2024-11-14 11:01:08 +0100

    shorten pkh e2e path and include them to testsuite again (#1424)

--- tests/e2e/flowPages/prozesskostenhilfe/formular/grundvoraussetzungen.ts
+++ tests/e2e/flowPages/prozesskostenhilfe/formular/grundvoraussetzungen.ts
@@ -6,13 +6,19 @@ export async function startGrundvoraussetzungen(
   formular: ProzesskostenhilfeFormular,
 ) {
   // /prozesskostenhilfe/formular/grundvoraussetzungen/nachueberpruefung-frage
-  await formular.fillRadioPage("formularArt", "erstantrag");
+  await formular.fillRadioPage("formularArt", "nachueberpruefung");
 
-  // /prozesskostenhilfe/formular/grundvoraussetzungen/antrag/klageersteller
-  await formular.fillRadioPage("verfahrenArt", "verfahrenAnwalt");
+  // /prozesskostenhilfe/formular/grundvoraussetzungen/nachueberpruefung/name-gericht
+  await formular.clickNext();
+
+  // /prozesskostenhilfe/formular/grundvoraussetzungen/nachueberpruefung/aktenzeichen
+  await formular.clickNext();
 
   // /prozesskostenhilfe/formular/grundvoraussetzungen/einreichung/fall
   await formular.fillRadioPage("versandArt", "digital");
+
+  // /prozesskostenhilfe/formular/grundvoraussetzungen/einreichung/mjp
+  await formular.clickNext();
 
   // /prozesskostenhilfe/formular/grundvoraussetzungen/einreichung/hinweis-digital-einreichung
   await formular.clickNext();
