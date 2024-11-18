c10d6215b9d3abbb9a43d23d571073f9b8898d04
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-11-18 13:28:13 +0100

    Move to pdfkit (#1435)
    * refactor(pdf-service): move createHeader out of domains folder
    
    * feat(pdf-service): add function for creating the heading
    
    * feat(pdf-service): generate attachment with pdfkit instead of react-pdf for BerH
    
    * feat(pdf-service): generate attachment with pdfkit instead of react-pdf for PKH
    
    * feat(pdf-service): delete React pdf files and re-use styles
    
    * chore(pdf-service): remove react-pdf/renderer package

---
+++ app/services/pdf/header/createHeader.ts
@@ -0,0 +1,29 @@
+import type PDFDocument from "pdfkit";
+import type { BeratungshilfeFormularContext } from "~/domains/beratungshilfe/formular";
+import type { ProzesskostenhilfeFormularContext } from "~/domains/prozesskostenhilfe/formular";
+import { pdfStyles } from "~/domains/shared/pdf/pdfStyles";
+
+export function createHeader<
+  TContext extends
+    | BeratungshilfeFormularContext
+    | ProzesskostenhilfeFormularContext,
+>(
+  doc: typeof PDFDocument,
+  documentStruct: PDFKit.PDFStructureElement,
+  userData: TContext,
+  headerText: string,
+) {
+  const headerSect = doc.struct("Sect");
+
+  headerSect.add(
+    doc.struct("P", {}, () => {
+      doc
+        .fontSize(pdfStyles.pageHeader.fontSize)
+        .font(pdfStyles.pageHeader.font)
+        .text(`${headerText} von ${userData.vorname} ${userData.nachname}`)
+        .moveDown(2);
+    }),
+  );
+
+  documentStruct.add(headerSect);
+}
