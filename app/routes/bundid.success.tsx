f21f46d25f4682de004ff251aff2179c4e54a082
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-12-05 13:29:58 +0100

    feat(bundid): Parse and print saml response

--- app/routes/bundid.success.tsx
+++ app/routes/bundid.success.tsx
@@ -1,6 +1,12 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import Button from "~/components/Button";
 import { throw404OnProduction } from "~/services/errorPages/throw404";
+import invariant from "tiny-invariant";
+import { config } from "~/services/env/env.server";
+import saml from "samlify";
+import path from "path";
+import fs from "fs";
+import * as samlify from "samlify";
 
 export const loader = () => {
   throw404OnProduction();
@@ -9,8 +15,66 @@ export const loader = () => {
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   throw404OnProduction();
-  console.log(request);
-  process.stdout.write(JSON.stringify(await request.formData()) + "\n");
+
+  invariant(config().SAML_IDP_CERT, "SAML_IDP_CERT has to be set");
+  // eslint-disable-next-line sonarjs/new-cap
+  const idp = saml.IdentityProvider({
+    // Reading the data from file does not work because wantAuthnRequestsSigned is not set
+    // and even setting it (WantAuthnRequestsSigned="true") doesn't solve the issue.
+    wantAuthnRequestsSigned: true,
+    entityID: "https://int.id.bund.de/idp",
+    singleSignOnService: [
+      {
+        Binding: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+        Location: "https://int.id.bund.de/idp/profile/SAML2/POST/SSO",
+      },
+    ],
+    signingCert: config().SAML_IDP_CERT,
+    isAssertionEncrypted: true,
+    messageSigningOrder: "encrypt-then-sign",
+  });
+
+  const pathToSpMetadata = path.resolve(config().SAML_SP_METADATA_PATH);
+  const spMetadata = fs.readFileSync(pathToSpMetadata);
+
+  const pathToPrivateKey = path.resolve(config().SAML_SP_SECRET_KEY_PATH);
+  const privateKey = fs.readFileSync(pathToPrivateKey);
+
+  // eslint-disable-next-line sonarjs/new-cap
+  const sp = saml.ServiceProvider({
+    metadata: spMetadata,
+    encPrivateKey: privateKey,
+    privateKey,
+    wantAssertionsSigned: true,
+  });
+
+  const formData = await request.formData();
+  console.log("FormData:", formData);
+
+  const samlResponse = formData.get("SAMLResponse");
+  console.log({ samlResponse });
+  if (typeof samlResponse !== "string") {
+    throw new Error("Invalid SAML Response");
+  }
+
+  const samlHttpRequest = {
+    body: {
+      SAMLResponse: samlResponse,
+    },
+  };
+
+  samlify.setSchemaValidator({
+    validate: (response: string) => {
+      return Promise.resolve("skipped");
+    },
+  });
+
+  const response = await sp.parseLoginResponse(idp, "post", samlHttpRequest);
+  console.log("HERE");
+  console.log({ response });
+
+  console.log("Attributes:", response.extract.attributes);
+  console.log("Nachname:", response.extract.attributes["urn:oid:2.5.4.4"]);
   return null;
 };
 
