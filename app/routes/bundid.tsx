65406524e808592cebfec2747db5be929e9fb54a
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2024-12-11 15:12:02 +0100

    refactor(bundid): Extract methods to create sp and idp

--- app/routes/bundid.tsx
+++ app/routes/bundid.tsx
@@ -1,53 +1,25 @@
-import fs from "fs";
-import path from "path";
-import { json } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
-import saml from "samlify";
-import invariant from "tiny-invariant";
 import Button from "~/components/Button";
-import { config } from "~/services/env/env.server";
+import {
+  getBundIdIdentityProvider,
+  getBundIdServiceProvider,
+} from "~/services/bundid/index.server";
 import { throw404OnProduction } from "~/services/errorPages/throw404";
-
-// IDP xml
-// Ort für xmls finden
-// Keys + certs in sealed secret
-// proper feature toggle
-// extend XML with specific BundID attributes
 
 export const loader = () => {
   throw404OnProduction();
 
-  invariant(config().SAML_IDP_CERT, "SAML_IDP_CERT has to be set");
-  const idp = saml.IdentityProvider({
-    // Reading the data from file does not work because wantAuthnRequestsSigned is not set
-    // and even setting it (WantAuthnRequestsSigned="true") doesn't solve the issue.
-    wantAuthnRequestsSigned: true,
-    entityID: "https://int.id.bund.de/idp",
-    singleSignOnService: [
-      {
-        Binding: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
-        Location: "https://int.id.bund.de/idp/profile/SAML2/POST/SSO",
-      },
-    ],
-    signingCert: config().SAML_IDP_CERT,
-  });
+  const identityProvider = getBundIdIdentityProvider();
+  const serviceProvider = getBundIdServiceProvider();
 
-  const pathToSpMetadata = path.resolve(config().SAML_SP_METADATA_PATH);
-  const spMetadata = fs.readFileSync(pathToSpMetadata);
-
-  const pathToPrivateKey = path.resolve(config().SAML_SP_SECRET_KEY_PATH);
-  const privateKey = fs.readFileSync(pathToPrivateKey);
-
-  const sp = saml.ServiceProvider({
-    metadata: spMetadata,
-    privateKey,
-  });
-
-  const loginRequest = sp.createLoginRequest(idp, "post");
-  return json({
+  const loginRequest = serviceProvider.createLoginRequest(
+    identityProvider,
+    "post",
+  );
+  return {
     url: "https://int.id.bund.de/idp/profile/SAML2/POST/SSO",
     samlRequest: loginRequest.context,
-  });
+  };
 };
 
 export default function View() {
