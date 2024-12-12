5e49c7f5807c7be8982924cdfc3ae58a4541c096
Flo Drews <florian.drews@digitalservice.bund.de>
2024-12-12 11:23:52 +0100

    preparing for tets

---
+++ app/services/validation/__test__/getArrivalTimeDelayValidator.test.ts
@@ -0,0 +1,5 @@
+describe("getArrivalTimeDelayValidator", () => {
+  it("should return a validator that checks if the arrival time is at least 3 hours after the departure time", () => {
+    assert(true);
+  });
+});
