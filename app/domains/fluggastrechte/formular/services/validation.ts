548c16a816bf3d4b3711f9b97f3b06bed4ae9e7f
aaschlote <alex.schlote@digitalservice.bund.de>
2024-12-23 15:44:21 +0100

    validation(refactor): remove functions and add new issue

--- app/domains/fluggastrechte/formular/services/validation.ts
+++ app/domains/fluggastrechte/formular/services/validation.ts
@@ -7,53 +7,6 @@ function convertToTimestamp(date: string, time: string): number {
   const [hours, minutes] = time.split(":").map(Number);
   return new Date(year, month - 1, day, hours, minutes).getTime();
 }
-function convertToDate(date: string): number {
-  const [day, month, year] = date.split(".").map(Number);
-  return new Date(year, month - 1, day).getTime();
-}
-
-export function getArrivalTimeDelayValidator(
-  baseSchema: ValidationMultipleFieldsBaseSchema,
-) {
-  return baseSchema.refine(
-    (data) => {
-      const arrivalTimestamp = convertToTimestamp(
-        data.tatsaechlicherAnkunftsDatum,
-        data.tatsaechlicherAnkunftsZeit,
-      );
-      const departureTimestamp = convertToTimestamp(
-        data.direktAbflugsDatum,
-        data.direktAbflugsZeit,
-      );
-
-      const minimumTimeDifferenceInMs = 3 * 60 * 60 * 1000; // 3 hours in milliseconds
-      const actualTimeDifferenceInMs = arrivalTimestamp - departureTimestamp;
-      return actualTimeDifferenceInMs >= minimumTimeDifferenceInMs;
-    },
-    {
-      message: "invalidTimeDelay",
-      path: ["tatsaechlicherAnkunftsZeit"],
-    },
-  );
-}
-
-export function getArrivalDateValidator(
-  baseSchema: ValidationMultipleFieldsBaseSchema,
-) {
-  return baseSchema.refine(
-    (data) => {
-      const arrivalDate = convertToDate(data.tatsaechlicherAnkunftsDatum);
-      const departureDate = convertToDate(data.direktAbflugsDatum);
-
-      const actualTimeDifferenceInMs = arrivalDate - departureDate;
-      return actualTimeDifferenceInMs >= 0;
-    },
-    {
-      message: "invalidDate",
-      path: ["tatsaechlicherAnkunftsDatum"],
-    },
-  );
-}
 
 export function validateDepartureAfterArrival(
   baseSchema: ValidationMultipleFieldsBaseSchema,
@@ -70,6 +23,14 @@ export function validateDepartureAfterArrival(
     );
 
     if (departureDateTime > arrivalDateTime) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "too_late",
+        path: ["direktAnkunftsZeit"],
+        fatal: true,
+      });
+
+      // add new issue to invalidate this field as well
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: "too_late",
