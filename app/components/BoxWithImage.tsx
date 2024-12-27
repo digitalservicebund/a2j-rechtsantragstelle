bf12524675535a30b88c91cdf501f11aef783e97
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-12-27 11:51:57 +0100

    modify BoxWithImage and storybook to align with new size specifications

--- app/components/BoxWithImage.tsx
+++ app/components/BoxWithImage.tsx
@@ -4,24 +4,39 @@ import RichText from "./RichText";
 
 export type BoxWithImageProps = {
   image: ImageProps;
-  variant?: "default" | "ImgMTextL";
+  variant?: keyof typeof variantWidths;
   identifier?: string;
   heading?: HeadingProps;
   content?: string;
 };
 
-const variantStyles = {
+const variantWidths = {
+  // TODO: Remove after variant implementation
   default: {
-    textSize: "1rem",
-    imageContainer: "basis-1/6",
-    columnBreakpoint: "md:flex-row",
+    imageMaxWidth: "max-w-[120px]",
   },
   ImgMTextL: {
-    textSize: "1.25rem",
-    imageContainer: "basis-1/3",
-    columnBreakpoint: "lg:flex-row",
+    imageMaxWidth: "max-w-[280px]",
   },
-} as const;
+  XS: {
+    imageMaxWidth: "max-w-[80px]",
+  },
+  S: {
+    imageMaxWidth: "max-w-[120px]",
+  },
+  M: {
+    imageMaxWidth: "max-w-[280px]",
+  },
+  L: {
+    imageMaxWidth: "max-w-[400px]",
+  },
+  XL: {
+    imageMaxWidth: "max-w-[630px]",
+  },
+  XXL: {
+    imageMaxWidth: "max-w-[848px]",
+  },
+};
 
 const BoxWithImage = ({
   variant = "default",
@@ -30,18 +45,18 @@ const BoxWithImage = ({
   image,
   content,
 }: BoxWithImageProps) => {
+  const shouldWrapByDefault = variant === "XL" || variant === "XXL";
   return (
     <div
       id={identifier}
-      className={`flex flex-col items-start gap-24 ${variantStyles[variant].columnBreakpoint}`}
-      style={{ fontSize: variantStyles[variant].textSize }}
+      className={`flex flex-wrap ${shouldWrapByDefault ? "md:flex-wrap" : "sm:flex-nowrap"} items-start gap-24 text-base`}
     >
       <div
-        className={`shrink-0 overflow-hidden max-w-[70ch] ${content ? variantStyles[variant].imageContainer : "basis-full"}`}
+        className={`lg:shrink-0 overflow-hidden ${content ? variantWidths[variant].imageMaxWidth : "max-w-full"}`}
       >
         <Image {...image} />
       </div>
-      <div className={"ds-stack-8 break-words basis-auto"}>
+      <div className={`ds-stack-8 break-words min-w-[120px] max-w-[696px]`}>
         {heading && <Heading {...heading} />}
         {content && <RichText markdown={content} />}
       </div>
--- stories/BoxWithImage.stories.tsx
+++ stories/BoxWithImage.stories.tsx
@@ -12,18 +12,13 @@ export default meta;
 export const Default: StoryObj<typeof meta> = {
   args: {
     image: { url: "public/og-image.png" },
+    variant: "ImgMTextL",
     heading: {
-      text: "Heading text",
-      tagName: "h2",
-      look: "ds-heading-03-bold",
+      text: "Ein Pilotprojekt des Bundesministeriums der Justiz und der Justizministerien der Länder.",
+      tagName: "h1",
+      look: "ds-label-01-reg",
     },
-    content: `Non blanditiis vel quis. A molestias id quia modi veniam sunt. Quia eligendi quos ut.
-
-Totam quis saepe sed qui in. Beatae occaecati et aperiam non iusto sequi. Ut nihil similique aut magni neque.
-
-- Laboriosam quae esse libero eum. Iure et veritatis voluptates. Fugiat voluptates sunt aperiam accusantium ab voluptatum doloribus veniam. Maiores esse et est.
-- Quia distinctio earum accusamus aut ullam aut. Porro quis beatae ut rerum quas nemo itaque sed. Dolores voluptates in laborum deserunt cupiditate pariatur est.
-
-Est perspiciatis blanditiis aliquam. Ut perferendis et illo eligendi aliquid. Est mollitia vel molestiae. Enim sed eius et saepe voluptatem occaecati quasi voluptas.`,
+    content:
+      "In diesem Projekt geht es darum, den Zugang zum Recht zu verbessern. Diese Seite befindet sich im Aufbau. Weitere Funktionen und Dienstleistungen werden mit der Zeit ergänzt.",
   },
 };
