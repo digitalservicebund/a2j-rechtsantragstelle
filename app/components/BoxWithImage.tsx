f20f1a0aa85c364ab83d3bea394febcf9ad80dfc
Spencer Peace <spencer.peace@digitalservice.bund.de>
2024-12-27 12:18:43 +0100

    simplify variantWidths interface, add unit test

--- app/components/BoxWithImage.tsx
+++ app/components/BoxWithImage.tsx
@@ -4,38 +4,32 @@ import RichText from "./RichText";
 
 export type BoxWithImageProps = {
   image: ImageProps;
-  variant?: keyof typeof variantWidths;
+  variant?: Variant;
   identifier?: string;
   heading?: HeadingProps;
   content?: string;
 };
 
-const variantWidths = {
-  // TODO: Remove after variant implementation
-  default: {
-    imageMaxWidth: "max-w-[120px]",
-  },
-  ImgMTextL: {
-    imageMaxWidth: "max-w-[280px]",
-  },
-  XS: {
-    imageMaxWidth: "max-w-[80px]",
-  },
-  S: {
-    imageMaxWidth: "max-w-[120px]",
-  },
-  M: {
-    imageMaxWidth: "max-w-[280px]",
-  },
-  L: {
-    imageMaxWidth: "max-w-[400px]",
-  },
-  XL: {
-    imageMaxWidth: "max-w-[630px]",
-  },
-  XXL: {
-    imageMaxWidth: "max-w-[848px]",
-  },
+export type Variant =
+  | "default"
+  | "ImgMTextL"
+  | "XS"
+  | "S"
+  | "M"
+  | "L"
+  | "XL"
+  | "XXL";
+
+export const variantWidths: Record<Variant, string> = {
+  // TODO: Remove after new variant implementation
+  default: "max-w-[120px]",
+  ImgMTextL: "max-w-[280px]",
+  XS: "max-w-[80px]",
+  S: "max-w-[120px]",
+  M: "max-w-[280px]",
+  L: "max-w-[400px]",
+  XL: "max-w-[630px]",
+  XXL: "max-w-[848px]",
 };
 
 const BoxWithImage = ({
@@ -52,7 +46,7 @@ const BoxWithImage = ({
       className={`flex flex-wrap ${shouldWrapByDefault ? "md:flex-wrap" : "sm:flex-nowrap"} items-start gap-24 text-base`}
     >
       <div
-        className={`lg:shrink-0 overflow-hidden ${content ? variantWidths[variant].imageMaxWidth : "max-w-full"}`}
+        className={`lg:shrink-0 overflow-hidden ${content ? variantWidths[variant] : "max-w-full"}`}
       >
         <Image {...image} />
       </div>
