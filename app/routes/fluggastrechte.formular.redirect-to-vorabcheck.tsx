4214a37edd41b471e85bdf3633d6c17a91eaefb6
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-01-09 09:59:30 +0100

    feat: update comment

--- app/routes/fluggastrechte.formular.redirect-to-vorabcheck.tsx
+++ app/routes/fluggastrechte.formular.redirect-to-vorabcheck.tsx
@@ -1,6 +1,8 @@
 import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { getSessionManager } from "~/services/session.server";
-//workaround to redirect to the vorabcheck result
+
+// Workaround to reset FGR data when user wants to update flight data
+// This includes Amtsgericht and Airport data
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const cookieHeader = request.headers.get("Cookie");
 
