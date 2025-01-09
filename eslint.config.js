790669e0aa845a1c28115ca361e1c1abfc429818
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-01-09 13:03:58 +0100

    fix(lint): enable optional chain & nullish coalescing

--- eslint.config.js
+++ eslint.config.js
@@ -142,14 +142,11 @@ export default tseslint.config(
       // fix/reevaluate
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
       "@typescript-eslint/prefer-regexp-exec": "off",
-      "@typescript-eslint/prefer-optional-chain": "off",
       "@typescript-eslint/dot-notation": "off",
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/prefer-nullish-coalescing": "off",
 
       "no-restricted-syntax": [
         "error",
