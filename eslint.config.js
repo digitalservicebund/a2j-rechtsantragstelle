83a6f9e926070e0bb95e9b94087b065e6d9c1827
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-01-09 13:11:16 +0100

    fix(lint): delete rule

--- eslint.config.js
+++ eslint.config.js
@@ -142,7 +142,6 @@ export default tseslint.config(
       // fix/reevaluate
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/prefer-regexp-exec": "off",
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
