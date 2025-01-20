8e5f411a5d7e8b001925ca2d535719b5ec4656d4
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-01-20 17:51:45 +0100

    test(pkh flow): add missing partner einkuenfte

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/__test__/testcasesPartner.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/__test__/testcasesPartner.ts
@@ -1,6 +1,10 @@
+import { CheckboxValue } from "~/components/inputs/Checkbox";
 import type { TestCases } from "~/domains/__test__/TestCases";
 import type { ProzesskostenhilfeFinanzielleAngabenContext } from "~/domains/prozesskostenhilfe/formular/finanzielleAngaben/context";
 const prefix = "/finanzielle-angaben/partner";
+
+// Since these are generated from the same config as einkuenftige, not every transition is tested
+const prefixEinkuenfte = "/finanzielle-angaben/partner/partner-einkuenfte";
 
 export const testCasesPKHFormularFinanzielleAngabenPartner = [
   [
@@ -67,6 +71,126 @@ export const testCasesPKHFormularFinanzielleAngabenPartner = [
       prefix + "/unterhalt",
       prefix + "/keine-rolle",
       "/finanzielle-angaben/kinder/kinder-frage",
+    ],
+  ],
+  [
+    {
+      "partner-staatlicheLeistungen": "keine",
+      "partner-employmentType": "employed",
+      "partner-arbeitsweg": "publicTransport",
+      "partner-hasArbeitsausgaben": "yes",
+    },
+    [
+      prefixEinkuenfte + "/partner-staatliche-leistungen",
+      prefixEinkuenfte + "/partner-einkommen/partner-erwerbstaetig",
+      prefixEinkuenfte + "/partner-einkommen/partner-art",
+      prefixEinkuenfte + "/partner-einkommen/partner-netto-einkommen",
+      prefixEinkuenfte + "/partner-abzuege/partner-arbeitsweg",
+      prefixEinkuenfte + "/partner-abzuege/partner-opnv-kosten",
+      prefixEinkuenfte + "/partner-abzuege/partner-arbeitsplatz-entfernung",
+      prefixEinkuenfte +
+        "/partner-abzuege/partner-arbeitsausgaben/partner-arbeitsausgaben-frage",
+      prefixEinkuenfte +
+        "/partner-abzuege/partner-arbeitsausgaben/partner-uebersicht",
+      prefixEinkuenfte +
+        "/partner-abzuege/partner-arbeitsausgaben/partner-warnung",
+    ],
+  ],
+  [
+    { "partner-arbeitsweg": "bike" },
+    [
+      prefixEinkuenfte + "/partner-abzuege/partner-arbeitsweg",
+      prefixEinkuenfte + "/partner-abzuege/partner-keine-rolle",
+    ],
+  ],
+  [
+    {
+      "partner-receivesPension": "yes",
+      "partner-receivesSupport": "yes",
+      "partner-hasFurtherIncome": "yes",
+    },
+    [
+      prefixEinkuenfte + "/partner-rente-frage",
+      prefixEinkuenfte + "/partner-rente",
+      prefixEinkuenfte + "/partner-unterhalt-frage",
+      prefixEinkuenfte + "/partner-unterhalt",
+      prefixEinkuenfte + "/partner-leistungen/partner-frage",
+      prefixEinkuenfte + "/partner-weitere-einkuenfte/partner-frage",
+      prefixEinkuenfte + "/partner-weitere-einkuenfte/partner-uebersicht",
+      prefixEinkuenfte + "/partner-weitere-einkuenfte/partner-warnung",
+    ],
+  ],
+  [
+    { partnerHasBesondersAusgaben: "yes", partnerschaft: "yes" },
+    [
+      prefixEinkuenfte + "/partner-besonders-ausgaben",
+      prefixEinkuenfte + "/add-partner-besonders-ausgaben",
+      "/finanzielle-angaben/kinder/kinder-frage",
+    ],
+  ],
+  [
+    { "partner-staatlicheLeistungen": "buergergeld" },
+    [
+      prefixEinkuenfte + "/partner-staatliche-leistungen",
+      prefixEinkuenfte + "/partner-buergergeld",
+    ],
+  ],
+  [
+    { "partner-staatlicheLeistungen": "arbeitslosengeld" },
+    [
+      prefixEinkuenfte + "/partner-staatliche-leistungen",
+      prefixEinkuenfte + "/partner-arbeitslosengeld",
+    ],
+  ],
+  [
+    { partnerschaft: "yes", "partner-staatlicheLeistungen": "grundsicherung" },
+    [
+      prefixEinkuenfte + "/partner-staatliche-leistungen",
+      "/finanzielle-angaben/kinder/kinder-frage",
+    ],
+  ],
+  [
+    {
+      partnerschaft: "yes",
+      "partner-staatlicheLeistungen": "asylbewerberleistungen",
+    },
+    [
+      prefixEinkuenfte + "/partner-staatliche-leistungen",
+      "/finanzielle-angaben/kinder/kinder-frage",
+    ],
+  ],
+  [
+    {
+      "partner-employmentType": "selfEmployed",
+      "partner-selbststaendigMonatlichesEinkommen": "1000",
+      "partner-selbststaendigBruttoNetto": "brutto",
+      "partner-selbststaendigAbzuege": "100",
+    },
+    [
+      prefixEinkuenfte + "/partner-einkommen/partner-art",
+      prefixEinkuenfte + "/partner-einkommen/partner-selbststaendig",
+      prefixEinkuenfte + "/partner-einkommen/partner-selbststaendig-abzuege",
+      prefixEinkuenfte + "/partner-abzuege/partner-arbeitsweg",
+    ],
+  ],
+  [
+    {
+      "partner-hasWohngeld": CheckboxValue.on,
+      "partner-hasKrankengeld": CheckboxValue.on,
+      "partner-hasElterngeld": CheckboxValue.on,
+      "partner-hasKindergeld": CheckboxValue.on,
+      "partner-wohngeldAmount": "500",
+      "partner-krankengeldAmount": "500",
+      "partner-elterngeldAmount": "500",
+      "partner-kindergeldAmount": "500",
+    },
+    [
+      prefixEinkuenfte + "/partner-leistungen/partner-frage",
+      prefixEinkuenfte + "/partner-leistungen/partner-wohngeld",
+      prefixEinkuenfte + "/partner-leistungen/partner-krankengeld",
+      prefixEinkuenfte + "/partner-leistungen/partner-elterngeld",
+      prefixEinkuenfte + "/partner-leistungen/partner-kindergeld",
+      prefixEinkuenfte + "/partner-weitere-einkuenfte/partner-frage",
     ],
   ],
 ] as const satisfies TestCases<ProzesskostenhilfeFinanzielleAngabenContext>;
