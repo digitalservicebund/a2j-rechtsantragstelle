2590376ef6f68ce71743b1ad1a059a2ce31dec72
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-01-24 08:30:57 +0100

    re-check in generated files

---
+++ data/xml/generated/js/xoev-de/kosit/xoev/datentyp/din-91379_2022-08.js
@@ -0,0 +1,20 @@
+var cxml = require("@wikipathways/cxml");
+var Primitive = require('../../../../xml-primitives');
+
+cxml.register('urn:xoev-de:kosit:xoev:datentyp:din-91379_2022-08', exports, [
+	[Primitive, ['string'], []]
+], [
+	'datatypeA',
+	'datatypeB',
+	'datatypeC',
+	'datatypeD',
+	'datatypeE'
+], [
+	[0, 0, [], []],
+	[3, 1, [], []],
+	[3, 1, [], []],
+	[3, 1, [], []],
+	[3, 1, [], []],
+	[3, 1, [], []]
+], [
+]);
