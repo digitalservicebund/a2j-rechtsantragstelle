b16e4d5fc90d037616daf3d406f8a4ce3f0165da
aaschlote <alex.schlote@digitalservice.bund.de>
2025-02-11 13:51:37 +0100

    refactor: improve SummaryOverview component

--- app/components/summaryOverview/SummaryOverview.tsx
+++ app/components/summaryOverview/SummaryOverview.tsx
@@ -14,23 +14,26 @@ type Props = {
 const SummaryOverview = ({ navigation }: Props) => {
   return (
     <>
-      {navigation.map(({ title, boxes, id }) => {
-        return (
-          <div key={id}>
-            {title && (
-              <Heading
-                text={title}
-                tagName="p"
-                className="mt-40"
-                look="ds-heading-03-bold"
-              />
-            )}
-            {boxes.map((box) => (
-              <SummaryOverviewBox key={box.id} {...box} />
-            ))}
-          </div>
-        );
-      })}
+      {navigation.map(({ title, boxes, id }) => (
+        <div key={id}>
+          <Heading
+            text={title}
+            tagName="p"
+            className="mt-40"
+            look="ds-heading-03-bold"
+          />
+          {boxes.map(({ id, stepId, hiddenFields, sortedFields, title }) => (
+            <SummaryOverviewBox
+              key={id}
+              title={title}
+              stepId={stepId}
+              id={id}
+              hiddenFields={hiddenFields}
+              sortedFields={sortedFields}
+            />
+          ))}
+        </div>
+      ))}
     </>
   );
 };
