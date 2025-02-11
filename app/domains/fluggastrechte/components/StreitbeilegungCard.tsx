4dc241469f22609ba2dbeeb34afa9d7d3d0f5811
judithmh <40442827+judithmh@users.noreply.github.com>
2025-02-11 18:29:33 +0100

    add code improvementes to aussergerichtliche streitbeteiligung in summary page

--- app/domains/fluggastrechte/components/StreitbeilegungCard.tsx
+++ app/domains/fluggastrechte/components/StreitbeilegungCard.tsx
@@ -7,14 +7,14 @@ const TITLE = "Außergerichtliche Streitbeilegung";
 
 function getReasonsAgainstStreitbeilegung(
   userData: FluggastrechtContext,
-): string | undefined {
+): string {
   if (userData.streitbeilegungGruende === "no")
     return "Der Versuch einer außergerichtlichen Streitbeilegung hat nicht stattgefunden und es bestehen keine Bedenken dagegen.";
   if (userData.streitbeilegungGruende === "yesAirlineAgainst")
     return "Der Versuch einer außergerichtlichen Streitbeilegung hat nicht stattgefunden und es bestehen Bedenken, weil das bisherige Verhalten der Fluggesellschaft dagegen spricht.";
   if (userData.streitbeilegungGruende === "yesOtherReasons")
     return "Der Versuch einer außergerichtlichen Streitbeilegung hat nicht stattgefunden und es bestehen Bedenken dagegen.";
-  return undefined;
+  return "";
 }
 function StreitbeilegungCard({
   userData,
@@ -23,28 +23,22 @@ function StreitbeilegungCard({
   readonly userData: FluggastrechtContext;
   readonly translations: Translations;
 }) {
-  if (userData.streitbeilegung === "yes") {
-    return (
-      <SummaryDataOverviewCard
-        title={TITLE}
-        showValueHeading={false}
-        buttonUrl={`${FLOW_ID}/flugdaten/ersatzverbindung-daten`}
-        data={{
-          streitbeilegung:
-            "Der Versuch einer außergerichtlichen Streitbeilegung hat stattgefunden.",
-        }}
-        translations={translations}
-      />
-    );
-  }
+  const isStreitbeilegung = userData.streitbeilegung === "yes";
+  const buttonUrl = isStreitbeilegung
+    ? `${FLOW_ID}/grundvoraussetzungen/streitbeilegung`
+    : `${FLOW_ID}/grundvoraussetzungen/streitbeilegung-gruende`;
+
+  const data = {
+    streitbeilegung: isStreitbeilegung
+      ? "Der Versuch einer außergerichtlichen Streitbeilegung hat stattgefunden."
+      : getReasonsAgainstStreitbeilegung(userData),
+  };
   return (
     <SummaryDataOverviewCard
       title={TITLE}
       showValueHeading={false}
-      buttonUrl={`${FLOW_ID}/grundvoraussetzungen/streitbeilegung-gruende`}
-      data={{
-        streitbeilegung: getReasonsAgainstStreitbeilegung(userData),
-      }}
+      buttonUrl={buttonUrl}
+      data={data}
       translations={translations}
     />
   );
