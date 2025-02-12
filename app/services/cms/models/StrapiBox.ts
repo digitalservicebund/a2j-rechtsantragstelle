05423c8e06b0899d2042086d76a181869376bd13
khallad2 <khallad_3@hotmail.com>
2025-02-12 01:40:17 +0100

    ref: use optional instead of transform
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/services/cms/models/StrapiBox.ts
+++ app/services/cms/models/StrapiBox.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { omitNull } from "~/util/omitNull";
 import { HasOptionalStrapiIdSchema } from "./HasStrapiId";
 import { OptionalStrapiLinkIdentifierSchema } from "./HasStrapiLinkIdentifier";
 import { StrapiBackgroundSchema } from "./StrapiBackground";
@@ -11,12 +10,12 @@ import { getRichTextProps, StrapiParagraphSchema } from "./StrapiParagraph";
 export const StrapiBoxSchema = z
   .object({
     __component: z.literal("page.box"),
-    label: StrapiHeadingSchema.nullable().transform(omitNull),
-    heading: StrapiHeadingSchema.nullable().transform(omitNull),
+    label: StrapiHeadingSchema.optional(),
+    heading: StrapiHeadingSchema.optional(),
     content: StrapiParagraphSchema.nullable(),
     outerBackground: StrapiBackgroundSchema.nullable(),
     container: StrapiContainerSchema,
-    buttons: z.array(StrapiButtonSchema).nullable().transform(omitNull),
+    buttons: z.array(StrapiButtonSchema).optional(),
   })
   .merge(HasOptionalStrapiIdSchema)
   .merge(OptionalStrapiLinkIdentifierSchema)
