d50145602f72272ddeb530087de30b1e63a1c7c4
Khallad Sharafeldin <32259466+khallad2@users.noreply.github.com>
2025-02-12 17:56:10 +0100

    ref: migrate StrapiCookieBannerSchema (#1732)
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,6 @@ import Breadcrumbs from "./components/Breadcrumbs";
 import { CookieBanner } from "./components/cookieBanner/CookieBanner";
 import Footer from "./components/Footer";
 import Header from "./components/PageHeader";
-import { getCookieBannerProps } from "./services/cms/models/StrapiCookieBannerSchema";
 import { getFooterProps } from "./services/cms/models/StrapiFooter";
 import { ErrorBox } from "./services/errorPages/ErrorBox";
 import { getFeedbackData } from "./services/feedback/getFeedbackData";
@@ -228,7 +227,7 @@ function App() {
             )}
             target={skipToContentLinkTarget}
           />
-          <CookieBanner content={getCookieBannerProps(cookieBannerContent)} />
+          <CookieBanner content={cookieBannerContent} />
           <Header {...header} translations={pageHeaderTranslations} />
           <Breadcrumbs
             breadcrumbs={breadcrumbs}
