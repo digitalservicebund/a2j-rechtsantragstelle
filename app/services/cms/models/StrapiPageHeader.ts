37ca7d00004fc418403f6be3061607e5d568c66e
khallad2 <khallad_3@hotmail.com>
2025-02-12 12:24:15 +0100

    ref: remove unused merge
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/services/cms/models/StrapiPageHeader.ts
+++ app/services/cms/models/StrapiPageHeader.ts
@@ -1,11 +1,6 @@
 import { z } from "zod";
-import { HasOptionalStrapiIdSchema } from "./HasStrapiId";
-import { HasStrapiLocaleSchema } from "./HasStrapiLocale";
 
-export const StrapiPageHeaderSchema = z
-  .object({
-    title: z.string(),
-    linkLabel: z.string(),
-  })
-  .merge(HasOptionalStrapiIdSchema)
-  .merge(HasStrapiLocaleSchema);
+export const StrapiPageHeaderSchema = z.object({
+  title: z.string(),
+  linkLabel: z.string(),
+});
