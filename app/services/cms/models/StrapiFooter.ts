0518a3792df0955dc9750844623657d44fa28467
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-02-13 10:51:47 +0100

    refactor(getXXXProps): Migrate StrapiFooter

--- app/services/cms/models/StrapiFooter.ts
+++ app/services/cms/models/StrapiFooter.ts
@@ -1,6 +1,5 @@
+import omit from "lodash/omit";
 import { z } from "zod";
-import { type FooterProps } from "~/components/Footer";
-import { omitNull } from "~/util/omitNull";
 import { HasStrapiLocaleSchema } from "./HasStrapiLocale";
 import { StrapiImageOptionalSchema } from "./StrapiImage";
 import { StrapiLinkSchema } from "./StrapiLink";
@@ -12,17 +11,5 @@ export const StrapiFooterSchema = z
     paragraphs: z.array(StrapiParagraphSchema),
     links: z.array(StrapiLinkSchema),
   })
-  .merge(HasStrapiLocaleSchema);
-
-export type StrapiFooter = z.infer<typeof StrapiFooterSchema>;
-
-export const getFooterProps = (
-  cmsData: StrapiFooter,
-): Omit<FooterProps, "deletionLabel" | "showDeletionBanner"> => {
-  const { links, paragraphs, image } = cmsData;
-  return omitNull({
-    links,
-    paragraphs,
-    image,
-  });
-};
+  .merge(HasStrapiLocaleSchema)
+  .transform((cmsData) => omit(cmsData, "locale"));
