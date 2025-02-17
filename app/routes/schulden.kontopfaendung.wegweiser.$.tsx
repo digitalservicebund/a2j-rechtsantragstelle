35cfc08b56171deb9ccda273452e7f2805d15685
khallad2 <khallad_3@hotmail.com>
2025-02-17 07:50:50 +0100

    ref: remove umlaut from feature flag type
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/routes/schulden.kontopfaendung.wegweiser.$.tsx
+++ app/routes/schulden.kontopfaendung.wegweiser.$.tsx
@@ -6,6 +6,6 @@ export { action } from "~/routes/shared/vorabcheck.server";
 export { VorabcheckPage as default } from "~/routes/shared/components/VorabcheckPage";
 
 export const loader = async (opts: LoaderFunctionArgs) => {
-  await throw404IfFeatureFlagDisabled("showKontopfändungFlow");
+  await throw404IfFeatureFlagDisabled("showKontopfaendungFlow");
   return await vorabcheckLoader(opts);
 };
