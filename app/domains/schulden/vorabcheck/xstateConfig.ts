f30ddb509111698734b4263f607b04a6762ecffe
khallad2 <khallad_3@hotmail.com>
2025-02-18 11:21:07 +0100

    feat: Build 1st step Kontopfändung Ja-Nein flow
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/domains/schulden/vorabcheck/xstateConfig.ts
+++ app/domains/schulden/vorabcheck/xstateConfig.ts
@@ -7,26 +7,48 @@ export const kontopfaendungWegweiserXstateConfig = {
   states: {
     start: {
       on: {
-        SUBMIT: "basicinformationen",
+        SUBMIT: "kontopfaendung",
       },
     },
-    basicinformationen: {
+    kontopfaendung: {
       on: {
         SUBMIT: [
           {
-            target: "",
+            target: "Ergebnisseite",
             guard: ({ context }) => context.hasKontopfaendung === "no",
           },
           {
-            target: "",
+            target: "pKonto",
             guard: ({ context }) => context.hasKontopfaendung === "yes",
           },
           {
-            target: "",
+            target: "pKonto",
             guard: ({ context }) => context.hasKontopfaendung === "maybe",
           },
         ],
         BACK: "start",
+      },
+    },
+    Ergebnisseite: {
+      on: { BACK: "start" },
+    },
+    pKonto: {
+      on: {
+        SUBMIT: [
+          {
+            target: "Ergebnisseite",
+            guard: ({ context }) => context.hasPKonto === "no",
+          },
+          {
+            target: "",
+            guard: ({ context }) => context.hasPKonto === "yes",
+          },
+          {
+            target: "",
+            guard: ({ context }) => context.hasPKonto === "maybe",
+          },
+        ],
+        BACK: "kontopfaendung",
       },
     },
   },
