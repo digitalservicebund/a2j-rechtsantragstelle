4394327d0696276d37b16d02c86e8dd042ba826a
khallad2 <khallad_3@hotmail.com>
2025-02-19 14:53:13 +0100

    ref: Build kontopfaendung basisinfo - error message
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/domains/schulden/vorabcheck/context.ts
+++ app/domains/schulden/vorabcheck/context.ts
@@ -1,30 +1,49 @@
 import { z } from "zod";
 
-export const kontopfaendungType = z.enum(["nein", "ja", "weissNicht"]);
+const message = `Ungültiger Wert: Bitte wählen Sie einen der oben genannten Werte`;
 
-export const pKontoType = z.enum(["nein", "ja", "nichtAktiv", "bank"]);
+export const kontopfaendungType = z.enum(["nein", "ja", "weissNicht"], {
+  errorMap: () => {
+    return { message };
+  },
+});
 
-export const schuldenBeiType = z.enum([
-  "privat",
-  "behoerden",
-  "kredit",
-  "krankenkasse",
-  "rechnung",
-  "beitragsservice",
-  "unterhalt",
-  "strafe",
-  "finanzamt",
-  "hauptzollamt",
-  "nichtSagen",
-  "weissNicht",
-]);
+export const pKontoType = z.enum(["nein", "ja", "nichtAktiv", "bank"], {
+  errorMap: () => {
+    return { message };
+  },
+});
 
-export const euroSchwelleType = z.enum([
-  "nein",
-  "ja",
-  "weissNicht",
-  "unterschiedlich",
-]);
+export const schuldenBeiType = z.enum(
+  [
+    "privat",
+    "behoerden",
+    "kredit",
+    "krankenkasse",
+    "rechnung",
+    "beitragsservice",
+    "unterhalt",
+    "strafe",
+    "finanzamt",
+    "hauptzollamt",
+    "nichtSagen",
+    "weissNicht",
+  ],
+  {
+    errorMap: () => {
+      return { message };
+    },
+  },
+);
+
+export const euroSchwelleType = z.enum(
+  ["nein", "ja", "weissNicht", "unterschiedlich"],
+  {
+    errorMap: () => {
+      return { message };
+    },
+  },
+);
 
 export const context = {
   hasKontopfaendung: kontopfaendungType,
