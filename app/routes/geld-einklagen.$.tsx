bb28f007792d0cceaf52b272255c34580cf7caa4
khallad2 <khallad_3@hotmail.com>
2025-02-20 11:01:20 +0100

    fix: suggested fix for sentry syntax error - No identifiers allowed directly after numeric literal Signed-off-by: khallad2 <khallad_3@hotmail.com>
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/routes/geld-einklagen.$.tsx
+++ app/routes/geld-einklagen.$.tsx
@@ -1,8 +1,8 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { throw404IfFeatureFlagDisabled } from "~/services/errorPages/throw404";
-import { loader as stepLoader } from "./$";
+import { loader as stepLoader } from "./index-context";
 
-export { default } from "./$";
+export { default } from "./index-context";
 
 export const loader = async (opts: LoaderFunctionArgs) => {
   await throw404IfFeatureFlagDisabled("showGeldEinklagenFlow");
--- app/routes/prozesskostenhilfe._index.tsx
+++ app/routes/prozesskostenhilfe._index.tsx
@@ -1,7 +1,7 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { loader as stepLoader } from "~/routes/$";
+import { loader as stepLoader } from "~/routes/index-context";
 
-export { default } from "./$";
+export { default } from "./index-context";
 
 export const loader = async (opts: LoaderFunctionArgs) => {
   return await stepLoader(opts);
