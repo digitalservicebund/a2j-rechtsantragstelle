021f14299f64a3dd315b2ebebde90b9905396cad
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-02-25 15:24:49 +0100

    fix: update text

--- app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/compensationAmount/addWitnessesInfo.ts
+++ app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/compensationAmount/addWitnessesInfo.ts
@@ -4,9 +4,9 @@ import { PDF_MARGIN_HORIZONTAL } from "~/services/pdf/createPdfKitDocument";
 import { addNewPageInCaseMissingVerticalSpace } from "../../addNewPageInCaseMissingVerticalSpace";
 
 export const PLAINTIFF_WITNESSES_TEXT =
-  "Zum Beweis dieses Sachverhalts wird die klagende Partei im Prozessverlauf bei Bedarf Zeugen benennen.";
+  "Zum Beweis des unter I. dargestellten Sachverhaltes wird die klagende Partei im Prozessverlauf bei Bedarf Beweise anbieten.";
 export const PLAINTIFF_WITNESSES_MULTIPLE_PERSONS_TEXT =
-  "Zum Beweis dieses Sachverhalts wird die klagende Partei im Prozessverlauf bei Bedarf weitere Zeugen benennen.";
+  "Zum Beweis dieses Sachverhalts wird die klagende Partei im Prozessverlauf bei Bedarf weitere Beweise anbieten.";
 
 export const addWitnessesInfo = (
   doc: typeof PDFDocument,
