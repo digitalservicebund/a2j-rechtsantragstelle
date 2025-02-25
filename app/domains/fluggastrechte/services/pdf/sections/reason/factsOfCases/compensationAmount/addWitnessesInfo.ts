a0f37c8631da5975efcf9a0decfe71b17149094d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-02-25 16:09:54 +0100

    fix: rename variables

--- app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/compensationAmount/addWitnessesInfo.ts
+++ app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/compensationAmount/addWitnessesInfo.ts
@@ -3,9 +3,9 @@ import type { FluggastrechtContext } from "~/domains/fluggastrechte/formular/con
 import { PDF_MARGIN_HORIZONTAL } from "~/services/pdf/createPdfKitDocument";
 import { addNewPageInCaseMissingVerticalSpace } from "../../addNewPageInCaseMissingVerticalSpace";
 
-export const PLAINTIFF_WITNESSES_TEXT =
+export const WITNESS_EVIDENCE_TEXT =
   "Zum Beweis des unter I. dargestellten Sachverhaltes wird die klagende Partei im Prozessverlauf bei Bedarf Beweise anbieten.";
-export const PLAINTIFF_WITNESSES_MULTIPLE_PERSONS_TEXT =
+export const WITNESS_EVIDENCE_MULTIPLE_PERSONS_TEXT =
   "Zum Beweis dieses Sachverhalts wird die klagende Partei im Prozessverlauf bei Bedarf weitere Beweise anbieten.";
 
 export const addWitnessesInfo = (
@@ -15,7 +15,7 @@ export const addWitnessesInfo = (
   if (hasZeugen === "yes") {
     addNewPageInCaseMissingVerticalSpace(doc);
     doc.text(
-      `${isWeiterePersonen === "no" ? PLAINTIFF_WITNESSES_TEXT : PLAINTIFF_WITNESSES_MULTIPLE_PERSONS_TEXT}`,
+      `${isWeiterePersonen === "no" ? WITNESS_EVIDENCE_TEXT : WITNESS_EVIDENCE_MULTIPLE_PERSONS_TEXT}`,
       PDF_MARGIN_HORIZONTAL,
     );
   }
