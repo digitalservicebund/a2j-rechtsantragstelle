a0e993320721c4747735874ff20d56b603b6025c
khallad2 <khallad_3@hotmail.com>
2025-03-05 10:10:40 +0100

    feat: add unit test to test SideNavMobile in FlowNavigation.test
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/components/navigation/__test__/FlowNavigation.test.tsx
+++ app/components/navigation/__test__/FlowNavigation.test.tsx
@@ -1,21 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import FlowNavigation from "~/components/navigation/FlowNavigation";
+import { NavItem } from "~/components/navigation/NavItem";
 import { NavState } from "~/services/navigation/navState";
 
-// TODO: Test scenario of different viewports
+const dummyNavItems: NavItem[] = [
+  { destination: "/page1", label: "Page 1", state: NavState.Current },
+  { destination: "/page2", label: "Page 2", state: NavState.DoneCurrent },
+];
+
 describe("FlowNavigation", () => {
-  it("renders a navigation element", () => {
-    const navItems = [
-      {
-        destination: "/destination",
-        label: "navLabel",
-        state: NavState.Current,
-      },
-    ];
-
-    render(<FlowNavigation navItems={navItems} />);
-
+  it("renders a navigation element with the correct aria-label", () => {
+    render(
+      <FlowNavigation
+        navItems={dummyNavItems}
+        a11yLabels={{ menuLabel: "Main Menu", itemFinished: "", itemOpen: "" }}
+      />,
+    );
     const nav = screen.getByRole("navigation");
     expect(nav).toBeInTheDocument();
+    expect(nav).toHaveAttribute("aria-label", "Main Menu");
+  });
+
+  it("renders the SideNavMobile with fixed props", () => {
+    render(<FlowNavigation navItems={dummyNavItems} />);
+    expect(screen.getByText(/Bereich/i)).toBeInTheDocument();
+    expect(screen.getByText(/Anwaltliche Vertretung/i)).toBeInTheDocument();
   });
 });
