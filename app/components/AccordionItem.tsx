d154b84ab0e973a317229a22dc274ecf7246c1c2
khallad2 <khallad_3@hotmail.com>
2025-03-14 15:02:34 +0100

    ref: update styles and remove unnecessary div Signed-off-by: khallad2 <khallad_3@hotmail.com>
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/components/AccordionItem.tsx
+++ app/components/AccordionItem.tsx
@@ -49,17 +49,15 @@ export default function AccordionItem({
   }
 
   return (
-    <div key={id} className="mb-4">
-      <details className="group border-2 border-blue-300">
-        <summary className="cursor-pointer text-lg font-medium px-2 bg-blue-100 flex justify-between items-center">
-          <span className="p-4 ds-label-01-bold">{title}</span>
-          <span className="p-4 text-blue-800">
-            <span className="block group-open:hidden">⌄ Einblenden</span>
-            <span className="hidden group-open:block">⌃ Ausblenden</span>
-          </span>
-        </summary>
-        <RichText className="ds-body-01-reg" html={description} />
-      </details>
-    </div>
+    <details key={id} className="group border-2 border-blue-300">
+      <summary className="cursor-pointer text-lg font-medium px-2 bg-blue-100 flex justify-between items-center">
+        <span className="p-16 ds-label-01-bold">{title}</span>
+        <span className="p-16 text-blue-800">
+          <span className="block group-open:hidden">⌄ Einblenden</span>
+          <span className="hidden group-open:block">⌃ Ausblenden</span>
+        </span>
+      </summary>
+      <RichText className="ds-body-01-reg p-16" html={description} />
+    </details>
   );
 }
