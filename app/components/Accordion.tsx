c6854772acbcd5aa790c6806c311748661a9f212
khallad2 <khallad_3@hotmail.com>
2025-03-14 10:35:26 +0100

    ref: remove check for accordion items
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -9,32 +9,25 @@ export type AccordionProps = Readonly<{
 export default function Accordion({ items, className }: AccordionProps) {
   const [jsEnabled, setJsEnabled] = useState<boolean>(false);
   const [openIndex, setOpenIndex] = useState<number>(-1);
-  const hasItems = items && items.length > 0;
 
   useEffect(() => {
     setJsEnabled(true);
   }, []);
 
   return (
-    <div>
-      {hasItems && (
-        <section
-          className={`rounded-lg border-2 border-blue-500 ${className ?? ""}`}
-        >
-          {items.map((item, index) => (
-            <AccordionItem
-              key={item.id ?? index}
-              title={item.title}
-              description={item.description}
-              isOpen={openIndex === index}
-              onToggle={() =>
-                setOpenIndex((prev) => (prev === index ? -1 : index))
-              }
-              jsEnabled={jsEnabled}
-            />
-          ))}
-        </section>
-      )}
-    </div>
+    <section
+      className={`rounded-lg border-2 border-blue-500 ${className ?? ""}`}
+    >
+      {items.map((item, index) => (
+        <AccordionItem
+          key={item.id ?? index}
+          title={item.title}
+          description={item.description}
+          isOpen={openIndex === index}
+          onToggle={() => setOpenIndex((prev) => (prev === index ? -1 : index))}
+          jsEnabled={jsEnabled}
+        />
+      ))}
+    </section>
   );
 }
