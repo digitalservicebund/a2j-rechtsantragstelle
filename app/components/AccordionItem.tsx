a51a30a20458fefd65fb826521c48629fcebc940
khallad2 <khallad_3@hotmail.com>
2025-03-18 09:47:50 +0100

    refactor: update accordionItem styles.
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/components/AccordionItem.tsx
+++ app/components/AccordionItem.tsx
@@ -38,7 +38,7 @@ export default function AccordionItem({
     >
       <summary
         onClick={handleSummaryClick}
-        className="w-full flex justify-between items-center text-left cursor-pointer text-lg font-medium px-16 gap-x-8 bg-blue-100"
+        className="flex justify-between cursor-pointer bg-blue-100"
       >
         <span className="p-16 ds-label-01-bold">{title}</span>
         <span className="p-16 text-blue-800 flex items-center">
