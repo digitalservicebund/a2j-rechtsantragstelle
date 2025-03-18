45c47659bb77be1ccd71085aaf5afed6a093236c
khallad2 <khallad_3@hotmail.com>
2025-03-18 09:48:19 +0100

    refactor: add validation for accordionItems
    Signed-off-by: khallad2 <khallad_3@hotmail.com>

--- app/services/cms/models/StrapiAccordion.ts
+++ app/services/cms/models/StrapiAccordion.ts
@@ -3,12 +3,14 @@ import { HasStrapiIdSchema } from "~/services/cms/models/HasStrapiId";
 import { buildRichTextValidation } from "~/services/validation/richtext";
 
 export const StrapiAccordionSchema = z.object({
-  items: z.array(
-    z
-      .object({
-        title: z.string(),
-        description: buildRichTextValidation(),
-      })
-      .merge(HasStrapiIdSchema),
-  ),
+  items: z
+    .array(
+      z
+        .object({
+          title: z.string(),
+          description: buildRichTextValidation(),
+        })
+        .merge(HasStrapiIdSchema),
+    )
+    .nonempty(),
 });
