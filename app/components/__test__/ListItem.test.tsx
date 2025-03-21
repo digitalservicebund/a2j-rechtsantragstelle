dddac59420a0849ace8cbd9522883fec6efef863
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-03-21 17:57:58 +0100

    test(ListItem): add component tests

--- app/components/__test__/ListItem.test.tsx
+++ app/components/__test__/ListItem.test.tsx
@@ -14,9 +14,14 @@ describe("ListItem", () => {
     expect(getByText("headlineText")).toBeInTheDocument();
   });
 
-  it("render indexLabel", () => {
-    const { getByText } = render(<ListItem index={1} variant="numbered" />);
-    expect(getByText("1")).toBeInTheDocument();
+  describe("render indexLabel", () => {
+    const variantsWithIndexLabel = ["numbered", "stepByStep"] as const;
+    variantsWithIndexLabel.forEach((variant) => {
+      it(`should render indexLabel for ${variant}`, () => {
+        const { getByText } = render(<ListItem index={1} variant={variant} />);
+        expect(getByText("1")).toBeInTheDocument();
+      });
+    });
   });
 
   it("renders no indexLabel for unordered", () => {
@@ -37,5 +42,25 @@ describe("ListItem", () => {
       <ListItem buttons={[]} variant="numbered" />,
     );
     expect(queryByRole("button")).not.toBeInTheDocument();
+  });
+
+  it("should render an accordion", () => {
+    const { getByRole } = render(
+      <ListItem
+        variant="numbered"
+        accordion={{
+          items: [{ title: "title", description: "content", id: 1 }],
+        }}
+      />,
+    );
+    const accordionGroup = getByRole("group");
+    expect(accordionGroup).toBeInTheDocument();
+    expect(accordionGroup).toHaveTextContent("title");
+    expect(accordionGroup).toHaveTextContent("content");
+  });
+
+  it("should render vertical line in stepByStep", () => {
+    const { container } = render(<ListItem variant="stepByStep" />);
+    expect(container.getElementsByClassName("w-2 h-full")).toHaveLength(1);
   });
 });
