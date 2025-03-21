b08432985efd681857aa2a43e0577b1c854f8c03
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-03-21 10:49:04 +0100

    fix(Textarea): proposal to change stringOptionalSchema to try to match browser and zod behaviour on counting whitespaces and newlines

--- app/services/validation/stringOptional.ts
+++ app/services/validation/stringOptional.ts
@@ -3,6 +3,16 @@ import { TEXTAREA_CHAR_LIMIT } from "./inputlimits";
 
 export const stringOptionalSchema = z
   .string()
-  .trim()
-  .max(TEXTAREA_CHAR_LIMIT, { message: "max" })
+  //processing the string before validation.
+  .transform((string) => {
+    // remove possible whitespaces
+    return string.trim();
+  })
+  // using refine instead of max() to show validation message according to the string length
+  .refine(
+    (string) =>
+      // using replace to remove newlines
+      string.replace(/\r\n|\r|\n/g, "").length <= TEXTAREA_CHAR_LIMIT,
+    { message: "max" },
+  )
   .optional();
