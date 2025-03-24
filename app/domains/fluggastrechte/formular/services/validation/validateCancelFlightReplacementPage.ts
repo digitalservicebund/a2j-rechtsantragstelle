af6f9a698e4e285942d3c7a7fdcdb94af6336684
aaschlote <alex.schlote@digitalservice.bund.de>
2025-03-24 14:57:18 +0100

    refactor(validateCancelFlightReplacementPage): make some improvements

--- app/domains/fluggastrechte/formular/services/validation/validateCancelFlightReplacementPage.ts
+++ app/domains/fluggastrechte/formular/services/validation/validateCancelFlightReplacementPage.ts
@@ -2,36 +2,42 @@ import { z } from "zod";
 import { MultiFieldsValidationBaseSchema } from "~/domains/multiFieldsFlowValidation";
 import { isFieldEmptyOrUndefined } from "~/util/isFieldEmptyOrUndefined";
 
+const FIELDS_FOR_VALIDATION = [
+  "annullierungErsatzverbindungFlugnummer",
+  "annullierungErsatzverbindungAbflugsDatum",
+  "annullierungErsatzverbindungAbflugsZeit",
+  "annullierungErsatzverbindungAnkunftsDatum",
+  "annullierungErsatzverbindungAnkunftsZeit",
+];
+
+const getFieldsForValidation = (data: Record<string, string>) => {
+  return FIELDS_FOR_VALIDATION.map((path) => ({
+    value: data[path],
+    path: [path],
+  }));
+};
+
 export function validateCancelFlightReplacementPage(
   baseSchema: MultiFieldsValidationBaseSchema,
 ) {
   return baseSchema.superRefine((data, ctx) => {
-    const fieldsForValidation = [
-      "annullierungErsatzverbindungFlugnummer",
-      "annullierungErsatzverbindungAbflugsDatum",
-      "annullierungErsatzverbindungAbflugsZeit",
-      "annullierungErsatzverbindungAnkunftsDatum",
-      "annullierungErsatzverbindungAnkunftsZeit",
-    ];
-
-    const fields = fieldsForValidation.map((path) => ({
-      value: data[path],
-      path: [path],
-    }));
+    const fields = getFieldsForValidation(data);
 
     const isAnyFieldFilled = fields.some(
       ({ value }) => !isFieldEmptyOrUndefined(value),
     );
 
-    if (isAnyFieldFilled) {
-      for (const { value, path } of fields) {
-        if (isFieldEmptyOrUndefined(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "fillAllOrNone",
-            path,
-          });
-        }
+    if (!isAnyFieldFilled) {
+      return;
+    }
+
+    for (const { value, path } of fields) {
+      if (isFieldEmptyOrUndefined(value)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "fillAllOrNone",
+          path,
+        });
       }
     }
   });
