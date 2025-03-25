b624b4f06d28e7db5738318adac102755dcf071d
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-03-25 18:56:29 +0100

    fix(iban): non-standard ESM import on Node

--- app/services/validation/iban.ts
+++ app/services/validation/iban.ts
@@ -1,8 +1,11 @@
 import isIBAN from "validator/lib/isIBAN";
 import { z } from "zod";
 
+// @ts-expect-error 'validator' is using non-standard ESM exports, which forces us to unpack one nested "default" property in Node
+const isIbanCheck = isIBAN.default ?? isIBAN;
+
 export const ibanSchema = z
   .string()
   .toUpperCase()
   .transform((ibanInput) => ibanInput.replaceAll(" ", ""))
-  .refine(isIBAN, { message: "invalid_iban_format" });
+  .refine(isIbanCheck, { message: "invalid_iban_format" });
