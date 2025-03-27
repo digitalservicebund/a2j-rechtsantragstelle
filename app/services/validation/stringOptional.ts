cb1b0d106b00d177e34efa5f1de9d3b480c125ac
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-03-27 14:15:09 +0100

    fix(textarea): add more meaningful message to stringOptionalSchema

--- app/services/validation/stringOptional.ts
+++ app/services/validation/stringOptional.ts
@@ -4,5 +4,7 @@ import { TEXTAREA_CHAR_LIMIT } from "./inputlimits";
 export const stringOptionalSchema = z
   .string()
   .trim()
-  .max(TEXTAREA_CHAR_LIMIT, { message: "max" })
+  .max(TEXTAREA_CHAR_LIMIT, {
+    message: `You have reached a limit of ${TEXTAREA_CHAR_LIMIT} characters`,
+  })
   .optional();
