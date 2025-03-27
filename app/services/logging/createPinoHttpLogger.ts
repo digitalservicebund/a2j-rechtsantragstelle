19b9f95f9e8eb36edb373d080936eb14be3c0e21
aaschlote <alex.schlote@digitalservice.bund.de>
2025-03-27 16:37:01 +0100

    refactor(createPinoHttpLogger): adding custom log level and add userAgent in the logs

--- app/services/logging/createPinoHttpLogger.ts
+++ app/services/logging/createPinoHttpLogger.ts
@@ -10,14 +10,24 @@ export const createPinoHttpLogger = () => {
         (req.url ?? "").startsWith("/node_modules/") ||
         (req.url ?? "").startsWith("/data/"),
     },
+    customLogLevel: function (req, res, err) {
+      if (res.statusCode >= 400 && res.statusCode < 500) {
+        return "warn";
+      } else if (res.statusCode >= 500 || err) {
+        return "error";
+      } else if (res.statusCode >= 300 && res.statusCode < 400) {
+        return "silent";
+      }
+      return "info";
+    },
     transport: {
       target: "pino-pretty", // Makes logs readable (optional)
     },
     serializers: {
       req: (req) => ({
-        id: req.id,
         method: req.method,
         url: req.url,
+        userAgent: req.headers["user-agent"],
       }),
       res: (res) => ({
         statusCode: res.statusCode,
