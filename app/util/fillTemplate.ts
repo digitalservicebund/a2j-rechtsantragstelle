e4c10d8ba9c50d968a7e5240e29a6df5215b8acc
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-03-31 16:34:25 +0200

    add new function, recursivelyReplaceStrings(), to handle deeply-nested, possibly invisible objects

--- app/util/fillTemplate.ts
+++ app/util/fillTemplate.ts
@@ -25,6 +25,41 @@ export function interpolateSerializableObject<T>(
   ) as T;
 }
 
+export function recursivelyReplaceStrings(
+  content: unknown,
+  stringReplacements?: Replacements,
+): unknown {
+  if (!stringReplacements || !content) return content;
+  if (typeof content === "string") {
+    return mustache.render(content, stringReplacements);
+  }
+
+  // Need to filter out content that shouldn't be visible
+  if (
+    typeof content === "object" &&
+    "isVisible" in content &&
+    typeof content.isVisible === "string" &&
+    mustache.render(content.isVisible, stringReplacements) === "false"
+  ) {
+    return undefined;
+  }
+
+  if (Array.isArray(content)) {
+    return content
+      .map((item) => recursivelyReplaceStrings(item, stringReplacements))
+      .filter((item) => item !== undefined);
+  }
+
+  return Object.fromEntries(
+    Object.entries(content)
+      .map(([key, value]) => [
+        key,
+        recursivelyReplaceStrings(value, stringReplacements),
+      ])
+      .filter(([, value]) => value !== undefined),
+  );
+}
+
 export const throwIfFunction = (key: string, val: unknown) => {
   if (typeof val === "function")
     throw Error(`key ${key} is a function and can't be serialized`);
