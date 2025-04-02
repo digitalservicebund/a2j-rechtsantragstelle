69d49258bddde4a37ebc39ae998e00b27541e29e
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-04-02 12:17:00 +0200

    feat(Kontopfaendung): adjust variables to meet AC and add tests

--- app/domains/kontopfaendung/wegweiser/index.ts
+++ app/domains/kontopfaendung/wegweiser/index.ts
@@ -1,6 +1,27 @@
 import type { Flow } from "~/domains/flows.server";
 import { type KontopfaendungWegweiserContext } from "./context";
-import { getWegweiserStrings } from "./stringReplacements";
+import {
+  getPKontoStrings,
+  getPrivilegierteForderungStrings,
+  getErhoehungsbetragStrings,
+  getKindergeldStrings,
+  getWohngeldStrings,
+  getEinmalSozialleistungStrings,
+  getNachzahlungSozialUnter500Strings,
+  getNachzahlungSozialMehr500Strings,
+  getNachzahlungArbeitUnter500Strings,
+  getNachzahlungArbeitMehr500Strings,
+  getBuergergeldStrings,
+  getGrundsicherungStrings,
+  getAsylbewerberleistungStrings,
+  getPflegegeldSelbstStrings,
+  getPflegegeldFremdStrings,
+  getBehordenschuldenStrings,
+  getArbeitsentgeltEinmaligStrings,
+  getSelbststaendigStrings,
+  getAngestelltStrings,
+  getKinderStrings,
+} from "./stringReplacements";
 import { kontopfaendungWegweiserXstateConfig } from "./xStateConfig";
 
 export const kontopfaendungWegweiser = {
@@ -9,6 +30,25 @@ export const kontopfaendungWegweiser = {
   guards: {},
 
   stringReplacements: (userData: KontopfaendungWegweiserContext) => ({
-    ...getWegweiserStrings(userData),
+    ...getPKontoStrings(userData),
+    ...getPrivilegierteForderungStrings(userData),
+    ...getErhoehungsbetragStrings(userData),
+    ...getKindergeldStrings(userData),
+    ...getWohngeldStrings(userData),
+    ...getEinmalSozialleistungStrings(userData),
+    ...getNachzahlungSozialUnter500Strings(userData),
+    ...getNachzahlungSozialMehr500Strings(userData),
+    ...getNachzahlungArbeitUnter500Strings(userData),
+    ...getNachzahlungArbeitMehr500Strings(userData),
+    ...getBuergergeldStrings(userData),
+    ...getGrundsicherungStrings(userData),
+    ...getAsylbewerberleistungStrings(userData),
+    ...getPflegegeldSelbstStrings(userData),
+    ...getPflegegeldFremdStrings(userData),
+    ...getBehordenschuldenStrings(userData),
+    ...getArbeitsentgeltEinmaligStrings(userData),
+    ...getSelbststaendigStrings(userData),
+    ...getAngestelltStrings(userData),
+    ...getKinderStrings(userData),
   }),
 } satisfies Flow;
