ff4ef3f5be89b5f98c4d9e27b398d301398af5f9
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-04-11 12:56:00 +0200

    rename env distinct ID

--- app/services/analytics/posthogHelpers.ts
+++ app/services/analytics/posthogHelpers.ts
@@ -14,9 +14,10 @@ export function idFromCookie(cookie: string | null) {
   // Note: can't use cookie.parse(): https://github.com/remix-run/remix/discussions/5198
   // Returns ENVIRONMENT if posthog's distinct_id can't be extracted
   const { POSTHOG_API_KEY, ENVIRONMENT } = config();
-  if (!POSTHOG_API_KEY) return ENVIRONMENT;
+  const envId = `client-${ENVIRONMENT}`;
+  if (!POSTHOG_API_KEY) return envId;
   const parsedCookie = parse(cookie ?? "");
   const phCookieString = parsedCookie[`ph_${POSTHOG_API_KEY}_posthog`] ?? "{}";
   const phCookieObject = JSON.parse(phCookieString) as Record<string, string>;
-  return phCookieObject.distinct_id ?? ENVIRONMENT;
+  return phCookieObject.distinct_id ?? envId;
 }
