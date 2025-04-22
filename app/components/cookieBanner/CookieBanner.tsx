35e071012ac15bc6eef85fdedeeb6091e7ddda39
aaschlote <alex.schlote@digitalservice.bund.de>
2025-04-22 11:19:38 +0200

    Revert "Merge remote-tracking branch 'origin/main' into upgrade-react-router-7"
    This reverts commit 05869a32debffd952bb4bcfae03c4aacc37c8d7a, reversing
    changes made to de1abc4b9e9bc1b011488482686b8aa69e9843f3.

--- app/components/cookieBanner/CookieBanner.tsx
+++ app/components/cookieBanner/CookieBanner.tsx
@@ -1,4 +1,3 @@
-import { posthog } from "posthog-js";
 import { useContext, useEffect, useState } from "react";
 import { useFetcher, useLocation } from "react-router";
 import Button from "~/components/Button";
@@ -6,8 +5,8 @@ import Container from "~/components/Container";
 import { CookieConsentContext } from "~/components/cookieBanner/CookieConsentContext";
 import Heading, { type HeadingProps } from "~/components/Heading";
 import RichText, { type RichTextProps } from "~/components/RichText";
-import { config } from "~/services/env/web";
-import { useJsAvailable } from "~/services/useJsAvailable";
+import { usePosthog } from "~/services/analytics/PosthogContext";
+import { idFromCookie } from "~/services/analytics/posthogHelpers";
 import { StandaloneLink } from "../StandaloneLink";
 
 export const acceptCookiesFieldName = "accept-cookies";
@@ -27,49 +26,41 @@ export function CookieBanner({
   content: CookieBannerContentProps;
 }>) {
   const hasTrackingConsent = useContext(CookieConsentContext);
-  const { POSTHOG_API_KEY, POSTHOG_API_HOST } = config();
-  const [posthogLoaded, setPosthogLoaded] = useState(false);
-  const jsAvailable = useJsAvailable();
+  const { posthog, cookieHeader } = usePosthog();
+  const [clientJavaScriptAvailable, setClientJavaScriptAvailable] =
+    useState(false);
   const analyticsFetcher = useFetcher();
   const location = useLocation();
 
   useEffect(() => {
-    if (hasTrackingConsent && !posthogLoaded && POSTHOG_API_KEY) {
-      posthog.init(POSTHOG_API_KEY, {
-        api_host: POSTHOG_API_HOST,
-        session_recording: {
-          // Masking input and text elements to prevent sensitive data being shown on pages
-          maskTextSelector: "*",
-          maskAllInputs: true,
-        },
+    const captureConsent = async () => {
+      if (!hasTrackingConsent && posthog?.optOut) {
+        await posthog.optOut();
+      } else if (hasTrackingConsent && posthog?.optIn) {
+        await posthog.optIn();
+      }
+    };
 
-        cross_subdomain_cookie: false, // set cookie for subdomain only
+    captureConsent().catch((reason) => {
+      throw Error(reason);
+    });
+  }, [location, hasTrackingConsent, posthog]);
 
-        opt_out_persistence_by_default: true,
-        loaded: () => {
-          setPosthogLoaded(true);
-        },
-      });
-    } else if (!hasTrackingConsent && posthogLoaded) {
-      posthog.opt_out_capturing();
-    } else if (hasTrackingConsent && posthogLoaded) {
-      posthog.opt_in_capturing();
-    }
-  }, [
-    location,
-    hasTrackingConsent,
-    posthogLoaded,
-    POSTHOG_API_KEY,
-    POSTHOG_API_HOST,
-  ]);
-
-  const buttonAcceptCookieTestId = jsAvailable
+  const buttonAcceptCookieTestId = clientJavaScriptAvailable
     ? "accept-cookie_with_js"
     : "accept-cookie_without_js";
 
   useEffect(() => {
-    if (posthogLoaded) posthog.capture("$pageview");
-  }, [posthogLoaded, location.pathname]);
+    if (posthog?.capture)
+      posthog.capture({
+        event: "$pageview",
+        distinctId: idFromCookie(cookieHeader),
+      });
+  }, [posthog, location.pathname, cookieHeader]);
+
+  useEffect(() => {
+    setClientJavaScriptAvailable(true);
+  }, []);
 
   if (hasTrackingConsent !== undefined) {
     return <></>;
@@ -83,7 +74,9 @@ export function CookieBanner({
     >
       <analyticsFetcher.Form
         method="post"
-        action={`/action/set-analytics${jsAvailable ? "?js=1" : ""}`}
+        action={`/action/set-analytics${
+          clientJavaScriptAvailable ? "?js=1" : ""
+        }`}
       >
         <Container paddingTop="32" paddingBottom="40">
           <div className="ds-stack ds-stack-16">
