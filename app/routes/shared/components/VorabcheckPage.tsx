56a937c7f2363c26403e762215315014bf324f6f
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-04-22 14:20:18 +0200

    refactor(VorabcheckPage): simplify divs

--- app/routes/shared/components/VorabcheckPage.tsx
+++ app/routes/shared/components/VorabcheckPage.tsx
@@ -1,5 +1,4 @@
 import { useLoaderData } from "@remix-run/react";
-import Background from "~/components/Background";
 import Container from "~/components/Container";
 import { ProgressBar } from "~/components/form/ProgressBar";
 import ValidatedFlowForm from "~/components/form/ValidatedFlowForm";
@@ -16,25 +15,21 @@ export function VorabcheckPage() {
     buttonNavigationProps,
   } = useLoaderData<typeof loader>();
   return (
-    <Background backgroundColor="blue">
-      <div className="min-h-screen">
-        <Container paddingTop="24" paddingBottom="64">
-          <div className="ds-stack ds-stack-40">
-            <ProgressBar {...progressProps} />
-            <PageContent
-              content={contentElements}
-              className="ds-stack ds-stack-16"
-              fullScreen={false}
-            />
-            <ValidatedFlowForm
-              stepData={stepData}
-              csrf={csrf}
-              formElements={formElements}
-              buttonNavigationProps={buttonNavigationProps}
-            />
-          </div>
-        </Container>
+    <Container paddingTop="24" paddingBottom="64" backgroundColor="blue">
+      <div className="min-h-screen ds-stack ds-stack-40">
+        <ProgressBar {...progressProps} />
+        <PageContent
+          content={contentElements}
+          className="ds-stack ds-stack-16"
+          fullScreen={false}
+        />
+        <ValidatedFlowForm
+          stepData={stepData}
+          csrf={csrf}
+          formElements={formElements}
+          buttonNavigationProps={buttonNavigationProps}
+        />
       </div>
-    </Background>
+    </Container>
   );
 }
