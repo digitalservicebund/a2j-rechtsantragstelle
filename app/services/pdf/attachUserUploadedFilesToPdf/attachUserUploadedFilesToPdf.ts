e592e5a331755515ed390d296edec7f072e013fd
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-05-05 15:28:22 +0200

    feat(Attach Uploaded Files to PDF): remove extractSavedFileKeys as we don't need all the keys and add logic to get the keys by beleg

--- app/services/pdf/attachUserUploadedFilesToPdf/attachUserUploadedFilesToPdf.ts
+++ app/services/pdf/attachUserUploadedFilesToPdf/attachUserUploadedFilesToPdf.ts
@@ -1,21 +1,8 @@
 import { PDFDocument } from "pdf-lib";
+import { dokumenteContext } from "~/domains/beratungshilfe/formular/abgabe/dokumente/context";
 import { type Context } from "~/domains/contexts";
 import { type FlowId } from "~/domains/flowIds";
 import { downloadUserFileFromS3 } from "~/services/externalDataStorage/userFileS3Helpers";
-
-/**
- * Extracts the saved file keys from the user data.
- * @param userData The user data to extract the saved file keys from
- * @returns An array of saved file keys
- */
-const extractSavedFileKeys = (userData: Context) =>
-  Object.values(userData)
-    .filter((value) => Array.isArray(value))
-    .flatMap((files) => files)
-    .filter(
-      (file) => file.savedFileKey && typeof file.savedFileKey === "string",
-    )
-    .map((file) => file.savedFileKey as string);
 
 /**
  * Attaches user uploaded files into a PDF document.
@@ -32,26 +19,42 @@ export async function attachUserUploadedFilesToPdf(
   flowId: FlowId,
 ): Promise<Uint8Array> {
   const mainPdfDocument = await PDFDocument.load(mainPdfBuffer);
-  const savedFileKeys = extractSavedFileKeys(userData);
 
-  for (const savedFileKey of savedFileKeys) {
-    const userFileBuffer = await downloadUserFileFromS3(
-      sessionId,
-      flowId,
-      savedFileKey,
-    );
-    const userPdfFile = await PDFDocument.load(userFileBuffer);
+  // Get all document beleg keys in userData
+  const documentBelegKeys = Object.keys(userData).filter(
+    (key) => key in dokumenteContext && Array.isArray(userData[key]),
+  );
 
-    // Copy the pages from the user PDF file to the main PDF document
-    const copiedPdfFilePages = await mainPdfDocument.copyPages(
-      userPdfFile,
-      userPdfFile.getPageIndices(),
-    );
+  // Get last beleg where files were uploaded
+  const lastSelectedBeleg = documentBelegKeys[documentBelegKeys.length - 1];
 
-    // Add the copied pages to the main PDF document
-    for (const copiedPdfFilePage of copiedPdfFilePages) {
-      mainPdfDocument.addPage(copiedPdfFilePage);
+  // Get uploaded files for the beleg
+  const uploadedFiles = userData[lastSelectedBeleg] ?? [];
+
+  // Check if uploaded files is a valid array
+  if (Array.isArray(uploadedFiles)) {
+    for (const file of uploadedFiles) {
+      const userFileBuffer = await downloadUserFileFromS3(
+        sessionId,
+        flowId,
+        String(file.savedFileKey),
+      );
+      const userPdfFile = await PDFDocument.load(userFileBuffer);
+
+      // Copy the pages from the user PDF file to the main PDF document
+      const copiedPdfFilePages = await mainPdfDocument.copyPages(
+        userPdfFile,
+        userPdfFile.getPageIndices(),
+      );
+
+      // Add the copied pages to the main PDF document
+      for (const copiedPdfFilePage of copiedPdfFilePages) {
+        mainPdfDocument.addPage(copiedPdfFilePage);
+      }
     }
+    // Return main PDF document with uploaded files
+    return await mainPdfDocument.save();
   }
-  return mainPdfDocument.save();
+  // Return the original PDF document if no files are uploaded
+  return mainPdfBuffer;
 }
