5e6c3545f7215ce71675f22798ca5a7e75085e7d
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-05-06 15:46:42 +0200

    feat(Attach Uploaded Files to PDF): refactor function to remove repetition, add necessary condition and evidences, update testcases

--- app/domains/beratungshilfe/services/pdf/attachUserUploadedFilesToPdf/__test__/attachUserUploadedFilesToPdf.test.ts
+++ app/domains/beratungshilfe/services/pdf/attachUserUploadedFilesToPdf/__test__/attachUserUploadedFilesToPdf.test.ts
@@ -1,5 +1,6 @@
 import { PDFDocument } from "pdf-lib";
 import { vi, type Mock } from "vitest";
+import { CheckboxValue } from "~/components/inputs/Checkbox";
 import { type BeratungshilfeFormularContext } from "~/domains/beratungshilfe/formular";
 import { downloadUserFileFromS3 } from "~/services/externalDataStorage/userFileS3Helpers";
 import { attachUserUploadedFilesToPdf } from "../attachUserUploadedFilesToPdf";
@@ -93,6 +94,97 @@ describe("attachUserUploadedFilesToPdf", () => {
     const resultPdf = await PDFDocument.load(resultBuffer);
 
     expect(resultPdf.getPageCount()).toBe(4);
+    expect(resultBuffer).toBeInstanceOf(Uint8Array);
+  });
+
+  it("should return the original PDF buffer if no relevant documents are found", async () => {
+    const mockedUserData: BeratungshilfeFormularContext = {
+      keineLeistungenBeweis: undefined,
+      staatlicheLeistungen: "keine",
+    };
+
+    const resultBuffer = await attachUserUploadedFilesToPdf(
+      mockMainPDFBuffer,
+      mockedUserData,
+      sessionId,
+      flowId,
+    );
+
+    const resultPdf = await PDFDocument.load(resultBuffer);
+
+    expect(resultPdf.getPageCount()).toBe(1);
+    expect(resultBuffer).toBeInstanceOf(Uint8Array);
+  });
+
+  it("should attach files only for conditions that are true", async () => {
+    const mockedUserData: BeratungshilfeFormularContext = {
+      keineLeistungenBeweis: [
+        {
+          filename: "testfile1.pdf",
+          fileType: "application/pdf",
+          fileSize: 5938,
+          savedFileKey: "xyz123-lmn456-opq789",
+        },
+      ],
+      wohngeldBeweis: [
+        {
+          filename: "testfile2.pdf",
+          fileType: "application/pdf",
+          fileSize: 19718,
+          savedFileKey: "xyz789-lmn654-opq321",
+        },
+      ],
+      staatlicheLeistungen: "keine",
+      weitereseinkommen: {
+        wohngeld: CheckboxValue.on,
+        unterhaltszahlungen: CheckboxValue.off,
+        arbeitlosengeld: CheckboxValue.off,
+        kindergeld: CheckboxValue.off,
+        bafoeg: CheckboxValue.off,
+        krankengeld: CheckboxValue.off,
+        rente: CheckboxValue.off,
+        elterngeld: CheckboxValue.off,
+        insolvenzgeld: CheckboxValue.off,
+        ueberbrueckungsgeld: CheckboxValue.off,
+        others: CheckboxValue.off,
+      },
+    };
+
+    const resultBuffer = await attachUserUploadedFilesToPdf(
+      mockMainPDFBuffer,
+      mockedUserData,
+      sessionId,
+      flowId,
+    );
+
+    const resultPdf = await PDFDocument.load(resultBuffer);
+
+    expect(resultPdf.getPageCount()).toBe(3);
+    expect(resultBuffer).toBeInstanceOf(Uint8Array);
+  });
+  it("should not attach files for conditions that are false", async () => {
+    const mockedUserData: BeratungshilfeFormularContext = {
+      buergergeldBeweis: [
+        {
+          filename: "testfile1.pdf",
+          fileType: "application/pdf",
+          fileSize: 5938,
+          savedFileKey: "xyz123-lmn456-opq789",
+        },
+      ],
+      staatlicheLeistungen: "asylbewerberleistungen",
+    };
+
+    const resultBuffer = await attachUserUploadedFilesToPdf(
+      mockMainPDFBuffer,
+      mockedUserData,
+      sessionId,
+      flowId,
+    );
+
+    const resultPdf = await PDFDocument.load(resultBuffer);
+
+    expect(resultPdf.getPageCount()).toBe(1);
     expect(resultBuffer).toBeInstanceOf(Uint8Array);
   });
 });
--- app/domains/beratungshilfe/services/pdf/attachUserUploadedFilesToPdf/attachUserUploadedFilesToPdf.ts
+++ app/domains/beratungshilfe/services/pdf/attachUserUploadedFilesToPdf/attachUserUploadedFilesToPdf.ts
@@ -1,16 +1,30 @@
 import { PDFDocument } from "pdf-lib";
 import { type BeratungshilfeFormularContext } from "~/domains/beratungshilfe/formular/index";
-import { getStaatlicheLeistungenStrings } from "~/domains/beratungshilfe/formular/stringReplacements";
+import {
+  getStaatlicheLeistungenStrings,
+  weiteresEinkommenStrings,
+  ausgabenStrings,
+} from "~/domains/beratungshilfe/formular/stringReplacements";
 import { type FlowId } from "~/domains/flowIds";
+import { geldAnlagenStrings } from "~/domains/shared/formular/stringReplacements";
 import { downloadUserFileFromS3 } from "~/services/externalDataStorage/userFileS3Helpers";
 
 /**
- * Attaches user uploaded files into a PDF document.
- * @param mainPdfBuffer The PDF document as a Uint8Array
- * @param userData The user data containing saved file keys
- * @param sessionId The session Id for the request for authentication
- * @param flowId The flow ID for the request
- * @returns The resulting PDF document as a Uint8Array
+ * Attaches user-uploaded PDF files to a main PDF document.
+ *
+ * This function downloads relevant user-uploaded files from S3 based on the user's data
+ * and appends them to the main PDF document. Various criteria from the user's data
+ * determine which files are considered relevant. The function returns a new PDF buffer
+ * that contains all the pages from the original main PDF document followed by the
+ * pages from the user-uploaded files.
+ *
+ * @param mainPdfBuffer - The buffer of the main PDF document to which user files will be attached.
+ * @param userData - The user's data context, which includes information about uploaded files
+ *                   and the criteria for their relevance.
+ * @param sessionId - The session ID of the user, used for accessing user-specific data in S3.
+ * @param flowId - The flow ID that identifies the relevant process or form context.
+ * @returns A promise that resolves to a Uint8Array buffer representing the new PDF document
+ *          with the user-uploaded files attached.
  */
 export async function attachUserUploadedFilesToPdf(
   mainPdfBuffer: Uint8Array,
@@ -18,48 +32,107 @@ export async function attachUserUploadedFilesToPdf(
   sessionId: string,
   flowId: FlowId,
 ): Promise<Uint8Array> {
-  const relevantDocumentsPromises = [];
-
+  const relevantDocumentsPromises: Array<Array<Promise<Uint8Array>>> = [];
   const leistungen = getStaatlicheLeistungenStrings(userData);
-  if (leistungen.hasBuergergeld && userData.buergergeldBeweis) {
-    relevantDocumentsPromises.push(
-      userData.buergergeldBeweis.map((fileMeta) =>
-        downloadUserFileFromS3(sessionId, flowId, fileMeta.savedFileKey ?? ""),
-      ),
-    );
-  }
-  if (
-    leistungen.hasAsylbewerberleistungen &&
-    userData.asylbewerberleistungenBeweis
-  ) {
-    relevantDocumentsPromises.push(
-      userData.asylbewerberleistungenBeweis.map((fileMeta) =>
-        downloadUserFileFromS3(sessionId, flowId, fileMeta.savedFileKey ?? ""),
-      ),
-    );
-  }
-  const relevantDocuments = await Promise.all(relevantDocumentsPromises.flat());
-
+  const weiteresEinkommen = weiteresEinkommenStrings(userData);
+  const ausgaben = ausgabenStrings(userData);
+  const geldAnlagen = geldAnlagenStrings(userData);
+  const addRelevantDocuments = (
+    condition: boolean,
+    evidence: Array<{ savedFileKey?: string }> | undefined,
+  ) => {
+    if (condition && evidence) {
+      relevantDocumentsPromises.push(
+        evidence.map((fileMeta) =>
+          downloadUserFileFromS3(
+            sessionId,
+            flowId,
+            fileMeta.savedFileKey ?? "",
+          ),
+        ),
+      );
+    }
+  };
+  addRelevantDocuments(
+    weiteresEinkommen.arbeitslosenGeld,
+    userData.arbeitslosengeldBeweis,
+  );
+  addRelevantDocuments(weiteresEinkommen.wohngeld, userData.wohngeldBeweis);
+  addRelevantDocuments(weiteresEinkommen.bafoeg, userData.bafoegBeweis);
+  addRelevantDocuments(
+    weiteresEinkommen.krankengeld,
+    userData.krankengeldBeweis,
+  );
+  addRelevantDocuments(weiteresEinkommen.elterngeld, userData.elterngeldBeweis);
+  addRelevantDocuments(leistungen.hasBuergergeld, userData.buergergeldBeweis);
+  addRelevantDocuments(
+    leistungen.hasAsylbewerberleistungen,
+    userData.asylbewerberleistungenBeweis,
+  );
+  addRelevantDocuments(
+    leistungen.hasNoSozialleistung,
+    userData.keineLeistungenBeweis,
+  );
+  addRelevantDocuments(
+    leistungen.hasGrundsicherung,
+    userData.grundsicherungBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasLebensversicherung ?? false,
+    userData.lebensversicherungBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasBausparvertrag ?? false,
+    userData.bausparvertragBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasWertpapiere ?? false,
+    userData.wertpapiereBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasGutenhabenKrypto ?? false,
+    userData.guthabenkontoBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasGiroTagesSparkonto ?? false,
+    userData.sparkontoBeweis,
+  );
+  addRelevantDocuments(
+    geldAnlagen.hasGrundeigentum ?? false,
+    userData.grundeigentumBeweis,
+  );
+  addRelevantDocuments(
+    ausgaben.hasSchwangerschaft,
+    userData.schwangerschaftAngabeBeweis,
+  );
+  addRelevantDocuments(
+    ausgaben.hasSchwerbehinderung,
+    userData.schwerbehinderungBeweis,
+  );
+  addRelevantDocuments(
+    ausgaben.hasMedicalReasons,
+    userData.medizinischeGruendeBeweis,
+  );
+  addRelevantDocuments(
+    ausgaben.hasWeitereAusgaben ?? false,
+    userData.weitereAusgabenBeweis,
+  );
+  const relevantDocuments: Uint8Array[] = await Promise.all(
+    relevantDocumentsPromises.flat(),
+  );
   if (relevantDocuments.length === 0) {
     return mainPdfBuffer;
   }
-
   const mainPdfDocument = await PDFDocument.load(mainPdfBuffer);
-
   for (const file of relevantDocuments) {
     const userPdfFile = await PDFDocument.load(file);
-
-    // Copy the pages from the user PDF file to the main PDF document
     const copiedPdfFilePages = await mainPdfDocument.copyPages(
       userPdfFile,
       userPdfFile.getPageIndices(),
     );
-
-    // Add the copied pages to the main PDF document
     for (const copiedPdfFilePage of copiedPdfFilePages) {
       mainPdfDocument.addPage(copiedPdfFilePage);
     }
   }
-  // Return main PDF document with uploaded files
   return mainPdfDocument.save();
 }
