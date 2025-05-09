18906fb3871eebdd5eadcfade90a409ec4b64ca8
aaschlote <alex.schlote@digitalservice.bund.de>
2025-05-09 11:46:36 +0200

    refactor(reactRouterContext): add options for component props

--- .storybook/reactRouterContext.ts
+++ .storybook/reactRouterContext.ts
@@ -1,19 +1,25 @@
-import { ActionFunction, LoaderFunction, createRoutesStub } from "react-router";
+import {
+  ActionFunction,
+  LoaderFunction,
+  createRoutesStub,
+  useActionData,
+  useLoaderData,
+  useMatches,
+  useParams,
+} from "react-router";
 import { ComponentType, createElement } from "react";
 
 export function reactRouterContext(
-  component: ComponentType,
+  component: ComponentType<{
+    params: ReturnType<typeof useParams>;
+    loaderData: ReturnType<typeof useLoaderData>;
+    actionData: ReturnType<typeof useActionData>;
+    matches: ReturnType<typeof useMatches>;
+  }>,
   loader?: LoaderFunction,
   action?: ActionFunction,
 ) {
   return createElement(
-    createRoutesStub([
-      {
-        path: "/",
-        Component: () => createElement(component),
-        loader,
-        action,
-      },
-    ]),
+    createRoutesStub([{ path: "/", Component: component, loader, action }]),
   );
 }
