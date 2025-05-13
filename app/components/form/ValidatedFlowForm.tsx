b5b65dc4d27545e40743c53d528e229f8b755a96
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-05-13 17:01:59 +0200

    feat(PKH): implement the icon and string when feature flag value is test

--- app/components/form/ValidatedFlowForm.tsx
+++ app/components/form/ValidatedFlowForm.tsx
@@ -1,4 +1,6 @@
+import TimerOutlinedIcon from "@digitalservicebund/icons/TimerOutlined";
 import { ValidatedForm } from "@rvf/react-router";
+import { posthog } from "posthog-js";
 import { useLocation } from "react-router";
 import type { ButtonNavigationProps } from "~/components/form/ButtonNavigation";
 import { ButtonNavigation } from "~/components/form/ButtonNavigation";
@@ -25,6 +27,19 @@ function ValidatedFlowForm({
   const fieldNames = formElements.map((entry) => entry.name);
   const schema = schemaForFieldNames(fieldNames, pathname);
 
+  // THIS IS A TEMPORARY SOLUTION FOR THE AB TEST IN PROZESSKOSTENHILFE
+
+  // Manually override the feature flag for testing purposes
+  posthog.featureFlags.overrideFeatureFlags({
+    flags: { "conversion-rate-pkh-flow": "test" },
+  });
+
+  // Get the feature flag value for the ab test
+  // Using the "test" variant to display the estimated time string in the form
+  // When the variant value is "control" the string will not be displayed
+  const getPKHABTestFeatureFlag =
+    posthog.getFeatureFlag("conversion-rate-pkh-flow") === "test";
+
   return (
     <ValidatedForm
       method="post"
@@ -37,6 +52,16 @@ function ValidatedFlowForm({
       <input type="hidden" name={CSRFKey} value={csrf} />
       <div className="ds-stack ds-stack-40">
         <StrapiFormComponents components={formElements} />
+        {/*THIS IS A TEMPORARY SOLUTION FOR THE AB TEST IN PROZESSKOSTENHILFE*/}
+        {/* Rendering the estimated time string in the form for the ab test in prozesskostenhilfe */}
+        {pathname.includes("prozesskostenhilfe/formular") &&
+          getPKHABTestFeatureFlag && (
+            <span className="flex items-center ds-body-02-reg text-gray-900">
+              <TimerOutlinedIcon className="shrink-0 fill-gray-900 mr-4" />{" "}
+              {"Geschätzte Zeit: 20 Minuten"}
+            </span>
+          )}
+
         <ButtonNavigation {...buttonNavigationProps} />
       </div>
     </ValidatedForm>
