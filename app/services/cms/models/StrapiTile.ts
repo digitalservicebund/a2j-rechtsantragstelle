6a22597291e28930f9ae0bf2f957c63e345a7d84
aaschlote <alex.schlote@digitalservice.bund.de>
2025-05-20 09:34:42 +0200

    refactor(StrapiTile): generate paragraph with id

--- app/services/cms/models/StrapiTile.ts
+++ app/services/cms/models/StrapiTile.ts
@@ -4,6 +4,12 @@ import { buildRichTextValidation } from "~/services/validation/richtext";
 import { HasOptionalStrapiIdSchema } from "./HasStrapiId";
 import { StrapiImageOptionalSchema } from "./StrapiImage";
 
+export const createTileRenderer = (value: string): Partial<Renderer> => ({
+  paragraph({ text }) {
+    return `<p id="${value}-description" class="ds-subhead">${text}</p>`;
+  },
+});
+
 export const tileRenderer: Partial<Renderer> = {
   paragraph({ text }) {
     return `<p class="ds-subhead">${text}</p>`;
@@ -14,8 +20,19 @@ export const StrapiTileSchema = z
   .object({
     title: z.string(),
     value: z.string(),
-    description: buildRichTextValidation(tileRenderer).nullable(),
+    description: z.string().nullable(),
     image: StrapiImageOptionalSchema,
     tagDescription: z.string().nullable(),
   })
-  .merge(HasOptionalStrapiIdSchema);
+  .merge(HasOptionalStrapiIdSchema)
+  .transform((cmsData) => {
+    const tileRenderer = createTileRenderer(cmsData.value);
+    const parsed = buildRichTextValidation(tileRenderer)
+      .nullable()
+      .safeParse(cmsData.description);
+
+    return {
+      ...cmsData,
+      description: parsed.success ? parsed.data : null,
+    };
+  });
---
+++ app/services/cms/models/__test__/StrapiTile.test.ts
@@ -0,0 +1,71 @@
+import { StrapiTileSchema } from "../StrapiTile";
+
+describe("StrapiTileSchema", () => {
+  it("should fail the parse in case is missing the title", () => {
+    const undefinedTitle = {
+      title: undefined,
+      value: "something",
+      description: null,
+      image: null,
+      tagDescription: null,
+    };
+
+    const actual = StrapiTileSchema.safeParse(undefinedTitle);
+
+    expect(actual.success).toBe(false);
+  });
+
+  it("should fail the parse in case is missing the value", () => {
+    const undefinedValue = {
+      title: "something",
+      value: null,
+      description: null,
+      image: null,
+      tagDescription: null,
+    };
+
+    const actual = StrapiTileSchema.safeParse(undefinedValue);
+
+    expect(actual.success).toBe(false);
+  });
+
+  it("should success the parse in case the object is ok", () => {
+    const undefinedValue = {
+      title: "something",
+      value: "something",
+      description: null,
+      image: null,
+      tagDescription: null,
+    };
+
+    const actual = StrapiTileSchema.safeParse(undefinedValue);
+
+    expect(actual.success).toBe(true);
+    expect(actual.data).toEqual({
+      title: "something",
+      value: "something",
+      tagDescription: null,
+      description: null,
+    });
+  });
+
+  it("should parse the description with `p` with all the attributes", () => {
+    const undefinedValue = {
+      title: "something",
+      value: "something",
+      description: "description",
+      image: null,
+      tagDescription: null,
+    };
+
+    const actual = StrapiTileSchema.safeParse(undefinedValue);
+
+    expect(actual.success).toBe(true);
+    expect(actual.data).toEqual({
+      title: "something",
+      value: "something",
+      tagDescription: null,
+      description: `<p id="something-description" class="ds-subhead">description</p>`,
+    });
+  });
+});
