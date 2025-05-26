ff4fba551226783a209c3c50727cd6a5b6919b56
aaschlote <alex.schlote@digitalservice.bund.de>
2025-05-26 12:28:00 +0200

    refactor(useFocusFirstH1): use querySelector instead of querySelectorAll

--- app/components/useFocusFirstH1.tsx
+++ app/components/useFocusFirstH1.tsx
@@ -5,11 +5,11 @@ const HEADING_ONE_TAG = "h1";
 export const useFocusFirstH1 = () => {
   useEffect(() => {
     const focusTimer = setTimeout(() => {
-      const headingOneTag = document.querySelectorAll(HEADING_ONE_TAG);
+      const headingOneTag = document.querySelector(HEADING_ONE_TAG);
 
-      if (headingOneTag.length > 0) {
-        headingOneTag[0].setAttribute("tabindex", "-1");
-        headingOneTag[0].focus();
+      if (headingOneTag) {
+        headingOneTag.setAttribute("tabindex", "-1");
+        headingOneTag.focus();
       }
     }, 0);
     return () => clearTimeout(focusTimer);
