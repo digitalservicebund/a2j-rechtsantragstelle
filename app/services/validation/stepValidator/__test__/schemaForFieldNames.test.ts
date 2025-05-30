3f245b117c8f7490733e7fa12f826db85290f4ff
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-05-30 10:42:22 +0200

    Revert "test(schemaForFieldNames): minimal test to verify coverage"
    This reverts commit d0937c6b2c475c78c54810ddafc1cc35eaf099bf.

--- app/services/validation/stepValidator/__test__/schemaForFieldNames.test.ts
+++
@@ -1,7 +0,0 @@
-import { schemaForFieldNames } from "../schemaForFieldNames";
-
-describe("schemaForFieldNames", () => {
-  test("should throw for pathnames without flowId", () => {
-    expect(() => schemaForFieldNames([], "/not/a/flowId")).toThrow();
-  });
-});
