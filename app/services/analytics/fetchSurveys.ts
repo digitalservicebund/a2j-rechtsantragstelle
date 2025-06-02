f21d217709b078ba7d9311eb7f96896c16152dc0
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-02 17:03:18 +0200

    fix(fetchSurvey): use getSurveys instead of getActiveMatchingSurveys

--- app/services/analytics/fetchSurveys.ts
+++ app/services/analytics/fetchSurveys.ts
@@ -2,7 +2,7 @@ import type { PostHog, Survey } from "posthog-js";
 
 export const fetchSurvey = (surveyId: string, posthogClient?: PostHog) => {
   let survey: Survey | undefined;
-  posthogClient?.getActiveMatchingSurveys((surveys) => {
+  posthogClient?.getSurveys((surveys) => {
     survey = surveys.find((survey) => survey.id === surveyId);
   });
   return survey;
