173fc3f534f3d42d0adcedad248cae24f01b088a
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-03 10:58:05 +0200

    chore(useAnalytics): make to ts + add comment

--- app/services/analytics/useAnalytics.ts
+++ app/services/analytics/useAnalytics.ts
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 
 export const AnalyticsContext = createContext<{
   posthogClient: PostHog | undefined;
-  hasTrackingConsent: boolean | undefined;
+  hasTrackingConsent: boolean | undefined; // hasTrackingConsent is undefined until the user responds to the cookie banner, and then will be true/false depending on the response
 }>({
   posthogClient: undefined,
   hasTrackingConsent: undefined,
