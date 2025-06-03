48aa4872daaf9f294442f674a0d3d983b065dcb7
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-03 20:01:56 +0200

    fix(ReportProblem): simplify Context & fix tests

--- app/services/analytics/useAnalytics.ts
+++ app/services/analytics/useAnalytics.ts
@@ -2,8 +2,8 @@ import { type PostHog } from "posthog-js";
 import { createContext, useContext } from "react";
 
 export const AnalyticsContext = createContext<{
-  posthogClient: PostHog | undefined;
-  hasTrackingConsent: boolean | undefined; // hasTrackingConsent is undefined until the user responds to the cookie banner, and then will be true/false depending on the response
+  posthogClient?: PostHog;
+  hasTrackingConsent?: boolean; // hasTrackingConsent is undefined until the user responds to the cookie banner, and then will be true/false depending on the response
 }>({
   posthogClient: undefined,
   hasTrackingConsent: undefined,
