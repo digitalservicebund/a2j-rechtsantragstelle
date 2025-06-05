3010b664d96dff43675ff5ea4615ee1354395a93
aaschlote <alex.schlote@digitalservice.bund.de>
2025-06-05 16:22:10 +0200

    refactor(CustomInput): force to focus in the input when it has an error

--- app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
+++ app/components/inputs/autoSuggestInput/customComponents/CustomInput.tsx
@@ -1,3 +1,5 @@
+import { useField } from "@rvf/react-router";
+import { useEffect } from "react";
 import {
   components,
   type InputProps,
@@ -6,6 +8,7 @@ import {
 } from "react-select";
 import type { DataListOptions } from "~/services/dataListOptions/getDataListOptions";
 import { INPUT_CHAR_LIMIT } from "~/services/validation/inputlimits";
+import { isFieldEmptyOrUndefined } from "~/util/isFieldEmptyOrUndefined";
 
 //Will become obsolete with the next version bump: https://github.com/JedWatson/react-select/pull/6016
 type CustomSelectProps = Props<
@@ -18,22 +21,52 @@ type CustomSelectProps = Props<
 
 const CustomInput = (props: InputProps<DataListOptions, false>) => {
   const selectProps = props.selectProps as CustomSelectProps;
+
+  const field = useField(props.selectProps.id ?? "");
+  const hasError = !isFieldEmptyOrUndefined(field.error() ?? "");
+  const divInputId = `input-div-${props.selectProps.id}`;
+
+  useEffect(() => {
+    const div = document.querySelector<HTMLDivElement>(`#${divInputId}`);
+    if (!div) return;
+
+    const handleFocus = () => {
+      const input = div.querySelector("input");
+      if (input) {
+        input.focus();
+      }
+    };
+
+    div.addEventListener("focus", handleFocus);
+
+    return () => {
+      div.removeEventListener("focus", handleFocus);
+    };
+  }, [divInputId]);
+
   return (
-    <components.Input
-      // avoid to clear the previous auto suggestion input when press enter
-      // this cannot be tested, thus manually tested with device(s)
-      onKeyDown={(e) => {
-        if (e.key === "Enter" && !props.selectProps.menuIsOpen) {
-          e.preventDefault();
-        }
-      }}
-      {...props}
-      maxLength={INPUT_CHAR_LIMIT}
-      aria-describedby={selectProps["aria-describedby"]}
-      aria-required={props.selectProps.className?.includes(
-        "auto-suggest-input-required",
-      )}
-    />
+    <div
+      id={divInputId}
+      tabIndex={-1}
+      className="inline-grid col-start-1 row-start-1"
+      ref={hasError ? field.refs.controlled() : null}
+    >
+      <components.Input
+        // avoid to clear the previous auto suggestion input when press enter
+        // this cannot be tested, thus manually tested with device(s)
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && !props.selectProps.menuIsOpen) {
+            e.preventDefault();
+          }
+        }}
+        {...props}
+        maxLength={INPUT_CHAR_LIMIT}
+        aria-describedby={selectProps["aria-describedby"]}
+        aria-required={props.selectProps.className?.includes(
+          "auto-suggest-input-required",
+        )}
+      />
+    </div>
   );
 };
 export default CustomInput;
