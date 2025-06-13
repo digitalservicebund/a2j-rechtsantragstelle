359ea437ce25b5c06f7126141e22f51ef9452806
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-13 13:21:03 +0200

    refactor(EstimatedTime): prefer gap over margin

--- app/components/EstimatedTime.tsx
+++ app/components/EstimatedTime.tsx
@@ -1,8 +1,8 @@
 import TimerOutlinedIcon from "@digitalservicebund/icons/TimerOutlined";
 
 export const EstimatedTime = () => (
-  <span className="flex items-center ds-body-02-reg text-gray-900">
-    <TimerOutlinedIcon className="shrink-0 fill-gray-900 mr-4" />
+  <span className="flex gap-4 ds-body-02-reg text-gray-900">
+    <TimerOutlinedIcon className="shrink-0 fill-gray-900" />
     Geschätzte Zeit: 20 Minuten
   </span>
 );
