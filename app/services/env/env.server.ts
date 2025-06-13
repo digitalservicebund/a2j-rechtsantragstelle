31746e438b9b026e2ee4cfca1341b17e6842c04f
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-13 12:36:37 +0200

    feat(env): dont cache env vars

--- app/services/env/env.server.ts
+++ app/services/env/env.server.ts
@@ -17,41 +17,35 @@ type Config = {
   S3_DATA_STORAGE_BUCKET_NAME: string;
 };
 
-let instance: Config | undefined = undefined;
-
 export function config(): Config {
-  if (instance === undefined) {
-    const STRAPI_API = process.env.STRAPI_API?.trim();
-    const ENVIRONMENT = process.env.ENVIRONMENT?.trim() ?? "development";
+  const STRAPI_API = process.env.STRAPI_API?.trim();
+  const ENVIRONMENT = process.env.ENVIRONMENT?.trim() ?? "development";
 
-    instance = {
-      STRAPI_API: STRAPI_API ?? "",
-      STRAPI_HOST: STRAPI_API?.replace("/api/", "") ?? "",
-      STRAPI_ACCESS_KEY: process.env.STRAPI_ACCESS_KEY?.trim() ?? "",
-      CMS: process.env.CMS?.trim() ?? "FILE",
-      REDIS_ENDPOINT: process.env.REDIS_ENDPOINT?.trim() ?? "localhost:6380",
-      REDIS_PASSWORD: process.env.REDIS_PASSWORD?.trim() ?? "",
-      FIT_CONNECT_ADAPTER_ENDPOINT:
-        process.env.FIT_CONNECT_ADAPTER_ENDPOINT?.trim() ??
-        "http://localhost:8080",
-      COOKIE_SESSION_SECRET:
-        process.env.COOKIE_SESSION_SECRET?.trim() ?? "s3cr3t",
-      CONTENT_FILE_PATH:
-        process.env.CONTENT_FILE_PATH?.trim() ?? "./content.json",
-      ENVIRONMENT,
-      CSP_REPORT_URI: process.env.CSP_REPORT_URI?.trim(),
-      S3_REGION: process.env.AWS_S3_REGION?.trim() ?? "eu-central-1",
-      S3_ENDPOINT:
-        process.env.S3_ENDPOINT?.trim() ??
-        "https://s3.localhost.localstack.cloud:4566",
-      S3_DATA_STORAGE_ACCESS_KEY:
-        process.env.S3_DATA_STORAGE_ACCESS_KEY?.trim() ?? "test",
-      S3_DATA_STORAGE_SECRET_KEY:
-        process.env.S3_DATA_STORAGE_SECRET_KEY?.trim() ?? "test",
-      S3_DATA_STORAGE_BUCKET_NAME:
-        process.env.S3_DATA_STORAGE_BUCKET_NAME?.trim() ?? "a2j-data-storage",
-    };
-  }
-
-  return instance;
+  return {
+    STRAPI_API: STRAPI_API ?? "",
+    STRAPI_HOST: STRAPI_API?.replace("/api/", "") ?? "",
+    STRAPI_ACCESS_KEY: process.env.STRAPI_ACCESS_KEY?.trim() ?? "",
+    CMS: process.env.CMS?.trim() ?? "FILE",
+    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT?.trim() ?? "localhost:6380",
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD?.trim() ?? "",
+    FIT_CONNECT_ADAPTER_ENDPOINT:
+      process.env.FIT_CONNECT_ADAPTER_ENDPOINT?.trim() ??
+      "http://localhost:8080",
+    COOKIE_SESSION_SECRET:
+      process.env.COOKIE_SESSION_SECRET?.trim() ?? "s3cr3t",
+    CONTENT_FILE_PATH:
+      process.env.CONTENT_FILE_PATH?.trim() ?? "./content.json",
+    ENVIRONMENT,
+    CSP_REPORT_URI: process.env.CSP_REPORT_URI?.trim(),
+    S3_REGION: process.env.AWS_S3_REGION?.trim() ?? "eu-central-1",
+    S3_ENDPOINT:
+      process.env.S3_ENDPOINT?.trim() ??
+      "https://s3.localhost.localstack.cloud:4566",
+    S3_DATA_STORAGE_ACCESS_KEY:
+      process.env.S3_DATA_STORAGE_ACCESS_KEY?.trim() ?? "test",
+    S3_DATA_STORAGE_SECRET_KEY:
+      process.env.S3_DATA_STORAGE_SECRET_KEY?.trim() ?? "test",
+    S3_DATA_STORAGE_BUCKET_NAME:
+      process.env.S3_DATA_STORAGE_BUCKET_NAME?.trim() ?? "a2j-data-storage",
+  };
 }
