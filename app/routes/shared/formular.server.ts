86e94713be4c365fa737a547976f6340b48bf741
aaschlote <alex.schlote@digitalservice.bund.de>
2025-06-18 18:22:40 +0200

    refactor(formular.server.ts): use new functions in the formular.server.ts

--- app/routes/shared/formular.server.ts
+++ app/routes/shared/formular.server.ts
@@ -4,29 +4,17 @@ import { data, redirectDocument } from "react-router";
 import { parsePathname } from "~/domains/flowIds";
 import { flows } from "~/domains/flows.server";
 import { sendCustomAnalyticsEvent } from "~/services/analytics/customEvent";
-import { getArraySummaryData } from "~/services/array/getArraySummaryData";
 import { resolveArraysFromKeys } from "~/services/array/resolveArraysFromKeys";
-import {
-  fetchFlowPage,
-  fetchMeta,
-  fetchMultipleTranslations,
-} from "~/services/cms/index.server";
-import { isStrapiArraySummary } from "~/services/cms/models/isStrapiArraySummary";
-import { isStrapiSelectComponent } from "~/services/cms/models/isStrapiSelectComponent";
-import { buildFormularServerTranslations } from "~/services/flow/formular/buildFormularServerTranslations";
 import { addPageDataToUserData } from "~/services/flow/pageData";
 import { buildFlowController } from "~/services/flow/server/buildFlowController";
 import { executeAsyncFlowActionByStepId } from "~/services/flow/server/executeAsyncFlowActionByStepId";
 import { insertIndexesIntoPath } from "~/services/flow/stepIdConverter";
 import { navItemsFromStepStates } from "~/services/flowNavigation.server";
 import { logWarning } from "~/services/logging";
-import { stepMeta } from "~/services/meta/formStepMeta";
-import { parentFromParams } from "~/services/params";
 import { validatedSession } from "~/services/security/csrf/validatedSession.server";
 import { getSessionManager, updateSession } from "~/services/session.server";
 import { deleteArrayItem } from "~/services/session.server/arrayDeletion";
 import { getMigrationData } from "~/services/session.server/crossFlowMigration";
-import { fieldsFromContext } from "~/services/session.server/fieldsFromContext";
 import { updateMainSession } from "~/services/session.server/updateSessionInHeader";
 import {
   deleteUserFile,
@@ -34,9 +22,8 @@ import {
   uploadUserFile,
 } from "~/services/upload/fileUploadHelpers.server";
 import { validateFormData } from "~/services/validation/validateFormData.server";
-import { applyStringReplacement } from "~/util/applyStringReplacement";
-import { getButtonNavigationProps } from "~/util/buttonProps";
 import { filterFormData } from "~/util/filterFormData";
+import { buildContentData } from "./formular/contentData/buildContentData";
 import { getUserDataAndFlow } from "./formular/userDataAndFlow/getUserDataAndFlow";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
@@ -60,89 +47,31 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const { pathname } = new URL(request.url);
   const cookieHeader = request.headers.get("Cookie");
 
-  const [formPageContent, parentMeta, translations] = await Promise.all([
-    fetchFlowPage("form-flow-pages", flowId, stepId),
-    fetchMeta({ filterValue: parentFromParams(pathname, params) }),
-    fetchMultipleTranslations([
-      `${flowId}/menu`,
-      "defaultTranslations",
-      flowId,
-      `${flowId}/summaryPage`,
-      "accessibility",
-    ]),
-  ]);
-
-  const arrayCategories = formPageContent.pre_form
-    .filter(isStrapiArraySummary)
-    .map((strapiSummary) => strapiSummary.category);
-
-  const arraySummaryData = getArraySummaryData(
-    arrayCategories,
-    flowController.getRootMeta()?.arrays,
+  const contentData = await buildContentData(
+    request,
+    params,
     userDataWithPageData,
   );
 
-  const migrationData = await getMigrationData(
+  const stringTranslations = contentData.getStringTranslations();
+  const translations = contentData.getTranslations();
+  const cmsContent = contentData.getCMSContent();
+  const formElements = contentData.getFormElements();
+  const meta = contentData.getMeta(currentFlow);
+  const migration = contentData.getMigration(currentFlow);
+  const arraySummaryData = contentData.arraySummaryData(flowController);
+  const stepData = contentData.getStepData();
+  const buttonNavigationProps = contentData.getButtonNavigationProps(
+    flowController,
+    pathname,
     stepId,
-    flowId,
-    currentFlow,
-    cookieHeader,
+    arrayIndexes,
   );
-
-  const { stringTranslations, cmsContent } =
-    await buildFormularServerTranslations({
-      currentFlow,
-      flowTranslations: translations[flowId],
-      migrationData,
-      arrayCategories,
-      overviewTranslations: translations[`${flowId}/summaryPage`],
-      formPageContent,
-      userDataWithPageData,
-    });
-
-  // Inject heading into <legend> inside radio groups
-  // TODO: only do for pages with *one* select?
-  const formElements = cmsContent.formContent.map((strapiFormElement) => {
-    if (
-      isStrapiSelectComponent(strapiFormElement) &&
-      strapiFormElement.label === null &&
-      cmsContent.heading
-    )
-      strapiFormElement.altLabel = cmsContent.heading;
-    return strapiFormElement;
-  });
-
-  const meta = applyStringReplacement(
-    stepMeta(formPageContent.pageMeta, parentMeta),
-    stringTranslations,
-  );
-
-  // Retrieve user data for current step
-  const fieldNames = formPageContent.form.map((entry) => entry.name);
-  const stepData = fieldsFromContext(userDataWithPageData, fieldNames);
 
   const { headers, csrf } = await updateMainSession({
     cookieHeader,
     flowId,
     stepId,
-  });
-
-  const backDestination = flowController.getPrevious(stepId);
-
-  const backDestinationWithArrayIndexes =
-    backDestination && arrayIndexes
-      ? insertIndexesIntoPath(pathname, backDestination, arrayIndexes)
-      : backDestination;
-
-  const defaultStrings = translations.defaultTranslations;
-
-  const buttonNavigationProps = getButtonNavigationProps({
-    backButtonLabel:
-      cmsContent.backButtonLabel ?? defaultStrings.backButtonDefaultLabel,
-    nextButtonLabel:
-      cmsContent.nextButtonLabel ?? defaultStrings.nextButtonDefaultLabel,
-    isFinal: flowController.isFinal(stepId),
-    backDestination: backDestinationWithArrayIndexes,
   });
 
   const navItems =
@@ -162,17 +91,7 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
       formElements,
       heading: cmsContent.heading,
       meta,
-      migration: {
-        userData: migrationData,
-        sortedFields:
-          "migration" in currentFlow
-            ? currentFlow.migration?.sortedFields
-            : undefined,
-        buttonUrl:
-          "migration" in currentFlow
-            ? currentFlow.migration?.buttonUrl
-            : undefined,
-      },
+      migration,
       navItems,
       postFormContent: cmsContent.postFormContent,
       preHeading: cmsContent.preHeading,
