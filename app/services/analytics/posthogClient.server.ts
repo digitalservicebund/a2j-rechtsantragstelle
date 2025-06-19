88c9c1c10104f696c30d8409cd5c211c4197a93b
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-19 13:42:51 +0200

    fix: restore old API_KEY check & add tests

--- app/services/analytics/posthogClient.server.ts
+++ app/services/analytics/posthogClient.server.ts
@@ -3,7 +3,7 @@ import { POSTHOG_API_HOST } from "./config";
 import { config } from "../env/public";
 
 const { POSTHOG_API_KEY } = config();
-const posthogNodeClient = POSTHOG_API_KEY
+const posthogNodeClient = POSTHOG_API_KEY?.startsWith("phc_")
   ? new PostHog(POSTHOG_API_KEY, { host: POSTHOG_API_HOST })
   : undefined;
 
