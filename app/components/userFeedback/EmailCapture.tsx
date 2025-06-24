88ae35155ec4360e2a63258efed23a54f27c6e8f
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-06-24 07:59:53 +0200

    build out action, add hidden inputs to handle CSRF and url redirection

--- app/components/userFeedback/EmailCapture.tsx
+++ app/components/userFeedback/EmailCapture.tsx
@@ -1,11 +1,13 @@
 import { useField } from "@rvf/react-router";
-import { useFetcher } from "react-router";
+import { useFetcher, useLoaderData, useLocation } from "react-router";
 import Button, { type ButtonProps } from "~/components/Button";
 import ButtonContainer from "~/components/ButtonContainer";
 import { type ErrorMessageProps } from "~/components/inputs";
 import Input from "~/components/inputs/Input";
 import InputLabel from "~/components/inputs/InputLabel";
 import RichText from "~/components/RichText";
+import { type loader } from "~/routes/shared/formular.server";
+import { CSRFKey } from "~/services/security/csrf/csrfKey";
 
 export type EmailCaptureProps = {
   name: string;
@@ -27,6 +29,8 @@ export const EmailCapture = ({
 }: EmailCaptureProps) => {
   const field = useField(name);
   const fetcher = useFetcher();
+  const { csrf } = useLoaderData<typeof loader>();
+  const { pathname } = useLocation();
   return (
     <div className="ds-stack ds-stack-16">
       {description && (
@@ -44,11 +48,17 @@ export const EmailCapture = ({
             {...field.getInputProps()}
             errorMessages={[invalidEmailError]}
           />
+          <input type="hidden" name={CSRFKey} value={csrf} />
+          <input type="hidden" name="_url" value={pathname} />
         </div>
         {submit && (
           <ButtonContainer className="h-[4rem]">
             <Button
-              {...{ ...submit, href: undefined, disabled: !!field.error() }}
+              {...{
+                ...submit,
+                href: undefined,
+                disabled: !!field.error() || field.value() === "",
+              }}
               type="submit"
             />
           </ButtonContainer>
--- app/routes/beratungshilfe.nachbefragung.tsx
+++ app/routes/beratungshilfe.nachbefragung.tsx
@@ -1,6 +1,23 @@
-import { type ActionFunctionArgs } from "react-router";
+import { validationError } from "@rvf/react-router";
+import { redirect, type ActionFunctionArgs } from "react-router";
+import { consentCookieFromRequest } from "~/services/analytics/gdprCookie.server";
+import { validateFormData } from "~/services/validation/validateFormData.server";
+import { filterFormData } from "~/util/filterFormData";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  console.log(request);
-  return Promise.resolve("success!");
+  const formData = await request.clone().formData();
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string
+  const url = formData.get("_url")?.toString() ?? "";
+  const relevantFormData = filterFormData(formData);
+  const validationResult = await validateFormData(url, relevantFormData);
+
+  if (validationResult?.error) {
+    return validationError(
+      validationResult.error,
+      validationResult.submittedData,
+    );
+  } else {
+    const headers = await consentCookieFromRequest({ request });
+    return redirect(`${url}?success`, { headers });
+  }
 };
