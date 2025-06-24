3e1e0b696417d117ba851b72dccbf0b0b47682a6
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-06-24 10:48:36 +0200

    feat: move over beratungshilfe/vorabcheck

--- app/domains/beratungshilfe/vorabcheck/pages.ts
+++ app/domains/beratungshilfe/vorabcheck/pages.ts
@@ -1,27 +1,53 @@
-import { beratungshilfeVorabcheckInputSchema } from "./userData";
+import pick from "lodash/pick";
+import { beratungshilfeVorabcheckInputSchema as schemas } from "./userData";
 import type { PagesConfig } from "../../pageConfig";
 
 export const beratungshilfeVorabcheckPages = {
   rechtsschutzversicherung: {
-    pageSchema: {
-      rechtsschutzversicherung:
-        beratungshilfeVorabcheckInputSchema.rechtsschutzversicherung,
-    },
+    pageSchema: pick(schemas, "rechtsschutzversicherung"),
   },
-  rechtsschutzversicherungDetails: {},
   "rechtsschutzversicherung-details": {
-    pageSchema: {
-      rsvCoverage: beratungshilfeVorabcheckInputSchema.rsvCoverage,
-    },
+    pageSchema: pick(schemas, "rsvCoverage"),
   },
+  "ergebnis/rechtsschutzversicherung-abbruch": {},
+  "rechtsschutzversicherung-hinweis-selbstbeteiligung": {},
+  "ergebnis/rechtsschutzversicherung-ungewiss-abbruch": {},
+  "ergebnis/rechtsschutzversicherung-hinweis-kostenuebernahme": {},
   "wurde-verklagt": {
-    pageSchema: {
-      wurdeVerklagt: beratungshilfeVorabcheckInputSchema.wurdeVerklagt,
-    },
+    pageSchema: pick(schemas, "wurdeVerklagt"),
   },
+  "ergebnis/wurde-verklagt-abbruch": {},
   "klage-eingereicht": {
-    pageSchema: {
-      klageEingereicht: beratungshilfeVorabcheckInputSchema.klageEingereicht,
-    },
+    pageSchema: pick(schemas, "klageEingereicht"),
   },
+  "ergebnis/klage-eingereicht-abbruch": {},
+  "hamburg-oder-bremen": { pageSchema: pick(schemas, "hamburgOderBremen") },
+  "ergebnis/hamburg-oder-bremen-abbruch": {},
+  "beratungshilfe-beantragt": {
+    pageSchema: pick(schemas, "beratungshilfeBeantragt"),
+  },
+  "ergebnis/beratungshilfe-beantragt-abbruch": {},
+  eigeninitiative: { pageSchema: pick(schemas, "eigeninitiative") },
+  "eigeninitiative-warnung": {},
+  bereich: { pageSchema: pick(schemas, "bereich") }, // TODO
+  "staatliche-leistungen": {
+    pageSchema: pick(schemas, "staatlicheLeistungen"),
+  },
+  "ergebnis/staatliche-leistungen-abschluss-vielleicht": {},
+  "ergebnis/staatliche-leistungen-abschluss-ja": {},
+  vermoegen: { pageSchema: pick(schemas, "vermoegen") },
+  "ergebnis/vermoegen-abschluss-ja": {},
+  "ergebnis/vermoegen-abschluss-vielleicht": {},
+  "ergebnis/vermoegen-abschluss-abbruch": {},
+  erwerbstaetigkeit: { pageSchema: pick(schemas, "erwerbstaetigkeit") },
+  partnerschaft: { pageSchema: pick(schemas, "partnerschaft") },
+  genauigkeit: { pageSchema: pick(schemas, "genauigkeit") },
+  "kinder-kurz": { pageSchema: pick(schemas, "kinderKurz") },
+  "kinder-anzahl-kurz": { pageSchema: pick(schemas, "kinderAnzahlKurz") }, // TODO
+  "verfuegbares-einkommen": {
+    pageSchema: pick(schemas, "verfuegbaresEinkommen"),
+  },
+  "ergebnis/verfuegbares-einkommen-abschluss-ja": {},
+  "ergebnis/verfuegbares-einkommen-abschluss-vielleicht": {},
+  "ergebnis/verfuegbares-einkommen-abschluss-nein": {},
 } as const satisfies PagesConfig;
