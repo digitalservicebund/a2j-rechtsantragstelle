97bac95e9cc1eec38a50f7d45e94fa21446974cc
aaschlote <alex.schlote@digitalservice.bund.de>
2025-06-27 09:27:43 +0200

    refactor(buildFileUploadError): improve function

--- app/services/upload/buildFileUploadError.ts
+++ app/services/upload/buildFileUploadError.ts
@@ -1,8 +1,20 @@
 import {
+  type FieldErrors,
   type ValidationErrorResponseData,
   type ValidationResult,
 } from "@rvf/react-router";
 import { type UserData } from "~/domains/userData";
+
+function filterFieldErrorsByInputName(
+  fieldErrors: FieldErrors,
+  inputName: string,
+) {
+  return Object.fromEntries(
+    Object.entries(fieldErrors)
+      .map(([key, val]) => [key.split(".")[0], val])
+      .filter(([key]) => key === inputName),
+  );
+}
 
 /**
  * Need to remove the validation error's object notation to conform to what the frontend expects
@@ -18,12 +30,14 @@ export function buildFileUploadError(
   validationResult: ValidationResult<UserData>,
   inputName: string,
 ): ValidationErrorResponseData {
+  const fieldErrors = validationResult.error?.fieldErrors ?? {};
+  const filteredFieldErrors = filterFieldErrorsByInputName(
+    fieldErrors,
+    inputName,
+  );
+
   return {
-    fieldErrors: Object.fromEntries(
-      Object.entries(validationResult.error?.fieldErrors ?? {})
-        .map(([key, val]) => [key.split(".")[0], val])
-        .filter(([key]) => key === inputName),
-    ),
+    fieldErrors: filteredFieldErrors,
     repopulateFields: validationResult.submittedData,
   };
 }
