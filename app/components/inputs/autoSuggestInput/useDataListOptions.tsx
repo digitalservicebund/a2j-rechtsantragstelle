828499260e96953d71ddf3e23d0357fe00f63c7b
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-07-01 07:27:21 +0200

    PR fixes

--- app/components/inputs/autoSuggestInput/useDataListOptions.tsx
+++ app/components/inputs/autoSuggestInput/useDataListOptions.tsx
@@ -4,34 +4,18 @@ import { type z } from "zod";
 import { type adresseSchema } from "~/domains/shared/formular/persoenlicheDaten/userData";
 import type { DataListType } from "~/services/cms/components/StrapiAutoSuggestInput";
 import type { DataListOptions } from "~/services/dataListOptions/getDataListOptions";
-import { fetchStreetnamesForZipcode } from "~/services/gerichtsfinder/openPLZ";
 
 const API_PATH = "/api";
 
-export type FetcherFunction = (args?: string) => Promise<DataListOptions[]>;
-
-function getDataFetcher(
-  type: DataListType,
-  pathArgs?: string,
-): FetcherFunction {
-  if (type === "streetNames" && !pathArgs) {
-    throw new Error(
-      "Postleitzahl is undefined, but required to call the OpenPLZ API",
-    );
-  }
+function getResourcePath(type: DataListType, routeArgs?: string): string {
   switch (type) {
     case "airports":
-      return async () => {
-        const response = await fetch(`${API_PATH}/airports/list`);
-        return response.ok ? await response.json() : [];
-      };
+      return `${API_PATH}/airports/list`;
     case "airlines":
-      return async () => {
-        const response = await fetch(`${API_PATH}/airlines/list`);
-        return response.ok ? await response.json() : [];
-      };
-    case "streetNames":
-      return fetchStreetnamesForZipcode;
+      return `${API_PATH}/airlines/list`;
+    case "streetNames": {
+      return `${API_PATH}/streetNames/list/${routeArgs}`;
+    }
     default: {
       throw new Error(`Unhandled type: ${String(type)}`);
     }
@@ -48,23 +32,25 @@ const useDataListOptions = (dataListType: DataListType) => {
   const postleitzahl = loaderData?.prunedUserData?.plz as
     | z.infer<typeof adresseSchema.plz>
     | undefined;
+  const resourcePath = getResourcePath(dataListType, postleitzahl);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const dataFetcher = getDataFetcher(dataListType, postleitzahl);
-        const options = await dataFetcher(postleitzahl);
-        setDataListOptions(options);
-      } catch (err: unknown) {
-        // eslint-disable-next-line no-console
-        console.error(err);
+        const response = await fetch(resourcePath);
+
+        if (response.ok) {
+          const json = await response.json();
+          setDataListOptions(json);
+        }
+      } catch {
         setDataListOptions([]);
       }
     };
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     fetchData();
-  }, [dataListType, postleitzahl]);
+  }, [resourcePath, dataListType]);
 
   return dataListOptions;
 };
