1c99202496471ddbf3995f203a6b3abc2af5f767
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-07-04 15:30:54 +0200

    refactor(getNestedSchema): arrow function

--- app/components/form/schemaToForm/getNestedSchema.tsx
+++ app/components/form/schemaToForm/getNestedSchema.tsx
@@ -1,10 +1,7 @@
 import type { z } from "zod";
 import type { ZodEnum } from "./renderZodEnum";
 
-export const getNestedSchema = (
-  schema: z.ZodTypeAny,
-): ZodEnum | z.ZodString => {
-  return schema._def.typeName === "ZodEffects"
+export const getNestedSchema = (schema: z.ZodTypeAny): ZodEnum | z.ZodString =>
+  schema._def.typeName === "ZodEffects"
     ? getNestedSchema(schema._def.schema)
     : (schema as ZodEnum | z.ZodString);
-};
