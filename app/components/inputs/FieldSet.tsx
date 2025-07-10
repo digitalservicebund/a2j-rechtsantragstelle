abedfa6a607e1c1327e04d942ec6e3b99f3e30e8
aaschlote <alex.schlote@digitalservice.bund.de>
2025-07-10 11:12:58 +0200

    refactor(FieldSet): get fieldSet data for the name schema

--- app/components/inputs/FieldSet.tsx
+++ app/components/inputs/FieldSet.tsx
@@ -1,6 +1,7 @@
 import { type StrapiFieldSet } from "~/services/cms/components/StrapiFieldSet";
 import { keyFromElement } from "~/services/cms/keyFromElement";
 import { FormComponent } from "../FormComponents";
+import RichText from "../RichText";
 
 type FieldSetProps = Readonly<
   Pick<StrapiFieldSet, "fieldSetGroup" | "heading">
@@ -8,15 +9,13 @@ type FieldSetProps = Readonly<
 
 export const FieldSet = ({
   heading,
-  fieldSetGroup: {
-    data: {
-      attributes: { formComponents },
-    },
-  },
+  fieldSetGroup: { formComponents },
 }: FieldSetProps) => {
   return (
     <fieldset>
-      <legend>{heading}</legend>
+      <legend>
+        <RichText html={heading} />
+      </legend>
       {formComponents.map((componentProps) => (
         <div key={keyFromElement(componentProps)} className="pl-40 pt-16">
           <FormComponent componentProps={componentProps} />
