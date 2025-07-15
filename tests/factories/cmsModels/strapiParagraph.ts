df5a239256039563d00822c24624ec5d48fa2f95
aaschlote <alex.schlote@digitalservice.bund.de>
2025-07-15 10:59:55 +0200

    refactor: improve schema heading and paragraph

--- tests/factories/cmsModels/strapiParagraph.ts
+++ tests/factories/cmsModels/strapiParagraph.ts
@@ -4,6 +4,7 @@ import { type StrapiParagraph } from "~/services/cms/models/StrapiParagraph";
 export function getStrapiParagraph(): StrapiParagraph {
   return {
     text: faker.lorem.paragraph(),
+    __component: "basic.paragraph",
     id: -1,
   };
 }
