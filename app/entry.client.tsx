a57fbb59b7a223028f8750b6d4464fd3d10e0dd2
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2025-07-16 10:28:32 +0200

    undo deleted code

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,6 +5,13 @@ import { HydratedRouter } from "react-router/dom";
 import { config } from "~/services/env/public";
 
 const { SENTRY_DSN, ENVIRONMENT } = config();
+// Ignore a few common errors that are not useful to track
+const SENTRY_IGNORE_ERRORS = [
+  "Error in input stream",
+  "Load failed",
+  "Detected manifest version mismatch, reloading...",
+];
+
 if (SENTRY_DSN !== undefined) {
   Sentry.init({
     dsn: SENTRY_DSN,
@@ -21,6 +28,7 @@ if (SENTRY_DSN !== undefined) {
       /^\/[^/]*/, //  This enables trace propagation for all relative paths on the same domain.
     ],
     integrations: [Sentry.reactRouterTracingIntegration()],
+    ignoreErrors: SENTRY_IGNORE_ERRORS,
   });
 }
 
