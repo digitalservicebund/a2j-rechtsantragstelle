a7fc0366ec046f167e27c876eafd1858c5093194
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-07-18 18:28:49 +0200

    test: add uploadUserFile success case

--- app/services/upload/__test__/fileUploadHelpers.server.test.ts
+++ app/services/upload/__test__/fileUploadHelpers.server.test.ts
@@ -1,10 +1,44 @@
-import { deleteUserFile } from "../fileUploadHelpers.server";
+import { getContext } from "~/domains/userData";
+import { uploadUserFileToS3 } from "~/services/externalDataStorage/userFileS3Helpers";
+import { deleteUserFile, uploadUserFile } from "../fileUploadHelpers.server";
+import { fileUploadRequiredSchema } from "~/domains/prozesskostenhilfe/formular/abgabe/dokumente/userData";
 
+vi.mock("~/domains/userData");
 vi.mock("~/services/externalDataStorage/userFileS3Helpers", () => ({
   deleteUserFileFromS3: vi.fn(),
+  uploadUserFileToS3: vi.fn(),
 }));
 
 describe("fileUploadHelpers.server", () => {
+  const savedFileKey = "a-b-c-d-e";
+  vi.mocked(uploadUserFileToS3).mockResolvedValue(savedFileKey);
+  vi.mocked(getContext).mockReturnValue({
+    test: fileUploadRequiredSchema,
+  });
+  const filename = "file.pdf";
+  const fileType = "application/pdf";
+  const fileBlob = new Blob(["fileContent"], { type: fileType });
+  const fileSize = 11;
+
+  describe("uploadUserFile", () => {
+    it("should add a file to userData", async () => {
+      const formData = new FormData();
+      formData.set("test[0]", fileBlob, filename);
+      const flowId = "/beratungshilfe/antrag";
+
+      const actual = await uploadUserFile(
+        "test[0]",
+        null,
+        formData,
+        { test: [] },
+        flowId,
+      );
+      expect(actual).toStrictEqual({
+        userData: { test: [{ filename, fileType, fileSize, savedFileKey }] },
+      });
+    });
+  });
+
   describe("deleteUserFile", () => {
     it("should delete item from userData", async () => {
       const actual = await deleteUserFile(
