910f75ccae1d42f162f2b6739008293ef244f555
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-07-21 07:54:00 +0200

    add asyncFlowAction that submits eligibility

--- app/domains/beratungshilfe/formular/index.ts
+++ app/domains/beratungshilfe/formular/index.ts
@@ -1,7 +1,9 @@
+import { isFinanciallyEligibleForBerH } from "~/domains/beratungshilfe/formular/abgabe/isFinanciallyEligibleForBerH";
 import { getRechtsproblemStrings } from "~/domains/beratungshilfe/formular/rechtsproblem/stringReplacements";
 import type { Flow } from "~/domains/flows.server";
 import { type AbgabeUserData } from "~/domains/shared/formular/abgabe/userData";
 import { type WeitereAngabenUserData } from "~/domains/shared/formular/weitereAngaben/userData";
+import { sendCustomAnalyticsEvent } from "~/services/analytics/customEvent";
 import { type DokumenteUserData } from "./abgabe/dokumente/userData";
 import { type BeratungshilfeAnwaltlicheVertretungUserData } from "./anwaltlicheVertretung/userData";
 import { type BeratungshilfeFinanzielleAngabenUserData } from "./finanzielleAngaben/userData";
@@ -45,6 +47,18 @@ export const beratungshilfeFormularUserData = {
     ...weiteresEinkommenStrings(context),
     ...getWeitereDokumenteStrings(context),
   }),
+  asyncFlowActions: {
+    "/abgabe/art": (request, userData) =>
+      Promise.resolve(
+        sendCustomAnalyticsEvent({
+          request,
+          eventName: "financial eligibility calculated",
+          properties: {
+            isEligible: isFinanciallyEligibleForBerH({ context: userData }),
+          },
+        }),
+      ),
+  },
 } satisfies Flow;
 
 export type BeratungshilfeFormularUserData =
