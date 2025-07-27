efc896d41dafff52afaae301b6cbf508d839d333
judithmh <40442827+judithmh@users.noreply.github.com>
2025-07-27 22:50:31 +0200

    revert changes in addTable: will be part of other PR

--- app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/table/addTable.ts
+++ app/domains/fluggastrechte/services/pdf/sections/reason/factsOfCases/table/addTable.ts
@@ -13,19 +13,18 @@ export function addTable(
 ) {
   const tableSect = doc.struct("Sect"); // Create new section for the table
   const table = doc.struct("Table"); // Create new table structure element
+
   drawTableRowHead(doc, table, startTableY, userData); // Pass table as parent to avoid reusing `documentStruct`
   drawTableColumnsHead(doc, table, startTableY); // Use the table structure element
   drawTableColumnsValues(doc, table, startTableY, userData); // Continue with table structure element
 
-  tableSect.add(table);
+  tableSect.add(table); // Add the table to the section
+  documentStruct.add(tableSect); // Add the section to the parent structure
   doc.fill("black"); // Fill black due next pages of the table
-
-  documentStruct.add(tableSect);
   // Get end position of the table generated
   const tableEndYPosition = startTableY + COLUMN_HEIGHT * 4 + MARGIN_BOTTOM;
   // reset the position of the table
   doc.y = tableEndYPosition;
-
   addTableInfo(
     doc,
     documentStruct,
