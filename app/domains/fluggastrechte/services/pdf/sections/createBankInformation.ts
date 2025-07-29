54b5ec6ea0317812d8f84bbfcea4a156722a586d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-07-29 10:36:18 +0200

    feat(pdf): properly tag footer for a11y (#2384)

--- app/domains/fluggastrechte/services/pdf/sections/createBankInformation.ts
+++ app/domains/fluggastrechte/services/pdf/sections/createBankInformation.ts
@@ -6,10 +6,18 @@ import {
   PDF_MARGIN_HORIZONTAL,
 } from "~/services/pdf/createPdfKitDocument";
 
+function drawBankInfo(doc: PDFKit.PDFDocument, text: string) {
+  doc
+    .fontSize(7)
+    .font(FONTS_BUNDESSANS_REGULAR)
+    .text(text, PDF_MARGIN_HORIZONTAL, PDF_HEIGHT_SEIZE);
+}
+
 export const createBankInformation = (
   doc: typeof PDFDocument,
   footerSect: PDFKit.PDFStructureElement,
   { kontoinhaber, vorname, nachname, iban }: FluggastrechteUserData,
+  isLastPage: boolean,
 ) => {
   const bankAccountHolder =
     typeof kontoinhaber !== "undefined" && kontoinhaber.trim().length > 0
@@ -18,13 +26,16 @@ export const createBankInformation = (
 
   if (iban) {
     const bankInfo = `Kontoinhaber: ${bankAccountHolder} | IBAN: ${iban}`;
-    footerSect.add(
-      doc.struct("P", {}, () => {
-        doc
-          .fontSize(7)
-          .font(FONTS_BUNDESSANS_REGULAR)
-          .text(bankInfo, PDF_MARGIN_HORIZONTAL, PDF_HEIGHT_SEIZE);
-      }),
-    );
+
+    if (isLastPage) {
+      const bankInfoParagraph = doc.struct("P", {}, () => {
+        drawBankInfo(doc, bankInfo);
+      });
+      footerSect.add(bankInfoParagraph);
+    } else {
+      doc.markContent("Artifact", { type: "Pagination" });
+      drawBankInfo(doc, bankInfo);
+      doc.endMarkedContent();
+    }
   }
 };
