ebda352d98b105d85b318c9492b4a30c0e508d8e
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-07-31 14:00:39 +0200

    feat(checkbox): add more expectation

--- app/components/inputs/__test__/Checkbox.test.tsx
+++ app/components/inputs/__test__/Checkbox.test.tsx
@@ -132,7 +132,7 @@ describe("Checkbox", () => {
     expect(controlledRefMock).toHaveBeenCalled();
   });
 
-  it("applies a focus-visible inset-shadow class when there is an error", () => {
+  it("applies error styling correctly when there is an error", () => {
     mockedUseField.mockImplementation(() =>
       createMockFieldReturn({
         error: () => "some error",
@@ -149,7 +149,7 @@ describe("Checkbox", () => {
 
     const checkbox = screen.getByRole("checkbox", { name: "Checkbox Label" });
     expect(checkbox).toHaveClass(
-      "focus-visible:shadow-[inset_0_0_0_4px_theme(colors.red.800)]",
+      "has-error focus-visible:shadow-[inset_0_0_0_4px_theme(colors.red.800)]",
     );
   });
 });
