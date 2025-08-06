b8c9278286ad5c274d30f2303863f32bcffa7a9c
aaschlote <alex.schlote@digitalservice.bund.de>
2025-08-06 11:39:34 +0200

    refactor(StrapiArraySummary): add new fields to schema

--- app/services/cms/models/StrapiArraySummary.ts
+++ app/services/cms/models/StrapiArraySummary.ts
@@ -1,9 +1,16 @@
 import { z } from "zod";
+import { StrapiRichTextOptionalSchema } from "~/services/validation/richtext";
+import { stringOptionalSchema } from "~/services/validation/stringOptional";
 import { HasStrapiIdSchema } from "./HasStrapiId";
+import { StrapiHeadingOptionalSchema } from "./StrapiHeading";
 
 export const StrapiArraySummaryComponentSchema = z.object({
   category: z.string(),
   categoryUrl: z.string(),
+  title: StrapiHeadingOptionalSchema,
+  description: StrapiRichTextOptionalSchema(),
+  subtitle: StrapiHeadingOptionalSchema,
+  buttonLabel: stringOptionalSchema,
   __component: z.literal("page.array-summary"),
   ...HasStrapiIdSchema.shape,
 });
