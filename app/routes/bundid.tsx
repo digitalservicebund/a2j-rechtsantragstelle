925f5b49b001c8871396d89b08be56bc0f9d3672
Kai Bernhard <17817496+kaibernhard@users.noreply.github.com>
2025-08-07 11:08:38 +0200

    feat(bundid): configure SAML request with AKDB extension

--- app/routes/bundid.tsx
+++ app/routes/bundid.tsx
@@ -1,20 +1,47 @@
 import { Outlet, useLoaderData } from "react-router";
+import type { LoaderFunctionArgs } from "react-router";
+import invariant from "tiny-invariant";
 import Button from "~/components/Button";
 import {
   getBundIdIdentityProvider,
   getBundIdServiceProvider,
 } from "~/services/bundid/index.server";
+import { config } from "~/services/env/env.server";
 import { throw404IfFeatureFlagDisabled } from "~/services/errorPages/throw404";
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   await throw404IfFeatureFlagDisabled("showBundID");
+
+  const assertionConsumerServiceURL =
+    config().SAML_ASSERTION_CONSUMER_SERVICE_URL;
+  invariant(
+    assertionConsumerServiceURL,
+    "SAML_ASSERTION_CONSUMER_SERVICE_URL has to be set",
+  );
 
   const identityProvider = getBundIdIdentityProvider();
   const serviceProvider = getBundIdServiceProvider();
 
+  const onlineServiceID = config().BUNDID_AUTH_BMI_ID;
+  invariant(onlineServiceID, "BUNDID_AUTH_BMI_ID has to be set");
+
+  const backURL = request.url;
+
   const loginRequest = serviceProvider.createLoginRequest(
     identityProvider,
     "post",
+    (template: string) => {
+      const id = crypto.randomUUID();
+      return {
+        id: id,
+        context: template
+          .replace("{AssertionConsumerServiceURL}", assertionConsumerServiceURL)
+          .replace("{ID}", id)
+          .replace("{IssueInstant}", new Date().toISOString())
+          .replace("{OnlineServiceID}", onlineServiceID)
+          .replace("{BackURL}", backURL),
+      };
+    },
   );
   return {
     url: "https://int.id.bund.de/idp/profile/SAML2/POST/SSO",
