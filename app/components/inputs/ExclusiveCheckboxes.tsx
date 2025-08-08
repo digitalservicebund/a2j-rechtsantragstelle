58362af9d04023c0a9e7b059f97038e39e31323d
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-08-08 13:55:22 +0200

    vastly simplified, common changeHandler

--- app/components/inputs/ExclusiveCheckboxes.tsx
+++ app/components/inputs/ExclusiveCheckboxes.tsx
@@ -1,14 +1,11 @@
-import { useField } from "@rvf/react-router";
+import { type FieldApi, useField } from "@rvf/react-router";
 import { useState } from "react";
 import { useJsAvailable } from "~/components/hooks/useJsAvailable";
 import { type CheckboxValue } from "~/components/inputs/Checkbox";
 import InputError from "~/components/inputs/InputError";
 import RichText from "~/components/RichText";
 import { type StrapiCheckboxComponent } from "~/services/cms/components/StrapiCheckbox";
-import {
-  type CheckedOptional,
-  type ExclusiveCheckboxes as ExclusiveCheckboxesType,
-} from "~/services/validation/checkedCheckbox";
+import { type ExclusiveCheckboxes as ExclusiveCheckboxesType } from "~/services/validation/checkedCheckbox";
 
 type ExclusiveCheckboxesProps = Readonly<{
   name: string;
@@ -20,11 +17,11 @@ export const ExclusiveCheckboxes = ({
   cmsCheckboxes,
 }: ExclusiveCheckboxesProps) => {
   const field = useField<ExclusiveCheckboxesType>(name);
-  const [noneCheckboxValue, setNoneCheckboxValue] = useState<CheckedOptional>(
+  const [noneCheckboxValue, setNoneCheckboxValue] = useState<CheckboxValue>(
     field.value().none,
   );
   const [checkboxes, setCheckboxes] = useState<
-    Array<ControlledCheckboxProps | undefined>
+    Array<Omit<ControlledCheckboxProps, "onChange"> | undefined>
   >(
     Object.entries(field.value())
       .filter(([key]) => key !== "none")
@@ -37,50 +34,36 @@ export const ExclusiveCheckboxes = ({
           name: matchingCmsCheckbox.name,
           label: matchingCmsCheckbox.label,
           value: checkboxValue,
-          onChange: (checked: CheckboxValue) => {
-            field.setValue({ ...field.value(), [checkboxName]: checked });
-            setCheckboxes((prev) =>
-              prev.map((c) =>
-                c?.name.split(".").pop() === checkboxName
-                  ? { ...c, value: checked }
-                  : c,
-              ),
-            );
-            if (noneCheckboxValue && checked === "on") {
-              setNoneCheckboxValue("off");
-            }
-            field.validate();
-          },
         };
       }),
   );
   const errorId = `${name}-error`;
-  const onNoneChanged = (checked: CheckboxValue) => {
-    const newFieldValues =
-      checked === "on"
-        ? Object.fromEntries(
-            Object.entries(field.value()).map(([key]) => [key, "off"]),
-          )
-        : field.value();
-    field.setValue({ ...newFieldValues, none: checked });
-    setNoneCheckboxValue(checked);
-    if (checked === "on") {
-      setCheckboxes((prev) => prev.map((c) => c && { ...c, value: "off" }));
-    }
-    field.validate();
-  };
 
   return (
     <div>
       {checkboxes.filter(Boolean).map((checkbox) => (
-        <ControlledCheckbox key={checkbox?.name} {...checkbox!} />
+        <ControlledCheckbox
+          {...checkbox!}
+          key={checkbox?.name}
+          onChange={onCheckboxChange(
+            field,
+            noneCheckboxValue,
+            setNoneCheckboxValue,
+            setCheckboxes,
+          )}
+        />
       ))}
       <p className="ds-label-01-reg">oder</p>
       <ControlledCheckbox
         name={`${name}.none`}
         key={`${name}.none`}
         value={noneCheckboxValue}
-        onChange={onNoneChanged}
+        onChange={onCheckboxChange(
+          field,
+          noneCheckboxValue,
+          setNoneCheckboxValue,
+          setCheckboxes,
+        )}
         label={
           cmsCheckboxes.find((c) => c.name === `${name}.none`)?.label ??
           "Nichts trifft zu"
@@ -94,8 +77,8 @@ export const ExclusiveCheckboxes = ({
 type ControlledCheckboxProps = {
   name: string;
   label: string;
-  value: CheckedOptional;
-  onChange: (checked: CheckedOptional) => void;
+  value: CheckboxValue;
+  onChange: (name: string, checked: CheckboxValue) => void;
 };
 
 function ControlledCheckbox({
@@ -121,7 +104,9 @@ function ControlledCheckbox({
         checked={value === "on"}
         value={jsAvailable ? value : "on"}
         className="ds-checkbox forced-colors:outline forced-colors:border-[ButtonText]"
-        onChange={(e) => onChange(e.target.checked ? "on" : "off")}
+        onChange={(e) =>
+          onChange(name.split(".").pop() ?? "", e.target.checked ? "on" : "off")
+        }
       />
       {label && (
         <label htmlFor={name}>
@@ -131,3 +116,44 @@ function ControlledCheckbox({
     </div>
   );
 }
+
+const onCheckboxChange =
+  (
+    parentField: FieldApi<ExclusiveCheckboxesType>,
+    noneCheckboxValue: CheckboxValue,
+    setNoneCheckboxValue: React.Dispatch<React.SetStateAction<CheckboxValue>>,
+    setCheckboxes: React.Dispatch<
+      React.SetStateAction<
+        Array<Omit<ControlledCheckboxProps, "onChange"> | undefined>
+      >
+    >,
+  ) =>
+  (checkboxName: string, checked: CheckboxValue) => {
+    if (checkboxName === "none") {
+      const newFieldValues =
+        checked === "on"
+          ? Object.fromEntries(
+              Object.entries(parentField.value()).map(([key]) => [key, "off"]),
+            )
+          : parentField.value();
+      parentField.setValue({ ...newFieldValues, none: checked });
+      setNoneCheckboxValue(checked);
+      if (checked === "on") {
+        setCheckboxes((prev) => prev.map((c) => c && { ...c, value: "off" }));
+      }
+      parentField.validate();
+    } else {
+      parentField.setValue({ ...parentField.value(), [checkboxName]: checked });
+      setCheckboxes((prev) =>
+        prev.map((c) =>
+          c?.name.split(".").pop() === checkboxName
+            ? { ...c, value: checked }
+            : c,
+        ),
+      );
+      if (noneCheckboxValue && checked === "on") {
+        setNoneCheckboxValue("off");
+      }
+      parentField.validate();
+    }
+  };
