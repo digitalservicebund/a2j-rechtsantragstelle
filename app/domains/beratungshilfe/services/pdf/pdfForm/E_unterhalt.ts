06ef4afc8224a567ddeb1fccb2f981886ef4f3af
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-08-14 06:48:31 +0200

    refactor(PDF): Decouple PKH/BerH PDF logic and enumerate support recipients in fields D/E

--- app/domains/beratungshilfe/services/pdf/pdfForm/E_unterhalt.ts
+++ app/domains/beratungshilfe/services/pdf/pdfForm/E_unterhalt.ts
@@ -1,12 +1,94 @@
-import { getFillUnterhalt } from "~/domains/shared/services/pdf/unterhaltHelpers";
-import { SEE_IN_ATTACHMENT_DESCRIPTION } from "~/services/pdf/attachment";
+import {
+  addSupportRecipientsToAttachment,
+  familyRelationshipMap,
+} from "~/domains/shared/services/pdf/unterhaltHelpers";
 import type { BerHPdfFillFunction } from "../types";
 
 export const ATTACHMENT_DESCRIPTION_SECTION_E = "Feld E: Unterhaltszahlungen";
 
-export const fillUnterhalt: BerHPdfFillFunction = getFillUnterhalt(
-  ATTACHMENT_DESCRIPTION_SECTION_E,
-  (pdfValues) => {
-    pdfValues.e1Person1.value = SEE_IN_ATTACHMENT_DESCRIPTION;
-  },
-);
+export const fillUnterhalt: BerHPdfFillFunction = ({ userData, pdfValues }) => {
+  const zahltPartnerUnterhalt = userData.partnerUnterhaltsSumme !== undefined;
+  const kinder = userData.kinder ?? [];
+  const hasKinder = kinder.length > 0;
+
+  const unterhaltszahlungen = userData.unterhaltszahlungen ?? [];
+  const hasUnterhaltszahlungen = unterhaltszahlungen.length > 0;
+
+  if (!zahltPartnerUnterhalt && !hasUnterhaltszahlungen && !hasKinder) {
+    return { pdfValues };
+  } else if (
+    kinder.length +
+      unterhaltszahlungen.length +
+      (zahltPartnerUnterhalt ? 1 : 0) >
+    4
+  ) {
+    return addSupportRecipientsToAttachment({
+      userData,
+      pdfValues,
+    })(ATTACHMENT_DESCRIPTION_SECTION_E, "e1Person1");
+  }
+
+  return enumerateSupportRecipients({ userData, pdfValues });
+};
+
+const enumerateSupportRecipients: BerHPdfFillFunction = ({
+  userData,
+  pdfValues,
+}) => {
+  let startCell = 1;
+  if (userData.partnerUnterhaltsSumme !== undefined) {
+    pdfValues.e1Person1.value = `${userData.partnerVorname} ${userData.partnerNachname}`;
+    pdfValues.e3Familienverhaeltnis.value = "Ehepartner";
+    pdfValues.e4Zahlung1.value = userData.partnerUnterhaltsSumme + " €";
+    if (userData.partnerEinkommen === "yes") {
+      pdfValues.e6Betrag1.value = userData.partnerEinkommenSumme + " €";
+    } else {
+      pdfValues.e5Einnahmen1.value = true;
+    }
+    startCell += 1;
+  }
+
+  if (userData.kinder && userData.kinder.length > 0) {
+    userData.kinder.forEach((kind) => {
+      pdfValues[`e1Person${startCell}` as keyof typeof pdfValues].value =
+        `${kind.vorname} ${kind.nachname}`;
+      pdfValues[
+        `e2Geburtsdatum${startCell === 1 ? "" : startCell}` as keyof typeof pdfValues
+      ].value = kind.geburtsdatum;
+      pdfValues[
+        `e3Familienverhaeltnis${startCell === 1 ? "" : startCell}` as keyof typeof pdfValues
+      ].value = "Kind";
+      if (kind.unterhalt === "yes") {
+        pdfValues[`e4Zahlung${startCell}` as keyof typeof pdfValues].value =
+          kind.unterhaltsSumme + " €";
+      }
+      if (kind.eigeneEinnahmen === "yes") {
+        pdfValues[`e6Betrag${startCell}` as keyof typeof pdfValues].value =
+          kind.einnahmen + " €";
+      } else {
+        pdfValues[`e5Einnahmen${startCell}` as keyof typeof pdfValues].value =
+          true;
+      }
+      startCell += 1;
+    });
+  }
+
+  if (userData.unterhaltszahlungen && userData.unterhaltszahlungen.length > 0) {
+    userData.unterhaltszahlungen.forEach((supportRecipient) => {
+      pdfValues[`e1Person${startCell}` as keyof typeof pdfValues].value =
+        `${supportRecipient.firstName} ${supportRecipient.surname}`;
+      pdfValues[
+        `e2Geburtsdatum${startCell === 1 ? "" : startCell}` as keyof typeof pdfValues
+      ].value = supportRecipient.birthday;
+      pdfValues[
+        `e3Familienverhaeltnis${startCell === 1 ? "" : startCell}` as keyof typeof pdfValues
+      ].value = familyRelationshipMap[supportRecipient.familyRelationship];
+      pdfValues[`e4Zahlung${startCell}` as keyof typeof pdfValues].value =
+        supportRecipient.monthlyPayment + " €";
+      pdfValues[`e5Einnahmen${startCell}` as keyof typeof pdfValues].value =
+        true;
+      startCell += 1;
+    });
+  }
+  return { pdfValues };
+};
