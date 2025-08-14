5ade88808aa366284524323f763582ff6bb8c3ed
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-08-14 11:32:01 +0200

    undo wrong unrelated changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,21 +1,21 @@
 import type { ButtonProps } from "./Button";
 import Button from "./Button";
 import ButtonContainer from "./ButtonContainer";
-import Heading from "./Heading";
-import RichText from "./RichText";
+import Heading, { type HeadingProps } from "./Heading";
+import RichText, { type RichTextProps } from "./RichText";
 
 type Props = Readonly<{
-  title: string;
-  subTitleHtml?: string;
+  heading: HeadingProps;
+  content?: RichTextProps;
   button?: ButtonProps;
 }>;
 
-export default function Hero({ title, button, subTitleHtml }: Props) {
+export default function Hero({ heading, content, button }: Props) {
   return (
     <div className="container !pt-24 !pb-24 ds-stack ds-stack-16">
-      <Heading look="ds-heading-01-reg" tagName="h1" text={title} />
-      {subTitleHtml && (
-        <RichText className="ds-heading-03-reg" html={subTitleHtml} />
+      <Heading {...heading} />
+      {content && (
+        <RichText className="ds-heading-03-reg" html={content.html} />
       )}
       {button && (
         <ButtonContainer className="pb-64 sm:pt-24">
