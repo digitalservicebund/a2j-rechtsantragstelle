22a29142a359174ae7c2e2fcb7a1bce0c6a89e51
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-08-19 15:39:38 +0200

    Revert "revert vite config"
    This reverts commit 4d81812e4a5559356b58a9ccaef71e87fb9df1ca.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ import { envOnlyMacros } from "vite-env-only";
 
 const isStorybook = process.argv[1]?.includes("storybook");
 const isVitest = process.env.VITEST !== undefined;
-const buildSentrySourceMaps = Boolean(process.env.SENTRY_AUTH_TOKEN);
+const isAppBuild = !isStorybook && !isVitest;
+const useSentry = isAppBuild && Boolean(process.env.SENTRY_AUTH_TOKEN);
 
 const sentryConfig: SentryReactRouterBuildOptions = {
   org: "digitalservice",
@@ -26,19 +27,13 @@ export default defineConfig((config) => ({
   },
   plugins: [
     envOnlyMacros(),
-    !isStorybook && !isVitest && reactRouter(),
-    !isStorybook &&
-      !isVitest &&
-      buildSentrySourceMaps &&
-      sentryVitePlugin(sentryConfig),
-    !isStorybook &&
-      !isVitest &&
-      buildSentrySourceMaps &&
-      sentryReactRouter(sentryConfig, config),
+    isAppBuild && reactRouter(),
+    useSentry && sentryVitePlugin(sentryConfig),
+    useSentry && sentryReactRouter(sentryConfig, config),
     tsconfigPaths(),
   ],
   build: {
-    sourcemap: buildSentrySourceMaps,
+    sourcemap: useSentry,
     target: config.isSsrBuild ? "esnext" : undefined, // Allows top-level await in server-only files
   },
   test: {
