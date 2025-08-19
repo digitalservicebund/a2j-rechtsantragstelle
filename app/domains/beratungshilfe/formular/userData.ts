525a76a9cce963312660e61a7178bb999cfd9141
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-08-19 14:17:51 +0200

    RAST-195 move berH weitere angaben to pageSchemas

--- app/domains/beratungshilfe/formular/userData.ts
+++ app/domains/beratungshilfe/formular/userData.ts
@@ -1,18 +1,17 @@
 import { z } from "zod";
 import { beratungshilfePersoenlicheDatenInputSchema } from "~/domains/beratungshilfe/formular/persoenlicheDaten/userData";
 import { abgabeInputSchema } from "~/domains/shared/formular/abgabe/userData";
-import { weitereAngabenSchema } from "~/domains/shared/formular/weitereAngaben/userData";
 import { dokumenteInputSchema } from "./abgabe/dokumente/userData";
 import { type BeratungshilfeAnwaltlicheVertretungUserData } from "./anwaltlicheVertretung/userData";
 import { beratungshilfeFinanzielleAngabenInputSchema } from "./finanzielleAngaben/userData";
 import { type BeratungshilfeGrundvoraussetzungenUserData } from "./grundvoraussetzung/userData";
 import { type BeratungshilfeRechtsproblemUserData } from "./rechtsproblem/userData";
+import { type BeratungshilfeWeitereAngabenUserData } from "./weitereAngaben/userData";
 
 export const beratungshilfeFormularUserData = {
   ...beratungshilfeFinanzielleAngabenInputSchema,
   ...beratungshilfePersoenlicheDatenInputSchema,
   ...dokumenteInputSchema,
-  ...weitereAngabenSchema,
   ...abgabeInputSchema,
 } as const;
 
@@ -20,4 +19,5 @@ const _partialSchema = z.object(beratungshilfeFormularUserData).partial();
 export type BeratungshilfeFormularUserData = z.infer<typeof _partialSchema> &
   BeratungshilfeGrundvoraussetzungenUserData &
   BeratungshilfeRechtsproblemUserData &
-  BeratungshilfeAnwaltlicheVertretungUserData;
+  BeratungshilfeAnwaltlicheVertretungUserData &
+  BeratungshilfeWeitereAngabenUserData;
