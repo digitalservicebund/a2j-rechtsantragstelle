0fb4994378f016a5fd1ef341c21555cd54347643
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-08-20 12:11:50 +0200

    separate env getters and add unit tests

---
+++ app/services/env/__test__/environmentVariableHelpers.test.ts
@@ -0,0 +1,65 @@
+import fs from "node:fs";
+import { config } from "~/services/env/env.server";
+
+vi.mock("node:fs");
+const mockedReadFileSync = vi.fn();
+
+const envMap: Array<[keyof ReturnType<typeof config>, string, string]> = [
+  ["STRAPI_API", "/etc/strapi-api-secret/password", "test://cms/api/"],
+  [
+    "STRAPI_ACCESS_KEY",
+    "/etc/strapi-access-key-secret/password",
+    "FAKE_STRAPI_ACCESS_KEY",
+  ],
+  [
+    "GERICHTSFINDER_ENCRYPTION_KEY",
+    "/etc/courtdata-secrets/password",
+    "FAKE_GERICHTSFINDER_KEY",
+  ],
+  [
+    "REDIS_PASSWORD",
+    "/etc/redis-password-secret/password",
+    "FAKE_REDIS_PASSWORD",
+  ],
+  [
+    "COOKIE_SESSION_SECRET",
+    "/etc/cookie-session-secret/password",
+    "FAKE_COOKIE_SESSION_SECRET",
+  ],
+  ["SAML_IDP_CERT", "/etc/saml/idp_cert", "FAKE_SAML_IDP_CERT"],
+  [
+    "S3_DATA_STORAGE_ACCESS_KEY",
+    "/etc/s3-storage-credentials-secret-access-key/password",
+    "FAKE_S3_DATA_STORAGE_ACCESS_KEY",
+  ],
+  [
+    "S3_DATA_STORAGE_SECRET_KEY",
+    "/etc/s3-storage-credentials-secret-key/password",
+    "FAKE_S3_DATA_STORAGE_SECRET_KEY",
+  ],
+];
+
+describe("environment variables", () => {
+  describe.each(envMap)("%s", (key, path, value) => {
+    beforeEach(() => {
+      vi.mocked(fs.readFileSync).mockImplementation(mockedReadFileSync);
+      mockedReadFileSync.mockReturnValue(value);
+    });
+    it("can be read from a local file", () => {
+      expect(config()[key]).toBe(value);
+      expect(mockedReadFileSync).toHaveBeenCalledWith(path, "utf8");
+    });
+
+    it("can be read from process.env when local file is missing", () => {
+      mockedReadFileSync.mockReturnValue(undefined);
+      vi.stubEnv(key, value);
+      expect(config()[key]).toBe(value);
+    });
+
+    it("return a default empty string", () => {
+      mockedReadFileSync.mockReturnValue(undefined);
+      vi.stubEnv(key, undefined);
+      expect(config()[key]).toBe("");
+    });
+  });
+});
--- app/services/env/env.server.ts
+++ app/services/env/env.server.ts
@@ -1,6 +1,15 @@
-import fs from "node:fs";
 import path from "path";
 import { configDotenv } from "dotenv";
+import {
+  getStrapiApi,
+  getStrapiAccessKey,
+  getGerichtsfinderEncryptionKey,
+  getRedisPassword,
+  getCookieSessionSecret,
+  getSamlIDPCert,
+  getS3DataStorageAccessKey,
+  getS3DataStorageSecretKey,
+} from "~/services/env/environmentVariableHelpers";
 
 configDotenv({ override: true, quiet: true }); // override is needed to enable live reloading on change
 
@@ -29,32 +38,17 @@ type Config = {
 };
 
 export function config(): Config {
-  const STRAPI_API =
-    readSecretVolume("/etc/strapi-api-secret/password") ??
-    process.env.STRAPI_API ??
-    "";
+  const STRAPI_API = getStrapiApi();
 
   return {
     STRAPI_API: STRAPI_API ?? "",
     STRAPI_HOST: STRAPI_API?.replace("/api/", "") ?? "",
-    STRAPI_ACCESS_KEY:
-      readSecretVolume("/etc/strapi-access-key-secret/password") ??
-      process.env.STRAPI_ACCESS_KEY ??
-      "",
+    STRAPI_ACCESS_KEY: getStrapiAccessKey(),
     CMS: process.env.CMS ?? "FILE",
-    GERICHTSFINDER_ENCRYPTION_KEY:
-      readSecretVolume("/etc/courtdata-secrets/password") ??
-      process.env.GERICHTSFINDER_ENCRYPTION_KEY ??
-      "",
+    GERICHTSFINDER_ENCRYPTION_KEY: getGerichtsfinderEncryptionKey(),
     REDIS_ENDPOINT: process.env.REDIS_ENDPOINT ?? "localhost:6380",
-    REDIS_PASSWORD:
-      readSecretVolume("/etc/redis-password-secret/password") ??
-      process.env.REDIS_PASSWORD ??
-      "",
-    COOKIE_SESSION_SECRET:
-      readSecretVolume("/etc/cookie-session-secret/password") ??
-      process.env.COOKIE_SESSION_SECRET ??
-      "s3cr3t",
+    REDIS_PASSWORD: getRedisPassword(),
+    COOKIE_SESSION_SECRET: getCookieSessionSecret(),
     CONTENT_FILE_PATH: process.env.CONTENT_FILE_PATH ?? "./content.json",
     CSP_REPORT_URI: process.env.CSP_REPORT_URI,
     BUNDID_AUTH_BMI_ID: process.env.BUNDID_AUTH_BMI_ID?.trim(),
@@ -70,31 +64,13 @@ export function config(): Config {
     SAML_SP_SECRET_KEY_PATH:
       process.env.SAML_SP_SECRET_KEY_PATH?.trim() ??
       path.join(process.cwd(), "data/saml/sp_privateKey.pem"),
-    SAML_IDP_CERT:
-      readSecretVolume("/etc/saml/idp_cert") ??
-      process.env.SAML_IDP_CERT?.trim(),
+    SAML_IDP_CERT: getSamlIDPCert(),
     S3_REGION: process.env.AWS_S3_REGION ?? "eu-central-1",
     S3_ENDPOINT:
       process.env.S3_ENDPOINT ?? "https://s3.localhost.localstack.cloud:4566",
-    S3_DATA_STORAGE_ACCESS_KEY:
-      readSecretVolume(
-        "/etc/s3-storage-credentials-secret-access-key/password",
-      ) ??
-      process.env.S3_DATA_STORAGE_ACCESS_KEY ??
-      "test",
-    S3_DATA_STORAGE_SECRET_KEY:
-      readSecretVolume("/etc/s3-storage-credentials-secret-key/password") ??
-      process.env.S3_DATA_STORAGE_SECRET_KEY ??
-      "test",
+    S3_DATA_STORAGE_ACCESS_KEY: getS3DataStorageAccessKey(),
+    S3_DATA_STORAGE_SECRET_KEY: getS3DataStorageSecretKey(),
     S3_DATA_STORAGE_BUCKET_NAME:
       process.env.S3_DATA_STORAGE_BUCKET_NAME ?? "a2j-data-storage",
   };
-}
-
-function readSecretVolume(path: string): string | null {
-  try {
-    return fs.readFileSync(path, "utf8");
-  } catch {
-    return null;
-  }
 }
