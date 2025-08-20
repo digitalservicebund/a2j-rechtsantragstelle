3f04b35c1f292ff4a4eff4c279dc41a6a0981dd5
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-08-20 08:20:37 +0200

    fully remove start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euf
-
-# Exporting only if not already set, see https://stackoverflow.com/a/11686912
-
-# NODE_ENV is only used by express, see https://expressjs.com/en/advanced/best-practice-performance.html#set-node_env-to-production
-NODE_ENV=production node ./server.js
