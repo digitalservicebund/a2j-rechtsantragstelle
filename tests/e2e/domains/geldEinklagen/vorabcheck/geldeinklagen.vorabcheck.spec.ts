57a5405502439861a4c465f74e280f6ba388ea01
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-08-21 15:00:55 +0200

    chore: remove redundant e2e steps test

--- tests/e2e/domains/geldEinklagen/vorabcheck/geldeinklagen.vorabcheck.spec.ts
+++ tests/e2e/domains/geldEinklagen/vorabcheck/geldeinklagen.vorabcheck.spec.ts
@@ -13,52 +13,8 @@ test.beforeEach(async ({ page }) => {
   await cookieSettings.acceptCookieBanner();
 });
 
-test.skip("forwarded to initial step", async ({ page }) => {
-  await expect(page).toHaveURL(
-    new RegExp(`.+${geldEinklagen.url}/${geldEinklagen.initialStep}$`),
-  );
-});
-
-test.skip("geldeinklagen can be traversed", async ({ page }) => {
+test("forwarded to initial step", async ({ page }) => {
   await expectPageToBeAccessible({ page });
-  await geldEinklagen.clickNext();
-
-  await geldEinklagen.fillRadioPage("forderung", "money");
-  await geldEinklagen.fillDropdownPage("geldspanne", "above_1000");
-
-  // gerichtskostenvorschuss
-  await expectPageToBeAccessible({ page });
-  await geldEinklagen.clickNext();
-
-  await geldEinklagen.fillRadioPage("bereich", "travel");
-
-  await geldEinklagen.fillRadioPage("flug", "no");
-
-  await geldEinklagen.fillRadioPage("privatperson", "yes");
-
-  await geldEinklagen.fillRadioPage("gegenseite", "unternehmen");
-
-  await geldEinklagen.fillRadioPage("gegenseiteUnternehmenDeutschland", "yes");
-
-  await geldEinklagen.fillInputPage("gegenseiteUnternehmenPlz", "84104"); // not a partner court
-
-  await geldEinklagen.fillInputPage("ortLeistungPlz", "85435"); // partner court
-
-  await geldEinklagen.fillRadioPage("gegenseiteKontakt", "yes");
-
-  await geldEinklagen.fillRadioPage("gegenseiteFrist", "yes");
-
-  await geldEinklagen.fillRadioPage("digitalAusweisen", "yesWithId");
-
-  await expect(
-    page.getByRole("heading").filter({
-      hasText: "Ein Pilotgericht ist wahrscheinlich für Ihren Fall zuständig.",
-    }),
-  ).toHaveCount(1);
-});
-
-test.skip("funnel: invalid step redirects to start", async ({ page }) => {
-  await page.goto(`${geldEinklagen.url}/stepDoesNotExist`);
   await expect(page).toHaveURL(
     new RegExp(`.+${geldEinklagen.url}/${geldEinklagen.initialStep}$`),
   );
