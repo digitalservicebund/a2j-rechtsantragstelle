091e5dfa9eb5c1168048770b5490297f475e6bd6
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-08-22 10:56:24 +0200

    refactor(readSecretOrEnvVar): move to utility function + config

--- app/services/env/env.server.ts
+++ app/services/env/env.server.ts
@@ -1,15 +1,6 @@
 import path from "path";
 import { configDotenv } from "dotenv";
-import {
-  getStrapiApi,
-  getStrapiAccessKey,
-  getGerichtsfinderEncryptionKey,
-  getRedisPassword,
-  getCookieSessionSecret,
-  getSamlIDPCert,
-  getS3DataStorageAccessKey,
-  getS3DataStorageSecretKey,
-} from "~/services/env/environmentVariableHelpers";
+import { readSecretOrEnvVar } from "~/services/env/readSecretOrEnvVar.server";
 
 configDotenv({ override: true, quiet: true }); // override is needed to enable live reloading on change
 
@@ -38,17 +29,33 @@ type Config = {
 };
 
 export function config(): Config {
-  const STRAPI_API = getStrapiApi();
+  const STRAPI_API = readSecretOrEnvVar(
+    "/etc/strapi-api-secret/password",
+    "STRAPI_API",
+  );
 
   return {
-    STRAPI_API: STRAPI_API ?? "",
-    STRAPI_HOST: STRAPI_API?.replace("/api/", "") ?? "",
-    STRAPI_ACCESS_KEY: getStrapiAccessKey(),
+    STRAPI_API,
+    STRAPI_HOST: STRAPI_API.replace("/api/", ""),
+    STRAPI_ACCESS_KEY: readSecretOrEnvVar(
+      "/etc/strapi-access-key-secret/password",
+      "STRAPI_ACCESS_KEY",
+    ),
     CMS: process.env.CMS ?? "FILE",
-    GERICHTSFINDER_ENCRYPTION_KEY: getGerichtsfinderEncryptionKey(),
+    GERICHTSFINDER_ENCRYPTION_KEY: readSecretOrEnvVar(
+      "/etc/courtdata-secrets/password",
+      "GERICHTSFINDER_ENCRYPTION_KEY",
+    ),
     REDIS_ENDPOINT: process.env.REDIS_ENDPOINT ?? "localhost:6380",
-    REDIS_PASSWORD: getRedisPassword(),
-    COOKIE_SESSION_SECRET: getCookieSessionSecret(),
+    REDIS_PASSWORD: readSecretOrEnvVar(
+      "/etc/redis-password-secret/password",
+      "REDIS_PASSWORD",
+    ),
+    COOKIE_SESSION_SECRET: readSecretOrEnvVar(
+      "/etc/cookie-session-secret/password",
+      "COOKIE_SESSION_SECRET",
+      "s3cr3t",
+    ),
     CONTENT_FILE_PATH: process.env.CONTENT_FILE_PATH ?? "./content.json",
     CSP_REPORT_URI: process.env.CSP_REPORT_URI,
     BUNDID_AUTH_BMI_ID: process.env.BUNDID_AUTH_BMI_ID?.trim(),
@@ -64,12 +71,24 @@ export function config(): Config {
     SAML_SP_SECRET_KEY_PATH:
       process.env.SAML_SP_SECRET_KEY_PATH?.trim() ??
       path.join(process.cwd(), "data/saml/sp_privateKey.pem"),
-    SAML_IDP_CERT: getSamlIDPCert(),
+    SAML_IDP_CERT: readSecretOrEnvVar(
+      "/etc/saml/idp_cert",
+      "SAML_IDP_CERT",
+      "test",
+    ),
     S3_REGION: process.env.AWS_S3_REGION ?? "eu-central-1",
     S3_ENDPOINT:
       process.env.S3_ENDPOINT ?? "https://s3.localhost.localstack.cloud:4566",
-    S3_DATA_STORAGE_ACCESS_KEY: getS3DataStorageAccessKey(),
-    S3_DATA_STORAGE_SECRET_KEY: getS3DataStorageSecretKey(),
+    S3_DATA_STORAGE_ACCESS_KEY: readSecretOrEnvVar(
+      "/etc/s3-storage-credentials-secret-access-key/password",
+      "S3_DATA_STORAGE_ACCESS_KEY",
+      "test",
+    ),
+    S3_DATA_STORAGE_SECRET_KEY: readSecretOrEnvVar(
+      "/etc/s3-storage-credentials-secret-key/password",
+      "S3_DATA_STORAGE_SECRET_KEY",
+      "test",
+    ),
     S3_DATA_STORAGE_BUCKET_NAME:
       process.env.S3_DATA_STORAGE_BUCKET_NAME ?? "a2j-data-storage",
   };
