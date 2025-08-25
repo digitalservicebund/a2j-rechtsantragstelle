6c76dd8f068739a2510db55a3beec040ba998278
Ingrid do Rio Schulze <46021179+ingriddorioschulze@users.noreply.github.com>
2025-08-25 11:54:37 +0200

    refactor: move berh persoenliche daten to page schemas (#2490)
    * refactor: move BerH persoenlichen daten to pageSchemas RAST-194
    
    * refactor: add telefonnumber to the test context

---
+++ app/domains/beratungshilfe/formular/persoenlicheDaten/pages.ts
@@ -0,0 +1,53 @@
+import { type PagesConfig } from "~/domains/pageSchemas";
+import { createDateSchema } from "~/services/validation/date";
+import { germanHouseNumberSchema } from "~/services/validation/germanHouseNumber";
+import { phoneNumberSchema } from "~/services/validation/phoneNumber";
+import { postcodeSchema } from "~/services/validation/postcode";
+import { schemaOrEmptyString } from "~/services/validation/schemaOrEmptyString";
+import { stringRequiredSchema } from "~/services/validation/stringRequired";
+import { addYears, today } from "~/util/date";
+
+export const berHAntragPersoenlicheDatenPages = {
+  persoenlicheDatenStart: {
+    stepId: "persoenliche-daten/start",
+  },
+  name: {
+    stepId: "persoenliche-daten/name",
+    pageSchema: {
+      vorname: stringRequiredSchema,
+      nachname: stringRequiredSchema,
+    },
+  },
+  geburtsdatum: {
+    stepId: "persoenliche-daten/geburtsdatum",
+    pageSchema: {
+      geburtsdatum: createDateSchema({
+        earliest: () => addYears(today(), -150),
+        latest: () => today(),
+      }),
+    },
+  },
+  plz: {
+    stepId: "persoenliche-daten/plz",
+    pageSchema: {
+      plz: stringRequiredSchema.pipe(postcodeSchema),
+    },
+  },
+  adresse: {
+    stepId: "persoenliche-daten/adresse",
+    pageSchema: {
+      street: stringRequiredSchema,
+      houseNumber: germanHouseNumberSchema,
+      ort: stringRequiredSchema,
+    },
+  },
+  telefonnummer: {
+    stepId: "persoenliche-daten/telefonnummer",
+    pageSchema: {
+      telefonnummer: schemaOrEmptyString(phoneNumberSchema),
+    },
+  },
+  nachbefragung: {
+    stepId: "persoenliche-daten/nachbefragung",
+  },
+} as const satisfies PagesConfig;
--- app/domains/beratungshilfe/formular/xstateConfig.ts
+++ app/domains/beratungshilfe/formular/xstateConfig.ts
@@ -1,26 +1,19 @@
-import mapValues from "lodash/mapValues";
-import {
-  staatlicheLeistungenIsBuergergeld,
-  staatlicheLeistungenIsKeine,
-} from "~/domains/shared/formular/finanzielleAngaben/guards";
-import { getPersoenlicheDatenXstateConfig } from "~/domains/shared/formular/persoenlicheDaten/xStateConfig";
+import { xStateTargetsFromPagesConfig } from "~/domains/pageSchemas";
 import type { Config } from "~/services/flow/server/buildFlowController";
 import { isFeatureFlagEnabled } from "~/services/isFeatureFlagEnabled.server";
 import { abgabeXstateConfig } from "./abgabe/xstateConfig";
 import { anwaltlicheVertretungXstateConfig } from "./anwaltlicheVertretung/xstateConfig";
 import { finanzielleAngabenArrayConfig as beratungshilfeFormularFinanzielleAngabenArrayConfig } from "./finanzielleAngaben/arrayConfiguration";
-import { finanzielleAngabeGuards } from "./finanzielleAngaben/guards";
 import { finanzielleAngabenXstateConfig } from "./finanzielleAngaben/xstateConfig";
 import { grundvorraussetzungXstateConfig } from "./grundvoraussetzung/xstateConfig";
 import { beratungshilfeAntragPages } from "./pages";
-import { beratungshilfePersoenlicheDatenDone } from "./persoenlicheDaten/doneFunctions";
+import { persoenlicheDatenXstateConfig } from "./persoenlicheDaten/xstateConfig";
 import { rechtsproblemXstateConfig } from "./rechtsproblem/xstateConfig";
 import type { BeratungshilfeFormularUserData } from "./userData";
 import { weitereAngabenDone } from "./weitereAngaben/doneFunctions";
 
 const showNachbefragung = await isFeatureFlagEnabled("showNachbefragung");
-
-const stepIds = mapValues(beratungshilfeAntragPages, (v) => v.stepId);
+const steps = xStateTargetsFromPagesConfig(beratungshilfeAntragPages);
 
 export const beratungshilfeXstateConfig = {
   id: "/beratungshilfe/antrag",
@@ -36,49 +29,14 @@ export const beratungshilfeXstateConfig = {
       initial: "start",
       meta: { done: () => true },
       states: {
-        [stepIds.start]: { on: { SUBMIT: "#grundvoraussetzungen" } },
+        [steps.start.relative]: { on: { SUBMIT: "#grundvoraussetzungen" } },
       },
     },
     grundvoraussetzungen: grundvorraussetzungXstateConfig,
     "anwaltliche-vertretung": anwaltlicheVertretungXstateConfig,
     rechtsproblem: rechtsproblemXstateConfig,
     "finanzielle-angaben": finanzielleAngabenXstateConfig,
-    "persoenliche-daten": getPersoenlicheDatenXstateConfig(
-      ({ context }) => beratungshilfePersoenlicheDatenDone({ context }),
-      {
-        backToCallingFlow: [
-          {
-            guard: ({ context }) =>
-              staatlicheLeistungenIsKeine({ context }) &&
-              finanzielleAngabeGuards.hasAusgabenYes({ context }),
-            target: "#ausgaben.uebersicht",
-          },
-          {
-            guard: staatlicheLeistungenIsKeine,
-            target: "#ausgaben",
-          },
-          {
-            guard: ({ context }) =>
-              staatlicheLeistungenIsBuergergeld({ context }) &&
-              finanzielleAngabeGuards.hasGrundeigentumYes({ context }),
-            target: "#eigentum.grundeigentum.uebersicht",
-          },
-          {
-            guard: staatlicheLeistungenIsBuergergeld,
-            target: "#eigentum.grundeigentum",
-          },
-          "#finanzielle-angaben.einkommen.staatliche-leistungen",
-        ],
-        nextFlowEntrypoint: showNachbefragung
-          ? "nachbefragung"
-          : "#weitere-angaben",
-      },
-      {
-        nachbefragung: {
-          on: { BACK: "telefonnummer", SUBMIT: "#weitere-angaben" },
-        },
-      },
-    ),
+    "persoenliche-daten": persoenlicheDatenXstateConfig,
     "weitere-angaben": {
       id: "weitere-angaben",
       meta: { done: weitereAngabenDone },
