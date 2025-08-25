d546fe4cc5642bb7b65ebbaacba5c9af8cf6b1cd
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-08-25 17:53:54 +0200

    feat(oxlint): ignore var usage in 2 files

--- app/services/gerichtsfinder/encryptedStorage.server.ts
+++ app/services/gerichtsfinder/encryptedStorage.server.ts
@@ -58,6 +58,7 @@ function updateZipfile(zipFilepath: string) {
 // Caching file read, decryption & parsing to survive server reload
 // See https://remix.run/docs/en/1.16.1/tutorials/jokes#connect-to-the-database
 declare global {
+  // oxlint-disable-next-line no-var
   var __encData: Record<string, any> | undefined; // NOSONAR
 }
 
