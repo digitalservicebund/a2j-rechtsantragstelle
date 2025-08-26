6af1ca5f6165b80bbd4de3e869c2eaa4b3b9d8f0
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2025-08-26 15:40:21 +0200

    feat(oxlint): unpack react config flat recommendation

--- eslint.config.js
+++ eslint.config.js
@@ -50,7 +50,32 @@ export default tseslint.config(
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [
-      react.configs.flat.recommended,
+      {
+        rules: {
+          "react/display-name": "error",
+          "react/jsx-key": "error",
+          "react/jsx-no-comment-textnodes": "error",
+          "react/jsx-no-duplicate-props": "error",
+          "react/jsx-no-target-blank": "error",
+          "react/jsx-no-undef": "error",
+          "react/jsx-uses-react": "error",
+          "react/jsx-uses-vars": "error",
+          "react/no-children-prop": "error",
+          "react/no-danger-with-children": "error",
+          "react/no-deprecated": "error",
+          "react/no-direct-mutation-state": "error",
+          "react/no-find-dom-node": "error",
+          "react/no-is-mounted": "error",
+          "react/no-render-return-value": "error",
+          "react/no-string-refs": "error",
+          "react/no-unescaped-entities": "error",
+          "react/no-unknown-property": "error",
+          "react/no-unsafe": "off",
+          "react/prop-types": "error",
+          "react/react-in-jsx-scope": "error",
+          "react/require-render-return": "error",
+        },
+      },
       react.configs.flat["jsx-runtime"],
       /**
        * TODO: flat config of this plugin isn't available yet https://github.com/facebook/react/pull/30774
