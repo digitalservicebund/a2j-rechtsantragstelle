012db37c4c73f0db08d0db34e82be0ef8e24be16
Ingrid do Rio Schulze <46021179+ingriddorioschulze@users.noreply.github.com>
2025-08-26 13:16:29 +0200

    refactor(BerH): move Finanziellen Angaben Wohnung to pageSchemas (#2503)
    * refactor(BerH): move Finanziellen Angaben Wohnung to pageSchemas RAST-193
    
    * refactor(BerH): change inputs order and fix closing parantheses

--- app/domains/beratungshilfe/formular/finanzielleAngaben/pages.ts
+++ app/domains/beratungshilfe/formular/finanzielleAngaben/pages.ts
@@ -2,9 +2,11 @@ import { type PagesConfig } from "~/domains/pageSchemas";
 import { berhAntragFinanzielleAngabenEinkommenPages } from "./einkommen/pages";
 import { berhAntragFinanzielleAngabenKinderPages } from "./kinder/pages";
 import { berhAntragFinanzielleAngabenPartnerPages } from "./partner/pages";
+import { berhAntragFinanzielleAngabenWohnungPages } from "./wohnung/pages";
 
 export const berhAntragFinanzielleAngabenPages = {
   ...berhAntragFinanzielleAngabenEinkommenPages,
   ...berhAntragFinanzielleAngabenPartnerPages,
   ...berhAntragFinanzielleAngabenKinderPages,
+  ...berhAntragFinanzielleAngabenWohnungPages,
 } as const satisfies PagesConfig;
---
+++ app/domains/beratungshilfe/formular/finanzielleAngaben/wohnung/xstateConfig.ts
@@ -0,0 +1,69 @@
+import { xStateTargetsFromPagesConfig } from "~/domains/pageSchemas";
+import { type Config } from "~/services/flow/server/types";
+import { type BeratungshilfeFinanzielleAngabenWohnungUserData } from "./userData";
+import { wohnungDone } from "../doneFunctions";
+import {
+  finanzielleAngabeGuards,
+  hasWeitereUnterhaltszahlungenYes,
+} from "../guards";
+import { berhAntragFinanzielleAngabenWohnungPages } from "./pages";
+
+const steps = xStateTargetsFromPagesConfig(
+  berhAntragFinanzielleAngabenWohnungPages,
+);
+
+export const berhAntragFinanzielleAngabenWohnungXstateConfig = {
+  id: "wohnung",
+  initial: steps.wohnsituation.relative,
+  meta: { done: wohnungDone },
+  on: {
+    SUBMIT: "#eigentum",
+  },
+  states: {
+    [steps.wohnsituation.relative]: {
+      on: {
+        BACK: [
+          {
+            guard: hasWeitereUnterhaltszahlungenYes,
+            target: "#andere-unterhaltszahlungen.uebersicht",
+          },
+          "#andere-unterhaltszahlungen.frage",
+        ],
+        SUBMIT: steps.wohnungGroesse.relative,
+      },
+    },
+    [steps.wohnungGroesse.relative]: {
+      on: {
+        BACK: steps.wohnsituation.relative,
+        SUBMIT: [
+          {
+            target: steps.wohnkostenAllein.relative,
+            guard: finanzielleAngabeGuards.livesAlone,
+          },
+          {
+            target: steps.personenAnzahl.relative,
+            guard: finanzielleAngabeGuards.livesNotAlone,
+          },
+        ],
+      },
+    },
+    [steps.wohnkostenAllein.relative]: {
+      on: {
+        BACK: steps.wohnungGroesse.relative,
+        SUBMIT: "#eigentum",
+      },
+    },
+    [steps.personenAnzahl.relative]: {
+      on: {
+        BACK: steps.wohnungGroesse.relative,
+        SUBMIT: steps.wohnkostenGeteilt.relative,
+      },
+    },
+    [steps.wohnkostenGeteilt.relative]: {
+      on: {
+        BACK: steps.personenAnzahl.relative,
+        SUBMIT: "#eigentum",
+      },
+    },
+  },
+} satisfies Config<BeratungshilfeFinanzielleAngabenWohnungUserData>;
