115ae914bc37866639f7ca32d3a40373ec37b8fd
aaschlote <alex.schlote@digitalservice.bund.de>
2025-09-03 20:58:43 +0200

    refactor: add correct initial page

--- app/domains/fluggastrechte/formular/flugdaten/xstateConfig.ts
+++ app/domains/fluggastrechte/formular/flugdaten/xstateConfig.ts
@@ -5,7 +5,7 @@ import { flugdatenDone } from "./doneFunctions";
 export const flugdatenXstateConfig = {
   meta: { done: flugdatenDone },
   id: "flugdaten",
-  initial: "adresse-fluggesellschaft",
+  initial: "adresse-fluggesellschaft-auswahl",
   states: {
     "adresse-fluggesellschaft-auswahl": {
       on: {
