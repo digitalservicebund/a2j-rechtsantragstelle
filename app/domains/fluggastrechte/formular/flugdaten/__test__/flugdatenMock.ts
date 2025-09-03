d166d9b25f136629d4cfbf92618ce319457da947
aaschlote <alex.schlote@digitalservice.bund.de>
2025-09-03 11:39:54 +0200

    test(fgr): add tests cases for the new page

--- app/domains/fluggastrechte/formular/flugdaten/__test__/flugdatenMock.ts
+++ app/domains/fluggastrechte/formular/flugdaten/__test__/flugdatenMock.ts
@@ -1,6 +1,9 @@
+import { type FluggastrechteFlugdatenUserData } from "../userData";
+
 export const fluggesellschaftAddresse = {
+  fluggesellschaftAuswahlAddress: "filledByUser",
   fluggesellschaftStrasseHausnummer: "Musterstr. 30",
   fluggesellschaftPostleitzahl: "10970",
   fluggesellschaftOrt: "Frankfurt",
   fluggesellschaftLand: "Deutschland",
-};
+} satisfies Partial<FluggastrechteFlugdatenUserData>;
