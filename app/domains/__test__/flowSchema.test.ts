0a9d2522867197c86c0e0fa85193066b9adf57cc
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-09-09 19:05:06 +0200

    refactor testcases to be generic

--- app/domains/__test__/flowSchema.test.ts
+++ app/domains/__test__/flowSchema.test.ts
@@ -1,31 +1,45 @@
 import z from "zod";
-import { beratungshilfeVorabcheckXstateConfig } from "../beratungshilfe/vorabcheck/xstateConfig";
 import { getPageSchema } from "../pageSchemas";
 import { buildFlowController } from "~/services/flow/server/buildFlowController";
-import { beratungshilfeVorabcheckE2E } from "../beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs";
+import { beratungshilfeVorabcheckTestCases } from "../beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs";
 
-describe("beratungshilfeVorabcheck", () => {
-  const xstateConfig = beratungshilfeVorabcheckXstateConfig;
-  const { flowId, expectedSteps } = beratungshilfeVorabcheckE2E;
+const flowSchemaTests = { beratungshilfeVorabcheckTestCases };
 
-  it("can be traversed", () => {
-    expectedSteps.slice(0, -1).forEach(({ url, userInput }, idx) => {
-      const pageSchema = getPageSchema(flowId + url);
-      if (!userInput) {
-        expect(pageSchema).toBeUndefined();
-      } else {
-        expect(pageSchema).toBeDefined();
-        const validationResult = z.object(pageSchema!).safeParse(userInput);
-        expect(validationResult.error).toBeUndefined();
-      }
+describe("flowSchemas", () => {
+  Object.entries(flowSchemaTests).forEach(
+    ([testConfigName, { xstateConfig, testcases }]) => {
+      const flowId = xstateConfig.id;
+      describe(testConfigName, () => {
+        Object.entries(testcases).forEach(([testName, expectedSteps]) => {
+          test(testName, () => {
+            expectedSteps.slice(0, -1).forEach(({ stepId, userInput }, idx) => {
+              const currentUrl = flowId + stepId;
+              const pageSchema = getPageSchema(currentUrl);
 
-      const flowController = buildFlowController({
-        config: xstateConfig,
-        data: userInput,
+              if (!userInput) {
+                // Without userInput we don't expect a pageSchema
+                expect(pageSchema).toBeUndefined();
+              } else {
+                // With userInput we expect to validate against the pageSchema
+                expect(pageSchema).toBeDefined();
+                const validationResult = z
+                  .object(pageSchema!)
+                  .safeParse(userInput);
+                expect(validationResult.error).toBeUndefined();
+              }
+
+              const flowController = buildFlowController({
+                config: xstateConfig,
+                data: userInput,
+              });
+
+              // Given the current data and url we expect the next url
+              const nextStepId = expectedSteps[idx + 1]?.stepId;
+              expect(flowController.getNext(stepId)).toBe(flowId + nextStepId);
+            });
+          });
+        });
       });
-
-      const expectedNextUrl = flowId + expectedSteps[idx + 1]?.url;
-      expect(flowController.getNext(url)).toBe(expectedNextUrl);
-    });
-  });
+    },
+  );
 });
--- app/domains/beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs.ts
+++ app/domains/beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs.ts
@@ -1,81 +1,73 @@
-import type { FlowId } from "~/domains/flowIds";
-import type { UserData } from "~/domains/userData";
+import type { FlowTestCases } from "~/domains/__test__/TestCases";
+import { beratungshilfeVorabcheckXstateConfig } from "../xstateConfig";
 
-type FlowTest = {
-  existingUserData?: UserData;
-  flowId: FlowId;
-  expectedSteps: Array<{
-    url: string;
-    userInput?: UserData;
-  }>;
-};
-
-export const beratungshilfeVorabcheckE2E = {
-  flowId: "/beratungshilfe/vorabcheck",
-  expectedSteps: [
-    { url: "/start" },
-    {
-      url: "/rechtsschutzversicherung",
-      userInput: { rechtsschutzversicherung: "no" },
-    },
-    {
-      url: "/wurde-verklagt",
-      userInput: { wurdeVerklagt: "no" },
-    },
-    {
-      url: "/klage-eingereicht",
-      userInput: { klageEingereicht: "no" },
-    },
-    {
-      url: "/hamburg-oder-bremen",
-      userInput: { hamburgOderBremen: "no" },
-    },
-    {
-      url: "/beratungshilfe-beantragt",
-      userInput: { beratungshilfeBeantragt: "no" },
-    },
-    {
-      url: "/eigeninitiative",
-      userInput: { eigeninitiative: "no" },
-    },
-
-    { url: "/eigeninitiative-warnung" },
-    {
-      url: "/bereich",
-      userInput: { bereich: "other" },
-    },
-    {
-      url: "/staatliche-leistungen",
-      userInput: { staatlicheLeistungen: "keine" },
-    },
-    {
-      url: "/vermoegen",
-      userInput: { vermoegen: "below_10k" },
-    },
-    {
-      url: "/erwerbstaetigkeit",
-      userInput: { erwerbstaetigkeit: "no" },
-    },
-    {
-      url: "/partnerschaft",
-      userInput: { partnerschaft: "no" },
-    },
-    {
-      url: "/genauigkeit",
-      userInput: { genauigkeit: "no" },
-    },
-    {
-      url: "/kinder-kurz",
-      userInput: { kinderKurz: "yes" },
-    },
-    {
-      url: "/kinder-anzahl-kurz",
-      userInput: { kinderAnzahlKurz: "5" },
-    },
-    {
-      url: "/verfuegbares-einkommen",
-      userInput: { verfuegbaresEinkommen: "yes" },
-    },
-    { url: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
-  ],
-} satisfies FlowTest;
+export const beratungshilfeVorabcheckTestCases = {
+  xstateConfig: beratungshilfeVorabcheckXstateConfig,
+  testcases: {
+    fullFlow: [
+      { stepId: "/start" },
+      {
+        stepId: "/rechtsschutzversicherung",
+        userInput: { rechtsschutzversicherung: "no" },
+      },
+      {
+        stepId: "/wurde-verklagt",
+        userInput: { wurdeVerklagt: "no" },
+      },
+      {
+        stepId: "/klage-eingereicht",
+        userInput: { klageEingereicht: "no" },
+      },
+      {
+        stepId: "/hamburg-oder-bremen",
+        userInput: { hamburgOderBremen: "no" },
+      },
+      {
+        stepId: "/beratungshilfe-beantragt",
+        userInput: { beratungshilfeBeantragt: "no" },
+      },
+      {
+        stepId: "/eigeninitiative",
+        userInput: { eigeninitiative: "no" },
+      },
+      { stepId: "/eigeninitiative-warnung" },
+      {
+        stepId: "/bereich",
+        userInput: { bereich: "other" },
+      },
+      {
+        stepId: "/staatliche-leistungen",
+        userInput: { staatlicheLeistungen: "keine" },
+      },
+      {
+        stepId: "/vermoegen",
+        userInput: { vermoegen: "below_10k" },
+      },
+      {
+        stepId: "/erwerbstaetigkeit",
+        userInput: { erwerbstaetigkeit: "no" },
+      },
+      {
+        stepId: "/partnerschaft",
+        userInput: { partnerschaft: "no" },
+      },
+      {
+        stepId: "/genauigkeit",
+        userInput: { genauigkeit: "no" },
+      },
+      {
+        stepId: "/kinder-kurz",
+        userInput: { kinderKurz: "yes" },
+      },
+      {
+        stepId: "/kinder-anzahl-kurz",
+        userInput: { kinderAnzahlKurz: "5" },
+      },
+      {
+        stepId: "/verfuegbares-einkommen",
+        userInput: { verfuegbaresEinkommen: "yes" },
+      },
+      { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
+    ],
+  },
+} satisfies FlowTestCases;
