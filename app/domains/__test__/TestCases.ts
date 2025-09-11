e86e0bea56dfd187aa76bce4930795f99292bd84
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-09-11 19:48:21 +0200

    feat(pageSchemas): replace e2e with flowTest (#2572)
    * feat(BHvorabcheck): replace e2e with flowTest
    
    * remove BH vorabcheck e2e tests
    
    * refactor testcases to be generic
    
    * feat(flowSchemaTests): add backwards test

--- app/domains/__test__/TestCases.ts
+++ app/domains/__test__/TestCases.ts
@@ -1,5 +1,19 @@
+import type { Config } from "~/services/flow/server/types";
 import type { UserData } from "../userData";
 
+// Old flow tests: forward & backward using full user data
 export type TestCases<T extends UserData> = Readonly<
   Array<Readonly<[T, readonly string[]]>>
 >;
+
+// New flow tests: testing data submission with page schemas
+export type FlowTestCases = {
+  xstateConfig: Config;
+  testcases: Record<
+    string,
+    Array<{
+      stepId: string;
+      userInput?: UserData;
+    }>
+  >;
+};
