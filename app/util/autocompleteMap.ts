85987a0488de71793bb38a0d8bf2c4614f438f90
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-09-11 10:14:45 +0200

    feat(autocomplete): remove autocomplete for street fields RAST-239

--- app/util/autocompleteMap.ts
+++ app/util/autocompleteMap.ts
@@ -1,9 +1,6 @@
 export const autocompleteMap: Record<string, string> = {
   vorname: "given-name",
   nachname: "family-name",
-  adresse: "street-address",
-  hausnummer: "address-line1",
-  ort: "address-level2",
   plz: "postal-code",
   telefonnummer: "tel",
   email: "email",
