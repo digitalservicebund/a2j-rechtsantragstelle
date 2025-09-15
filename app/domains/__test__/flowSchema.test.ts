ac9407964bcbf734eca37a76374ec376cb530dd1
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-09-15 09:07:07 +0200

    add more BerH Vorabcheck tests, move machine utils to util file, add testcases property to Flow, simplify testcases interface, add flow coverage test

--- app/domains/__test__/flowSchema.test.ts
+++ app/domains/__test__/flowSchema.test.ts
@@ -1,14 +1,17 @@
+import { createMachine } from "xstate";
 import z from "zod";
 import { getPageSchema } from "../pageSchemas";
 import { buildFlowController } from "~/services/flow/server/buildFlowController";
-import { beratungshilfeVorabcheckTestCases } from "../beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs";
 import type { FlowTestCases } from "./TestCases";
+import { FlowStateMachine } from "~/services/flow/server/types";
+import { beratungshilfeVorabcheck } from "~/domains/beratungshilfe/vorabcheck";
+import { allStepsFromMachine } from "~/services/flow/machineUtils";
 
-const flowSchemaTests = { beratungshilfeVorabcheckTestCases };
+const flowSchemas = { beratungshilfeVorabcheck };
 
 // Build full user input from all previous expectedSteps
 const buildFullUserInput = (
-  expectedSteps: FlowTestCases["testcases"][string],
+  expectedSteps: FlowTestCases[string],
   idx: number,
 ) =>
   expectedSteps
@@ -16,40 +19,78 @@ const buildFullUserInput = (
     .reduce((acc, step) => ({ ...acc, ...step.userInput }), {});
 
 describe("flowSchemas", () => {
-  Object.entries(flowSchemaTests).forEach(
-    ([testConfigName, { xstateConfig, testcases }]) => {
-      const flowId = xstateConfig.id;
+  const allVisitedSteps: Record<
+    string,
+    { stepIds: Set<string | undefined>; machine: FlowStateMachine }
+  > = {};
+  Object.entries(flowSchemas).forEach(([testConfigName, schema]) => {
+    const { config: xstateConfig, testcases, guards } = schema;
+    const flowId = xstateConfig.id;
+    const machine: FlowStateMachine = createMachine(xstateConfig, { guards });
+    if (!allVisitedSteps[machine.id]) {
+      allVisitedSteps[machine.id] = { machine, stepIds: new Set() };
+    }
 
-      describe(testConfigName, () => {
-        Object.entries(testcases).forEach(([testName, expectedSteps]) => {
-          test(testName, () => {
-            expectedSteps.slice(0, -1).forEach(({ stepId, userInput }, idx) => {
-              const currentUrl = flowId + stepId;
-              const pageSchema = getPageSchema(currentUrl);
+    describe(testConfigName, () => {
+      Object.entries(testcases).forEach(([testName, expectedSteps]) => {
+        test(testName, () => {
+          expectedSteps.slice(0, -1).forEach(({ stepId, userInput }, idx) => {
+            const currentUrl = flowId + stepId;
+            const pageSchema = getPageSchema(currentUrl);
 
-              if (!userInput) {
-                expect(pageSchema).toBeUndefined(); // Without userInput we don't expect a pageSchema
-              } else {
-                expect(pageSchema).toBeDefined(); // With userInput we expect it to validate against the pageSchema
-                const validationResult = z
-                  .object(pageSchema!)
-                  .safeParse(userInput);
-                expect(validationResult.error).toBeUndefined();
-              }
+            if (!userInput) {
+              expect(pageSchema).toBeUndefined(); // Without userInput we don't expect a pageSchema
+            } else {
+              expect(pageSchema).toBeDefined(); // With userInput we expect it to validate against the pageSchema
+              const validationResult = z
+                .object(pageSchema)
+                .safeParse(userInput);
+              expect(validationResult.error).toBeUndefined();
+            }
 
-              const flowController = buildFlowController({
-                config: xstateConfig,
-                data: buildFullUserInput(expectedSteps, idx),
-              });
-
-              // Given the current data and url we expect the next and previous url
-              const nextStepId = expectedSteps[idx + 1]?.stepId;
-              expect(flowController.getNext(stepId)).toBe(flowId + nextStepId);
-              expect(flowController.getPrevious(nextStepId)).toBe(currentUrl);
+            const flowController = buildFlowController({
+              config: xstateConfig,
+              data: buildFullUserInput(expectedSteps, idx),
             });
+
+            // Given the current data and url we expect the next and previous url
+            const nextStepId = expectedSteps[idx + 1]?.stepId;
+            allVisitedSteps[machine.id].stepIds.add(
+              flowController.getPrevious(nextStepId)?.replace(flowId, ""),
+            );
+            allVisitedSteps[machine.id].stepIds.add(
+              flowController.getNext(stepId)?.replace(flowId, ""),
+            );
+            expect(flowController.getNext(stepId)).toBe(flowId + nextStepId);
+            expect(flowController.getPrevious(nextStepId)).toBe(currentUrl);
           });
         });
       });
-    },
-  );
+    });
+  });
+
+  test("all steps are visited", () => {
+    const missingStepsEntries = Object.entries(allVisitedSteps)
+      .map(([machineId, { machine, stepIds }]) => {
+        const visitedSteps = new Set(stepIds);
+        const missingSteps = allStepsFromMachine(machine).filter(
+          (x) => !visitedSteps.has(x),
+        );
+        // .filter((x) => !ignoreVisitedSteps.includes(x));
+        return [machineId, missingSteps] as const;
+      })
+      .filter(([_, missingSteps]) => missingSteps.length > 0);
+
+    const totalMissingStepCount = missingStepsEntries.reduce(
+      (total, [_, missingSteps]) => total + missingSteps.length,
+      0,
+    );
+
+    // oxlint-disable-next-line no-console
+    console.warn(
+      `Total of ${totalMissingStepCount} untested stepIds: `,
+      Object.fromEntries(missingStepsEntries),
+    );
+    expect(totalMissingStepCount).toBeLessThanOrEqual(0);
+  });
 });
--- app/domains/beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs.ts
+++ app/domains/beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs.ts
@@ -1,5 +1,4 @@
 import type { FlowTestCases } from "~/domains/__test__/TestCases";
-import { beratungshilfeVorabcheckXstateConfig } from "../xstateConfig";
 
 const rsvTestCases = {
   rsvYes: [
@@ -45,7 +44,7 @@ const rsvTestCases = {
       stepId: "/rechtsschutzversicherung-hinweis-kostenuebernahme",
     },
   ],
-} satisfies FlowTestCases["testcases"];
+} satisfies FlowTestCases;
 
 const staatlicheLeistungenTestCases = {
   asylbewerberleistungen: [
@@ -93,210 +92,246 @@ const staatlicheLeistungenTestCases = {
     },
     { stepId: "/ergebnis/vermoegen-abschluss-vielleicht" },
   ],
-} satisfies FlowTestCases["testcases"];
+} satisfies FlowTestCases;
 
 export const beratungshilfeVorabcheckTestCases = {
-  xstateConfig: beratungshilfeVorabcheckXstateConfig,
-  testcases: {
-    fullFlow: [
-      { stepId: "/start" },
-      {
-        stepId: "/rechtsschutzversicherung",
-        userInput: { rechtsschutzversicherung: "no" },
+  fullFlow: [
+    { stepId: "/start" },
+    {
+      stepId: "/rechtsschutzversicherung",
+      userInput: { rechtsschutzversicherung: "no" },
+    },
+    {
+      stepId: "/wurde-verklagt",
+      userInput: { wurdeVerklagt: "no" },
+    },
+    {
+      stepId: "/klage-eingereicht",
+      userInput: { klageEingereicht: "no" },
+    },
+    {
+      stepId: "/hamburg-oder-bremen",
+      userInput: { hamburgOderBremen: "no" },
+    },
+    {
+      stepId: "/beratungshilfe-beantragt",
+      userInput: { beratungshilfeBeantragt: "no" },
+    },
+    {
+      stepId: "/eigeninitiative",
+      userInput: { eigeninitiative: "no" },
+    },
+    { stepId: "/eigeninitiative-warnung" },
+    {
+      stepId: "/bereich",
+      userInput: { bereich: "other" },
+    },
+    {
+      stepId: "/staatliche-leistungen",
+      userInput: { staatlicheLeistungen: "keine" },
+    },
+    {
+      stepId: "/vermoegen",
+      userInput: { vermoegen: "below_10k" },
+    },
+    {
+      stepId: "/erwerbstaetigkeit",
+      userInput: { erwerbstaetigkeit: "no" },
+    },
+    {
+      stepId: "/partnerschaft",
+      userInput: { partnerschaft: "no" },
+    },
+    {
+      stepId: "/genauigkeit",
+      userInput: { genauigkeit: "no" },
+    },
+    {
+      stepId: "/kinder-kurz",
+      userInput: { kinderKurz: "yes" },
+    },
+    {
+      stepId: "/kinder-anzahl-kurz",
+      userInput: { kinderAnzahlKurz: "5" },
+    },
+    {
+      stepId: "/verfuegbares-einkommen",
+      userInput: { verfuegbaresEinkommen: "yes" },
+    },
+    { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
+  ],
+  ...rsvTestCases,
+  wurdeVerklagt: [
+    { stepId: "/wurde-verklagt", userInput: { wurdeVerklagt: "yes" } },
+    { stepId: "/ergebnis/wurde-verklagt-abbruch" },
+  ],
+  klageEingereicht: [
+    { stepId: "/klage-eingereicht", userInput: { klageEingereicht: "yes" } },
+    { stepId: "/ergebnis/klage-eingereicht-abbruch" },
+  ],
+  hamburgBremen: [
+    {
+      stepId: "/hamburg-oder-bremen",
+      userInput: { hamburgOderBremen: "yes" },
+    },
+    { stepId: "/ergebnis/hamburg-oder-bremen-abbruch" },
+  ],
+  schonBeantragt: [
+    {
+      stepId: "/beratungshilfe-beantragt",
+      userInput: { beratungshilfeBeantragt: "yes" },
+    },
+    { stepId: "/ergebnis/beratungshilfe-beantragt-abbruch" },
+  ],
+  eigeninitiative: [
+    { stepId: "/eigeninitiative", userInput: { eigeninitiative: "no" } },
+    { stepId: "/eigeninitiative-warnung" },
+    { stepId: "/bereich" },
+  ],
+  ...staatlicheLeistungenTestCases,
+  vermoegenAbove10k: [
+    { stepId: "/vermoegen", userInput: { vermoegen: "above_10k" } },
+    { stepId: "/ergebnis/vermoegen-abbruch" },
+  ],
+  vermoegenBelow10k: [
+    {
+      stepId: "/vermoegen",
+      userInput: {
+        vermoegen: "below_10k",
+        staatlicheLeistungen: "buergergeld",
       },
-      {
-        stepId: "/wurde-verklagt",
-        userInput: { wurdeVerklagt: "no" },
-      },
-      {
-        stepId: "/klage-eingereicht",
-        userInput: { klageEingereicht: "no" },
-      },
-      {
-        stepId: "/hamburg-oder-bremen",
-        userInput: { hamburgOderBremen: "no" },
-      },
-      {
-        stepId: "/beratungshilfe-beantragt",
-        userInput: { beratungshilfeBeantragt: "no" },
-      },
-      {
-        stepId: "/eigeninitiative",
-        userInput: { eigeninitiative: "no" },
-      },
-      { stepId: "/eigeninitiative-warnung" },
-      {
-        stepId: "/bereich",
-        userInput: { bereich: "other" },
-      },
-      {
-        stepId: "/staatliche-leistungen",
-        userInput: { staatlicheLeistungen: "keine" },
-      },
-      {
-        stepId: "/vermoegen",
-        userInput: { vermoegen: "below_10k" },
-      },
-      {
-        stepId: "/erwerbstaetigkeit",
-        userInput: { erwerbstaetigkeit: "no" },
-      },
-      {
-        stepId: "/partnerschaft",
-        userInput: { partnerschaft: "no" },
-      },
-      {
-        stepId: "/genauigkeit",
-        userInput: { genauigkeit: "no" },
-      },
-      {
-        stepId: "/kinder-kurz",
-        userInput: { kinderKurz: "yes" },
-      },
-      {
-        stepId: "/kinder-anzahl-kurz",
-        userInput: { kinderAnzahlKurz: "5" },
-      },
-      {
-        stepId: "/verfuegbares-einkommen",
-        userInput: { verfuegbaresEinkommen: "yes" },
-      },
-      { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
-    ],
-    ...rsvTestCases,
-    wurdeVerklagt: [
-      { stepId: "/wurde-verklagt", userInput: { wurdeVerklagt: "yes" } },
-      { stepId: "/ergebnis/wurde-verklagt-abbruch" },
-    ],
-    klageEingereicht: [
-      { stepId: "/klage-eingereicht", userInput: { klageEingereicht: "yes" } },
-      { stepId: "/ergebnis/klage-eingereicht-abbruch" },
-    ],
-    hamburgBremen: [
-      {
-        stepId: "/hamburg-oder-bremen",
-        userInput: { hamburgOderBremen: "yes" },
-      },
-      { stepId: "/ergebnis/hamburg-oder-bremen-abbruch" },
-    ],
-    schonBeantragt: [
-      {
-        stepId: "/beratungshilfe-beantragt",
-        userInput: { beratungshilfeBeantragt: "yes" },
-      },
-      { stepId: "/ergebnis/beratungshilfe-beantragt-abbruch" },
-    ],
-    eigeninitiative: [
-      { stepId: "/eigeninitiative", userInput: { eigeninitiative: "no" } },
-      { stepId: "/eigeninitiative-warnung" },
-      { stepId: "/bereich" },
-    ],
-    ...staatlicheLeistungenTestCases,
-    vermoegenAbove10k: [
-      { stepId: "/vermoegen", userInput: { vermoegen: "above_10k" } },
-      { stepId: "/ergebnis/vermoegen-abbruch" },
-    ],
-    vermoegenBelow10k: [
-      {
-        stepId: "/vermoegen",
-        userInput: {
-          vermoegen: "below_10k",
-          staatlicheLeistungen: "buergergeld",
+    },
+    { stepId: "/ergebnis/vermoegen-abschluss-ja" },
+  ],
+  genauigkeitNein: [
+    { stepId: "/genauigkeit", userInput: { genauigkeit: "no" } },
+    { stepId: "/kinder-kurz" },
+  ],
+  kinderKurzYes: [
+    { stepId: "/kinder-kurz", userInput: { kinderKurz: "yes" } },
+    { stepId: "/kinder-anzahl-kurz" },
+  ],
+  kinderKurzNo: [
+    { stepId: "/kinder-kurz", userInput: { kinderKurz: "no" } },
+    { stepId: "/verfuegbares-einkommen" },
+  ],
+  kinderAnzahl: [
+    {
+      stepId: "/kinder-anzahl-kurz",
+      userInput: { kinderAnzahlKurz: "1", kinderKurz: "yes" },
+    },
+    { stepId: "/verfuegbares-einkommen" },
+  ],
+  verfugbaresEinkommenYes: [
+    {
+      stepId: "/verfuegbares-einkommen",
+      userInput: { verfuegbaresEinkommen: "yes" },
+    },
+    { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
+  ],
+  verfugbaresEinkommenNoEigeninitiativeYes: [
+    {
+      stepId: "/verfuegbares-einkommen",
+      userInput: { verfuegbaresEinkommen: "no", eigeninitiative: "yes" },
+    },
+    { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-ja" },
+  ],
+  verfugbaresEinkommenNoEigeninitiativeNo: [
+    {
+      stepId: "/verfuegbares-einkommen",
+      userInput: { verfuegbaresEinkommen: "no", eigeninitiative: "no" },
+    },
+    { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-vielleicht" },
+  ],
+  partnerschaftNo: [
+    {
+      stepId: "/einkommen",
+      userInput: { partnerschaft: "no", einkommen: "0" },
+    },
+    { stepId: "/kinder" },
+  ],
+  einkommenPartner: [
+    {
+      stepId: "/einkommen",
+      userInput: { partnerschaft: "yes", einkommen: "0" },
+    },
+    { stepId: "/einkommen-partner" },
+  ],
+  kinderNo: [
+    {
+      stepId: "/kinder",
+      userInput: { kinder: "no" },
+    },
+    { stepId: "/unterhalt" },
+  ],
+  kinderYes: [
+    {
+      stepId: "/kinder",
+      userInput: { kinder: "yes" },
+    },
+    {
+      stepId: "/kinder-anzahl",
+      userInput: {
+        kids: {
+          kids6Below: "1",
+          kids7To14: "0",
+          kids15To18: "0",
+          kids18Above: "0",
         },
       },
-      { stepId: "/ergebnis/vermoegen-abschluss-ja" },
-    ],
-    genauigkeitNein: [
-      { stepId: "/genauigkeit", userInput: { genauigkeit: "no" } },
-      { stepId: "/kinder-kurz" },
-    ],
-    kinderKurzYes: [
-      { stepId: "/kinder-kurz", userInput: { kinderKurz: "yes" } },
-      { stepId: "/kinder-anzahl-kurz" },
-    ],
-    kinderKurzNo: [
-      { stepId: "/kinder-kurz", userInput: { kinderKurz: "no" } },
-      { stepId: "/verfuegbares-einkommen" },
-    ],
-    kinderAnzahl: [
-      {
-        stepId: "/kinder-anzahl-kurz",
-        userInput: { kinderAnzahlKurz: "1", kinderKurz: "yes" },
+    },
+    {
+      stepId: "/einkommen-kinder",
+      userInput: {
+        einkommenKinder: "0",
       },
-      { stepId: "/verfuegbares-einkommen" },
-    ],
-    verfugbaresEinkommenYes: [
-      {
-        stepId: "/verfuegbares-einkommen",
-        userInput: { verfuegbaresEinkommen: "yes" },
+    },
+    { stepId: "/unterhalt" },
+  ],
+  unterhaltNo: [
+    {
+      stepId: "/unterhalt",
+      userInput: { unterhalt: "no" },
+    },
+    { stepId: "/miete" },
+  ],
+  unterhaltYes: [
+    {
+      stepId: "/unterhalt",
+      userInput: { unterhalt: "yes" },
+    },
+    { stepId: "/unterhalt-summe", userInput: { unterhaltSumme: "100" } },
+    { stepId: "/miete" },
+  ],
+  einkommenHoch: [
+    {
+      stepId: "/weitere-zahlungen-summe",
+      userInput: { einkommen: "10000000", weitereZahlungenSumme: "0" },
+    },
+    {
+      stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-nein",
+    },
+  ],
+  weitereZahlungenHoch: [
+    {
+      stepId: "/weitere-zahlungen-summe",
+      userInput: { einkommen: "0", weitereZahlungenSumme: "100000" },
+    },
+    {
+      stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-ja",
+    },
+  ],
+  einkommenBalanced: [
+    {
+      stepId: "/weitere-zahlungen-summe",
+      userInput: {
+        einkommen: "100",
+        weitereZahlungenSumme: "100",
+        eigeninitiative: "no",
       },
-      { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-nein" },
-    ],
-    verfugbaresEinkommenNoEigeninitiativeYes: [
-      {
-        stepId: "/verfuegbares-einkommen",
-        userInput: { verfuegbaresEinkommen: "no", eigeninitiative: "yes" },
-      },
-      { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-ja" },
-    ],
-    verfugbaresEinkommenNoEigeninitiativeNo: [
-      {
-        stepId: "/verfuegbares-einkommen",
-        userInput: { verfuegbaresEinkommen: "no", eigeninitiative: "no" },
-      },
-      { stepId: "/ergebnis/verfuegbares-einkommen-abschluss-vielleicht" },
-    ],
-    partnerschaftNo: [
-      {
-        stepId: "/einkommen",
-        userInput: { partnerschaft: "no", einkommen: "0" },
-      },
-      { stepId: "/kinder" },
-    ],
-    kinderNo: [
-      {
-        stepId: "/kinder",
-        userInput: { kinder: "no" },
-      },
-      { stepId: "/unterhalt" },
-    ],
-    unterhaltNo: [
-      {
-        stepId: "/unterhalt",
-        userInput: { unterhalt: "no" },
-      },
-      { stepId: "/miete" },
-    ],
-    einkommenHoch: [
-      {
-        stepId: "/weitere-zahlungen-summe",
-        userInput: { einkommen: "10000000", weitereZahlungenSumme: "0" },
-      },
-      {
-        stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-nein",
-      },
-    ],
-    weitereZahlungenHoch: [
-      {
-        stepId: "/weitere-zahlungen-summe",
-        userInput: { einkommen: "0", weitereZahlungenSumme: "100000" },
-      },
-      {
-        stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-ja",
-      },
-    ],
-    einkommenBalanced: [
-      {
-        stepId: "/weitere-zahlungen-summe",
-        userInput: {
-          einkommen: "100",
-          weitereZahlungenSumme: "100",
-          eigeninitiative: "no",
-        },
-      },
-      {
-        stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-vielleicht",
-      },
-    ],
-  },
+    },
+    {
+      stepId: "/ergebnis/weitere-zahlungen-summe-abschluss-vielleicht",
+    },
+  ],
 } satisfies FlowTestCases;
