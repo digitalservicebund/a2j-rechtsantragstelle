0471bce3d7d2d293de5843a368eb276d7e612f2d
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-09-17 11:02:20 +0200

    refactor(autocomplete):  add street back to autocomplete map and set housenumber to address-line2 and add autocomplete and name attributes to form and inputs

--- app/util/autocompleteMap.ts
+++ app/util/autocompleteMap.ts
@@ -1,7 +1,8 @@
 export const autocompleteMap: Record<string, string> = {
   vorname: "given-name",
   nachname: "family-name",
-  houseNumber: "address-line1",
+  street: "address-line1",
+  houseNumber: "address-line2",
   ort: "address-level2",
   plz: "postal-code",
   telefonnummer: "tel",
