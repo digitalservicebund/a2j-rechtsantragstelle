18eb2d0a8625d271248051d05e49d139b0989d70
Rafael Falk <rafael.falk@digitalservice.bund.de>
2025-09-23 09:30:07 +0200

    feat(RAST-305): return original content and do not throw 500 on syntax errors in mustache template strings
    feat(RAST-305): return original content and do not throw 500 on syntax errors in mustache template strings

--- app/util/applyStringReplacement.ts
+++ app/util/applyStringReplacement.ts
@@ -1,6 +1,7 @@
 import mustache from "mustache";
 import type { Flow } from "~/domains/flows.server";
 import type { UserData } from "~/domains/userData";
+import { logError } from "~/services/logging";
 
 export type Replacements = Record<string, string | boolean | undefined>;
 
@@ -14,52 +15,61 @@ export function applyStringReplacement<T>(
   replacements?: Replacements,
   skipIndexArrayReplacement = false,
 ): T {
-  if (
-    !content ||
-    !replacements ||
-    typeof content === "number" ||
-    typeof content === "boolean"
-  )
+  try {
+    if (
+      !content ||
+      !replacements ||
+      typeof content === "number" ||
+      typeof content === "boolean"
+    )
+      return content;
+
+    // For array summaries, we might have a string that contains "indexArray" which we want to skip replacing in the back-end
+    // This is because the indexArray is used in the front-end to display the correct index
+    if (
+      typeof content === "string" &&
+      content.includes("indexArray") &&
+      skipIndexArrayReplacement
+    ) {
+      return content;
+    }
+
+    if (typeof content === "string") {
+      return mustache.render(content, replacements) as T;
+    }
+
+    // Need to filter out content that shouldn't be visible
+    if (
+      typeof content === "object" &&
+      "isVisible" in content &&
+      typeof content.isVisible === "string" &&
+      mustache.render(`{{${content.isVisible}}}`, replacements) === "false"
+    ) {
+      return undefined as T;
+    }
+
+    if (Array.isArray(content)) {
+      return content
+        .map((item) =>
+          applyStringReplacement(item, replacements, skipIndexArrayReplacement),
+        )
+        .filter((item) => item !== undefined) as T;
+    }
+
+    return Object.fromEntries(
+      Object.entries(content)
+        .map(([key, value]) => [
+          key,
+          applyStringReplacement(
+            value,
+            replacements,
+            skipIndexArrayReplacement,
+          ),
+        ])
+        .filter(([, value]) => value !== undefined),
+    );
+  } catch (e) {
+    logError({ message: "Could not apply string replacements", error: e });
     return content;
-
-  // For array summaries, we might have a string that contains "indexArray" which we want to skip replacing in the back-end
-  // This is because the indexArray is used in the front-end to display the correct index
-  if (
-    typeof content === "string" &&
-    content.includes("indexArray") &&
-    skipIndexArrayReplacement
-  ) {
-    return content;
   }
-
-  if (typeof content === "string") {
-    return mustache.render(content, replacements) as T;
-  }
-
-  // Need to filter out content that shouldn't be visible
-  if (
-    typeof content === "object" &&
-    "isVisible" in content &&
-    typeof content.isVisible === "string" &&
-    mustache.render(`{{${content.isVisible}}}`, replacements) === "false"
-  ) {
-    return undefined as T;
-  }
-
-  if (Array.isArray(content)) {
-    return content
-      .map((item) =>
-        applyStringReplacement(item, replacements, skipIndexArrayReplacement),
-      )
-      .filter((item) => item !== undefined) as T;
-  }
-
-  return Object.fromEntries(
-    Object.entries(content)
-      .map(([key, value]) => [
-        key,
-        applyStringReplacement(value, replacements, skipIndexArrayReplacement),
-      ])
-      .filter(([, value]) => value !== undefined),
-  );
 }
