8e54ee2c7d0f4b02557efe03670e5d08789438e2
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-09-24 08:52:13 +0200

    fix type inference for ausgaben xStateConfig

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/ausgaben/xstateConfig.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/ausgaben/xstateConfig.ts
@@ -8,176 +8,178 @@ const steps = xStateTargetsFromPagesConfig(
   pkhFormularFinanzielleAngabenAusgabenPages,
 );
 
-export const ausgabenXstateConfig = {
-  id: "ausgaben",
-  initial: steps.ausgabenFrage.relative,
-  meta: { done: ausgabenDone },
-  states: {
-    [steps.ausgabenFrage.relative]: {
-      on: {
-        BACK: [
-          {
-            guard: "eigentumYesAndEmptyArray",
-            target: "#eigentum-zusammenfassung.warnung",
-          },
-          {
-            guard: "eigentumDone",
-            target: "#eigentum-zusammenfassung.zusammenfassung",
-          },
-          "#eigentum.kraftfahrzeuge-frage",
-        ],
-        SUBMIT: [
-          {
-            guard: ({ context }) => context.hasAusgaben === "yes",
-            target: steps.ausgabenZusammenfassung.relative,
-          },
-          steps.ausgabenBesondereBelastungen.relative,
-        ],
-      },
-    },
-    [steps.ausgabenBesondereBelastungen.relative]: {
-      on: {
-        BACK: [
-          {
-            guard: ({ context }) => context.hasAusgaben === "yes",
-            target: steps.ausgabenZusammenfassung.absolute,
-          },
-          steps.ausgabenFrage.relative,
-        ],
-        SUBMIT: ["#gesetzliche-vertretung"],
-      },
-    },
-    [steps.ausgabenZusammenfassung.relative]: {
-      on: {
-        BACK: steps.ausgabenFrage.relative,
-        SUBMIT: steps.ausgabenBesondereBelastungen.relative,
-        "add-versicherungen": steps.ausgabenVersicherungen.relative,
-        "add-ratenzahlungen": steps.ausgabenRatenzahlungen.relative,
-        "add-sonstigeAusgaben": steps.ausgabenSonstigeAusgaben.relative,
-      },
-    },
-    [steps.ausgabenVersicherungen.relative]: {
-      initial: "daten",
-      states: {
-        daten: {
-          on: {
-            BACK: steps.ausgabenZusammenfassung.absolute,
-            SUBMIT: [
-              {
-                guard: "isSonstigeVersicherung",
-                target: "sonstige-art",
-              },
-              steps.ausgabenZusammenfassung.absolute,
-            ],
-          },
-        },
-        "sonstige-art": {
-          on: {
-            BACK: "daten",
-            SUBMIT: steps.ausgabenZusammenfassung.absolute,
-          },
+// Type must be explicitly defined instead of inferred using `satisfies`, as TS/xState incorrectly infers multi-step array items as being `undefined`
+export const ausgabenXstateConfig: Config<ProzesskostenhilfeFinanzielleAngabenUserData> =
+  {
+    id: "ausgaben",
+    initial: steps.ausgabenFrage.relative,
+    meta: { done: ausgabenDone },
+    states: {
+      [steps.ausgabenFrage.relative]: {
+        on: {
+          BACK: [
+            {
+              guard: "eigentumYesAndEmptyArray",
+              target: "#eigentum-zusammenfassung.warnung",
+            },
+            {
+              guard: "eigentumDone",
+              target: "#eigentum-zusammenfassung.zusammenfassung",
+            },
+            "#eigentum.kraftfahrzeuge-frage",
+          ],
+          SUBMIT: [
+            {
+              guard: ({ context }) => context.hasAusgaben === "yes",
+              target: steps.ausgabenZusammenfassung.relative,
+            },
+            steps.ausgabenBesondereBelastungen.relative,
+          ],
         },
       },
-    },
-    [steps.ausgabenRatenzahlungen.relative]: {
-      initial: "daten",
-      states: {
-        daten: {
-          on: {
-            BACK: steps.ausgabenZusammenfassung.absolute,
-            SUBMIT: "zahlungspflichtiger",
-          },
+      [steps.ausgabenBesondereBelastungen.relative]: {
+        on: {
+          BACK: [
+            {
+              guard: ({ context }) => context.hasAusgaben === "yes",
+              target: steps.ausgabenZusammenfassung.absolute,
+            },
+            steps.ausgabenFrage.relative,
+          ],
+          SUBMIT: ["#gesetzliche-vertretung"],
         },
-        zahlungspflichtiger: {
-          on: {
-            BACK: "daten",
-            SUBMIT: [
-              {
-                guard: "ratenzahlungAnteiligYes",
-                target: "betragGemeinsamerAnteil",
-              },
-              "betragGesamt",
-            ],
-          },
+      },
+      [steps.ausgabenZusammenfassung.relative]: {
+        on: {
+          BACK: steps.ausgabenFrage.relative,
+          SUBMIT: steps.ausgabenBesondereBelastungen.relative,
+          "add-versicherungen": steps.ausgabenVersicherungen.relative,
+          "add-ratenzahlungen": steps.ausgabenRatenzahlungen.relative,
+          "add-sonstigeAusgaben": steps.ausgabenSonstigeAusgaben.relative,
         },
-        betragGemeinsamerAnteil: {
-          on: {
-            BACK: "zahlungspflichtiger",
-            SUBMIT: "betragEigenerAnteil",
+      },
+      [steps.ausgabenVersicherungen.relative]: {
+        initial: "daten",
+        states: {
+          daten: {
+            on: {
+              BACK: steps.ausgabenZusammenfassung.absolute,
+              SUBMIT: [
+                {
+                  guard: "isSonstigeVersicherung",
+                  target: "sonstige-art",
+                },
+                steps.ausgabenZusammenfassung.absolute,
+              ],
+            },
           },
-        },
-        betragEigenerAnteil: {
-          on: {
-            BACK: "betragGemeinsamerAnteil",
-            SUBMIT: "restschuld",
-          },
-        },
-        betragGesamt: {
-          on: {
-            BACK: "zahlungspflichtiger",
-            SUBMIT: "restschuld",
-          },
-        },
-        restschuld: {
-          on: {
-            BACK: [
-              {
-                guard: "ratenzahlungAnteiligYes",
-                target: "betragEigenerAnteil",
-              },
-              "betragGesamt",
-            ],
-            SUBMIT: "laufzeitende",
-          },
-        },
-        laufzeitende: {
-          on: {
-            BACK: "restschuld",
-            SUBMIT: steps.ausgabenZusammenfassung.absolute,
+          "sonstige-art": {
+            on: {
+              BACK: "daten",
+              SUBMIT: steps.ausgabenZusammenfassung.absolute,
+            },
           },
         },
       },
-    },
-    [steps.ausgabenSonstigeAusgaben.relative]: {
-      initial: "daten",
-      states: {
-        daten: {
-          on: {
-            BACK: steps.ausgabenZusammenfassung.absolute,
-            SUBMIT: "zahlungspflichtiger",
+      [steps.ausgabenRatenzahlungen.relative]: {
+        initial: "daten",
+        states: {
+          daten: {
+            on: {
+              BACK: steps.ausgabenZusammenfassung.absolute,
+              SUBMIT: "zahlungspflichtiger",
+            },
+          },
+          zahlungspflichtiger: {
+            on: {
+              BACK: "daten",
+              SUBMIT: [
+                {
+                  guard: "ratenzahlungAnteiligYes",
+                  target: "betragGemeinsamerAnteil",
+                },
+                "betragGesamt",
+              ],
+            },
+          },
+          betragGemeinsamerAnteil: {
+            on: {
+              BACK: "zahlungspflichtiger",
+              SUBMIT: "betragEigenerAnteil",
+            },
+          },
+          betragEigenerAnteil: {
+            on: {
+              BACK: "betragGemeinsamerAnteil",
+              SUBMIT: "restschuld",
+            },
+          },
+          betragGesamt: {
+            on: {
+              BACK: "zahlungspflichtiger",
+              SUBMIT: "restschuld",
+            },
+          },
+          restschuld: {
+            on: {
+              BACK: [
+                {
+                  guard: "ratenzahlungAnteiligYes",
+                  target: "betragEigenerAnteil",
+                },
+                "betragGesamt",
+              ],
+              SUBMIT: "laufzeitende",
+            },
+          },
+          laufzeitende: {
+            on: {
+              BACK: "restschuld",
+              SUBMIT: steps.ausgabenZusammenfassung.absolute,
+            },
           },
         },
-        zahlungspflichtiger: {
-          on: {
-            BACK: "daten",
-            SUBMIT: [
-              {
-                guard: "sonstigeAusgabeAnteiligYes",
-                target: "betragGemeinsamerAnteil",
-              },
-              "betragGesamt",
-            ],
+      },
+      [steps.ausgabenSonstigeAusgaben.relative]: {
+        initial: "daten",
+        states: {
+          daten: {
+            on: {
+              BACK: steps.ausgabenZusammenfassung.absolute,
+              SUBMIT: "zahlungspflichtiger",
+            },
           },
-        },
-        betragGemeinsamerAnteil: {
-          on: {
-            BACK: "zahlungspflichtiger",
-            SUBMIT: "betragEigenerAnteil",
+          zahlungspflichtiger: {
+            on: {
+              BACK: "daten",
+              SUBMIT: [
+                {
+                  guard: "sonstigeAusgabeAnteiligYes",
+                  target: "betragGemeinsamerAnteil",
+                },
+                "betragGesamt",
+              ],
+            },
           },
-        },
-        betragEigenerAnteil: {
-          on: {
-            BACK: "betragGemeinsamerAnteil",
-            SUBMIT: steps.ausgabenZusammenfassung.absolute,
+          betragGemeinsamerAnteil: {
+            on: {
+              BACK: "zahlungspflichtiger",
+              SUBMIT: "betragEigenerAnteil",
+            },
           },
-        },
-        betragGesamt: {
-          on: {
-            BACK: "zahlungspflichtiger",
-            SUBMIT: steps.ausgabenZusammenfassung.absolute,
+          betragEigenerAnteil: {
+            on: {
+              BACK: "betragGemeinsamerAnteil",
+              SUBMIT: steps.ausgabenZusammenfassung.absolute,
+            },
+          },
+          betragGesamt: {
+            on: {
+              BACK: "zahlungspflichtiger",
+              SUBMIT: steps.ausgabenZusammenfassung.absolute,
+            },
           },
         },
       },
     },
-  },
-} satisfies Config<ProzesskostenhilfeFinanzielleAngabenUserData>;
+  };
