c0a40ba461652a81525819b71a8bb3f8a5138a4d
Alex August Schlote <alex.schlote@digitalservice.bund.de>
2025-09-24 13:42:16 +0200

    Implement stepper config (#2639)
    * refactor(flow.server): add useStepper parameter
    
    * refactor(getContentData): render nav props with subStates if useStepper is true
    
    * refactor(buildFlowController): add option to show unreachable steps
    
    * test(getContentData): add beforeEach
    
    * refactor(getContentData): rename variable
    
    * make lint happy
    
    * refactor(buildFlowController): make parameter default false
    
    * refactor: remove currentFlow from getContentData as parameter and pass useStepper as parameter for getNavProps
    
    * test(getContentData.test): remove unneeded variable
    
    * Update app/services/flow/userDataAndFlow/getUserDataAndFlow.ts
    
    Co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
    
    ---------
    
    Co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>

--- app/services/flow/userDataAndFlow/getUserDataAndFlow.ts
+++ app/services/flow/userDataAndFlow/getUserDataAndFlow.ts
@@ -13,6 +13,7 @@ import { getPrunedUserDataFromPathname } from "../getPrunedUserDataFromPathname"
 import { type UserDataWithPageData } from "../pageData";
 import { type FeatureFlag } from "~/services/isFeatureFlagEnabled.server";
 import { throw404IfFeatureFlagDisabled } from "~/services/errorPages/throw404";
+import { type Flow } from "~/domains/flows.server";
 
 type OkResult = {
   userData: UserDataWithPageData;
@@ -21,6 +22,7 @@ type OkResult = {
     controller: ReturnType<typeof buildFlowController>;
     validFlowPaths: ValidFlowPagesType;
     userVisitedValidationPage?: boolean;
+    useStepper: boolean;
   };
   page: {
     stepId: string;
@@ -90,6 +92,10 @@ export const getUserDataAndFlow = async (
       userVisitedValidationPage:
         flowSession.get(userVisitedValidationPageKey) ??
         flowController.getMeta(stepId)?.triggerValidation,
+      useStepper:
+        "useStepper" in currentFlow
+          ? ((currentFlow as Flow).useStepper ?? false)
+          : false,
     },
     page: {
       stepId,
