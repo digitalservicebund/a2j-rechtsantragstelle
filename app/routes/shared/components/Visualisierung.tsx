2b12805f403c47f9fba47203fc666615d6a90d33
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-02 10:19:15 +0200

    refactor(Visualisierung): add grids components and use InlineSvgImage

--- app/routes/shared/components/Visualisierung.tsx
+++ app/routes/shared/components/Visualisierung.tsx
@@ -1,7 +1,24 @@
 import { useLoaderData } from "react-router";
 import type { loader } from "../visualisierung";
+import { GridSection } from "~/components/layout/grid/GridSection";
+import { Grid } from "~/components/layout/grid/Grid";
+import { GridItem } from "~/components/layout/grid/GridItem";
+import { InlineSvgImage } from "~/components/common/InlineSvgImage";
 
 export function Visualisierung() {
-  const { url } = useLoaderData<typeof loader>();
-  return <img alt="current flow chart" src={url}></img>;
+  const { svgString } = useLoaderData<typeof loader>();
+  return (
+    <GridSection>
+      <Grid>
+        <GridItem
+          smColumn={{ start: 1, span: 12 }}
+          mdColumn={{ start: 1, span: 12 }}
+          lgColumn={{ start: 1, span: 12 }}
+          xlColumn={{ start: 1, span: 12 }}
+        >
+          <InlineSvgImage svgString={svgString} />
+        </GridItem>
+      </Grid>
+    </GridSection>
+  );
 }
