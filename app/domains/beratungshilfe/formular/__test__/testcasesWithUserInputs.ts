2ba798b887ebec84d44c60f20083982ec7956a24
Spencer Peace <47868304+Spencer6497@users.noreply.github.com>
2025-10-06 10:45:49 +0200

    refactor(BerH flowSchema Tests): Add Ausgaben tests, add array step handling (#2669)
    * working array handling in flowSchema tests
    
    * complete test cases
    
    * clean up/simplify flowSchema.test.ts

--- app/domains/beratungshilfe/formular/__test__/testcasesWithUserInputs.ts
+++ app/domains/beratungshilfe/formular/__test__/testcasesWithUserInputs.ts
@@ -2,11 +2,13 @@ import type { FlowTestCases } from "~/domains/__test__/TestCases";
 import { beratungshilfeXstateConfig } from "../xstateConfig";
 import { testCasesBeratungshilfeFormularDefault } from "~/domains/beratungshilfe/formular/__test__/testcases";
 import { testCasesBeratungshilfeFormularAnwaltlicheVertretung } from "~/domains/beratungshilfe/formular/anwaltlicheVertretung/__test__/testcases";
+import { testCasesBeratungshilfeFormularFinanzielleAngabenAusgabe } from "~/domains/beratungshilfe/formular/finanzielleAngaben/__test__/testcasesAusgaben";
 
 export const beratungshilfeAntragTestCases = {
   xstateConfig: beratungshilfeXstateConfig,
   testcases: {
     ...testCasesBeratungshilfeFormularDefault,
     ...testCasesBeratungshilfeFormularAnwaltlicheVertretung,
+    ...testCasesBeratungshilfeFormularFinanzielleAngabenAusgabe,
   },
 } satisfies FlowTestCases;
