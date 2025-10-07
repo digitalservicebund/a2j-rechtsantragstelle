d0c893cde9721ca7a9a7461639a3dc5103779faf
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-07 17:10:38 +0200

    refactor(SideNavMobile): create sub component to render button

--- app/components/navigation/SideNavMobile.tsx
+++ app/components/navigation/SideNavMobile.tsx
@@ -6,6 +6,32 @@ import { stateIsCurrent } from "~/services/navigation/navState";
 import { translations } from "~/services/translations/translations";
 import { type NavItem } from "./types";
 
+const SideNavButton = ({
+  menuOpen,
+  currentPageTitle,
+  toggleMenu,
+}: Readonly<{
+  menuOpen: boolean;
+  currentPageTitle: string | undefined;
+  toggleMenu: () => void;
+}>) => {
+  const buttonClasses = "h-[24px] w-1/12 cursor-pointer text-blue-800";
+  const Icon = menuOpen ? Close : MenuIcon;
+  return (
+    <button
+      onClick={toggleMenu}
+      aria-expanded={menuOpen}
+      className="flex items-center gap-8 text-sm py-20 px-10 cursor-pointer w-full"
+    >
+      <Icon className={buttonClasses} />
+      <span className="text-gray-900">
+        {translations.navigationMobile.currentArea.de}:{" "}
+      </span>
+      <span className="font-semibold">{currentPageTitle}</span>
+    </button>
+  );
+};
+
 export default function SideNavMobile({
   userVisitedValidationPage,
   navItems,
@@ -20,9 +46,6 @@ export default function SideNavMobile({
     stateIsCurrent(state),
   )?.label;
 
-  const buttonClasses = "h-[24px] w-1/12 cursor-pointer text-blue-800";
-  const Icon = menuOpen ? Close : MenuIcon;
-
   return (
     <div className={`flex flex-col lg:hidden`}>
       {menuOpen && (
@@ -35,17 +58,11 @@ export default function SideNavMobile({
       )}
 
       <div className="bg-white max-h-[80vh] border border-blue-400 overflow-auto">
-        <button
-          onClick={toggleMenu}
-          aria-expanded={menuOpen}
-          className="flex items-center  gap-8 text-sm py-20 px-10 cursor-pointer w-full"
-        >
-          <Icon className={buttonClasses} />
-          <span className="text-gray-900">
-            {translations.navigationMobile.currentArea.de}:{" "}
-          </span>
-          <span className="font-semibold">{currentPageTitle}</span>
-        </button>
+        <SideNavButton
+          currentPageTitle={currentPageTitle}
+          menuOpen={menuOpen}
+          toggleMenu={toggleMenu}
+        />
 
         {menuOpen && (
           <div className="pb-10">
