54b415489f79a41e1ee6ed6ddab9316692b2128b
Rafael Falk <rafael.falk@digitalservice.bund.de>
2025-10-07 13:27:42 +0200

    refactor(grid): merge redundant backgroundClass into className

--- app/components/layout/grid/GridSection.tsx
+++ app/components/layout/grid/GridSection.tsx
@@ -2,7 +2,6 @@ import cx from "classnames";
 import { getPaddingClasses } from "./util";
 
 type SectionProps = React.PropsWithChildren<{
-  backgroundClass?: string;
   className?: string;
   pt?: string;
   pb?: string;
@@ -10,22 +9,11 @@ type SectionProps = React.PropsWithChildren<{
   ["data-testid"]?: string;
 }>;
 
-export function GridSection({
-  children,
-  pt,
-  pb,
-  backgroundClass,
-  className,
-  id,
-}: SectionProps) {
+export function GridSection({ children, pt, pb, className, id }: SectionProps) {
   return (
     <section
       id={id}
-      className={cx(
-        backgroundClass,
-        className,
-        getPaddingClasses(pt ?? "", pb ?? ""),
-      )}
+      className={cx(className, getPaddingClasses(pt ?? "", pb ?? ""))}
     >
       {children}
     </section>
