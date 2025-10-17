f3848294ab4a181d093addafff14762517197afe
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-17 15:26:54 +0200

    test(navState): add new tests

--- app/services/navigation/__test__/navState.test.ts
+++ app/services/navigation/__test__/navState.test.ts
@@ -1,20 +1,77 @@
 import { navState } from "../navState";
 
 describe("navState", () => {
-  const defaultValues = {
-    isCurrent: false,
-    isReachable: false,
-    isDone: false,
-  };
+  it("should return state Disabled in case isCurrent, isReachable, isDone are false", () => {
+    const actual = navState({
+      isCurrent: false,
+      isDone: false,
+      isReachable: false,
+    });
 
-  const validStates = [
-    [{}, "Disabled"],
-    [{ isCurrent: true }, "Current"],
-    [{ isReachable: true }, "Open"],
-    [{ isDone: true, isReachable: true }, "Done"],
-  ] as const;
+    expect(actual).toBe("Disabled");
+  });
 
-  test.each(validStates)("navState(%o) => %i", (testValues, expected) => {
-    expect(navState({ ...defaultValues, ...testValues })).toBe(expected);
+  it("should return state Current in case isCurrent is true and isDone is false", () => {
+    const actual = navState({
+      isCurrent: true,
+      isDone: false,
+      isReachable: false,
+    });
+
+    expect(actual).toBe("Current");
+  });
+
+  it("should return state Open in case isCurrent and isDone are false, but isReachable is true", () => {
+    const actual = navState({
+      isCurrent: false,
+      isDone: false,
+      isReachable: true,
+    });
+
+    expect(actual).toBe("Open");
+  });
+
+  it("should return state Done in case isReachable and isDone true, but isCurrent is false", () => {
+    const actual = navState({
+      isCurrent: false,
+      isDone: true,
+      isReachable: true,
+    });
+
+    expect(actual).toBe("Done");
+  });
+
+  it("should return state DoneCurrent in case isCurrent and isDone true", () => {
+    const actual = navState({
+      isCurrent: true,
+      isDone: true,
+      isReachable: false,
+    });
+
+    expect(actual).toBe("DoneCurrent");
+  });
+
+  it("should return state WarningCurrent in case userVisitedValidationPage, isCurrent and isReachable are true, but excludedFromValidation and isDone are false", () => {
+    const actual = navState({
+      isCurrent: true,
+      isDone: false,
+      isReachable: true,
+      excludedFromValidation: false,
+      userVisitedValidationPage: true,
+    });
+
+    expect(actual).toBe("WarningCurrent");
+  });
+
+  it("should return state Warning in case userVisitedValidationPage and isReachable are true, but excludedFromValidation, isCurrent isDone are false", () => {
+    const actual = navState({
+      isCurrent: false,
+      isDone: false,
+      isReachable: true,
+      excludedFromValidation: false,
+      userVisitedValidationPage: true,
+    });
+
+    expect(actual).toBe("Warning");
   });
 });
