619c4016cb3c3fd3d0e1d4cb18f479e43bb76b31
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-29 11:35:20 +0100

    refactor(fgr-vorabcheck): move schema to pageSchema
    Co-authored-by: judithmh <40442827+judithmh@users.noreply.github.com>

--- app/domains/fluggastrechte/vorabcheck/userData.ts
+++ app/domains/fluggastrechte/vorabcheck/userData.ts
@@ -1,21 +1,13 @@
-import { z } from "zod";
+import { type z } from "zod";
+import { type UserDataFromPagesSchema } from "~/domains/pageSchemas";
 import { airlineSchema } from "~/services/validation/airline";
 import { airportSchema } from "~/services/validation/airport";
 import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
-
-const fluggastBereichSchema = z.enum([
-  "nichtbefoerderung",
-  "verspaetet",
-  "annullierung",
-  "anderes",
-]);
-
-const ankuendigungSchema = z.enum([
-  "no",
-  "until6Days",
-  "between7And13Days",
-  "moreThan13Days",
-]);
+import {
+  ankuendigungSchema,
+  fluggastBereichSchema,
+  type fluggastrechteVorabcheckPages,
+} from "./pages";
 
 export const fluggastrechteVorabcheckInputSchema = {
   startAirport: airportSchema,
@@ -44,7 +36,9 @@ export const fluggastrechteVorabcheckInputSchema = {
   ersatzflugLandenVierStunden: YesNoAnswer,
 } as const;
 
-const _partialSchema = z.object(fluggastrechteVorabcheckInputSchema).partial();
-export type FluggastrechtVorabcheckUserData = z.infer<typeof _partialSchema>;
+export type FluggastrechtVorabcheckUserData = UserDataFromPagesSchema<
+  typeof fluggastrechteVorabcheckPages
+>;
+
 export type FluggastrechtBereichType = z.infer<typeof fluggastBereichSchema>;
 export type FluggastrechtAnkuendigungType = z.infer<typeof ankuendigungSchema>;
