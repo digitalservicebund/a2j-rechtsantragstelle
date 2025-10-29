c3fcff68e7e57aeaeb5d63a99ef5d28c4be232d1
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-29 14:12:37 +0100

    refactor(fgr-vorabcheck): remove inputSchema and set multiFieldsValidation with schema object

--- app/domains/fluggastrechte/vorabcheck/userData.ts
+++ app/domains/fluggastrechte/vorabcheck/userData.ts
@@ -1,40 +1,10 @@
 import { type z } from "zod";
 import { type UserDataFromPagesSchema } from "~/domains/pageSchemas";
-import { airlineSchema } from "~/services/validation/airline";
-import { airportSchema } from "~/services/validation/airport";
-import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
-import {
+import type {
   ankuendigungSchema,
   fluggastBereichSchema,
-  type fluggastrechteVorabcheckPages,
+  fluggastrechteVorabcheckPages,
 } from "./pages";
-
-export const fluggastrechteVorabcheckInputSchema = {
-  startAirport: airportSchema,
-  endAirport: airportSchema,
-  fluggesellschaft: airlineSchema,
-  bereich: fluggastBereichSchema,
-  verspaetung: YesNoAnswer,
-  verjaehrung: YesNoAnswer,
-  checkin: YesNoAnswer,
-  gruende: YesNoAnswer,
-  entschaedigung: YesNoAnswer,
-  gericht: YesNoAnswer,
-  abtretung: YesNoAnswer,
-  kostenlos: YesNoAnswer,
-  rabatt: YesNoAnswer,
-  buchung: YesNoAnswer,
-  ausgleich: YesNoAnswer,
-  ausgleichAngenommen: YesNoAnswer,
-  vertretbareGruende: YesNoAnswer,
-  ankuendigung: ankuendigungSchema,
-  ersatzflug: YesNoAnswer,
-  ersatzflugStartenEinStunde: YesNoAnswer,
-  ersatzflugLandenZweiStunden: YesNoAnswer,
-  vertretbareGruendeAnnullierung: YesNoAnswer,
-  ersatzflugStartenZweiStunden: YesNoAnswer,
-  ersatzflugLandenVierStunden: YesNoAnswer,
-} as const;
 
 export type FluggastrechtVorabcheckUserData = UserDataFromPagesSchema<
   typeof fluggastrechteVorabcheckPages
