857163510126d6689f788ec4992b975d6636ffcd
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-11-05 11:31:46 +0100

    feat(saml): change import and export

--- app/services/bundid/index.server.ts
+++ app/services/bundid/index.server.ts
@@ -1,12 +1,12 @@
-import { SAML } from "@node-saml/node-saml";
 import { config } from "~/services/env/env.server";
-
+import { SAML } from "@node-saml/node-saml";
 export function getBundIdServiceProvider() {
-  return new SAML({
+  const saml = new SAML({
     entryPoint: "https://int.id.bund.de/idp/profile/SAML2/POST/SSO",
     issuer: config().BUNDID_AUTH_BMI_ID ?? "",
     callbackUrl: config().SAML_ASSERTION_CONSUMER_SERVICE_URL ?? "",
     idpCert: config().SAML_IDP_CERT ?? "",
     wantAssertionsSigned: true,
   });
+  return saml;
 }
