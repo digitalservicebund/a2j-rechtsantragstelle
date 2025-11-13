984d3340b1bc3139bc23d18d36344763d8fce1c6
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-11-13 16:06:06 +0100

    preliminary solution for ausgaben arrays

--- app/domains/isStepDone.ts
+++ app/domains/isStepDone.ts
@@ -7,13 +7,16 @@ import {
   type UserDataFromPagesSchema,
 } from "~/domains/pageSchemas";
 import type { SchemaObject } from "~/domains/userData";
-import type { ArrayConfigServer } from "~/services/array";
+import { type ArrayConfigServer } from "~/services/array";
+import partition from "lodash/partition";
+
+type ArrayConfigurations = Record<string, ArrayConfigServer>;
 
 const filterPageSchemas =
   <T extends PagesConfig>(
     context: UserDataFromPagesSchema<T>,
     reachableSteps: string[],
-    arrayConfigurations?: Record<string, ArrayConfigServer>,
+    arrayConfigurations?: ArrayConfigurations,
   ) =>
   (config: PageConfig) => {
     if ("arrayPages" in config) {
@@ -30,17 +33,64 @@ const filterPageSchemas =
     );
   };
 
+function findArraysWithCommonCondition(
+  relevantPageSchemas: SchemaObject,
+  arrayConfigurations?: ArrayConfigurations,
+): string[] {
+  const uniqueStatementKeys = new Set();
+  const duplicateStatementKeys = new Set();
+  return Object.entries(arrayConfigurations ?? {})
+    .filter(([arrayName]) => arrayName in relevantPageSchemas)
+    .map(([arrayName, arrayConfig]) => {
+      const { statementKey } = arrayConfig;
+      if (uniqueStatementKeys.has(statementKey)) {
+        duplicateStatementKeys.add(statementKey);
+      } else {
+        uniqueStatementKeys.add(statementKey);
+      }
+      return [arrayName, arrayConfig] as const;
+    })
+    .filter(([, config]) => duplicateStatementKeys.has(config.statementKey))
+    .map(([arrayName]) => arrayName);
+}
+
 export const isStepDone = <T extends PagesConfig>(
   pageSchema: T,
   context: UserDataFromPagesSchema<T>,
   reachableSteps: string[],
-  arrayConfigurations?: Record<string, ArrayConfigServer>,
+  arrayConfigurations?: ArrayConfigurations,
 ) => {
   // Retrieve only the pageSchemas that are reachable, unless we're on an array page
   const relevantPageSchemas = Object.values(pageSchema)
     .filter(filterPageSchemas(context, reachableSteps, arrayConfigurations))
     .reduce<SchemaObject>((acc, v) => ({ ...acc, ...v.pageSchema }), {});
   if (Object.keys(relevantPageSchemas).length === 0) return true;
+  if (
+    Object.values(relevantPageSchemas).filter(
+      (schema) => schema.type === "array",
+    ).length > 1
+  ) {
+    // Multiple arrays on the same page, need to check if any of them share the same statementKey/conditional relevance
+    const arraysWithCommonCondition = findArraysWithCommonCondition(
+      relevantPageSchemas,
+      arrayConfigurations,
+    );
+    if (arraysWithCommonCondition.length > 1) {
+      const [arrayORSchemas, otherPageSchemas] = partition(
+        Object.entries(relevantPageSchemas),
+        ([schemaName]) => arraysWithCommonCondition.includes(schemaName),
+      ).map((entries) => Object.fromEntries(entries) as SchemaObject);
+      const orSchema = z.union(
+        Object.entries(arrayORSchemas).map(([arrayName, arraySchema]) => {
+          return z.object({ [arrayName]: arraySchema });
+        }),
+      );
+      return (
+        z.object(otherPageSchemas).safeParse(context).success &&
+        orSchema.safeParse(context).success
+      );
+    }
+  }
   return z.object(relevantPageSchemas).safeParse(context).success;
 };
 
