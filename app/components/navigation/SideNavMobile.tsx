48c9c70352c35fd16355134346b7562e7fd99717
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-11-18 15:56:39 +0100

    first pass wo JS

--- app/components/navigation/SideNavMobile.tsx
+++ app/components/navigation/SideNavMobile.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { NavigationList } from "~/components/navigation/NavigationList";
 import { translations } from "~/services/translations/translations";
 import type { StepStepper, NavItem } from "./types";
-import { SideNavMobileButton } from "./SideNavMobileButton";
 import { StandaloneLink } from "../common/StandaloneLink";
 import { arrayIsNonEmpty } from "~/util/array";
 import {
@@ -12,6 +11,10 @@ import {
 } from "~/services/navigation/navState";
 import KeyboardArrowLeft from "@digitalservicebund/icons/KeyboardArrowLeft";
 import SvgWarningAmber from "@digitalservicebund/icons/WarningAmberRounded";
+import { getMobileButtonAreaTitles } from "~/components/navigation/getMobileButtonAreaTitles";
+import classNames from "classnames";
+import KeyboardArrowDown from "@digitalservicebund/icons/KeyboardArrowDown";
+import KeyboardArrowUp from "@digitalservicebund/icons/KeyboardArrowUp";
 
 const StepStepperLinks = ({
   stepsStepper,
@@ -57,49 +60,61 @@ export default function SideNavMobile({
   navItems: NavItem[];
   stepsStepper: StepStepper[];
 }>) {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen((prev) => !prev);
-
   const firstItemRef = useRef<HTMLAnchorElement | null>(null);
 
-  useEffect(() => {
-    if (menuOpen) {
-      firstItemRef.current?.focus();
-    }
-  }, [menuOpen]);
+  const { currentAreaTitle, currentNavTitle } = getMobileButtonAreaTitles(
+    navItems,
+    stepsStepper,
+  );
+
+  const isStateCurrentWarning = arrayIsNonEmpty(stepsStepper)
+    ? stepsStepper.some(({ state }) => state === "WarningCurrent")
+    : navItems.some(({ state }) => state === "WarningCurrent");
 
   return (
-    <div className={`flex flex-col lg:hidden`}>
-      {menuOpen && (
+    <details className="group flex flex-col lg:hidden bg-white border border-blue-400">
+      {/* {menuOpen && (
         <button
           onClick={toggleMenu}
           data-testid="close-overlay"
           aria-label={translations.navigationMobile.closeMenu.de}
           className="bg-black h-screen opacity-70"
         />
-      )}
+      )} */}
       {/* col-reverse needed to preserve correct tab order 
        (top close button at the end of the tab order)*/}
-      <div className="flex flex-col-reverse bg-white max-h-[80vh] border border-blue-400 overflow-auto">
-        {menuOpen && (
-          <>
-            <StepStepperLinks stepsStepper={stepsStepper} />
-            <div className="pb-10 flex flex-col">
-              <NavigationList
-                navItems={navItems}
-                className="border border-blue-400 mx-16 mb-10 overflow-auto"
-                firstItemRef={firstItemRef}
-              />
-            </div>
-          </>
+      <summary
+        className={classNames(
+          "flex flex-row items-center not-group-open:not-bg-yellow-200 justify-between py-8 px-16 cursor-pointer w-full outline-none not-group-open:active:bg-blue-400 not-group-open:focus-within:shadow-[inset_0_0_0_4px_#004b76]",
+          {
+            "not-group-open:bg-yellow-200 not-group-open:active:bg-yellow-300":
+              isStateCurrentWarning,
+          },
         )}
-        <SideNavMobileButton
-          navItems={navItems}
-          menuOpen={menuOpen}
-          toggleMenu={toggleMenu}
-          stepsStepper={stepsStepper}
-        />
+      >
+        <div className="flex flex-row gap-8">
+          <div className="flex flex-col items-start">
+            <span className="ds-label-02-bold truncate text-left w-[70vw]">
+              {currentAreaTitle}
+            </span>
+            <span className="ds-body-03-reg text-gray-900">
+              {currentNavTitle}
+            </span>
+          </div>
+        </div>
+        <KeyboardArrowUp className="hidden group-open:block h-[24px] text-blue-800 forced-colors:text-white" />
+        <KeyboardArrowDown className="block group-open:hidden h-[24px] text-blue-800 forced-colors:text-white" />
+      </summary>
+      <div className="max-h-[80vh] overflow-auto">
+        <StepStepperLinks stepsStepper={stepsStepper} />
+        <div className="pb-10 flex flex-col">
+          <NavigationList
+            navItems={navItems}
+            className="border border-blue-400 mx-16 mb-10 overflow-auto"
+            firstItemRef={firstItemRef}
+          />
+        </div>
       </div>
-    </div>
+    </details>
   );
 }
