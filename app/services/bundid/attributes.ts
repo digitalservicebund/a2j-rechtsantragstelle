9e4f63f87ce5f95282e89c18939fb74c90cbe934
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-11-20 22:17:29 +0100

    doc(saml): add flow and attributes

---
+++ app/services/bundid/attributes.ts
@@ -0,0 +1,31 @@
+export const bundIdSamlAttributes = {
+  givenName: "urn:oid:2.5.4.42",
+  surname: "urn:oid:2.5.4.4",
+  mail: "urn:oid:0.9.2342.19200300.100.1.3",
+  postalAddress: "urn:oid:2.5.4.16",
+  postalCode: "urn:oid:2.5.4.17",
+  localityName: "urn:oid:2.5.4.7",
+  country: "urn:oid:1.2.40.0.10.2.1.1.225599",
+  personalTitle: "urn:oid:0.9.2342.19200300.100.1.40",
+  gender: "urn:oid:1.3.6.1.4.1.33592.1.3.5",
+  birthdate: "urn:oid:1.2.40.0.10.2.1.1.55",
+  placeOfBirth: "urn:oid:1.3.6.1.5.5.7.9.2",
+  birthName: "urn:oid:1.2.40.0.10.2.1.1.225566",
+  nationality: "urn:oid:1.2.40.0.10.2.1.1.225577",
+  documentType: "urn:oid: 1.2.40.0.10.2.1.1.552255",
+  DeMail: "urn:oid:1.3.6.1.4.1.55605.70737875.1.1.1.7.1",
+  telephoneNumber: "urn:oid:2.5.4.20",
+  "eIDAS-Issuing-Country": "urn:oid:1.3.6.1.4.1.25484.494450.10.1",
+  communityId: "urn:oid:1.3.6.1.4.1.25484.494450.5 ",
+} as const;
+
+export const bundIdSamlTechnicalAttributes = {
+  bPK2: "urn:oid:1.3.6.1.4.1.25484.494450.3",
+  "EID-CITIZEN-QAA-LEVEL": "urn:oid: 1.2.40.0.10.2.1.1.261.94",
+  "akdb:TrustLevel": "",
+  Version: "urn:oid:1.3.6.1.4.1.25484.494450.1",
+  AssertionProvedBy: "urn:oid:1.3.6.1.4.1.25484.494450.2",
+  legacyPostfachHandle: "urn:oid:2.5.4.18",
+  applicationId: "urn:oid:1.3.6.1.4.1.25484.494450.4 ",
+  pseudonym: "urn:oid:1.2.40.0.10.2.1.1.226699 ",
+} as const;
