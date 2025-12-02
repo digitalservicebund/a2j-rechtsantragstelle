96588621985eb013c79f8908a893d75aa7d828fe
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2025-12-02 18:25:59 +0100

    feat(Kern): put OpenQuestion back without kern

--- app/components/reportProblem/OpenQuestion.tsx
+++ app/components/reportProblem/OpenQuestion.tsx
@@ -1,5 +1,6 @@
 import { type BasicSurveyQuestion } from "posthog-js";
 import { useId, type Dispatch, type SetStateAction } from "react";
+import { useFeedbackTranslations } from "~/components/content/userFeedback/feedbackTranslations";
 import { TEXT_AREA_ROWS } from "~/components/formElements/Textarea";
 import { TEXTAREA_CHAR_LIMIT } from "~/services/validation/inputlimits";
 import { questionToAnswerId } from "../../services/analytics/surveys/questionToAnswerId";
@@ -12,19 +13,17 @@ type OpenQuestionProps = {
 };
 
 export const OpenQuestion = ({ question, setResponses }: OpenQuestionProps) => {
+  const feedbackTranslations = useFeedbackTranslations();
   const id = useId();
   const hasDescription = Boolean(question.description);
   const descriptionId = hasDescription ? id + "Description" : undefined;
   return (
-    <div className="kern-stack-large">
-      <label className="kern-label py-8" htmlFor={id}>
+    <div className="ds-stack ds-stack-8">
+      <label className="ds-body-01-bold" htmlFor={id}>
         {question.question}
       </label>
       {hasDescription && (
-        <p
-          className="kern-hint text-kern-18 font-normal py-8 mb-8"
-          id={descriptionId}
-        >
+        <p className="ds-body-01-reg text-gray-900" id={descriptionId}>
           {question.description}
         </p>
       )}
@@ -32,7 +31,7 @@ export const OpenQuestion = ({ question, setResponses }: OpenQuestionProps) => {
         id={id}
         name={question.id}
         aria-describedby={descriptionId}
-        className="w-full px-16 py-8 gap-8 kern-form-input__input"
+        className="ds-textarea forced-colors:border-4"
         onChange={(event) =>
           setResponses((surveyResponses) => ({
             ...surveyResponses,
@@ -40,6 +39,7 @@ export const OpenQuestion = ({ question, setResponses }: OpenQuestionProps) => {
           }))
         }
         maxLength={TEXTAREA_CHAR_LIMIT}
+        placeholder={feedbackTranslations["open-feedback-placeholder"]}
         rows={TEXT_AREA_ROWS}
       />
     </div>
