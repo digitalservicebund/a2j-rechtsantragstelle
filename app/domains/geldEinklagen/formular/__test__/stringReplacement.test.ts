76efa32604b63407757306da7b58c0f13ddc84a2
aaschlote <11893663+aaschlote@users.noreply.github.com>
2025-12-02 18:09:28 +0100

    test(stringReplacement): improve source code

--- app/domains/geldEinklagen/formular/__test__/stringReplacement.test.ts
+++ app/domains/geldEinklagen/formular/__test__/stringReplacement.test.ts
@@ -3,6 +3,7 @@ import {
   hasExclusivePlaceJurisdictionOrSelectCourt,
   isBeklagtePerson,
 } from "../stringReplacements";
+import { type GeldEinklagenFormularUserData } from "../userData";
 
 describe("stringReplacement", () => {
   describe("isBeklagtePerson", () => {
@@ -36,10 +37,10 @@ describe("stringReplacement", () => {
 
   describe("hasClaimVertrag", () => {
     it("should return hasClaimVertrag as true in case is versicherungVertrag yes", () => {
-      const context = {
-        versicherungVertrag: "yes" as const,
-        klagendeVertrag: "no" as const,
-        mietePachtVertrag: "no" as const,
+      const context: GeldEinklagenFormularUserData = {
+        versicherungVertrag: "yes",
+        klagendeVertrag: "no",
+        mietePachtVertrag: "no",
       };
 
       const actual = hasClaimVertrag(context);
@@ -47,10 +48,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return hasClaimVertrag as true in case is klagendeVertrag yes", () => {
-      const context = {
-        versicherungVertrag: "no" as const,
-        klagendeVertrag: "yes" as const,
-        mietePachtVertrag: "no" as const,
+      const context: GeldEinklagenFormularUserData = {
+        versicherungVertrag: "no",
+        klagendeVertrag: "yes",
+        mietePachtVertrag: "no",
       };
 
       const actual = hasClaimVertrag(context);
@@ -58,10 +59,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return hasClaimVertrag as true in case is mietePachtVertrag yes", () => {
-      const context = {
-        versicherungVertrag: "no" as const,
-        klagendeVertrag: "no" as const,
-        mietePachtVertrag: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        versicherungVertrag: "no",
+        klagendeVertrag: "no",
+        mietePachtVertrag: "yes",
       };
 
       const actual = hasClaimVertrag(context);
@@ -69,10 +70,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return hasClaimVertrag as false in case is mietePachtVertrag, versicherungVertrag and klagendeVertrag are no", () => {
-      const context = {
-        mietePachtVertrag: "no" as const,
-        versicherungVertrag: "no" as const,
-        klagendeVertrag: "no" as const,
+      const context: GeldEinklagenFormularUserData = {
+        mietePachtVertrag: "no",
+        versicherungVertrag: "no",
+        klagendeVertrag: "no",
       };
 
       const actual = hasClaimVertrag(context);
@@ -82,10 +83,10 @@ describe("stringReplacement", () => {
 
   describe("hasExclusivePlaceJurisdictionOrSelectCourt", () => {
     it("should return true if sachgebiet is miete and mietePachtRaum and mietePachtVertrag are yes", () => {
-      const context = {
-        sachgebiet: "miete" as const,
-        mietePachtRaum: "yes" as const,
-        mietePachtVertrag: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "miete",
+        mietePachtRaum: "yes",
+        mietePachtVertrag: "yes",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -93,10 +94,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return false if sachgebiet is versicherung", () => {
-      const context = {
-        sachgebiet: "versicherung" as const,
-        mietePachtRaum: "yes" as const,
-        mietePachtVertrag: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "versicherung",
+        mietePachtRaum: "yes",
+        mietePachtVertrag: "yes",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -104,10 +105,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return false if mietePachtRaum is no", () => {
-      const context = {
-        sachgebiet: "miete" as const,
-        mietePachtRaum: "no" as const,
-        mietePachtVertrag: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "miete",
+        mietePachtRaum: "no",
+        mietePachtVertrag: "yes",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -115,10 +116,10 @@ describe("stringReplacement", () => {
     });
 
     it("should return false if mietePachtVertrag is not yes", () => {
-      const context = {
-        sachgebiet: "miete" as const,
-        mietePachtRaum: "yes" as const,
-        mietePachtVertrag: "no" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "miete",
+        mietePachtRaum: "yes",
+        mietePachtVertrag: "no",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -126,8 +127,8 @@ describe("stringReplacement", () => {
     });
 
     it("should return true if gerichtsstandsvereinbarung yes", () => {
-      const context = {
-        gerichtsstandsvereinbarung: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        gerichtsstandsvereinbarung: "yes",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -135,9 +136,9 @@ describe("stringReplacement", () => {
     });
 
     it("should return true if sachgebiet is urheberrecht and beklagtePersonGeldVerdienen is no", () => {
-      const context = {
-        sachgebiet: "urheberrecht" as const,
-        beklagtePersonGeldVerdienen: "no" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "urheberrecht",
+        beklagtePersonGeldVerdienen: "no",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -145,9 +146,9 @@ describe("stringReplacement", () => {
     });
 
     it("should return false if sachgebiet is urheberrecht and beklagtePersonGeldVerdienen is yes", () => {
-      const context = {
-        sachgebiet: "urheberrecht" as const,
-        beklagtePersonGeldVerdienen: "yes" as const,
+      const context: GeldEinklagenFormularUserData = {
+        sachgebiet: "urheberrecht",
+        beklagtePersonGeldVerdienen: "yes",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
@@ -155,8 +156,8 @@ describe("stringReplacement", () => {
     });
 
     it("should return true pilotGerichtAuswahl is not undefined", () => {
-      const context = {
-        pilotGerichtAuswahl: "beklagteCourt" as const,
+      const context: GeldEinklagenFormularUserData = {
+        pilotGerichtAuswahl: "beklagteCourt",
       };
 
       const actual = hasExclusivePlaceJurisdictionOrSelectCourt(context);
