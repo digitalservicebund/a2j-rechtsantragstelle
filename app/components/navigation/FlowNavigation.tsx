e50c730a797ed669e60eb54eec642f837017d213
Spencer Peace <47868304+Spencer6497@users.noreply.github.com>
2025-12-02 16:17:35 +0100

    refactor(Mobile SideNav): Implement Mobile SideNav without JavaScript (#2969)
    * first pass wo JS
    
    * add opaque background when menu is open, and correct focus styling
    
    * add test-ids and remove now-unused SideNavMobileButton component, begin updating tests
    
    * add correct focus styling to summary, fix failing unit test

--- app/components/navigation/FlowNavigation.tsx
+++ app/components/navigation/FlowNavigation.tsx
@@ -4,37 +4,23 @@ import { NavigationList } from "~/components/navigation/NavigationList";
 import SideNavMobile from "~/components/navigation/SideNavMobile";
 import { type RootLoader } from "~/root";
 import { type FlowNavigationProps } from "./types";
-import { useJsAvailable } from "../hooks/useJsAvailable";
 
 export default function FlowNavigation(props: FlowNavigationProps) {
-  const jsAvailable = useJsAvailable();
-
   const rootLoaderData = useRouteLoaderData<RootLoader>("root");
 
   return (
     <nav
       aria-label={rootLoaderData?.accessibilityTranslations?.navigationLabel}
-      className={classNames(
-        "w-full lg:border lg:border-blue-400 print:hidden",
-        {
-          "fixed left-0 bottom-0 z-50 lg:static lg:z-auto": jsAvailable,
-        },
-      )}
+      className={
+        "fixed left-0 bottom-0 z-50 w-full lg:border lg:static lg:z-auto lg:border-blue-400 print:hidden"
+      }
     >
-      {jsAvailable && (
-        <SideNavMobile
-          navItems={props.navItems}
-          stepsStepper={props.stepsStepper}
-        />
-      )}
-
-      <NavigationList
-        {...props}
-        className={classNames("lg:block", {
-          hidden: jsAvailable,
-          "lg:block": !jsAvailable,
-        })}
+      <SideNavMobile
+        navItems={props.navItems}
+        stepsStepper={props.stepsStepper}
       />
+
+      <NavigationList {...props} className={classNames("hidden lg:block")} />
     </nav>
   );
 }
