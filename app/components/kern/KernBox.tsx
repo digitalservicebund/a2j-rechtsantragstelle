5f7b80342202a1d8b12e348dc1706232314b1c24
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2025-12-03 18:14:55 +0100

    Kern box

---
+++ app/components/kern/KernBox.tsx
@@ -0,0 +1,59 @@
+import KernButton, { type ButtonProps } from "~/components/kern/KernButton";
+import ButtonContainer from "~/components/common/ButtonContainer";
+import KernHeading, { type HeadingProps } from "~/components/kern/KernHeading";
+import KernRichText, {
+  type RichTextProps,
+} from "~/components/kern/KernRichText";
+import { GridItem } from "~/components/layout/grid/GridItem";
+import { arrayIsNonEmpty } from "~/util/array";
+
+type BoxProps = {
+  identifier?: string;
+  label?: HeadingProps;
+  heading?: HeadingProps;
+  content?: RichTextProps;
+  buttons?: ButtonProps[];
+};
+
+const KernBox = ({
+  identifier,
+  label,
+  heading,
+  content,
+  buttons,
+}: BoxProps) => {
+  return (
+    <GridItem
+      mdColumn={{ start: 1, span: 8 }}
+      lgColumn={{ start: 3, span: 8 }}
+      xlColumn={{ start: 3, span: 8 }}
+      className="py-32 px-16"
+      id={identifier}
+    >
+      <div className="gap-kern-space-x-large flex flex-col ">
+        <div className="kern-stack-sm">
+          {label && (
+            <p className="kern-label text-kern-layout-text-muted!">
+              {label.text}
+            </p>
+          )}
+          {heading && <KernHeading {...heading} />}
+          {content && (
+            <div className="kern-text-container">
+              <KernRichText {...content} />
+            </div>
+          )}
+        </div>
+        {arrayIsNonEmpty(buttons) && (
+          <ButtonContainer className="kern-button-group pt-kern-space-small">
+            {buttons.map((button) => (
+              <KernButton key={button.text ?? button.href} {...button} />
+            ))}
+          </ButtonContainer>
+        )}
+      </div>
+    </GridItem>
+  );
+};
+
+export default KernBox;
