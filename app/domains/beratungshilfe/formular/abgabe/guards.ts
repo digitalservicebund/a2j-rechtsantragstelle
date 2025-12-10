7283dd2728459d0f9b002a2e75c85cdfadce52d3
Spencer Peace <spencer.peace@digitalservice.bund.de>
2025-12-10 14:31:32 +0100

    exclude steps that have excludeFromValidation flag

---
+++ app/domains/beratungshilfe/formular/abgabe/guards.ts
@@ -0,0 +1,30 @@
+import type { BeratungshilfeFormularUserData } from "~/domains/beratungshilfe/formular/userData";
+import type { Guards } from "../../../guards.server";
+import { buildFlowController } from "~/services/flow/server/buildFlowController";
+import { beratungshilfeFormular } from "~/domains/beratungshilfe/formular";
+
+export const beratungshilfeAbgabeGuards = {
+  readyForAbgabe: ({ context }): boolean => {
+    // Need to strip off 'abgabe' lest we infinitely recurse
+    const stepStates = buildFlowController({
+      config: {
+        ...beratungshilfeFormular.config,
+        states: {
+          ...beratungshilfeFormular.config.states,
+          abgabe: {
+            id: "abgabe",
+          },
+        },
+      },
+      data: context,
+    }).stepStates();
+    return stepStates
+      .filter(
+        (stepState) =>
+          stepState.isReachable && !stepState.excludedFromValidation,
+      )
+      .every((stepState) => stepState.isDone);
+  },
+  abgabeOnline: ({ context }) => context.abgabeArt == "online",
+  abgabeAusdrucken: ({ context }) => context.abgabeArt == "ausdrucken",
+} satisfies Guards<BeratungshilfeFormularUserData>;
