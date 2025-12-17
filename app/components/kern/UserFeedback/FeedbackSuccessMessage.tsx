a116ca689e4d0ae7b34447c35bc73a96854176f4
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2025-12-17 12:03:34 +0100

    fix feedback success message

--- app/components/kern/UserFeedback/FeedbackSuccessMessage.tsx
+++ app/components/kern/UserFeedback/FeedbackSuccessMessage.tsx
@@ -10,10 +10,6 @@ export const FeedbackSuccessMessage = ({
   return (
     <output className="flex flex-col gap-kern-space-small">
       <div className="flex items-center">
-        <span
-          className="kern-icon kern-icon--success kern-icon--default mr-kern-space-x-small"
-          aria-hidden="true"
-        ></span>
         <p className="font-bold mr-4" ref={innerRef} tabIndex={-1}>
           {translations.feedback["success-message"].de}
         </p>
