4f1e3af872dc5433160d25ba500dd64a512f67eb
aaschlote <11893663+aaschlote@users.noreply.github.com>
2025-12-22 15:04:10 +0100

    feat(pages): add new pages

--- app/domains/geldEinklagen/formular/klage-erstellen/pages.ts
+++ app/domains/geldEinklagen/formular/klage-erstellen/pages.ts
@@ -1,4 +1,17 @@
+import z from "zod";
 import { type PagesConfig } from "~/domains/pageSchemas";
+import { hiddenInputSchema } from "~/services/validation/hiddenInput";
+import { ibanSchema } from "~/services/validation/iban";
+import { postcodeSchema } from "~/services/validation/postcode";
+import { schemaOrEmptyString } from "~/services/validation/schemaOrEmptyString";
+import { stringOptionalSchema } from "~/services/validation/stringOptional";
+import { stringRequiredSchema } from "~/services/validation/stringRequired";
+
+const sharedBeklagteAddress = {
+  beklagteStrasseHausnummer: stringRequiredSchema,
+  beklagtePlz: stringRequiredSchema.pipe(postcodeSchema),
+  beklagteOrt: stringRequiredSchema,
+};
 
 export const geldEinklagenKlageErstellenPages = {
   klageErstellenIntroStart: {
@@ -9,5 +22,42 @@ export const geldEinklagenKlageErstellenPages = {
   },
   streitwertKostenWeitereKosten: {
     stepId: "klage-erstellen/streitwert-kosten/weitere-kosten",
+  },
+  klagendePersonKontaktdaten: {
+    stepId: "klage-erstellen/klagende-person/kontaktdaten",
+    pageSchema: {
+      klagendePersonAnrede: z.enum(["herr", "frau", "none"]),
+      klagendePersonTitle: z.enum(["", "dr"]),
+      klagendePersonVorname: stringRequiredSchema,
+      klagendePersonNachname: stringRequiredSchema,
+      klagendePersonStrasseHausnummer: stringRequiredSchema,
+      klagendePersonPlz: stringRequiredSchema.pipe(postcodeSchema),
+      klagendePersonOrt: stringRequiredSchema,
+      klagendePersonIban: schemaOrEmptyString(ibanSchema),
+      klagendePersonKontoinhaber: stringOptionalSchema,
+    },
+  },
+  beklagtePersonMenschen: {
+    stepId: "klage-erstellen/beklagte-person/mensch",
+    pageSchema: {
+      gegenWenBeklagen: hiddenInputSchema(z.enum(["person", "organisation"])),
+      beklagteAnrede: z.enum(["herr", "frau", "none"]),
+      beklagteTitle: z.enum(["", "dr"]),
+      beklagteVorname: stringRequiredSchema,
+      beklagteNachname: stringRequiredSchema,
+      ...sharedBeklagteAddress,
+    },
+  },
+  beklagtePersonOrganisation: {
+    stepId: "klage-erstellen/beklagte-person/organisation",
+    pageSchema: {
+      gegenWenBeklagen: hiddenInputSchema(z.enum(["person", "organisation"])),
+      beklagteNameOrganisation: stringRequiredSchema,
+      beklagteGesetzlichenVertretung: stringRequiredSchema,
+      ...sharedBeklagteAddress,
+    },
+  },
+  rechtsproblemIntoStart: {
+    stepId: "klage-erstellen/rechtsproblem/intro/start",
   },
 } as const satisfies PagesConfig;
