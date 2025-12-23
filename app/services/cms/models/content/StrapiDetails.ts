979131b99d290868cd1ea2f9272b5ae3db501d8b
aaschlote <11893663+aaschlote@users.noreply.github.com>
2025-12-23 14:45:52 +0100

    feat(StrapiDetailsSchema): add outerBackground and container to schema

--- app/services/cms/models/content/StrapiDetails.ts
+++ app/services/cms/models/content/StrapiDetails.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 import { buildRichTextValidation } from "~/services/validation/richtext";
 import { HasStrapiIdSchema } from "../HasStrapiId";
+import { StrapiBackgroundOptionalSchema } from "../StrapiBackground";
 
 export const StrapiDetailsSchema = z
   .object({
     title: z.string(),
     content: buildRichTextValidation(),
+    outerBackground: StrapiBackgroundOptionalSchema,
+    container: StrapiBackgroundOptionalSchema,
     ...HasStrapiIdSchema.shape,
   })
   .transform((cmsData) => ({
