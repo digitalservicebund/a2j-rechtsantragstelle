175dc3f3db78116fc5905abbd727e082c86075f3
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2026-01-08 15:59:01 +0100

    feat(prozessfuehrung): add guard for filled prozessfuehrung data

--- app/domains/geldEinklagen/formular/klage-erstellen/prozessfuehrung/xStateConfig.ts
+++ app/domains/geldEinklagen/formular/klage-erstellen/prozessfuehrung/xStateConfig.ts
@@ -1,10 +1,31 @@
 import { geldEinklagenKlageErstellenPages } from "~/domains/geldEinklagen/formular/klage-erstellen/pages";
 import type { GeldEinklagenFormularKlageErstellenUserData } from "~/domains/geldEinklagen/formular/klage-erstellen/userData";
+import { type GenericGuard } from "~/domains/guards.server";
 import { xStateTargetsFromPagesConfig } from "~/domains/pageSchemas";
 import type { Config } from "~/services/flow/server/types";
+import { objectKeysNonEmpty } from "~/util/objectKeysNonEmpty";
 
 const steps = xStateTargetsFromPagesConfig(geldEinklagenKlageErstellenPages);
 
+type GeldEinklagenKlageErstellenDaten =
+  GenericGuard<GeldEinklagenFormularKlageErstellenUserData>;
+
+const hasFilledProzessfuehrung: GeldEinklagenKlageErstellenDaten = ({
+  context,
+}) => {
+  return (
+    (objectKeysNonEmpty(context, [
+      "prozesszinsen",
+      "anwaltskosten",
+      "streitbeilegung",
+      "muendlicheVerhandlung",
+      "videoVerhandlung",
+      "versaeumnisurteil",
+    ]) &&
+      context.streitbeilegung !== "yes") ||
+    objectKeysNonEmpty(context, ["streitbeilegungGruende"])
+  );
+};
 export const prozessfuehrungXstateConfig = {
   id: "prozessfuehrung",
   initial: "prozesszinsen",
@@ -65,7 +86,12 @@ export const prozessfuehrungXstateConfig = {
     },
     [steps.prozessfuehrungZahlungNachKlageeinreichung.relative]: {
       on: {
-        SUBMIT: steps.rechtlicherZusatzWeitereAntraege.absolute,
+        SUBMIT: [
+          {
+            target: steps.rechtlicherZusatzWeitereAntraege.absolute,
+            guard: hasFilledProzessfuehrung,
+          },
+        ],
         BACK: steps.prozessfuehrungVersaeumnisurteil.relative,
       },
     },
