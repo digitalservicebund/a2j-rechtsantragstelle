b473cce0416e827b43ba313ff15349c058cccffd
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-01-27 15:48:56 +0100

    refactor: move aria-label to nav element and aside to FormFlowPage

--- app/components/kern/navigation/FlowNavigation.tsx
+++ app/components/kern/navigation/FlowNavigation.tsx
@@ -8,15 +8,13 @@ export default function FlowNavigation({
   const rootLoaderData = useRouteLoaderData<RootLoader>("root");
 
   return (
-    <aside>
-      <nav
-        aria-label={rootLoaderData?.accessibilityTranslations?.navigationLabel}
-        className={
-          "fixed left-0 bottom-0 z-50 w-full lg:border lg:static lg:z-auto lg:border-kern-neutral-200 print:hidden rounded-sm"
-        }
-      >
-        {children}
-      </nav>
-    </aside>
+    <nav
+      aria-label={rootLoaderData?.accessibilityTranslations?.navigationLabel}
+      className={
+        "fixed left-0 bottom-0 z-50 w-full lg:border lg:static lg:z-auto lg:border-kern-neutral-200 print:hidden rounded-sm"
+      }
+    >
+      {children}
+    </nav>
   );
 }
--- app/components/navigation/FlowNavigation.tsx
+++ app/components/navigation/FlowNavigation.tsx
@@ -8,16 +8,13 @@ export default function FlowNavigation({
   const rootLoaderData = useRouteLoaderData<RootLoader>("root");
 
   return (
-    <aside
+    <nav
       aria-label={rootLoaderData?.accessibilityTranslations?.navigationLabel}
+      className={
+        "fixed left-0 bottom-0 z-50 w-full lg:border lg:static lg:z-auto lg:border-blue-400 print:hidden"
+      }
     >
-      <nav
-        className={
-          "fixed left-0 bottom-0 z-50 w-full lg:border lg:static lg:z-auto lg:border-blue-400 print:hidden"
-        }
-      >
-        {children}
-      </nav>
-    </aside>
+      {children}
+    </nav>
   );
 }
