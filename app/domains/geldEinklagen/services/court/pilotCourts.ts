dffc970baa068b8f58fd708b91c9726b0821285d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2026-01-28 15:26:16 +0100

    feat(TGA): update pilot courts (#3134)
    * feat(pilotCourts): update pilot courts list to include additional courts
    
    * fix(gerichtPruefen): update postal code e2e
    
    * fix(tests): update secondary edge case zip code to Leipzig

--- app/domains/geldEinklagen/services/court/pilotCourts.ts
+++ app/domains/geldEinklagen/services/court/pilotCourts.ts
@@ -1,8 +1,11 @@
 export const PILOT_COURTS = new Set([
-  "05_3_02_01_zivilgericht-amtsgericht", // AG BONN
   "05_2_06_03_zivilgericht-amtsgericht", // AG ESSEN
   "07_1_04_02_zivilgericht-amtsgericht", // AG BITBURG
   "07_1_02_14_zivilgericht-amtsgericht", // AG SINZIG
   "08_1_06_01_zivilgericht-amtsgericht", // AG MANNHEIM
   "11_1_01_07_zivilgericht-amtsgericht", // AG Schöneberg
+  "22_1_05_05_zivilgericht-amtsgericht", // AG Leipzig
+  "09_3_03_06_zivilgericht-amtsgericht", // AG Nürnberg
+  "02_1_01_01_zivilgericht-amtsgericht", // AG Hamburg
+  "08_2_06_07_zivilgericht-amtsgericht", // AG Nürtingen
 ]);
