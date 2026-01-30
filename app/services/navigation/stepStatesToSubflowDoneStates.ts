aaefe320654212469f5ff135ccf59e43e6e5dc0a
chris <christoph.hohnerlein@digitalservice.bund.de>
2026-01-30 18:26:42 +0100

    refactor(stepStatesToSubflowDoneStates): don't filter excludedFromValidation

--- app/services/navigation/stepStatesToSubflowDoneStates.ts
+++ app/services/navigation/stepStatesToSubflowDoneStates.ts
@@ -2,10 +2,8 @@ import type { StepState } from "../flow/server/buildFlowController";
 
 export const stepStatesToSubflowDoneStates = (stepStates: StepState[]) =>
   Object.fromEntries(
-    stepStates
-      .filter((stepState) => !stepState.excludedFromValidation)
-      .map((stepState) => [
-        stepState.stepId,
-        stepState.isReachable && stepState.isDone,
-      ]),
+    stepStates.map((stepState) => [
+      stepState.stepId,
+      stepState.isReachable && stepState.isDone,
+    ]),
   );
