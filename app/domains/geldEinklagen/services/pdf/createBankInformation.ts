c7f15e0942bc936d7b9311abbb932baa3d9813eb
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-02-02 15:42:13 +0100

    feat(createBankInformation): add function for bank information

---
+++ app/domains/geldEinklagen/services/pdf/createBankInformation.ts
@@ -0,0 +1,46 @@
+import type PDFDocument from "pdfkit";
+import {
+  FONTS_BUNDESSANS_REGULAR,
+  PDF_HEIGHT_SEIZE,
+  PDF_MARGIN_HORIZONTAL,
+} from "~/services/pdf/createPdfKitDocument";
+import type { GeldEinklagenFormularUserData } from "../../formular/userData";
+
+function drawBankInfo(doc: PDFKit.PDFDocument, text: string) {
+  doc
+    .fontSize(7)
+    .font(FONTS_BUNDESSANS_REGULAR)
+    .text(text, PDF_MARGIN_HORIZONTAL, PDF_HEIGHT_SEIZE);
+}
+
+export const createBankInformation = (
+  isLastPage: boolean,
+  doc: typeof PDFDocument,
+  footerSect: PDFKit.PDFStructureElement,
+  {
+    klagendePersonKontoinhaber,
+    klagendePersonVorname,
+    klagendePersonNachname,
+    klagendePersonIban,
+  }: GeldEinklagenFormularUserData,
+) => {
+  const bankAccountHolder =
+    klagendePersonKontoinhaber && klagendePersonKontoinhaber.trim().length > 0
+      ? klagendePersonKontoinhaber
+      : `${klagendePersonVorname} ${klagendePersonNachname}`;
+
+  if (klagendePersonIban) {
+    const bankInfo = `Kontoinhaber: ${bankAccountHolder} | IBAN: ${klagendePersonIban}`;
+
+    if (isLastPage) {
+      const bankInfoParagraph = doc.struct("P", {}, () => {
+        drawBankInfo(doc, bankInfo);
+      });
+      footerSect.add(bankInfoParagraph);
+    } else {
+      doc.markContent("Artifact", { type: "Pagination" });
+      drawBankInfo(doc, bankInfo);
+      doc.endMarkedContent();
+    }
+  }
+};
--- app/domains/geldEinklagen/services/pdf/geldEinklagenPdfFromUserdata.ts
+++ app/domains/geldEinklagen/services/pdf/geldEinklagenPdfFromUserdata.ts
@@ -1,10 +1,10 @@
 import type { PDFDocumentBuilder } from "~/services/pdf/pdfFromUserData";
 import { pdfFromUserData } from "~/services/pdf/pdfFromUserData";
 import { createFooter } from "~/services/pdf/footer/createFooter";
-import { createBankInformation } from "~/domains/fluggastrechte/services/pdf/sections/createBankInformation";
 import { setPdfMetadata } from "~/services/pdf/setPdfMetadata";
 import { createFirstPage } from "./sections/firstPage/createFirstPage";
 import type { GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
+import { createBankInformation } from "./createBankInformation";
 
 export const TITLE = "Klage Neueingang";
 export const SUBJECT = "Klageschrift";
