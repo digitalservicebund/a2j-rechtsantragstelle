3037b43edbe5a90e7f5d3654a444cb6946f93db9
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-02-02 14:46:29 +0100

    feat(createClaimData): add claim data in pdf

---
+++ app/domains/geldEinklagen/services/pdf/sections/firstPage/claimData/addPlaintiffDetails.ts
@@ -0,0 +1,42 @@
+import type PDFDocument from "pdfkit";
+import {
+  FONTS_BUNDESSANS_BOLD,
+  FONTS_BUNDESSANS_REGULAR,
+} from "~/services/pdf/createPdfKitDocument";
+import { type GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
+import { getFullPlaintiffName } from "~/domains/fluggastrechte/services/pdf/sections/getFullPlaintiffName";
+
+export const PLAINTIFF_TEXT = "– Klagende Partei –";
+export const SEPARATOR = " | ";
+
+export const addPlaintiffDetails = (
+  doc: typeof PDFDocument,
+  {
+    klagendePersonAnrede,
+    klagendePersonTitle,
+    klagendePersonVorname,
+    klagendePersonNachname,
+    klagendePersonStrasseHausnummer,
+    klagendePersonPlz,
+    klagendePersonOrt,
+  }: GeldEinklagenFormularUserData,
+) => {
+  const plaintiffName = getFullPlaintiffName(
+    klagendePersonAnrede,
+    klagendePersonTitle === "none" ? "" : klagendePersonTitle,
+    klagendePersonVorname,
+    klagendePersonNachname,
+  );
+  const address = klagendePersonStrasseHausnummer ?? "";
+  const zipCode = klagendePersonPlz ?? "";
+  const city = klagendePersonOrt ?? "";
+
+  doc
+    .fontSize(10)
+    .font(FONTS_BUNDESSANS_BOLD)
+    .text(plaintiffName, { continued: true })
+    .font(FONTS_BUNDESSANS_REGULAR)
+    .text(SEPARATOR, { continued: true })
+    .text(`${address}, ${zipCode} ${city}, Deutschland`)
+    .text(PLAINTIFF_TEXT, { align: "left" });
+};
--- app/domains/geldEinklagen/services/pdf/sections/firstPage/createFirstPage.ts
+++ app/domains/geldEinklagen/services/pdf/sections/firstPage/createFirstPage.ts
@@ -1,6 +1,7 @@
 import type PDFDocument from "pdfkit";
 import type { GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
 import { createLocalCourtAndDate } from "./createLocalCourtAndDate";
+import { createClaimData } from "./claimData/createClaimData";
 
 export const createFirstPage = (
   doc: typeof PDFDocument,
@@ -8,5 +9,12 @@ export const createFirstPage = (
   userData: GeldEinklagenFormularUserData,
 ) => {
   createLocalCourtAndDate(doc, documentStruct, userData);
+
+  doc.moveDown(2);
+
+  const moneyClaimSection = doc.struct("Sect");
+  createClaimData(doc, moneyClaimSection, userData);
+  documentStruct.add(moneyClaimSection);
+
   doc.moveDown(2);
 };
