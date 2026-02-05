578c5f3862affc8b51d7d816a91e04817a50b91f
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-02-05 11:13:39 +0100

    test(addFreeTextApplication): add test

---
+++ app/domains/geldEinklagen/services/pdf/sections/firstPage/__test__/addFreeTextApplication.test.ts
@@ -0,0 +1,30 @@
+import {
+  mockPdfKitDocument,
+  mockPdfKitDocumentStructure,
+} from "tests/factories/mockPdfKit";
+import { addFreeTextApplication } from "../claimData/addFreeTextApplication";
+
+describe("addFreeTextApplication", () => {
+  it("should not add free text application section if no free text application is provided", () => {
+    const mockStruct = mockPdfKitDocumentStructure();
+    const mockDoc = mockPdfKitDocument(mockStruct);
+
+    addFreeTextApplication(mockDoc, undefined, mockStruct);
+
+    expect(mockDoc.struct).not.toHaveBeenCalledWith("Sect");
+  });
+
+  it("should add free text application section if free text application is provided", () => {
+    const mockStruct = mockPdfKitDocumentStructure();
+    const mockDoc = mockPdfKitDocument(mockStruct);
+
+    const freeTextApplication = "This is a free text application.";
+    addFreeTextApplication(mockDoc, freeTextApplication, mockStruct);
+
+    expect(mockDoc.struct).toHaveBeenCalledWith("Sect");
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      freeTextApplication,
+      expect.any(Number),
+    );
+  });
+});
