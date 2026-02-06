23812835fb94eda370b7254be3641b6e55d6f12f
chris <christoph.hohnerlein@digitalservice.bund.de>
2026-02-06 12:14:36 +0100

    refactor: remove unused donefunctions (#3024)
    * chore: remove unused donefunctions
    
    * utilize new PageData type, inject pageData when evaluating tests
    
    * chore(PKH test data): remove happyPathData, refactor createFinancialEntry
    
    * refactor(pkh partner): simplify paymentSchema
    
    ---------
    
    Co-authored-by: Spencer Peace <spencer.peace@digitalservice.bund.de>

--- app/domains/prozesskostenhilfe/services/pdf/pdfForm/E_bruttoEinnahmen/__test__/bruttoEinnahmen_eigenes.test.ts
+++ app/domains/prozesskostenhilfe/services/pdf/pdfForm/E_bruttoEinnahmen/__test__/bruttoEinnahmen_eigenes.test.ts
@@ -1,8 +1,8 @@
 import { faker } from "@faker-js/faker";
 import type { ProzesskostenhilfePDF } from "data/pdf/prozesskostenhilfe/prozesskostenhilfe.generated";
 import { getProzesskostenhilfeParameters } from "data/pdf/prozesskostenhilfe/prozesskostenhilfe.generated";
-import { createFinancialEntry } from "~/domains/prozesskostenhilfe/formular/__test__/prozesskostenhilfeFormularData";
 import { SEE_IN_ATTACHMENT_DESCRIPTION } from "~/services/pdf/attachment";
+import { createFinancialEntry } from "./createFinancialEntry";
 import {
   fillAndereLeistungen,
   fillEinkommenType,
