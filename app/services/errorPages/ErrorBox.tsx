a9fc07b0c0b198ffd6e80ab4c14b3eeae18d0cc8
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-02-11 18:06:51 +0100

    migrate error pages

--- app/services/errorPages/ErrorBox.tsx
+++ app/services/errorPages/ErrorBox.tsx
@@ -9,19 +9,42 @@ function jsError(routeError: unknown) {
   if (routeError instanceof Error) return routeError.message;
 }
 
-function matchingError(routeError: unknown) {
-  if (isRouteErrorResponse(routeError) && ERROR_PAGES[routeError.status]) {
-    return ERROR_PAGES[routeError.status];
+function matchingError(routeError: unknown, showKernUX: boolean) {
+  if (isRouteErrorResponse(routeError)) {
+    const status = routeError.status;
+
+    if (showKernUX && ERROR_PAGES[`${status}_kern`]) {
+      return ERROR_PAGES[`${status}_kern`];
+    }
+
+    if (ERROR_PAGES[status]) {
+      return ERROR_PAGES[status];
+    }
   }
+
+  if (showKernUX && ERROR_PAGES["500_kern"]) {
+    return ERROR_PAGES["500_kern"];
+  }
+
   return ERROR_PAGES[500];
 }
 
-export function ErrorBox() {
+export function ErrorBox({ showKernUX = false }: { showKernUX?: boolean }) {
   const routerError = useRouteError();
   const isProd = config().ENVIRONMENT === "production";
+  const content = matchingError(routerError, showKernUX);
+
+  if (showKernUX) {
+    return (
+      <div className="flex flex-col py-kern-space-x-large">
+        <ContentComponents content={content} showKernUX />
+        <div>{!isProd && <pre>{jsError(routerError)}</pre>}</div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col grow">
-      <ContentComponents content={matchingError(routerError)} />
+      <ContentComponents content={content} />
       <Container>{!isProd && <pre>{jsError(routerError)}</pre>}</Container>
     </div>
   );
