0a87fb839e8fa91ef36858cbef7d9ebe9f2d276d
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-02-12 17:39:08 +0100

    formatting

--- app/components/kern/KernBoxItem.tsx
+++ app/components/kern/KernBoxItem.tsx
@@ -7,65 +7,65 @@ import KernButton, { type ButtonProps } from "./KernButton";
 import KernHeadline, { type KernHeadlineProps } from "./KernHeadline";
 import KernLabel, { type KernLabelProps } from "./KernLabel";
 import {
-    KernInlineNotice,
-    type KernInlineNoticeProps,
+  KernInlineNotice,
+  type KernInlineNoticeProps,
 } from "./KernInlineNotice";
 import KernAccordion, { type KernAccordionProps } from "./KernAccordion";
 
 export type KernBoxItemProps = {
-    id: number; // Strapi id
-    identifier?: string;
-    label?: KernLabelProps;
-    headline?: KernHeadlineProps;
-    image?: ImageProps;
-    content?: string;
-    details?: DetailsProps[];
-    inlineNotices?: KernInlineNoticeProps[];
-    buttons?: ButtonProps[];
-    separator?: boolean;
-    accordion?: KernAccordionProps;
+  id: number; // Strapi id
+  identifier?: string;
+  label?: KernLabelProps;
+  headline?: KernHeadlineProps;
+  image?: ImageProps;
+  content?: string;
+  details?: DetailsProps[];
+  inlineNotices?: KernInlineNoticeProps[];
+  buttons?: ButtonProps[];
+  separator?: boolean;
+  accordion?: KernAccordionProps;
 };
 
 const KernBoxItem = ({
-    identifier,
-    label,
-    headline,
-    image,
-    content,
-    details,
-    inlineNotices,
-    buttons,
-    accordion,
+  identifier,
+  label,
+  headline,
+  image,
+  content,
+  details,
+  inlineNotices,
+  buttons,
+  accordion,
 }: KernBoxItemProps) => {
-    return (
-        <div id={identifier}>
-            {image && (
-                <Image
-                    {...image}
-                    className="max-[499px]:mb-16 max-[499px]:w-[144px] max-[499px]:h-[144px] h-[168px] w-[168px] self-baseline"
-                />
-            )}
-            <div className="flex flex-col gap-kern-space-x-large">
-                {label && <KernLabel {...label} />}
-                {headline && <KernHeadline {...headline} />}
-                {content && <KernRichText html={content} />}
-                {details?.map((details) => (
-                    <Details key={details.title} {...details} />
-                ))}
-                {inlineNotices?.map((inlineNotice) => (
-                    <KernInlineNotice key={inlineNotice.title} {...inlineNotice} nested />
-                ))}
-                {arrayIsNonEmpty(buttons) && (
-                    <ButtonContainer>
-                        {buttons.map((button) => (
-                            <KernButton key={button.text ?? button.href} {...button} />
-                        ))}
-                    </ButtonContainer>
-                )}
-                {accordion && <KernAccordion {...accordion} />}
-            </div>
-        </div>
-    );
+  return (
+    <div id={identifier}>
+      {image && (
+        <Image
+          {...image}
+          className="max-[499px]:mb-16 max-[499px]:w-[144px] max-[499px]:h-[144px] h-[168px] w-[168px] self-baseline"
+        />
+      )}
+      <div className="flex flex-col gap-kern-space-x-large">
+        {label && <KernLabel {...label} />}
+        {headline && <KernHeadline {...headline} />}
+        {content && <KernRichText html={content} />}
+        {details?.map((details) => (
+          <Details key={details.title} {...details} />
+        ))}
+        {inlineNotices?.map((inlineNotice) => (
+          <KernInlineNotice key={inlineNotice.title} {...inlineNotice} nested />
+        ))}
+        {arrayIsNonEmpty(buttons) && (
+          <ButtonContainer>
+            {buttons.map((button) => (
+              <KernButton key={button.text ?? button.href} {...button} />
+            ))}
+          </ButtonContainer>
+        )}
+        {accordion && <KernAccordion {...accordion} />}
+      </div>
+    </div>
+  );
 };
 
 export default KernBoxItem;
