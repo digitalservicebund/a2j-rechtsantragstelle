7209b74c653d4667ac7d6475d37bfcc783cc867f
Rafael Falk <rafael.falk@digitalservice.bund.de>
2026-02-23 11:57:31 +0100

    refactor(bundid): switch to new secret paths in context of cloud migration

--- app/services/env/env.server.ts
+++ app/services/env/env.server.ts
@@ -80,17 +80,23 @@ export function config(): Config {
       "data/saml/sp_login_request_template.xml",
     ),
     SAML_SP_METADATA_PATH:
-      process.env.SAML_SP_METADATA_PATH?.trim() ??
-      path.join(process.cwd(), "data/saml/sp_metadata.xml"),
+      readSecretOrEnvVar(
+        "/etc/saml/sp_metadata/sp_metadata.xml",
+        "SAML_SP_METADATA_PATH") ??
+      "test",
     SAML_SP_SECRET_KEY_PATH:
-      process.env.SAML_SP_SECRET_KEY_PATH?.trim() ??
-      path.join(process.cwd(), "data/saml/sp_privateKey.pem"),
+      readSecretOrEnvVar(
+        "/etc/saml/sp_private_key/sp_private_key.pem",
+        "SAML_SP_SECRET_KEY_PATH") ??
+      "test",
     SAML_SP_SECRET_KEY_ENCRYPTION_PATH:
-      process.env.SAML_SP_SECRET_KEY_ENCRYPTION_PATH?.trim() ??
-      path.join(process.cwd(), "data/saml/sp_privateKeyEncryption.pem"),
-    SAML_IDP_CERT: (
-      readSecretOrEnvVar("/etc/saml/idp_cert", "SAML_IDP_CERT") ?? "test"
-    ).replaceAll(" ", ""),
+      readSecretOrEnvVar(
+        "/etc/saml/sp_private_key_encryption/sp_private_key_encryption.pem",
+        "SAML_SP_SECRET_KEY_ENCRYPTION_PATH") ??
+      "test",
+    SAML_IDP_CERT:
+      process.env.SAML_IDP_CERT?.replaceAll(" ", "") ??
+      "test",
     S3_REGION: process.env.AWS_S3_REGION ?? "eu-central-1",
     S3_ENDPOINT:
       process.env.S3_ENDPOINT ?? "https://s3.localhost.localstack.cloud:4566",
