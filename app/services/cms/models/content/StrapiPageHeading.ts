64b93302927e7db177235ed0ee873b0c65a9a6da
Mostafa elgaafary <3999755+melgaafary@users.noreply.github.com>
2026-03-03 14:46:01 +0100

    RAST-674 New Strapi component - Page Heading (#3298)
    * Strapi page heading
    
    * Strapi page heading test
    
    * lint and formatting
    
    * still need basic heading for legacy reasons
    
    * still need basic heading for legacy reasons

---
+++ app/services/cms/models/content/StrapiPageHeading.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { HasStrapiIdSchema } from "../HasStrapiId";
+import { OptionalStrapiLinkIdentifierSchema } from "../HasStrapiLinkIdentifier";
+import { StrapiHeadingOptionalSchema } from "./StrapiHeading";
+import { StrapiPaddingOptionalSchema } from "../StrapiPadding";
+
+export const StrapiPageHeadingSchema = z.object({
+  heading: StrapiHeadingOptionalSchema,
+  paddingTop: StrapiPaddingOptionalSchema,
+  paddingBottom: StrapiPaddingOptionalSchema,
+  __component: z.literal("page.heading"),
+  ...HasStrapiIdSchema.shape,
+  ...OptionalStrapiLinkIdentifierSchema.shape,
+});
