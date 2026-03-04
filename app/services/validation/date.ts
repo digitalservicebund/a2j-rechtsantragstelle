46d4c270ebe33073b1e9c97226aa51eccfeb555f
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-03-04 13:37:06 +0100

    feat(DateInput): remove casting

--- app/services/validation/date.ts
+++ app/services/validation/date.ts
@@ -23,18 +23,13 @@ export const toDateStringFromSplitDate = (
   value: SplitDate | "" | undefined,
 ) => {
   if (value && typeof value === "object") {
-    const { day, month, year } = value as {
-      day: string | number;
-      month: string | number;
-      year: string | number;
-    };
+    const { day, month, year } = value;
     return toDateString(
       Number(day),
       Number(month),
       typeof year === "string" ? Number(year) : year,
     );
   }
-
   return "";
 };
 
