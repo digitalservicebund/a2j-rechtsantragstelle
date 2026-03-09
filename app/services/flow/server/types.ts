132d252de149a31826042000d9a09f4b6e303704
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-03-09 16:59:25 +0100

    refactor(buildFlowController): use MachineFactory

--- app/services/flow/server/types.ts
+++ app/services/flow/server/types.ts
@@ -1,58 +1,49 @@
-import {
-  setup,
-  type MachineConfig,
-  type MachineContext,
-  type TransitionConfigOrTarget as XStateTransitionConfigOrTarget,
-} from "xstate";
+import { setup, type MachineContext } from "xstate";
 import type { GenericGuard, Guards } from "~/domains/guards.server";
 import { type UserData } from "~/domains/userData";
 import { type ArrayConfigServer } from "~/services/array";
 
 export type NavigationEvent = "SUBMIT" | "BACK" | ArrayConfigServer["event"];
 
-type FlowStateMachineEvents =
-  | { type: NavigationEvent }
-  | { type: ArrayConfigServer["event"] };
+type FlowStateMachineEvents = { type: NavigationEvent };
 
 export type StateMachineTypes = {
   context: UserData;
   events: FlowStateMachineEvents;
 };
 
-const _genericMachine = setup({
-  types: {} as StateMachineTypes,
-  guards: {} as Guards,
-});
+class ConfigExtractor<TContext extends UserData> {
+  getSetup() {
+    return setup({
+      types: {
+        context: {} as TContext,
+        events: {} as FlowStateMachineEvents,
+        meta: {} as Meta<TContext>,
+      },
+      guards: {} as Guards,
+    });
+  }
+}
 
-export type FlowStateMachine = ReturnType<typeof _genericMachine.createMachine>;
+type MachineFactory<T extends UserData> = ReturnType<
+  ConfigExtractor<T>["getSetup"]
+>["createMachine"];
 
-export type Config<TContext extends MachineContext = UserData> = MachineConfig<
-  TContext,
-  FlowStateMachineEvents,
-  never,
-  never,
-  { type: string; params: unknown },
-  never,
-  never,
-  never,
-  never,
-  never,
-  Meta<TContext>
+export type FlowStateMachine<T extends UserData = UserData> = ReturnType<
+  MachineFactory<T>
 >;
 
-export type TransitionConfigOrTarget<
-  TUserData extends MachineContext = UserData,
-> = XStateTransitionConfigOrTarget<
-  TUserData,
-  FlowStateMachineEvents,
-  FlowStateMachineEvents,
-  never,
-  never,
-  { type: string; params: unknown },
-  never,
-  never,
-  Meta
+export type Config<T extends UserData = UserData> = Parameters<
+  MachineFactory<T>
+>[0];
+
+export type XstateStates<T extends UserData = UserData> = NonNullable<
+  Config<T>["states"]
 >;
+
+// 2. Index directly using NavigationEvent and XState's wildcard
+export type TransitionConfigOrTarget<T extends UserData = UserData> =
+  NonNullable<XstateStates<T>[string]["on"]>[NavigationEvent | "*"];
 
 export type FlowConfigTransitions = {
   backToCallingFlow?: TransitionConfigOrTarget;
