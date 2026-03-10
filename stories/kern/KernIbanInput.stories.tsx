1c60467df156047540e7a4c7add18458136f78ac
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-03-10 14:47:25 +0100

    add IbanInput storybook components

---
+++ stories/kern/KernIbanInput.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { reactRouterFormContext } from "../.storybook/reactRouterFormContext";
+import KernIbanInput from "~/components/kern/formElements/input/KernIbanInput";
+
+const meta = {
+  title: "FormElements/KernIbanInput",
+  component: KernIbanInput,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof KernIbanInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    name: "iban",
+  },
+  decorators: [(Story) => reactRouterFormContext(<Story />)],
+};
