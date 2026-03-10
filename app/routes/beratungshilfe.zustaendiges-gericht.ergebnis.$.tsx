bce45b2d6fe5ce02da908a094c774942311c7600
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-03-10 15:26:47 +0100

    refactor: use splatFromParams
    Co-Authored-By: Rafael Falk <94931094+rbrtrfl@users.noreply.github.com>

--- app/routes/beratungshilfe.zustaendiges-gericht.ergebnis.$.tsx
+++ app/routes/beratungshilfe.zustaendiges-gericht.ergebnis.$.tsx
@@ -16,12 +16,11 @@ import {
   findCourt,
 } from "~/services/gerichtsfinder/amtsgerichtData.server";
 import KernZuestandigesGerichErgebnis from "./kern/kern-beratungshilfe.zustaendiges-gericht.ergebnis.$";
+import { splatFromParams } from "~/services/params";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const splat = params["*"];
-  invariant(splat !== undefined);
-
-  const [zipCode, streetSlug, ...houseNumberSplit] = splat.split("/");
+  const [zipCode, streetSlug, ...houseNumberSplit] =
+    splatFromParams(params).split("/");
   const houseNumber = houseNumberSplit.join("/");
   if (edgeCasesForPlz(zipCode).length > 0 && !streetSlug) {
     return redirect(`/beratungshilfe/zustaendiges-gericht/auswahl/${zipCode}`);
