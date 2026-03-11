de500dd609eef8f39b100ca5a8e5f823c5500b04
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-03-11 17:00:48 +0100

    formatting

--- app/components/kern/navigation/SkipToContentLink.tsx
+++ app/components/kern/navigation/SkipToContentLink.tsx
@@ -10,13 +10,9 @@ export function KernSkipToContentLink({
   return (
     <div
       id="skip-to-content-link"
-      className="flex justify-center absolute pt-kern-space-default left-1/2 -translate-x-1/2 -translate-y-full focus-within:translate-y-0 z-11">
-      <KernButton
-        href={target}
-
-        text={label}
-      />
+      className="flex justify-center absolute pt-kern-space-default left-1/2 -translate-x-1/2 -translate-y-full focus-within:translate-y-0 z-11"
+    >
+      <KernButton href={target} text={label} />
     </div>
-
   );
 }
