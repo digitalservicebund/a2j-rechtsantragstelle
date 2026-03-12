fe4f1057601153dc7353cc6ca9c37679ba39892a
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-03-12 19:15:25 +0100

    align result page links

--- app/routes/shared/components/kern/KernResultPage.tsx
+++ app/routes/shared/components/kern/KernResultPage.tsx
@@ -112,8 +112,8 @@ export function KernResultPage() {
           </GridItem>
           <GridItem
             mdColumn={{ start: 1, span: 8 }}
-            lgColumn={{ start: 3, span: 12 }}
-            xlColumn={{ start: 3, span: 12 }}
+            lgColumn={{ start: 2, span: 12 }}
+            xlColumn={{ start: 2, span: 12 }}
             className="py-24 px-16 md:px-16 lg:px-0 xl:px-0"
             row={2}
           >
