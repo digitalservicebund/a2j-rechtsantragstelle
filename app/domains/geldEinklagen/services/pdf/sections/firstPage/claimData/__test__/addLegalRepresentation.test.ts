553351ab78f2c511889fecadedf3e57c45afd193
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-03-17 14:37:43 +0100

    feat(addLegalRepresentation): add legal representation

---
+++ app/domains/geldEinklagen/services/pdf/sections/firstPage/claimData/__test__/addLegalRepresentation.test.ts
@@ -0,0 +1,49 @@
+import {
+  mockPdfKitDocument,
+  mockPdfKitDocumentStructure,
+} from "tests/factories/mockPdfKit";
+import { addLegalRepresentation } from "../addLegalRepresentation";
+import { userDataMock } from "~/domains/geldEinklagen/services/pdf/__test__/userDataMock";
+import { type GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
+
+describe("addLegalRepresentation", () => {
+  it("should not add legal representation if anwaltschaft is 'no'", () => {
+    const mockStruct = mockPdfKitDocumentStructure();
+    const mockDoc = mockPdfKitDocument(mockStruct);
+
+    addLegalRepresentation(mockDoc, userDataMock);
+
+    expect(mockDoc.text).not.toHaveBeenCalled();
+  });
+
+  it("should add legal representation if anwaltschaft is 'yes'", () => {
+    const mockStruct = mockPdfKitDocumentStructure();
+    const mockDoc = mockPdfKitDocument(mockStruct);
+
+    const userDataWithAnwaltschaft: GeldEinklagenFormularUserData = {
+      ...userDataMock,
+      anwaltschaft: "yes",
+      klagendePersonAnwaltschaftKanzlei: "Musterkanzlei",
+      klagendePersonAnwaltschaftAnrede: "herr",
+      klagendePersonAnwaltschaftNachname: "Mustermann",
+      klagendePersonAnwaltschaftTitle: "Dr.",
+      klagendePersonAnwaltschaftVorname: "Max",
+      klagendePersonAnwaltschaftStrasseHausnummer: "Musterstraße 1",
+      klagendePersonAnwaltschaftPlz: "12345",
+      klagendePersonAnwaltschaftOrt: "Musterstadt",
+    };
+
+    addLegalRepresentation(mockDoc, userDataWithAnwaltschaft);
+
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      "Musterkanzlei, Herr Dr. Max Mustermann Rechtsanwalt",
+      {
+        continued: true,
+      },
+    );
+
+    expect(mockDoc.text).toHaveBeenCalledWith(
+      "Musterstraße 1, 12345 Musterstadt, Deutschland",
+    );
+  });
+});
---
+++ app/domains/geldEinklagen/services/pdf/sections/firstPage/claimData/addLegalRepresentation.ts
@@ -0,0 +1,75 @@
+import type PDFDocument from "pdfkit";
+import {
+  FONTS_BUNDESSANS_BOLD,
+  FONTS_BUNDESSANS_REGULAR,
+} from "~/services/pdf/createPdfKitDocument";
+import { type GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
+import capitalize from "lodash/capitalize";
+
+export const SEPARATOR = " | ";
+
+const getRepresentationName = (userData: GeldEinklagenFormularUserData) => {
+  const {
+    klagendePersonAnwaltschaftKanzlei,
+    klagendePersonAnwaltschaftAnrede,
+    klagendePersonAnwaltschaftNachname,
+    klagendePersonAnwaltschaftTitle,
+    klagendePersonAnwaltschaftVorname,
+    klagendePersonAnwaltschaftBerufsbezeichnung,
+  } = userData;
+
+  const salutation =
+    klagendePersonAnwaltschaftAnrede === "none"
+      ? ""
+      : capitalize(klagendePersonAnwaltschaftAnrede);
+  const capitalizedVorname = capitalize(klagendePersonAnwaltschaftVorname);
+
+  return [
+    klagendePersonAnwaltschaftKanzlei
+      ? `${klagendePersonAnwaltschaftKanzlei},`
+      : "",
+    salutation,
+    klagendePersonAnwaltschaftTitle,
+    capitalizedVorname,
+    klagendePersonAnwaltschaftNachname,
+    klagendePersonAnwaltschaftBerufsbezeichnung,
+  ]
+    .filter(Boolean)
+    .join(" ");
+};
+
+const formatContactInfo = (phone?: string, email?: string): string => {
+  const parts = [phone, email].filter(Boolean);
+  return parts.join(` ${SEPARATOR} `);
+};
+
+export const addLegalRepresentation = (
+  doc: typeof PDFDocument,
+  userData: GeldEinklagenFormularUserData,
+) => {
+  if (userData.anwaltschaft === "no") {
+    return;
+  }
+
+  const representationName = getRepresentationName(userData);
+  const address = userData.klagendePersonAnwaltschaftStrasseHausnummer ?? "";
+  const zipCode = userData.klagendePersonAnwaltschaftPlz ?? "";
+  const city = userData.klagendePersonAnwaltschaftOrt ?? "";
+  const businessIdentification = userData.klagendePersonAnwaltschaftKanzlei
+    ? `Geschäftszeichen: ${userData.klagendePersonAnwaltschaftKanzlei}`
+    : "";
+  const contactInfo =
+    formatContactInfo(
+      userData.klagendePersonAnwaltschaftTelefonnummer,
+      userData.klagendePersonAnwaltschaftEmail,
+    ) || "";
+
+  doc
+    .font(FONTS_BUNDESSANS_BOLD)
+    .text(representationName, { continued: true })
+    .font(FONTS_BUNDESSANS_REGULAR)
+    .text(SEPARATOR, { continued: true })
+    .text(`${address}, ${zipCode} ${city}, Deutschland`)
+    .text(businessIdentification)
+    .text(contactInfo);
+};
