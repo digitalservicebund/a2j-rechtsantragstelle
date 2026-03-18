ea00f152ab8fa495ef8e9ceb2628703a91cdb015
Mostafa elgaafary <3999755+melgaafary@users.noreply.github.com>
2026-03-18 17:06:41 +0100

    RAST-732 fix e2e to check for the new datenschutz url (#3416)
    * check for the new datenschutz url
    
    * formatting
    
    * remove old links from kern footer
    
    * correct section name
    
    * remove datenschutz test for now until content is built

--- app/components/kern/layout/footer/footerContent.ts
+++ app/components/kern/layout/footer/footerContent.ts
@@ -30,23 +30,11 @@ export const footerContent: FooterContent[] = [
   {
     key: "datenschutzerklaerungen",
     type: ["internalLink"],
-    sectionName: "Datenschutzerklärungen",
+    sectionName: "Datenschutz",
     content: [
       {
-        text: "service.justiz.de Datenschutz",
+        text: "Datenschutzerklärung",
         url: "/datenschutzerklaerung",
-      },
-      {
-        text: "Beratungshilfe Datenschutz",
-        url: "/beratungshilfe/datenschutz",
-      },
-      {
-        text: "Prozesskostenhilfe Datenschutz",
-        url: "/prozesskostenhilfe/datenschutz",
-      },
-      {
-        text: "Fluggastrechte Datenschutz",
-        url: "/fluggastrechte/datenschutz",
       },
     ],
   },
