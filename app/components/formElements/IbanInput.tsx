24c5ba5668b8c18e30f9a849d01a265420830a5d
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-03-20 15:31:20 +0100

    Revert "feat(IbanInput): fix mask, hard-code charLimit & width"
    This reverts commit 373c246cb3bb19ec3d03d93c728d3cc4bc6a21d2.

--- app/components/formElements/IbanInput.tsx
+++ app/components/formElements/IbanInput.tsx
@@ -1,15 +1,13 @@
 import { type InputProps } from "~/components/formElements/Input";
 import MaskedInput from "~/components/formElements/MaskedInput";
 
-const IbanInput = (props: Omit<InputProps, "charLimit" | "width">) => {
+const IbanInput = (props: InputProps) => {
   return (
     <MaskedInput
-      {...props}
-      mask={"**** **** **** **** **** **"}
+      mask={"**** **** **** **** **** **** **** **** **"}
+      type="number"
       prepareChar={(str) => str.toUpperCase()}
-      charLimit={27}
-      width="36" // Could even be 24?
-      // placeholder="DE11 1111 1111 1111 1111 11"
+      {...props}
     />
   );
 };
