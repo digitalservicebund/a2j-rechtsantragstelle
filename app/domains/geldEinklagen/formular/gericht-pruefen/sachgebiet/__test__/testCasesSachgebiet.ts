9ecd8fa1b62b741951eb015f277e158554e730d3
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-03-20 14:46:00 +0100

    refactor(tga): use subflowDoneStates for gericht-pruefen/sachgebiet

--- app/domains/geldEinklagen/formular/gericht-pruefen/sachgebiet/__test__/testCasesSachgebiet.ts
+++ app/domains/geldEinklagen/formular/gericht-pruefen/sachgebiet/__test__/testCasesSachgebiet.ts
@@ -1,11 +1,22 @@
 import { type TestCases } from "~/domains/__test__/TestCases";
 import { type GeldEinklagenFormularUserData } from "../../../userData";
 
+const baseContext: GeldEinklagenFormularUserData = {
+  anwaltschaft: "no",
+  forderung: "maximal10000",
+  ausgeschlossen: "no",
+  pageData: {
+    subflowDoneStates: {
+      "/gericht-pruefen/sachgebiet": true,
+    },
+    arrayIndexes: [],
+  },
+};
+
 export const testCasesSachgebiet = [
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
+      ...baseContext,
       ausgeschlossen: "yes",
     },
     [
@@ -16,9 +27,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "anderesRechtsproblem",
     },
     [
@@ -30,9 +39,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "urheberrecht",
     },
     [
@@ -44,9 +51,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "schaden",
     },
     [
@@ -58,9 +63,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "miete",
       mietePachtVertrag: "yes",
       mietePachtRaum: "yes",
@@ -76,9 +79,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "miete",
       mietePachtVertrag: "yes",
       mietePachtRaum: "no",
@@ -94,9 +95,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "miete",
       mietePachtVertrag: "no",
     },
@@ -110,9 +109,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "versicherung",
       versicherungVertrag: "yes",
       versicherungsnehmer: "yes",
@@ -128,9 +125,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "versicherung",
       versicherungVertrag: "yes",
       versicherungsnehmer: "no",
@@ -146,9 +141,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "versicherung",
       versicherungVertrag: "no",
     },
@@ -162,9 +155,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "reisen",
       reiseArt: "andereReise",
     },
@@ -178,9 +169,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "reisen",
       reiseArt: "flug",
     },
@@ -195,9 +184,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "verkehrsunfall",
       verkehrsunfallStrassenverkehr: "yes",
     },
@@ -211,9 +198,7 @@ export const testCasesSachgebiet = [
   ],
   [
     {
-      anwaltschaft: "no",
-      forderung: "maximal10000",
-      ausgeschlossen: "no",
+      ...baseContext,
       sachgebiet: "verkehrsunfall",
       verkehrsunfallStrassenverkehr: "no",
     },
