265ce33162c83cfe090ea5fa16273704385bfb74
Alex August Schlote <11893663+aaschlote@users.noreply.github.com>
2026-03-23 14:32:21 +0100

    refactor(tga-pdf): check if it has space for the texts before to print the titles (#3441)

--- app/domains/geldEinklagen/services/pdf/sections/reason/legalAssessment/createLegalAssessment.ts
+++ app/domains/geldEinklagen/services/pdf/sections/reason/legalAssessment/createLegalAssessment.ts
@@ -1,10 +1,15 @@
 import type PDFDocument from "pdfkit";
 import type { GeldEinklagenFormularUserData } from "~/domains/geldEinklagen/formular/userData";
-import { FONTS_BUNDESSANS_BOLD } from "~/services/pdf/createPdfKitDocument";
+import {
+  FONTS_BUNDESSANS_BOLD,
+  PDF_WIDTH_SEIZE,
+} from "~/services/pdf/createPdfKitDocument";
 import { addAdvanceCourtText } from "./addAdvanceCourtText";
 import { addRechtlicheWuerdigung } from "./addRechtlicheWuerdigung";
 import { addDisputeResolution } from "./addDisputeResolution";
 import { addSignature } from "./addSignature";
+import { getHeightOfString } from "~/services/pdf/getHeightOfString";
+import { addNewPageInCaseMissingVerticalSpace } from "~/services/pdf/addNewPageInCaseMissingVerticalSpace";
 
 const getLegalAssessmentTitle = (hasEvidencesOnFacts: boolean) =>
   `${hasEvidencesOnFacts ? "III" : "II"}. Rechtliche Würdigung`;
@@ -17,12 +22,23 @@ export const createLegalAssessment = (
 ) => {
   const legalAssessmentSect = doc.struct("Sect");
 
+  const legalAssessmentTitle = getLegalAssessmentTitle(hasEvidencesOnFacts);
+
+  const evidencesOnFactsTextHeight = getHeightOfString(
+    legalAssessmentTitle,
+    doc,
+    PDF_WIDTH_SEIZE,
+  );
+
+  // We need to check if there is enough space for the "Rechtliche Würdigung" title and the following texts below
+  addNewPageInCaseMissingVerticalSpace(doc, {
+    extraYPosition: evidencesOnFactsTextHeight,
+    moveDownFactor: 1,
+  });
+
   legalAssessmentSect.add(
     doc.struct("H3", {}, () => {
-      doc
-        .fontSize(14)
-        .font(FONTS_BUNDESSANS_BOLD)
-        .text(getLegalAssessmentTitle(hasEvidencesOnFacts));
+      doc.fontSize(14).font(FONTS_BUNDESSANS_BOLD).text(legalAssessmentTitle);
       doc.moveDown(1);
     }),
   );
