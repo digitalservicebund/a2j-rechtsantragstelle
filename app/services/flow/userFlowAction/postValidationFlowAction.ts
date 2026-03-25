c38104fe97544c202b8b7e33da06c3f81ef8a942
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-03-25 16:01:49 +0100

    refactor: add session parameter to the function asyncFlowActions

--- app/services/flow/userFlowAction/postValidationFlowAction.ts
+++ app/services/flow/userFlowAction/postValidationFlowAction.ts
@@ -1,14 +1,16 @@
 import type { Flow } from "~/domains/flows.server";
 import { type UserData } from "~/domains/userData";
 import { getPageAndFlowDataFromPathname } from "../getPageAndFlowDataFromPathname";
+import type { Session } from "react-router";
 
 export const postValidationFlowAction = async (
   request: Request,
   userData: UserData,
+  flowSession: Session,
 ) => {
   const { pathname } = new URL(request.url);
   const { stepId, currentFlow } = getPageAndFlowDataFromPathname(pathname);
   const { asyncFlowActions } = currentFlow as Flow;
   if (asyncFlowActions && stepId in asyncFlowActions)
-    await asyncFlowActions[stepId](request, userData);
+    await asyncFlowActions[stepId](request, userData, flowSession);
 };
