087a514d8c32d842d280595970e8cdaa83c82500
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-03-27 16:57:22 +0100

    chore: fix type imports [skip ci]

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/userData.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/userData.ts
@@ -1,6 +1,6 @@
 import { type UserDataFromPagesSchema } from "~/domains/pageSchemas";
 import { type pkhFormularFinanzielleAngabenPages } from "~/domains/prozesskostenhilfe/formular/finanzielleAngaben/pages";
-import { PageData } from "~/services/flow/pageDataSchema";
+import { type PageData } from "~/services/flow/pageDataSchema";
 
 export type ProzesskostenhilfeFinanzielleAngabenUserData =
   UserDataFromPagesSchema<typeof pkhFormularFinanzielleAngabenPages> & {
