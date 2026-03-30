39f49ed6dc35e9c625845a7684ff45d7a049aeed
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-03-30 11:07:23 +0200

    test(streetNames): add test for the function getCityNameByZipCode and improve code quality

--- app/services/streetNames/__test__/streetNames.test.ts
+++ app/services/streetNames/__test__/streetNames.test.ts
@@ -1,19 +1,44 @@
-import { streetNamesForZipcode } from "../index";
+import { getCityNameByZipCode, streetNamesForZipcode } from "../index";
 
 describe("streetNamesForZipcode", () => {
   it("should return empty array without input", async () => {
-    expect(await streetNamesForZipcode()).toStrictEqual([]);
+    const actual = await streetNamesForZipcode();
+
+    expect(actual).toStrictEqual([]);
   });
 
   it("should return empty array for invalid postcodes", async () => {
-    expect(await streetNamesForZipcode("asd")).toStrictEqual([]);
+    const actual = await streetNamesForZipcode("asd");
+    expect(actual).toStrictEqual([]);
   });
 
   it("should filter by unique results", async () => {
     // 27499 (Insel Neuwerk) has only two entries
-    expect(await streetNamesForZipcode("27499")).toEqual([
+    const actual = await streetNamesForZipcode("27499");
+
+    expect(actual).toEqual([
       { city: "Hamburg", name: "Herrengarten", postalCode: "27499" },
       { city: "Hamburg", name: "Mittelweg", postalCode: "27499" },
     ]);
+  });
+});
+
+describe("getCityNameByZipCode", () => {
+  it("should return empty string without input", () => {
+    const actual = getCityNameByZipCode("");
+
+    expect(actual).toBe("");
+  });
+
+  it("should return empty string for invalid postcodes", () => {
+    const actual = getCityNameByZipCode("asd");
+
+    expect(actual).toBe("");
+  });
+
+  it("should return the city name for a valid postcode", () => {
+    const actual = getCityNameByZipCode("27499");
+
+    expect(actual).toBe("Hamburg");
   });
 });
