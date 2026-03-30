b83a85b9249130ad4232f48b1b9664d2d1caf954
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-03-30 12:27:37 +0200

    fix: remove unused input

--- app/routes/__test__/$.test.ts
+++ app/routes/__test__/$.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 import { action, loader } from "../$";
 import { type LoaderFunctionArgs } from "react-router";
 import { redirectMap } from "~/services/routing/redirects";
