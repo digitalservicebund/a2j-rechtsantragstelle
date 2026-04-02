c7cdcc72aa927a63df6dd6f648d994c58702b656
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-04-02 11:18:57 +0200

    fix(KERN): remove gridSection from the footer to fix spacing

--- app/components/kern/layout/footer/KernFooterDeletePersonalData.tsx
+++ app/components/kern/layout/footer/KernFooterDeletePersonalData.tsx
@@ -1,6 +1,5 @@
 import { Grid } from "~/components/layout/grid/Grid";
 import { GridItem } from "~/components/layout/grid/GridItem";
-import { GridSection } from "~/components/layout/grid/GridSection";
 import { translations } from "~/services/translations/translations";
 import { footerContent } from "./footerContent";
 
@@ -10,22 +9,20 @@ export const KernFooterDeletePersonalData = () => {
   )?.content[0];
 
   return (
-    <GridSection className="bg-kern-neutral-025">
-      <Grid className="pb-40 lg:pb-0">
-        <GridItem
-          mdColumn={{ start: 1, span: 8 }}
-          lgColumn={{ start: 3, span: 8 }}
-          xlColumn={{ start: 2, span: 10 }}
-          className="text-white print:hidden text-center pt-16 pb-16"
-        >
-          <div className="text-center print:hidden">
-            <a className="kern-link" href={personalDataDeleteLink?.url}>
-              {translations["delete-data"].footerLinkLabel.de ??
-                personalDataDeleteLink?.text}
-            </a>
-          </div>
-        </GridItem>
-      </Grid>
-    </GridSection>
+    <Grid className="bg-kern-neutral-025 pb-40 lg:pb-0">
+      <GridItem
+        mdColumn={{ start: 1, span: 8 }}
+        lgColumn={{ start: 3, span: 8 }}
+        xlColumn={{ start: 2, span: 10 }}
+        className="text-white print:hidden text-center pt-16 pb-16"
+      >
+        <div className="text-center print:hidden">
+          <a className="kern-link" href={personalDataDeleteLink?.url}>
+            {translations["delete-data"].footerLinkLabel.de ??
+              personalDataDeleteLink?.text}
+          </a>
+        </div>
+      </GridItem>
+    </Grid>
   );
 };
