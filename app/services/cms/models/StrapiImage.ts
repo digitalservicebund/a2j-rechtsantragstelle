1a5aef40f804fd9ee120503fb86185cc03656bef
chohner <christoph.hohnerlein@digitalservice.bund.de>
2026-04-10 11:35:33 +0200

    feat(StrapiImage): logError on fetch failure
    Co-Authored-By: Spencer Peace <47868304+Spencer6497@users.noreply.github.com>

--- app/services/cms/models/StrapiImage.ts
+++ app/services/cms/models/StrapiImage.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { StrapiStringOptionalSchema } from "~/services/cms/models/StrapiStringOptional";
 import { config } from "~/services/env/env.server";
 import { config as publicConfig } from "~/services/env/public";
+import { logError } from "~/services/logging";
 import { omitNull } from "~/util/omitNull";
 import { decodeBase64 } from "~/util/strings";
 
@@ -35,7 +36,13 @@ export const StrapiImageSchema = z
     // We should find a more idiomatic way for this in the future
     const svgString = cmsImage.url.startsWith("data:image/svg+xml;base64")
       ? decodeBase64(cmsImage.url.split(",")[1])
-      : await axios.get<string>(cmsImage.url).then(({ data }) => data);
+      : await axios
+          .get<string>(cmsImage.url)
+          .then(({ data }) => data)
+          .catch((err) => {
+            logError(err);
+            return "";
+          });
     return { ...cmsImage, svgString, url: "" };
   });
 
