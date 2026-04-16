464a856953edf695a3e4737749414235a71a33a1
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-04-16 15:51:49 +0200

    cleanup storybook folder structure and naming

---
+++ stories/layout/Footer.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import KernFooter from "~/components/kern/layout/footer/KernFooter";
+import { GridSection } from "~/components/layout/grid/GridSection";
+import imgUrl from "../assets/bmj_logo.png";
+
+const meta = {
+  title: "layout/Footer",
+  component: KernFooter,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <GridSection>
+        <Story />
+      </GridSection>
+    ),
+  ],
+} satisfies Meta<typeof KernFooter>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    image: {
+      url: imgUrl,
+      alternativeText: "Bundesministerium der Justiz",
+    },
+    showDeletionBanner: false,
+  },
+};
+
+export const WithDeletionBanner: Story = {
+  args: {
+    ...Default.args,
+    showDeletionBanner: true,
+  },
+};
