010dcd2910917b518a81a7467ebdffc872a51576
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-04-23 17:05:18 +0200

    fix stepper hover. And fix warning and current state for stepper

--- app/components/kern/navigation/FlowStepperNavigation/getStepStyles.ts
+++ app/components/kern/navigation/FlowStepperNavigation/getStepStyles.ts
@@ -20,24 +20,26 @@ export function getStepStyles(state: NavState) {
       {
         "bg-kern-neutral-025 text-kern-neutral-400 pointer-events-none":
           stateIsDisabled(state),
-        "bg-kern-feedback-warning": stateIsWarning(state),
-        "bg-kern-neutral-200 font-semibold": stateIsCurrent(state),
+        "stepper-step--warning bg-kern-feedback-warning-background hover:bg-kern-orange-100": stateIsWarning(state),
+        "bg-kern-neutral-200 font-semibold": stateIsCurrent(state) && !stateIsWarning(state),
+        "hover:bg-kern-neutral-200": !stateIsWarning(state) && !stateIsDisabled(state),
+        "bg-red": stateIsCurrent(state) && stateIsWarning(state)
       },
     ),
 
-    arrowBg: stateIsCurrent(state)
+    arrowBg: stateIsCurrent(state) && !stateIsWarning(state)
       ? "var(--color-kern-neutral-200)"
       : stateIsDisabled(state)
         ? "var(--color-kern-neutral-025)"
         : stateIsWarning(state)
-          ? "var(--color-kern-feedback-warning)"
+          ? "var(--color-kern-feedback-warning-background)"
           : "var(--color-kern-neutral-025)",
 
     circle: classNames(
       "flex justify-center items-center w-[20px] h-[20px] rounded-full mr-3",
       {
         "bg-kern-action-default text-white":
-          stateIsCurrent(state) || stateIsOpen(state),
+          (stateIsCurrent(state) || stateIsOpen(state)) && !stateIsWarning(state),
         "bg-white": stateIsDone(state),
         "bg-kern-neutral-400 text-white": stateIsDisabled(state),
         "forced-colors:outline-solid forced-colors:border-0":
