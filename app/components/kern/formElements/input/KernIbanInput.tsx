1881bd3469c3db2cf04d1b9a92c7e0831665b493
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-04-24 06:42:50 +0200

    working sr changes

--- app/components/kern/formElements/input/KernIbanInput.tsx
+++ app/components/kern/formElements/input/KernIbanInput.tsx
@@ -1,5 +1,5 @@
 import { useField } from "@rvf/react-router";
-import { type FunctionComponent } from "react";
+import { useEffect, useState, type FunctionComponent } from "react";
 import { IMaskMixin, type IMaskMixinProps } from "react-imask";
 import { KernIcon } from "~/components/kern/common/KernIcon";
 
@@ -16,20 +16,34 @@ const MaskedIbanInput: FunctionComponent<MaskedIbanInputProps> = IMaskMixin<
 >((props) => <TextInput {...props} />);
 
 const KernIbanInput = (props: InputProps) => {
-  const iban = useField<string | undefined>(props.name).value();
-  const bankName = bankNameFromIBAN(iban);
+  const field = useField<string | undefined>(props.name);
+  const iban = field.value();
+  const [bankName, setBankName] = useState<string | null>(null);
+
+  // Debounce needed to not clobber the screen reader while typing
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const matchedBankName = bankNameFromIBAN(iban);
+      setBankName(matchedBankName || null);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [iban]);
+
+  const bankNameBadgeId = "bank-name-badge";
+
   return (
     <div key={props.name} className="flex flex-col gap-15">
       <MaskedIbanInput
         mask={"**** **** **** **** **** **** **** **** **"}
         type="number"
         prepareChar={(str) => str.toUpperCase()}
+        ariaDescribedBy={bankNameBadgeId}
         {...props}
       />
       {bankName && (
         <output
-          id="bank name"
-          aria-atomic={true}
+          id={bankNameBadgeId}
           className="kern-badge kern-badge-info border-2 border-kern-feedback-info bg-kern-feedback-info-background w-min"
         >
           <KernIcon name="info" className="fill-kern-feedback-info" />
@@ -38,6 +52,15 @@ const KernIbanInput = (props: InputProps) => {
           </span>
         </output>
       )}
+
+      <div
+        aria-live="polite"
+        aria-relevant="all"
+        aria-atomic="true"
+        className="sr-only"
+      >
+        <span key={bankName}>{bankName}</span>
+      </div>
     </div>
   );
 };
