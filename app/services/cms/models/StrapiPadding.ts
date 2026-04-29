efc9f7a89c915f66393dc16ab2ae9bad7c417a64
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-04-29 18:18:23 +0200

    fix tests

--- app/services/cms/models/StrapiPadding.ts
+++ app/services/cms/models/StrapiPadding.ts
@@ -3,8 +3,6 @@ import { omitNull } from "~/util/omitNull";
 
 export const strapiPaddingOptions = ["auto", "0", "40", "80"] as const;
 
-export const StrapiPaddingSchema = z.enum(strapiPaddingOptions);
-
 export const StrapiPaddingOptionalSchema = z
   .enum(strapiPaddingOptions)
   .nullable()
--- app/services/cms/models/__test__/StrapiPadding.test.ts
+++ app/services/cms/models/__test__/StrapiPadding.test.ts
@@ -1,9 +1,9 @@
-import { strapiPaddingOptions, StrapiPaddingSchema } from "../StrapiPadding";
+import { strapiPaddingOptions, StrapiPaddingOptionalSchema } from "../StrapiPadding";
 
 describe("StrapiPadding", () => {
   it("converts all valid options", () => {
     strapiPaddingOptions.forEach((option) => {
-      const parsed = StrapiPaddingSchema.safeParse(option);
+      const parsed = StrapiPaddingOptionalSchema.safeParse(option);
       expect(parsed.success).toBe(true);
     });
   });
