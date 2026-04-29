d9e147eec52479bf0009b550f7160689b642e32e
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-04-29 17:36:00 +0200

    add 80px padding for components and add padding to user feedback component

--- app/services/cms/models/StrapiPadding.ts
+++ app/services/cms/models/StrapiPadding.ts
@@ -1,37 +1,8 @@
 import { z } from "zod";
 import { omitNull } from "~/util/omitNull";
 
-export const strapiPaddingOptions = [
-  "default",
-  "px0",
-  "px8",
-  "px16",
-  "px24",
-  "px32",
-  "px40",
-  "px48",
-  "px56",
-  "px64",
-] as const;
 
-const strapiPaddingMap = {
-  default: "default",
-  px0: "0",
-  px8: "8",
-  px16: "16",
-  px24: "24",
-  px32: "32",
-  px40: "40",
-  px48: "48",
-  px56: "56",
-  px64: "64",
-} as const satisfies Record<(typeof strapiPaddingOptions)[number], string>;
-
-const paddingOptions = ["auto", "0", "40"] as const;
-
-export const StrapiPaddingSchema = z
-  .enum(strapiPaddingOptions)
-  .transform((val) => strapiPaddingMap[val]);
+const paddingOptions = ["auto", "0", "40", "80"] as const;
 
 export const StrapiPaddingOptionalSchema = z
   .enum(paddingOptions)
