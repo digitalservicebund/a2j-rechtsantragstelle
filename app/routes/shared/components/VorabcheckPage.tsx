fece77a5dbfc385082f263e8da05d859b96433d0
Ingrid <46021179+ingriddorio@users.noreply.github.com>
2026-04-29 11:19:52 +0200

    refactor(KERN): remove legacy design validated formflow (#3628)

--- app/routes/shared/components/VorabcheckPage.tsx
+++ app/routes/shared/components/VorabcheckPage.tsx
@@ -8,7 +8,7 @@ import { GridItem } from "~/components/layout/grid/GridItem";
 import classNames from "classnames";
 import { KernProgress } from "~/components/kern/KernProgressBar";
 import { KernReportProblem } from "~/components/kern/KernReportProblem";
-import KernValidatedFlowForm from "~/components/kernFormElements/KernValidatedFormFlow";
+import KernValidatedFlowForm from "~/components/formElements/KernValidatedFormFlow";
 
 export function VorabcheckPage() {
   const {
