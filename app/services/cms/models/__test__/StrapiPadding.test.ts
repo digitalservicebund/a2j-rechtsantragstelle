161e3340c192d20845ca63fa4b2cf835117132b2
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-04-30 11:34:39 +0200

    change test description

--- app/services/cms/models/__test__/StrapiPadding.test.ts
+++ app/services/cms/models/__test__/StrapiPadding.test.ts
@@ -4,10 +4,15 @@ import {
 } from "../StrapiPadding";
 
 describe("StrapiPadding", () => {
-  it("converts all valid options", () => {
+  it("accepts all valid padding values", () => {
     strapiPaddingOptions.forEach((option) => {
       const parsed = StrapiPaddingOptionalSchema.safeParse(option);
       expect(parsed.success).toBe(true);
     });
+  });
+
+  it("rejects invalid padding values", () => {
+    const parsed = StrapiPaddingOptionalSchema.safeParse("invalid");
+    expect(parsed.success).toBe(false);
   });
 });
