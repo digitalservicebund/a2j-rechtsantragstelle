a20282a7340aaba73f8a4a6fafb99aa39b2fefcf
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-05-13 15:56:48 +0200

    fix(KERN): remove items center

--- app/components/formElements/inputs/error/InputError.tsx
+++ app/components/formElements/inputs/error/InputError.tsx
@@ -9,14 +9,14 @@ const InputError = ({ id, children }: InputErrorProps) => {
   if (!children) return null;
   return (
     <p
-      className="kern-error flex! items-center! flex-none!"
+      className="kern-error flex! flex-none!"
       data-testid="inputError"
       id={id}
       role="alert"
     >
       <Icon
         name="emergency-home"
-        className="w-[1.2em]! h-[1.2em]! fill-kern-feedback-danger! forced-color-adjust-auto"
+        className="w-[1.2em]! h-[1.2em]! mt-2! fill-kern-feedback-danger! forced-color-adjust-auto"
       />
       <span className="text-kern-feedback-danger!">{children}</span>
     </p>
