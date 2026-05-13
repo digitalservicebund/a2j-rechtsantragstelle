bdf9e25b98ca0492bb858f40f97f9a1c2e46df30
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-05-13 15:39:22 +0200

    fix(KERN): run format

--- app/components/formElements/inputs/error/InputError.tsx
+++ app/components/formElements/inputs/error/InputError.tsx
@@ -8,7 +8,12 @@ type InputErrorProps = PropsWithChildren<{
 const InputError = ({ id, children }: InputErrorProps) => {
   if (!children) return null;
   return (
-    <p className="kern-error flex! items-center! flex-none!" data-testid="inputError" id={id} role="alert">
+    <p
+      className="kern-error flex! items-center! flex-none!"
+      data-testid="inputError"
+      id={id}
+      role="alert"
+    >
       <Icon
         name="emergency-home"
         className="w-[1.2em]! h-[1.2em]! fill-kern-feedback-danger! forced-color-adjust-auto"
