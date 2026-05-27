7c90ccff85f3a22da760198da7c8d2218a0da318
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-05-27 15:00:59 +0200

    refactor(createHeaderAndSubject): fix list and make bullet points

--- app/domains/nachlass/erbausschlagung/anfrage/services/pdf/sections/createHeaderAndSubject.ts
+++ app/domains/nachlass/erbausschlagung/anfrage/services/pdf/sections/createHeaderAndSubject.ts
@@ -71,12 +71,12 @@ const MAIN_TITLE = "Datenblatt zur Vorbereitung einer Erbausschlagung";
 const SUBJECT_TEXT =
   "Sehr geehrte Damen und Herren, \nmit diesem Dokument übermittle ich Ihnen meine Daten zur Vorbereitung der Erbausschlagung.\nMir ist bewusst:";
 
-const TEXT_BULLETS = {
-  "1. ": "Dieses Dokument ist keine rechtswirksame Erbausschlagung.",
-  "2. ":
-    "Die Frist (normalerweise 6 Wochen) wird hierdurch nicht unterbrochen.",
-  "3. ": "Ich muss für die Ausschlagung persönlich beim Gericht erscheinen.",
-};
+const TEXT_BULLETS = [
+  "• Dieses Dokument ist keine rechtswirksame Erbausschlagung.",
+  "• Die Frist (normalerweise 6 Wochen) wird hierdurch nicht unterbrochen.",
+  "• Ich muss für die Ausschlagung persönlich beim Gericht erscheinen.",
+];
+
 const APPOINTMENT_TEXT =
   "Ich mich zur Terminabsprache mit Ihnen in Verbindung setzen, falls noch nicht geschehen.";
 const BEST_REGARDS_TEXT = "Mit freundlichen Grüßen";
@@ -107,22 +107,21 @@ export const createHeaderAndSubject = (
     }),
   );
 
-  for (const [bullet, claim] of Object.entries(TEXT_BULLETS)) {
+  const subjectList = doc.struct("L");
+
+  for (const [index, textBullet] of TEXT_BULLETS.entries()) {
     const subjectListItem = doc.struct("LI");
     subjectListItem.add(
       doc.struct("LBody", {}, () => {
         doc
-          .font(FONTS_BUNDESSANS_BOLD)
-          .text(bullet, PDF_MARGIN_HORIZONTAL + 10, undefined, {
-            continued: true,
-          })
-          .font(FONTS_BUNDESSANS_REGULAR)
-          .text(claim);
-        doc.moveDown(0.5);
+          .font(index === 0 ? FONTS_BUNDESSANS_BOLD : FONTS_BUNDESSANS_REGULAR)
+          .text(textBullet, PDF_MARGIN_HORIZONTAL + 10);
       }),
     );
-    titleSubjectSection.add(subjectListItem);
+    subjectList.add(subjectListItem);
   }
+
+  titleSubjectSection.add(subjectList);
 
   titleSubjectSection.add(
     doc.struct("P", {}, () => {
@@ -132,7 +131,8 @@ export const createHeaderAndSubject = (
         .text(BEST_REGARDS_TEXT)
         .text(
           `${userData.ausschlagendePersonVorname ?? ""} ${userData.ausschlagendePersonNachname}`,
-        );
+        )
+        .moveDown(2);
     }),
   );
 
