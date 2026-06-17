99d4f59eb0aa22660ebedc6a4fd2454f2879b163
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-06-17 10:20:55 +0200

    feat(Erbausschlagung Court Finden): Build Vorabcheck through Pflegeheim

--- app/domains/nachlass/erbausschlagung/gericht-finden/pages.ts
+++ app/domains/nachlass/erbausschlagung/gericht-finden/pages.ts
@@ -1,7 +1,46 @@
+import { z } from "zod";
 import type { PagesConfig } from "~/domains/pageSchemas";
+import { germanHouseNumberSchema } from "~/services/validation/germanHouseNumber";
+import { postcodeSchema } from "~/services/validation/postcode";
+import { stringRequiredSchema } from "~/services/validation/stringRequired";
+import { YesNoAnswer } from "~/services/validation/YesNoAnswer";
 
 export const nachlassErbausschlagungGerichtFindenPages = {
   start: {
     stepId: "start",
+  },
+  lebensmittelpunkt: {
+    stepId: "lebensmittelpunkt",
+    pageSchema: {
+      lebensmittelpunkt: z.enum(["deutschland", "ausland"]),
+    },
+  },
+  plz: {
+    stepId: "plz",
+    pageSchema: {
+      plz: postcodeSchema,
+    },
+  },
+  strasseHausnummer: {
+    stepId: "verschiedene-zustaendige-gerichte",
+    pageSchema: {
+      strasse: stringRequiredSchema,
+      hausnummer: germanHouseNumberSchema,
+    },
+  },
+  ausschlagungsOrt: {
+    stepId: "wo-ausschlagen",
+    pageSchema: {
+      ausschlagungsOrt: z.enum(["courtNearMe", "courtNearDeceased"]),
+    },
+  },
+  pflegeheim: {
+    stepId: "pflegeheim",
+    pageSchema: {
+      pflegeheim: YesNoAnswer,
+    },
+  },
+  gerichtErmitteltWohnsitz: {
+    stepId: "ergebnis/gericht-ermittelt-wohnsitz",
   },
 } as const satisfies PagesConfig;
