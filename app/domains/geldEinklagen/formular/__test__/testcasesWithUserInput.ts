9fc131ccc94a5490bdf94d5cc81dbbd5fc12e71d
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-06-29 11:04:09 +0200

    test: add test for the sachgebiet section

--- app/domains/geldEinklagen/formular/__test__/testcasesWithUserInput.ts
+++ app/domains/geldEinklagen/formular/__test__/testcasesWithUserInput.ts
@@ -3,12 +3,14 @@ import { type GeldEinklagenFormularUserData } from "../userData";
 import { geldEinklagenFlowConfig } from "../flowConfig";
 import { geldEinklagenFormular } from "..";
 import { testCasesWithUserInputIntroForderung } from "../gericht-pruefen/__test__/testCasesWithUserInput";
+import { testCasesWithUserInputSachgebiet } from "../gericht-pruefen/sachgebiet/__test__/testCasesWithUserInput";
 
 export const geldEinklagenFormularTestCases = {
   xstateConfig: geldEinklagenFormular.config,
   newEngineConfig: geldEinklagenFlowConfig,
   testcases: {
     ...testCasesWithUserInputIntroForderung,
+    ...testCasesWithUserInputSachgebiet,
   },
 } satisfies FlowTestConfig<
   GeldEinklagenFormularUserData,
