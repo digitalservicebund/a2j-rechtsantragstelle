2f5cd78b81b6eafea1b7ad7461ae8b080905984f
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-07-28 10:33:26 +0200

    fix(Card): revert fetchPage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
+import { fetchPage } from "~/services/cms/index.server";
 import Homepage from "./shared/components/Homepage";
 
 export const loader = async () => {
-  return { pageMeta: { title: "Justiz-Services" } };
+  const { pageMeta, ...props } = await fetchPage("/");
+  return { meta: pageMeta, ...props };
 };
 
 export default function Index() {
