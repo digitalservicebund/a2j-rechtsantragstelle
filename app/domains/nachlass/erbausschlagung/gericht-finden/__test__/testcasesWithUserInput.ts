1c2e1cc2d43d2863424e6d0306fd2d528568c7fe
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-08-05 17:21:38 +0200

    refactor(Nachlass Gericht Finden): migrate to new flow engine

--- app/domains/nachlass/erbausschlagung/gericht-finden/__test__/testcasesWithUserInput.ts
+++ app/domains/nachlass/erbausschlagung/gericht-finden/__test__/testcasesWithUserInput.ts
@@ -2,11 +2,12 @@ import type {
   FlowTestCases,
   FlowTestConfig,
 } from "~/domains/__test__/TestCases";
+import { nachlassErbausschlagungGerichtFindenFlowConfig } from "../flowConfig";
 import type { NachlassErbausschlagungGerichtFindenUserData } from "../userData";
-import { nachlassErbausschlagungGerichtFindenXstateConfig } from "~/domains/nachlass/erbausschlagung/gericht-finden/xStateConfig";
 
 export const nachlassErbausschlagungGerichtFindenTestCases = {
-  xstateConfig: nachlassErbausschlagungGerichtFindenXstateConfig,
+  xstateConfig: { id: "/nachlass/erbausschlagung/gericht-finden" },
+  newEngineConfig: nachlassErbausschlagungGerichtFindenFlowConfig,
   testcases: {
     auslandLebensmittelpunkt: [
       {
@@ -351,5 +352,8 @@ export const nachlassErbausschlagungGerichtFindenTestCases = {
         stepId: "/ergebnis/gericht-ermittelt",
       },
     ],
-  } satisfies FlowTestCases<NachlassErbausschlagungGerichtFindenUserData>,
-} satisfies FlowTestConfig<NachlassErbausschlagungGerichtFindenUserData>;
+  },
+} satisfies FlowTestConfig<
+  NachlassErbausschlagungGerichtFindenUserData,
+  typeof nachlassErbausschlagungGerichtFindenFlowConfig.pages
+>;
