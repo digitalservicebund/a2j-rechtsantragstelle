813fec9af59ed55fd6efa181359ab5a7ecd3c8f4
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-08-07 14:42:44 +0200

    refactor: restructure the subfolder and add tests

--- app/domains/geldEinklagen/services/pdf/sections/reason/factsOfCase/addWitnessOfCase.ts
+++ app/domains/geldEinklagen/services/pdf/sections/reason/factsOfCase/addWitnessOfCase.ts
@@ -7,50 +7,15 @@ import {
 } from "~/services/pdf/createPdfKitDocument";
 import { arrayIsNonEmpty } from "~/util/array";
 
-const FACTS_OF_CASES_TEXT = "I. Sachverhalt";
 const bulletText = "•  ";
-const MARGIN_RIGHT_SPACE = PDF_MARGIN_HORIZONTAL + MARGIN_RIGHT;
+export const MARGIN_RIGHT_SPACE = PDF_MARGIN_HORIZONTAL + MARGIN_RIGHT;
 
 const WITNESS_TEXT =
   "Beweis angeboten durch Vernehmung der folgenden Personen als Zeugen oder Zeuginnen:";
 const PARTY_WITNESS_TEXT =
   "Beweis angeboten durch Parteivernehmung der folgenden Personen:";
 
-const addDocumentsFactsOfCase = (
-  doc: PDFKit.PDFDocument,
-  factsOfCasesSect: PDFKit.PDFStructureElement,
-  dokumenten: Exclude<
-    GeldEinklagenFormularUserData["abschnitte"],
-    undefined
-  >[number]["dokumenten"],
-  currentDocumentIndex: number,
-) => {
-  if (!arrayIsNonEmpty(dokumenten)) {
-    return;
-  }
-
-  for (const dokument of dokumenten) {
-    factsOfCasesSect.add(
-      doc.struct("P", {}, () => {
-        doc
-          .font(FONTS_BUNDESSANS_BOLD)
-          .text(
-            `Beweis K${currentDocumentIndex + 1}: `,
-            MARGIN_RIGHT_SPACE,
-            undefined,
-            {
-              continued: true,
-            },
-          )
-          .font(FONTS_BUNDESSANS_REGULAR)
-          .text(dokument.beschreibung ?? "")
-          .moveDown(1);
-      }),
-    );
-  }
-};
-
-const addWitnessOfCase = (
+export const addWitnessOfCase = (
   doc: PDFKit.PDFDocument,
   factsOfCasesSect: PDFKit.PDFStructureElement,
   personen: Exclude<
@@ -122,54 +87,4 @@ const addWitnessOfCase = (
     factsOfCasesSect.add(witnessList);
     doc.moveDown(1);
   }
-};
-
-export const addFactsOfCases = (
-  doc: PDFKit.PDFDocument,
-  reasonSect: PDFKit.PDFStructureElement,
-  { abschnitte }: GeldEinklagenFormularUserData,
-) => {
-  if (!arrayIsNonEmpty(abschnitte)) {
-    return;
-  }
-
-  const factsOfCasesSect = doc.struct("Sect");
-
-  factsOfCasesSect.add(
-    doc.struct("H3", {}, () => {
-      doc
-        .fontSize(14)
-        .font(FONTS_BUNDESSANS_BOLD)
-        .text(FACTS_OF_CASES_TEXT)
-        .moveDown(1);
-    }),
-  );
-
-  reasonSect.add(factsOfCasesSect);
-
-  let currentDocumentIndex = 0;
-
-  for (const abschnitt of abschnitte) {
-    factsOfCasesSect.add(
-      doc.struct("P", {}, () => {
-        doc
-          .fontSize(10)
-          .font(FONTS_BUNDESSANS_REGULAR)
-          .text(abschnitt.beschreibung, PDF_MARGIN_HORIZONTAL, undefined)
-          .moveDown(1);
-
-        addDocumentsFactsOfCase(
-          doc,
-          factsOfCasesSect,
-          abschnitt.dokumenten,
-          currentDocumentIndex,
-        );
-        addWitnessOfCase(doc, factsOfCasesSect, abschnitt.personen);
-
-        currentDocumentIndex += abschnitt.dokumenten?.length ?? 0;
-      }),
-    );
-  }
-
-  doc.moveDown(1.5);
 };
