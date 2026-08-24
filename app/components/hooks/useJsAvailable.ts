2faba71633a500a6cc449405c056b7ba54df0c94
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-08-24 16:22:25 +0200

    refactor: disable oxlint rule

--- app/components/hooks/useJsAvailable.ts
+++ app/components/hooks/useJsAvailable.ts
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 export function useJsAvailable() {
   const [isJsAvailable, setIsJsAvailable] = useState(false);
+  // oxlint-disable-next-line react/set-state-in-effect
   useEffect(() => setIsJsAvailable(true), []);
   return isJsAvailable;
 }
