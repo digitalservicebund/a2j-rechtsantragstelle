d1b8d7923ca291fe05f8a998b8cb96619116c34c
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-08-31 13:12:14 +0200

    refactor(useFocusFirstH1): rename variable

--- app/components/hooks/useFocusFirstH1.tsx
+++ app/components/hooks/useFocusFirstH1.tsx
@@ -5,10 +5,10 @@ const HEADING_ONE_TAG = "h1";
 export const useFocusFirstH1 = () => {
   useEffect(() => {
     const focusTimer = setTimeout(() => {
-      const hashUrl = document.location.hash;
+      const hasAnchorHash = document.location.hash;
       const headingOneTag = document.querySelector(HEADING_ONE_TAG);
 
-      if (headingOneTag && !hashUrl) {
+      if (headingOneTag && !hasAnchorHash) {
         headingOneTag.setAttribute("tabindex", "-1");
         headingOneTag.focus();
       }
