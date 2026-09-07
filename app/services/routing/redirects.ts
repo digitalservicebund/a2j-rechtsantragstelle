d1753a10797a709cb0a16dba8e9cea6bffed3cd6
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-09-07 16:30:16 +0200

    refactor: add routes and redirected

--- app/services/routing/redirects.ts
+++ app/services/routing/redirects.ts
@@ -9,6 +9,11 @@ export const redirectMap = {
     "/erbschein/nachlassgericht/start",
   "/nachlass/erbschein/wegweiser/start": "/erbschein/wegweiser/start",
   "/nachlass/erbschein": "/erbschein",
+  "/nachlass/erbausschlagung": "/erbausschlagung",
+  "/nachlass/erbausschlagung/gericht-finden/start":
+    "/erbausschlagung/gericht-finden/start",
+  "/nachlass/erbausschlagung/anfrage/start/start":
+    "/erbausschlagung/anfrage/start/start",
 } as const;
 
 export const getRedirect = (pathname: string) =>
