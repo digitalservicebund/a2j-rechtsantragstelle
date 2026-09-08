b801f27686ca034d326333ddf82d1790b951f7b7
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-09-08 14:45:58 +0200

    doc(extraLoaderConfiguration): add comment

--- app/domains/extraLoaderConfiguration.ts
+++ app/domains/extraLoaderConfiguration.ts
@@ -14,8 +14,8 @@ export type FlowExtras<
 };
 
 const extraFlowFeaturesById: Partial<Record<FlowId, FlowExtras>> = {
-  "/nachlass/erbschein/erbfolge": erbfolgeExtras,
-  "/geld-einklagen/formular": geldEinklageFormularExtras,
+  "/nachlass/erbschein/erbfolge": erbfolgeExtras, // Delete after /nachlass migration
+  "/geld-einklagen/formular": geldEinklageFormularExtras, // Delete after /nachlass migration
   "/nachlass/erbschein/anfrage": erbfolgeExtras,
   "/erbschein/erbfolge": erbfolgeExtras,
   "/erbschein/anfrage": erbfolgeExtras,
