2531fb35b789faa357d4efc5e51979a79c6681ce
Mostafa Elgaafary <mostafa.elgaafary@digitalservice.bund.de>
2026-09-08 10:24:33 +0200

    migrate andereUnterhaltszahlungen

--- app/domains/beratungshilfe/formular/finanzielleAngaben/andereUnterhaltszahlungen/userData.ts
+++ app/domains/beratungshilfe/formular/finanzielleAngaben/andereUnterhaltszahlungen/userData.ts
@@ -1,7 +1,13 @@
+import { type z } from "zod";
 import { type UserDataFromPagesSchema } from "~/domains/pageSchemas";
-import { type berhAntragFinanzielleAngabenAndereUnterhaltszahlungenPages } from "./pages";
+import {
+  type berhAntragFinanzielleAngabenAndereUnterhaltszahlungenPages,
+  type unterhaltszahlungenArraySchema,
+} from "./pages";
 
 export type BeratungshilfeFinanzielleAngabenAndereUnterhaltszahlungenUserData =
   UserDataFromPagesSchema<
     typeof berhAntragFinanzielleAngabenAndereUnterhaltszahlungenPages
-  >;
+  > & {
+    unterhaltszahlungen?: z.infer<typeof unterhaltszahlungenArraySchema>;
+  };
