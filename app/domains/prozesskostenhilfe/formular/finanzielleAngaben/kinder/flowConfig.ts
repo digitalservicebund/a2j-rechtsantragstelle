a5fb70ff7a1b02b8f7f444b112d59c29eacad1df
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-14 11:03:54 +0200

    refactor(PKH): migrate kinder to new flow engine

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/kinder/flowConfig.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/kinder/flowConfig.ts
@@ -1,11 +1,31 @@
 import { type TransitionConfigMap } from "~/services/flow/newFlowEngine/types";
 import { prozesskostenhilfeFormularPages } from "../../pages";
+import { arrayIsNonEmpty } from "~/util/array";
 
 export const kinderFlowConfig = {
-  kinderFrage: [],
-  kinderUebersicht: [],
-  kinderWarnung: [],
-  kinder: [],
+  kinderFrage: [
+    {
+      guard: (context) => context.hasKinder === "yes",
+      target: "kinderUebersicht",
+    },
+    { target: "andereUnterhaltszahlungenFrage" },
+  ],
+  kinderUebersicht: [
+    {
+      type: "addArrayItem",
+      target: "name",
+    },
+    {
+      guard: (context) =>
+        context.hasKinder === "yes" && !arrayIsNonEmpty(context.kinder),
+      target: "kinderWarnung",
+    },
+    { target: "andereUnterhaltszahlungenFrage" },
+  ],
+  kinderWarnung: "andereUnterhaltszahlungenFrage",
+  kinder: [
+    
+  ],
 } satisfies Partial<
   TransitionConfigMap<typeof prozesskostenhilfeFormularPages>
 >;
