f77534c8aadbab6bb98141249b45ae3f762875f9
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-15 12:49:04 +0200

    refactor(PKH): migrate andereUnterhaltszahlung to new flow engine

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/andere-unterhaltszahlungen/flowConfig.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/andere-unterhaltszahlungen/flowConfig.ts
@@ -1,11 +1,29 @@
 import { type TransitionConfigMap } from "~/services/flow/newFlowEngine/types";
 import { prozesskostenhilfeFormularPages } from "../../pages";
+import { hasWeitereUnterhaltszahlungenYesAndEmptyArray } from "../guards";
 
 export const andereUnterhaltszahlungenFlowConfig = {
-  andereUnterhaltszahlungenFrage: [],
-  andereUnterhaltszahlungenUebersicht: [],
-  andereUnterhaltszahlungenWarnung: [],
-  andereUnterhaltszahlungenPerson: [],
+  andereUnterhaltszahlungenFrage: [
+    {
+      guard: (context) => context.hasWeitereUnterhaltszahlungen === "yes",
+      target: "andereUnterhaltszahlungenUebersicht",
+    },
+    { target: "wohnungAlleineZusammen" },
+  ],
+  andereUnterhaltszahlungenUebersicht: [
+    {
+      type: "addArrayItem",
+      target: "andereUnterhaltszahlungenPerson"
+    },
+    {
+      guard: (context) =>
+        hasWeitereUnterhaltszahlungenYesAndEmptyArray({ context }),
+      target: "andereUnterhaltszahlungenWarnung",
+    },
+    { target: "wohnungAlleineZusammen" },
+  ],
+  andereUnterhaltszahlungenWarnung: "wohnungAlleineZusammen",
+  andereUnterhaltszahlungenPerson: "andereUnterhaltszahlungenUebersicht",
 } satisfies Partial<
   TransitionConfigMap<typeof prozesskostenhilfeFormularPages>
 >;
