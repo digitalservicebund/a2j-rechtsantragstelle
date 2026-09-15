9fb678f40634fbe4f15daefefd212fff0187088f
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-15 15:13:11 +0200

    refactor(PKH): migrate eigentum bankkonto and geldAnlage to new flow engine

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/eigentum/pages.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/eigentum/pages.ts
@@ -181,138 +181,168 @@ const wertsachenArraySchema = z.array(wertsacheSchema).min(1);
 export const pkhFormularFinanzielleAngabenEigentumPages = {
   eigentumInfo: {
     stepId: "/finanzielle-angaben/eigentum/eigentum-info",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumHeiratInfo: {
     stepId: "/finanzielle-angaben/eigentum/heirat-info",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumBankkonten: {
     stepId: "/finanzielle-angaben/eigentum/bankkonten",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumBankkontenFrage: {
     stepId: "/finanzielle-angaben/eigentum/bankkonten/bankkonten-frage",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       hasBankkonto: YesNoAnswer,
     },
   },
   eigentumBankkontenUebersicht: {
     stepId: "/finanzielle-angaben/eigentum/bankkonten/uebersicht",
+    shouldCollapseIntoParentNavItem: true,
+    arraySummary: {
+      name: "bankkonten",
+      schema: bankkontenArraySchema,
+      fieldName: "hasBankkonto",
+    },
   },
   eigentumBankkonto: {
-    stepId: "/finanzielle-angaben/eigentum/bankkonten/bankkonto",
-    pageSchema: { bankkonten: bankkontenArraySchema },
-    arrayPages: {
-      daten: {
-        pageSchema: {
-          "bankkonten#kontoEigentuemer":
-            bankkontenArraySchema.element.shape.kontoEigentuemer,
-          "bankkonten#bankName": bankkontenArraySchema.element.shape.bankName,
-          "bankkonten#kontostand":
-            bankkontenArraySchema.element.shape.kontostand,
-          "bankkonten#iban": bankkontenArraySchema.element.shape.iban,
-          "bankkonten#kontoDescription":
-            bankkontenArraySchema.element.shape.kontoDescription,
-        },
-      },
+    stepId: "/finanzielle-angaben/eigentum/bankkonten/bankkonto/#/daten",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "bankkonten#kontoEigentuemer":
+        bankkontenArraySchema.element.shape.kontoEigentuemer,
+      "bankkonten#bankName": bankkontenArraySchema.element.shape.bankName,
+      "bankkonten#kontostand": bankkontenArraySchema.element.shape.kontostand,
+      "bankkonten#iban": bankkontenArraySchema.element.shape.iban,
+      "bankkonten#kontoDescription":
+        bankkontenArraySchema.element.shape.kontoDescription,
     },
   },
   eigentumBankkontoWarnung: {
     stepId: "/finanzielle-angaben/eigentum/bankkonten/warnung",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumGeldanlagen: {
     stepId: "/finanzielle-angaben/eigentum/geldanlagen",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumGeldanlagenFrage: {
     stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlagen-frage",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       hasGeldanlage: YesNoAnswer,
     },
   },
   eigentumGeldanlagenUebersicht: {
     stepId: "/finanzielle-angaben/eigentum/geldanlagen/uebersicht",
+    shouldCollapseIntoParentNavItem: true,
+    arraySummary: {
+      name: "geldanlagen",
+      schema: geldanlagenArraySchema,
+      fieldName: "hasGeldanlage",
+    },
   },
   eigentumGeldanlage: {
     stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage",
+    shouldCollapseIntoParentNavItem: true,
+  },
+  eigentumGeldanlageArt: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/art",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
-      geldanlagen: geldanlagenArraySchema,
+      "geldanlagen#art": geldanlagenArtSchema,
     },
-    arrayPages: {
-      art: {
-        pageSchema: {
-          "geldanlagen#art": geldanlagenArtSchema,
-        },
-      },
-      bargeld: {
-        pageSchema: {
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-        },
-      },
-      wertpapiere: {
-        pageSchema: {
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-        },
-      },
-      "guthabenkonto-krypto": {
-        pageSchema: {
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-        },
-      },
-      "giro-tagesgeld-sparkonto": {
-        pageSchema: {
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-          "geldanlagen#kontoBankName": sparkontoSchema.shape.kontoBankName,
-          "geldanlagen#kontoIban": sparkontoSchema.shape.kontoIban,
-          "geldanlagen#kontoBezeichnung":
-            sparkontoSchema.shape.kontoBezeichnung,
-        },
-      },
-      befristet: {
-        pageSchema: {
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-          "geldanlagen#befristetArt": befristetSchema.shape.befristetArt,
-          "geldanlagen#verwendungszweck":
-            befristetSchema.shape.verwendungszweck,
-          "geldanlagen#auszahlungdatum": befristetSchema.shape.auszahlungdatum,
-        },
-      },
-      forderung: {
-        pageSchema: {
-          "geldanlagen#forderung": forderungSchema.shape.forderung,
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-        },
-      },
-      sonstiges: {
-        pageSchema: {
-          "geldanlagen#verwendungszweck":
-            sonstigesSchema.shape.verwendungszweck,
-          "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
-          "geldanlagen#wert": sharedGeldanlagenFields.wert,
-        },
-      },
+  },
+  eigentumGeldanlageBargeld: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/bargeld",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+    },
+  },
+  eigentumGeldanlageWertpapiere: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/wertpapiere",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+    },
+  },
+  eigentumGeldanlageGuthabenkontoKrypto: {
+    stepId:
+      "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/guthabenkonto-krypto",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+    },
+  },
+  eigentumGeldanlageGiroTagesgeldSparkonto: {
+    stepId:
+      "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/giro-tagesgeld-sparkonto",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+      "geldanlagen#kontoBankName": sparkontoSchema.shape.kontoBankName,
+      "geldanlagen#kontoIban": sparkontoSchema.shape.kontoIban,
+      "geldanlagen#kontoBezeichnung": sparkontoSchema.shape.kontoBezeichnung,
+    },
+  },
+  eigentumGeldanlageBefristet: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/befristet",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+      "geldanlagen#befristetArt": befristetSchema.shape.befristetArt,
+      "geldanlagen#verwendungszweck": befristetSchema.shape.verwendungszweck,
+      "geldanlagen#auszahlungdatum": befristetSchema.shape.auszahlungdatum,
+    },
+  },
+  eigentumGeldanlageForderung: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/forderung",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#forderung": forderungSchema.shape.forderung,
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
+    },
+  },
+  eigentumGeldanlageSonstiges: {
+    stepId: "/finanzielle-angaben/eigentum/geldanlagen/geldanlage/#/sonstiges",
+    shouldCollapseIntoParentNavItem: true,
+    pageSchema: {
+      "geldanlagen#verwendungszweck": sonstigesSchema.shape.verwendungszweck,
+      "geldanlagen#eigentuemer": sharedGeldanlagenFields.eigentuemer,
+      "geldanlagen#wert": sharedGeldanlagenFields.wert,
     },
   },
   eigentumGeldanlagenWarnung: {
     stepId: "/finanzielle-angaben/eigentum/geldanlagen/warnung",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumKraftfahrzeuge: {
     stepId: "/finanzielle-angaben/eigentum/kraftfahrzeuge",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumKraftfahrzeugeFrage: {
     stepId: "/finanzielle-angaben/eigentum/kraftfahrzeuge/kraftfahrzeuge-frage",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       hasKraftfahrzeug: YesNoAnswer,
     },
   },
   eigentumKraftfahrzeugeUebersicht: {
     stepId: "/finanzielle-angaben/eigentum/kraftfahrzeuge/uebersicht",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumKraftfahrzeug: {
     stepId: "/finanzielle-angaben/eigentum/kraftfahrzeuge/kraftfahrzeug",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       kraftfahrzeuge: kraftfahrzeugeArraySchema,
     },
@@ -349,22 +379,27 @@ export const pkhFormularFinanzielleAngabenEigentumPages = {
   },
   eigentumKraftfahrzeugeWarnung: {
     stepId: "/finanzielle-angaben/eigentum/kraftfahrzeuge/warnung",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumWertgegenstaende: {
     stepId: "/finanzielle-angaben/eigentum/wertgegenstaende",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumWertgegenstaendeFrage: {
     stepId:
       "/finanzielle-angaben/eigentum/wertgegenstaende/wertgegenstaende-frage",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       hasWertsache: YesNoAnswer,
     },
   },
   eigentumWertgegenstaendeUebersicht: {
     stepId: "/finanzielle-angaben/eigentum/wertgegenstaende/uebersicht",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumWertgegenstand: {
     stepId: "/finanzielle-angaben/eigentum/wertgegenstaende/wertgegenstand",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       wertsachen: wertsachenArraySchema,
     },
@@ -380,21 +415,26 @@ export const pkhFormularFinanzielleAngabenEigentumPages = {
   },
   eigentumWertgegenstaendeWarnung: {
     stepId: "/finanzielle-angaben/eigentum/wertgegenstaende/warnung",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumGrundeigentum: {
     stepId: "/finanzielle-angaben/eigentum/grundeigentum",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumGrundeigentumFrage: {
     stepId: "/finanzielle-angaben/eigentum/grundeigentum/grundeigentum-frage",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       hasGrundeigentum: YesNoAnswer,
     },
   },
   eigentumGrundeigentumUebersicht: {
     stepId: "/finanzielle-angaben/eigentum/grundeigentum/uebersicht",
+    shouldCollapseIntoParentNavItem: true,
   },
   eigentumGrundeigentumGrundeigentum: {
     stepId: "/finanzielle-angaben/eigentum/grundeigentum/grundeigentum",
+    shouldCollapseIntoParentNavItem: true,
     pageSchema: {
       grundeigentum: grundeigentumArraySchema,
     },
@@ -428,5 +468,6 @@ export const pkhFormularFinanzielleAngabenEigentumPages = {
   },
   eigentumGrundeigentumWarnung: {
     stepId: "/finanzielle-angaben/eigentum/grundeigentum/warnung",
+    shouldCollapseIntoParentNavItem: true,
   },
 } as const satisfies PagesConfig;
