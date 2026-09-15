c75bbe29b59cd94bfc2d19d35130a325bbc26376
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-15 14:03:59 +0200

    refactor(PKH): migrate wohnung to new flow engine

--- app/domains/prozesskostenhilfe/formular/finanzielleAngaben/wohnung/flowConfig.ts
+++ app/domains/prozesskostenhilfe/formular/finanzielleAngaben/wohnung/flowConfig.ts
@@ -2,17 +2,42 @@ import { type TransitionConfigMap } from "~/services/flow/newFlowEngine/types";
 import { prozesskostenhilfeFormularPages } from "../../pages";
 
 export const wohnungFlowConfig = {
-  wohnungAlleineZusammen: [],
-  wohnungAnzahlMitbewohner: [],
-  wohnungGroesse: [],
-  wohnungAnzahlZimmer: [],
-  wohnungMieteEigenheim: [],
-  wohnungMieteAlleine: [],
-  wohnungMieteZusammen: [],
-  wohnungGarageParkplatz: [],
-  wohnungNebenkosten: [],
-  wohnungEigenheimNebenkosten: [],
-  wohnungEigenheimNebenkostenGeteilt: [],
+  wohnungAlleineZusammen: [
+    {
+      guard: (context) => context.livingSituation === "alone",
+      target: "wohnungGroesse",
+    },
+    { target: "wohnungAnzahlMitbewohner" },
+  ],
+  wohnungAnzahlMitbewohner: "wohnungGroesse",
+  wohnungGroesse: "wohnungAnzahlZimmer",
+  wohnungAnzahlZimmer: "wohnungMieteEigenheim",
+  wohnungMieteEigenheim: [
+    {
+      guard: (context) =>
+        context.rentsApartment === "yes" && context.livingSituation === "alone",
+      target: "wohnungMieteAlleine",
+    },
+    {
+      guard: (context) =>
+        context.rentsApartment === "no" && context.livingSituation === "alone",
+      target: "wohnungEigenheimNebenkosten",
+    },
+    {
+      guard: (context) =>
+        context.rentsApartment === "no" &&
+        (context.livingSituation === "withOthers" ||
+          context.livingSituation === "withRelatives"),
+      target: "wohnungEigenheimNebenkostenGeteilt",
+    },
+    { target: "wohnungMieteZusammen" },
+  ],
+  wohnungMieteAlleine: "wohnungGarageParkplatz",
+  wohnungMieteZusammen: "wohnungGarageParkplatz",
+  wohnungGarageParkplatz: "wohnungNebenkosten",
+  wohnungNebenkosten: "eigentumInfo",
+  wohnungEigenheimNebenkosten: "eigentumInfo",
+  wohnungEigenheimNebenkostenGeteilt: "eigentumInfo",
 } satisfies Partial<
   TransitionConfigMap<typeof prozesskostenhilfeFormularPages>
 >;
