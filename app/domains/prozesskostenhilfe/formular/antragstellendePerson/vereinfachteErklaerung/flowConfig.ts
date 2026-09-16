6af7df87f1fdb1dd2f237b79542bd5b5ef61828c
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-16 14:32:14 +0200

    refactor(PKH): migrate vereinfachterErklaerung to new flow engine and add config to array

--- app/domains/prozesskostenhilfe/formular/antragstellendePerson/vereinfachteErklaerung/flowConfig.ts
+++ app/domains/prozesskostenhilfe/formular/antragstellendePerson/vereinfachteErklaerung/flowConfig.ts
@@ -1,28 +1,97 @@
 import { type TransitionConfigMap } from "~/services/flow/newFlowEngine/types";
 import { type prozesskostenhilfeFormularPages } from "../../pages";
+import {
+  frageVermoegen,
+  hasEinnahmen,
+  hasEinnahmenAndEmptyArray,
+  hasVermoegen,
+  hasVermoegenAndEmptyArray,
+  unterhaltsOrAbstammungssachen,
+  vermoegenUnder10000,
+} from "./guards";
 
 export const vereinfachteErklaerungFlowConfig = {
-  kind: [],
-  zusammenleben: [],
-  veUnterhalt: [],
-  minderjaehrig: [],
-  veGeburtsdatum: [],
-  worumGehts: [],
-  rechtlichesThema: [],
-  einnahmen: [],
-  einnahmenFrage: [],
-  einnahmenValue: [],
-  einnahmenUebersicht: [],
-  einnahme: [],
-  einnahmenWarnung: [],
-  vermoegen: [],
-  vermoegenFrage: [],
-  vermoegenValue: [],
-  vermoegenUebersicht: [],
-  vermoegenEintrag: [],
-  vermoegenWarnung: [],
-  hinweisWeiteresFormular: [],
-  hinweisVereinfachteErklaerung: [],
+  kind: "zusammenleben",
+  zusammenleben: [
+    {
+      guard: (context) => context.livesTogether === "no",
+      target: "veUnterhalt",
+    },
+    {
+      target: "minderjaehrig",
+    },
+  ],
+  veUnterhalt: "minderjaehrig",
+  minderjaehrig: "veGeburtsdatum",
+  veGeburtsdatum: "worumGehts",
+  worumGehts: [
+    {
+      guard: (context) => unterhaltsOrAbstammungssachen({ context }),
+      target: "rechtlichesThema",
+    },
+    { target: "einnahmen" },
+  ],
+  rechtlichesThema: "einnahmen",
+  einnahmen: "einnahmenFrage",
+  einnahmenFrage: [
+    {
+      guard: (context) => hasEinnahmen({ context }),
+      target: "einnahmenValue",
+    },
+    {
+      guard: (context) => frageVermoegen({ context }),
+      target: "vermoegen",
+    },
+    { target: "hinweisWeiteresFormular" },
+  ],
+  einnahmenValue: "einnahmenUebersicht",
+  einnahmenUebersicht: [
+    { type: "addArrayItem", target: "einnahme" },
+    {
+      guard: (context) => hasEinnahmenAndEmptyArray({ context }),
+      target: "einnahmenWarnung",
+    },
+    {
+      guard: (context) => frageVermoegen({ context }),
+      target: "vermoegen",
+    },
+    { target: "hinweisWeiteresFormular" },
+  ],
+  einnahme: "einnahmenUebersicht",
+  einnahmenWarnung: [
+    {
+      guard: (context) => frageVermoegen({ context }),
+      target: "vermoegen",
+    },
+    { target: "hinweisWeiteresFormular" },
+  ],
+  vermoegen: "vermoegenFrage",
+  vermoegenFrage: [
+    {
+      guard: (context) => hasVermoegen({ context }),
+      target: "vermoegenValue",
+    },
+    { target: "hinweisVereinfachteErklaerung" },
+  ],
+  vermoegenValue: [
+    {
+      guard: (context) => vermoegenUnder10000({ context }),
+      target: "vermoegenUebersicht",
+    },
+    { target: "hinweisWeiteresFormular" },
+  ],
+  vermoegenUebersicht: [
+    { type: "addArrayItem", target: "vermoegenEintrag" },
+    {
+      guard: (context) => hasVermoegenAndEmptyArray({ context }),
+      target: "vermoegenWarnung",
+    },
+    { target: "hinweisVereinfachteErklaerung" },
+  ],
+  vermoegenEintrag: "vermoegenUebersicht",
+  vermoegenWarnung: "hinweisVereinfachteErklaerung",
+  hinweisWeiteresFormular: "unterhaltsanspruch",
+  hinweisVereinfachteErklaerung: "unterhaltsanspruch",
 } satisfies Partial<
   TransitionConfigMap<typeof prozesskostenhilfeFormularPages>
 >;
