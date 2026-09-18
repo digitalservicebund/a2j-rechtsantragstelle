e8884600a270a24fb38c4ba5c3ccc3dbf65af4d7
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-09-18 14:28:51 +0200

    refactor validateFormUserData to not return migrationData, to separate concerns

--- app/services/flow/userFlowAction/validateFormUserData.ts
+++ app/services/flow/userFlowAction/validateFormUserData.ts
@@ -2,7 +2,6 @@ import { type ValidatorError } from "@rvf/react-router";
 import { Result } from "true-myth";
 import { type UserData } from "~/domains/userData";
 import { resolveArraysFromKeys } from "~/services/array/resolveArraysFromKeys";
-import { getMigrationData } from "~/services/session.server/getMigrationData";
 import { validateFormData } from "~/services/validation/validateFormData.server";
 import { filterFormData } from "~/util/filterFormData";
 import { getPageAndFlowDataFromPathname } from "../getPageAndFlowDataFromPathname";
@@ -10,21 +9,16 @@ import { getPageAndFlowDataFromPathname } from "../getPageAndFlowDataFromPathnam
 export const validateFormUserData = async (
   formData: FormData,
   pathname: string,
-  cookieHeader: string | null,
 ): Promise<
   Result<
-    { userData: UserData; migrationData: UserData | undefined },
+    { userData: UserData },
     { error: ValidatorError; submittedData: UserData }
   >
 > => {
   const relevantFormData = filterFormData(formData);
-  const { arrayIndexes, currentFlow, flowId, stepId } =
-    getPageAndFlowDataFromPathname(pathname);
+  const { arrayIndexes } = getPageAndFlowDataFromPathname(pathname);
 
-  const [validationResult, migrationData] = await Promise.all([
-    validateFormData(pathname, relevantFormData),
-    getMigrationData(stepId, flowId, currentFlow, cookieHeader),
-  ]);
+  const validationResult = await validateFormData(pathname, relevantFormData);
 
   if (validationResult?.error) {
     return Result.err({
@@ -40,6 +34,5 @@ export const validateFormUserData = async (
 
   return Result.ok({
     userData: resolvedData,
-    migrationData: migrationData,
   });
 };
