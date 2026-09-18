8cfc2b517039dcbe98b09ca74b0361f0b66f6265
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-09-18 15:15:22 +0200

    clean up test files

--- app/domains/__test__/flowSchemaNewEngine.test.ts
+++ app/domains/__test__/flowSchemaNewEngine.test.ts
@@ -23,7 +23,7 @@ import { erbausschlagungAnfrageTestCases } from "../nachlass/erbausschlagung/anf
 import { erbscheinWegweiserTestCases } from "../nachlass/erbschein/wegweiser/__test__/testcasesWithUserInputs";
 import { beratungshilfeVorabcheckTestCases } from "../beratungshilfe/vorabcheck/__test__/testcasesWithUserInputs";
 import { fluggastrechteVorabcheckTestCases } from "../fluggastrechte/vorabcheck/__test__/newFlowEngine/testcasesWithUserInput";
-import { fluggastrechteFormularTestCases } from "../fluggastrechte/formular/__test__/legacyTestcasesWithNewFlowEngine";
+import { fluggastrechteFormularTestCases } from "../fluggastrechte/formular/__test__/testcasesWithUserInput";
 
 const flowSchemaTests = {
   geldEinklagenFormularTestCases,
