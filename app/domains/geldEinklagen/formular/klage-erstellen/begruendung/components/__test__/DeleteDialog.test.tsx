aceb017a781903fb0c0471f6bd43f3c0bc145719
aaschlote <11893663+aaschlote@users.noreply.github.com>
2026-09-22 17:11:15 +0200

    test: update DeleteDialog tests to verify three buttons and improve button click handling

--- app/domains/geldEinklagen/formular/klage-erstellen/begruendung/components/__test__/DeleteDialog.test.tsx
+++ app/domains/geldEinklagen/formular/klage-erstellen/begruendung/components/__test__/DeleteDialog.test.tsx
@@ -1,10 +1,16 @@
 import { render } from "@testing-library/react";
 import { DeleteDialog } from "../DeleteDialog";
 
+// Needed as jsdom doesn't support the dialog API yet
+// https://github.com/jsdom/jsdom/issues/3294
+HTMLDialogElement.prototype.showModal = function (this: HTMLDialogElement) {
+  this.open = true;
+};
+
 const dialogRef = { current: document.createElement("dialog") };
 
 describe("DeleteDialog", () => {
-  it("should render the delete dialog with two buttons", () => {
+  it("should render the delete dialog with three buttons", () => {
     const { getByText, getAllByRole } = render(
       <DeleteDialog
         description="description"
@@ -14,15 +20,16 @@ describe("DeleteDialog", () => {
         dialogRef={dialogRef}
       />,
     );
+    dialogRef.current?.showModal();
 
     expect(getByText("description")).toBeInTheDocument();
     expect(getByText("title")).toBeInTheDocument();
-    expect(getAllByRole("button")).toHaveLength(2);
+    expect(getAllByRole("button")).toHaveLength(3);
   });
 
   it("should call onClick when the delete button is clicked", () => {
     const onClick = vi.fn();
-    const { getAllByRole } = render(
+    const { getByRole } = render(
       <DeleteDialog
         description="description"
         title="title"
@@ -31,8 +38,9 @@ describe("DeleteDialog", () => {
         dialogRef={dialogRef}
       />,
     );
+    dialogRef.current?.showModal();
 
-    getAllByRole("button")[0].click();
+    getByRole("button", { name: "Ja, löschen" }).click();
     expect(onClick).toHaveBeenCalled();
   });
 });
