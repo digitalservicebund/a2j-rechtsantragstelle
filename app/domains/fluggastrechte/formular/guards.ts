c21fd0f58abc44016dff5f2a37ecc75f97cd92fd
Spencer Peace <spencer.peace@digitalservice.bund.de>
2026-09-23 10:31:30 +0200

    clean up unused guards

--- app/domains/fluggastrechte/formular/guards.ts
+++ app/domains/fluggastrechte/formular/guards.ts
@@ -1,12 +1,10 @@
-import type { FluggastrechteUserData } from "./userData";
 import { fluggastrechteFlugdatenGuards } from "./flugdaten/guards";
-import { persoenlichDatenGuards } from "./persoenlicheDaten/guards";
 import { getTotalCompensationClaim } from "./services/getTotalCompensationClaim";
 import { MAX_TOTAL_COMPENSATION } from "./services/isTotalClaimAboveLimit";
+import type { FluggastrechteUserData } from "./userData";
 
 export const fluggastrechteGuards = {
   ...fluggastrechteFlugdatenGuards,
-  ...persoenlichDatenGuards,
   isClaimNotExceedingLimit: (context) =>
     getTotalCompensationClaim(context) < MAX_TOTAL_COMPENSATION,
 } satisfies Record<string, (context: FluggastrechteUserData) => boolean>;
--- app/domains/fluggastrechte/formular/persoenlicheDaten/doneFunctions.ts
+++
@@ -1,8 +0,0 @@
-import type { FluggastrechtePersoenlicheDatenUserData } from "./userData";
-import { arrayIsNonEmpty } from "~/util/array";
-
-export const weiterePersonenDone = ({
-  weiterePersonen,
-  isWeiterePersonen,
-}: FluggastrechtePersoenlicheDatenUserData) =>
-  isWeiterePersonen === "no" || arrayIsNonEmpty(weiterePersonen);
