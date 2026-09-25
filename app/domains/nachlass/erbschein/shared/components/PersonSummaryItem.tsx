a2f8467a3e0b99c22247571ef63a47b6219a86c8
Ingrid do Rio Schulze <ingrid.dorioschulze@digitalservice.bund.de>
2026-09-25 13:12:44 +0200

    fix(PersonSummaryItem): center list item key and value

--- app/domains/nachlass/erbschein/shared/components/PersonSummaryItem.tsx
+++ app/domains/nachlass/erbschein/shared/components/PersonSummaryItem.tsx
@@ -24,21 +24,21 @@ export function PersonSummaryItem({
           </div>
         )}
         <dl className="kern-description-list">
-          <div className="kern-description-list-item">
-            <dt className="kern-description-list-item__key">
+          <div className="kern-description-list-item items-center!">
+            <dt className="kern-description-list-item__key p-0! items-center!">
               {translations.personSummaryItem.personName.de}
             </dt>
-            <dd className="kern-description-list-item__value">
+            <dd className="kern-description-list-item__value p-0! content-center!">
               {personName(item)}
             </dd>
           </div>
           {"geburtsdatum" in item && (
             <>
-              <div className="kern-description-list-item">
-                <dt className="kern-description-list-item__key">
+              <div className="kern-description-list-item items-center!">
+                <dt className="kern-description-list-item__key p-0! items-center!">
                   {translations.personSummaryItem.personBirthDate.de}
                 </dt>
-                <dd className="kern-description-list-item__value flex items-center gap-kern-space-small">
+                <dd className="kern-description-list-item__value p-0! items-center!">
                   {!objectKeysNonEmpty(item.geburtsdatum, [
                     "day",
                     "month",
@@ -52,11 +52,11 @@ export function PersonSummaryItem({
                   )}
                 </dd>
               </div>
-              <div className="kern-description-list-item">
-                <dt className="kern-description-list-item__key">
+              <div className="kern-description-list-item items-center!">
+                <dt className="kern-description-list-item__key p-0! items-center!">
                   {translations.personSummaryItem.personBirthPlace.de}
                 </dt>
-                <dd className="kern-description-list-item__value">
+                <dd className="kern-description-list-item__value p-0! items-center!">
                   {migrationDataIsEmpty(item.geburtsort) ? (
                     <Badge icon="warning" variant="warning">
                       {translations.personSummaryItem.missingData.de}
@@ -68,22 +68,22 @@ export function PersonSummaryItem({
               </div>
             </>
           )}
-          <div className="kern-description-list-item">
-            <dt className="kern-description-list-item__key">
+          <div className="kern-description-list-item items-center!">
+            <dt className="kern-description-list-item__key p-0! items-center!">
               {translations.personSummaryItem.personAliveAtTimeOfDeath.de}
             </dt>
-            <dd className="kern-description-list-item__value">
+            <dd className="kern-description-list-item__value p-0! items-center!">
               {item.isAlive === "yes"
                 ? translations.personSummaryItem.yes.de
                 : translations.personSummaryItem.no.de}
             </dd>
           </div>
           {"strasse" in item && (
-            <div className="kern-description-list-item">
-              <dt className="kern-description-list-item__key">
+            <div className="kern-description-list-item items-center!">
+              <dt className="kern-description-list-item__key p-0! items-center!">
                 {translations.personSummaryItem.personAddress.de}
               </dt>
-              <dd className="kern-description-list-item__value">
+              <dd className="kern-description-list-item__value p-0! items-center!">
                 {!objectKeysNonEmpty(item, [
                   "strasse",
                   "hausnummer",
@@ -106,30 +106,30 @@ export function PersonSummaryItem({
           )}
           {"sterbedatum" in item && (
             <>
-              <div className="kern-description-list-item">
-                <dt className="kern-description-list-item__key">
+              <div className="kern-description-list-item items-center!">
+                <dt className="kern-description-list-item__key p-0! items-center!">
                   {translations.personSummaryItem.personDeathDate.de}
                 </dt>
-                <dd className="kern-description-list-item__value">
+                <dd className="kern-description-list-item__value p-0! items-center!">
                   {toDateString(item.sterbedatum)}
                 </dd>
               </div>
-              <div className="kern-description-list-item">
-                <dt className="kern-description-list-item__key">
+              <div className="kern-description-list-item items-center!">
+                <dt className="kern-description-list-item__key p-0! items-center!">
                   {translations.personSummaryItem.personDeathPlace.de}
                 </dt>
-                <dd className="kern-description-list-item__value">
+                <dd className="kern-description-list-item__value p-0! items-center!">
                   {item.sterbeort}
                 </dd>
               </div>
             </>
           )}
           {item.isAlive === "no" && (
-            <div className="kern-description-list-item">
-              <dt className="kern-description-list-item__key">
+            <div className="kern-description-list-item items-center!">
+              <dt className="kern-description-list-item__key p-0! items-center!">
                 {translations.personSummaryItem.personHadChildren.de}
               </dt>
-              <dd className="kern-description-list-item__value">
+              <dd className="kern-description-list-item__value p-0! items-center!">
                 {item.hatteKinder === "yes"
                   ? translations.personSummaryItem.yes.de
                   : translations.personSummaryItem.no.de}
